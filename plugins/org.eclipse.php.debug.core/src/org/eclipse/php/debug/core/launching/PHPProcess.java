622649296d208af7d7e71fc3a017fd43ee6005f0
sgibly <sgibly>
2006-06-12 13:22:39 +0000

    Fixed bug #145978 - The PHP launch could not be terminated from the lowest node (server / executable)

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/debug/core/launching/PHPProcess.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/debug/core/launching/PHPProcess.java
@@ -19,121 +19,150 @@ import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.IStreamsProxy;
+import org.eclipse.debug.internal.ui.actions.provisional.IAsynchronousTerminateAdapter;
+import org.eclipse.debug.internal.ui.actions.provisional.IBooleanRequestMonitor;
+import org.eclipse.debug.internal.ui.viewers.provisional.IAsynchronousRequestMonitor;
 import org.eclipse.debug.ui.console.IConsole;
 
-public class PHPProcess extends PlatformObject implements IProcess {
+public class PHPProcess extends PlatformObject implements IProcess, IAsynchronousTerminateAdapter {
 
-    private ILaunch fLaunch;
-    private String fName;
-    private Map fAttributes;
-    private boolean fTerminated;
-    private IConsole fConsole = null;
-    private PHPStreamsProxy fProxy;
-    private PHPHyperLink fPHPHyperLink;
-    private IDebugTarget fDebugTarget;
+	private ILaunch fLaunch;
+	private String fName;
+	private Map fAttributes;
+	private boolean fTerminated;
+	private IConsole fConsole = null;
+	private PHPStreamsProxy fProxy;
+	private PHPHyperLink fPHPHyperLink;
+	private IDebugTarget fDebugTarget;
 
-    public PHPProcess(ILaunch launch, String name) {
-        fLaunch = launch;
-        fName = name;
-        fTerminated = false;
-        launch.addProcess(this);
-        fProxy = new PHPStreamsProxy();
-    }
+	public PHPProcess(ILaunch launch, String name) {
+		fLaunch = launch;
+		fName = name;
+		fTerminated = false;
+		launch.addProcess(this);
+		fProxy = new PHPStreamsProxy();
+	}
 
-    public String getLabel() {
-        return fName;
-    }
+	public String getLabel() {
+		return fName;
+	}
 
-    public ILaunch getLaunch() {
+	public ILaunch getLaunch() {
 
-        return fLaunch;
-    }
+		return fLaunch;
+	}
 
-    protected void setLaunch(ILaunch launch) {
-        fLaunch = launch;
-    }
+	protected void setLaunch(ILaunch launch) {
+		fLaunch = launch;
+	}
 
-    public IStreamsProxy getStreamsProxy() {
-        return fProxy;
-    }
+	public IStreamsProxy getStreamsProxy() {
+		return fProxy;
+	}
 
-    public void setAttribute(String key, String value) {
-        if (fAttributes == null) {
-            fAttributes = new HashMap(5);
-        }
-        Object origVal = fAttributes.get(key);
-        if (origVal != null && origVal.equals(value)) {
-            return; //nothing changed.
-        }
+	public void setAttribute(String key, String value) {
+		if (fAttributes == null) {
+			fAttributes = new HashMap(5);
+		}
+		Object origVal = fAttributes.get(key);
+		if (origVal != null && origVal.equals(value)) {
+			return; //nothing changed.
+		}
 
-        fAttributes.put(key, value);
-    }
+		fAttributes.put(key, value);
+	}
 
-    public String getAttribute(String key) {
-        if (fAttributes == null) {
-            return null;
-        }
-        return (String) fAttributes.get(key);
-    }
+	public String getAttribute(String key) {
+		if (fAttributes == null) {
+			return null;
+		}
+		return (String) fAttributes.get(key);
+	}
 
-    public int getExitValue() throws DebugException {
-        return 0;
-    }
+	public int getExitValue() throws DebugException {
+		return 0;
+	}
 
-    public Object getAdapter(Class adapter) {
-        if (adapter.equals(IProcess.class)) {
-            return this;
-        }
-        if (adapter.equals(IDebugTarget.class)) {
-            ILaunch launch = getLaunch();
-            IDebugTarget[] targets = launch.getDebugTargets();
-            for (int i = 0; i < targets.length; i++) {
-                if (this.equals(targets[i].getProcess())) {
-                    return targets[i];
-                }
-            }
-            return null;
-        }
-        return null;
-    }
+	public Object getAdapter(Class adapter) {
+		if (adapter.equals(IProcess.class)) {
+			return this;
+		}
+		if (adapter.equals(IDebugTarget.class)) {
+			ILaunch launch = getLaunch();
+			IDebugTarget[] targets = launch.getDebugTargets();
+			for (int i = 0; i < targets.length; i++) {
+				if (this.equals(targets[i].getProcess())) {
+					return targets[i];
+				}
+			}
+			return null;
+		}
+		if (adapter.equals(IAsynchronousTerminateAdapter.class)) {
+			return this;
+		}
+		return null;
+	}
 
-    public boolean canTerminate() {
-        if (fTerminated)
-            return false;
-        return true;
-    }
+	public boolean canTerminate() {
+		if (fTerminated)
+			return false;
+		return true;
+	}
 
-    public boolean isTerminated() {
-        return fTerminated;
-    }
+	public boolean isTerminated() {
+		return fTerminated;
+	}
 
-    public void terminate() throws DebugException {
-        fTerminated = true;
+	public void terminate() throws DebugException {
+		fTerminated = true;
 
-    }
+	}
 
-    public void setPHPHyperLink(PHPHyperLink pLink) {
-        fPHPHyperLink = pLink;
-    }
+	public void setPHPHyperLink(PHPHyperLink pLink) {
+		fPHPHyperLink = pLink;
+	}
 
-    public PHPHyperLink getPHPHyperLink() {
-        return fPHPHyperLink;
-    }
+	public PHPHyperLink getPHPHyperLink() {
+		return fPHPHyperLink;
+	}
 
-    public IConsole getConsole() {
-        return fConsole;
-    }
+	public IConsole getConsole() {
+		return fConsole;
+	}
 
-    public void setConsole(IConsole console) {
-        fConsole = console;
-    }
-    
-    public IDebugTarget getDebugTarget() {
-        return fDebugTarget;
-    }
+	public void setConsole(IConsole console) {
+		fConsole = console;
+	}
 
-    public void setDebugTarget(IDebugTarget target) {
-        fDebugTarget = target;
-    }
+	public IDebugTarget getDebugTarget() {
+		return fDebugTarget;
+	}
 
+	public void setDebugTarget(IDebugTarget target) {
+		fDebugTarget = target;
+	}
+
+	public void canTerminate(Object element, IBooleanRequestMonitor monitor) {
+		if (this.equals(element)) {
+			monitor.setResult(canTerminate());
+			monitor.done();
+		}
+	}
+
+	public void isTerminated(Object element, IBooleanRequestMonitor monitor) {
+		if (this.equals(element)) {
+			monitor.setResult(isTerminated());
+			monitor.done();
+		}
+	}
+
+	public void terminate(Object element, IAsynchronousRequestMonitor monitor) {
+		if (this.equals(element)) {
+			try {
+				fLaunch.terminate();
+				monitor.done();
+			} catch (DebugException e) {
+			}
+		}
+	}
 }
