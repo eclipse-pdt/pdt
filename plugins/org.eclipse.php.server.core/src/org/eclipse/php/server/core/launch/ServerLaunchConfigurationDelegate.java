2e08dcdd40ddab3d76fe7457b143e96439a4cb18
sgibly <sgibly>
2006-06-15 08:57:46 +0000

    Server launch delegate update

--- plugins/org.eclipse.php.server.core/src/org/eclipse/php/server/core/launch/ServerLaunchConfigurationDelegate.java
+++ plugins/org.eclipse.php.server.core/src/org/eclipse/php/server/core/launch/ServerLaunchConfigurationDelegate.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.php.server.core.launch;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.*;
@@ -20,74 +21,19 @@ import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 import org.eclipse.php.server.core.Activator;
 import org.eclipse.php.server.core.Logger;
 import org.eclipse.php.server.core.Server;
+import org.eclipse.php.server.core.ServersManager;
 import org.eclipse.php.server.core.deploy.FileUtil;
 
 /**
- * 
+ * Server launch configuration delegate.
  */
 //public class ServerLaunchConfigurationDelegate extends AbstractJavaLaunchConfigurationDelegate {
 public class ServerLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
-	//
-	//    private ILaunch getLaunchableAdapter(Server server) {
-	//        //get the launchable adapter
-	//        ILaunchableAdapter launchableAdapter = null;
-	//        Object launchable = null;
-	//        ILaunchableAdapter[] adapters = ServerPlugin.getLaunchableAdapters();
-	//        if (adapters != null) {
-	//            int size2 = adapters.length;
-	//            for (int j = 0; j < size2; j++) {
-	//                ILaunchableAdapter adapter = adapters[j];
-	//                try {
-	//                    Object launchable2 = adapter.getLaunchable(server, moduleArtifact);
-	//                    if (launchable2 != null) {
-	//                        launchableAdapter = adapter;
-	//                        launchable = launchable2;
-	//                    }
-	//                } catch (Exception e) {
-	//                    Logger.logException("Error in launchable adapter", e);
-	//                }
-	//            }
-	//        }
-	//
-	//        return launchableAdapter;
-	//    }
 
-	//    private IClient[] getClients(IServer server, Object launchable, String launchMode) {
-	//        ArrayList list = new ArrayList();
-	//        IClient[] clients = ServerPlugin.getClients();
-	//        if (clients != null) {
-	//            int size = clients.length;
-	//            for (int i = 0; i < size; i++) {
-	//                if (clients[i].supports(server, launchable, launchMode))
-	//                    list.add(clients[i]);
-	//            }
-	//        }
-	//
-	//        IClient[] clients2 = new IClient[list.size()];
-	//        list.toArray(clients2);
-	//        return clients2;
-	//    }
-
-	//    public IModuleArtifact getModuleArtifact(ILaunchConfiguration configuration) {
-	//        IModuleArtifact moduleArtifact = null;
-	//
-	//        try {
-	//            String fileName = configuration.getAttribute(Activator.FILE_NAME, "");
-	//            IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-	//            IResource resource = root.findMember(fileName);
-	//
-	//            if (resource == null)
-	//                return null;
-	//
-	//            moduleArtifact = ServerPlugin.getModuleArtifact(resource);
-	//
-	//        } catch (CoreException e) {
-	//
-	//        }
-	//
-	//        return moduleArtifact;
-	//    }
-
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.eclipse.debug.core.ILaunchConfiguration, java.lang.String, org.eclipse.debug.core.ILaunch, org.eclipse.core.runtime.IProgressMonitor)
+	 */
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
 
 		IExtensionRegistry registry = Platform.getExtensionRegistry();
@@ -107,41 +53,32 @@ public class ServerLaunchConfigurationDelegate extends LaunchConfigurationDelega
 
 	public void doLaunch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
 
-		Server server = null; //ServersManager.getServer(configuration.); // TODO - Get the server name
+		Server server = ServersManager.getServer(configuration.getAttribute(Server.NAME, ""));
 		if (server == null) {
 			Logger.log(Logger.ERROR, "Launch configuration could not find server");
 			//throw CoreException();
 			return;
 		}
-
-		//        ApacheServerBehaviour apacheServerBehaviour = (ApacheServerBehaviour) server.loadAdapter(ApacheServerBehaviour.class, null);
-		//        apacheServerBehaviour.setupLaunch(launch, mode, monitor);
-		//
-		//        IModuleArtifact moduleArtifact = getModuleArtifact(configuration);
-		//        if (moduleArtifact == null)
-		//            return;
-		//
-		//        IModule module = moduleArtifact.getModule();
-		//        ILaunchableAdapter launchableAdapter = getLaunchableAdapter(server, moduleArtifact);
-		//        Object launchable = launchableAdapter.getLaunchable(server, moduleArtifact);
-		//        IClient client = getClient(launchable, server, mode);
-		//
-		//        if (client == null) {
-		//            //EclipseUtil.openError(Messages.errorNoClient);
-		//            Logger.log(Logger.WARNING, "No launchable clients!");
-		//            return;
-		//        }
-
-		//        IModule[] modules = new IModule[] { module }; // TODO: get parent heirarchy correct
+		String fileName = configuration.getAttribute(Server.FILE_NAME, (String) null);
+		// Get the project from the file name
+		IPath filePath = new Path(fileName);
+		IProject proj = null;
+		try {
+			proj = ResourcesPlugin.getWorkspace().getRoot().getProject(filePath.segment(0));
+		} catch (Throwable t) {
+		}
+		if (proj == null) {
+			Logger.log(Logger.ERROR, "Could not launch (Project is null).");
+			return;
+		}
 
 		boolean publish = configuration.getAttribute(Server.PUBLISH, false);
-
 		if (publish) {
-//			FileUtil.publish(null, configuration, monitor);
+			if (!FileUtil.publish(server, proj, configuration, monitor)) {
+				// Return if the publish failed.
+				return;
+			}
 		}
-		//        LaunchClientJob clientJob = new LaunchClientJob(server, modules, mode, moduleArtifact, launchableAdapter, client);
-		//        clientJob.schedule();
-
 	}
 
 	protected void run(IWorkspaceRunnable wr) throws DebugException {
