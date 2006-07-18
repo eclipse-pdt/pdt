f5a66a608b14c75a76906c436f185eac07b4e12b
sgibly <sgibly>
2006-07-18 09:02:00 +0000

    Remove any launch mapping if the launch was removed and we are still mapping it.

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/debug/core/debugger/PHPSessionLaunchMapper.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/debug/core/debugger/PHPSessionLaunchMapper.java
@@ -10,8 +10,12 @@
  *******************************************************************************/
 package org.eclipse.php.debug.core.debugger;
 
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchesListener;
 import org.eclipse.php.core.util.collections.IntHashtable;
+import org.eclipse.php.core.util.collections.IntMap;
+import org.eclipse.php.debug.core.launching.PHPServerLaunchDecorator;
 
 /**
  * This class is responsible for mapping debug session id's to the ILaunch that is responsible for
@@ -19,7 +23,7 @@ import org.eclipse.php.core.util.collections.IntHashtable;
  * 
  * @author Shalom Gibly
  */
-public class PHPSessionLaunchMapper {
+public class PHPSessionLaunchMapper implements ILaunchesListener {
 
 	private static PHPSessionLaunchMapper instance;
 	private IntHashtable map;
@@ -31,6 +35,7 @@ public class PHPSessionLaunchMapper {
 	private static PHPSessionLaunchMapper getInstance() {
 		if (instance == null) {
 			instance = new PHPSessionLaunchMapper();
+			DebugPlugin.getDefault().getLaunchManager().addLaunchListener(instance);
 		}
 		return instance;
 	}
@@ -65,5 +70,29 @@ public class PHPSessionLaunchMapper {
 	 */
 	public static ILaunch remove(int sessionID) {
 		return (ILaunch) getInstance().map.remove(sessionID);
+	}
+
+	public void launchesAdded(ILaunch[] launches) {
+	}
+
+	public void launchesChanged(ILaunch[] launches) {
+	}
+
+	public void launchesRemoved(ILaunch[] launches) {
+		// Remove any launch mapping if the launch was removed and we are still mapping it.
+		IntMap.Entry[] entries = new IntMap.Entry[map.size()];
+		map.entrySet().toArray(entries);
+		for (int i = 0; i < entries.length; i++) {
+			IntMap.Entry entry = entries[i];
+			for (int j = 0; j < launches.length; j++) {
+				if (entry.getValue() == launches[j]) {
+					map.remove(entry.getKey());
+				} else if (entry.getValue() instanceof PHPServerLaunchDecorator) {
+					if (((PHPServerLaunchDecorator) entry.getValue()).getLaunch() == launches[j]) {
+						map.remove(entry.getKey());
+					}
+				}
+			}
+		}
 	}
 }
