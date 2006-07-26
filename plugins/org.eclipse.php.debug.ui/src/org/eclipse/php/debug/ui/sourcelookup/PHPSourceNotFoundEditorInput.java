d352920487ae4a6f521832a8230c226b0d36884c
sgibly <sgibly>
2006-07-26 13:47:05 +0000

    Added another constructor to enable custom message

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/debug/ui/sourcelookup/PHPSourceNotFoundEditorInput.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/debug/ui/sourcelookup/PHPSourceNotFoundEditorInput.java
@@ -29,66 +29,87 @@ import org.eclipse.ui.IPersistableElement;
  */
 public class PHPSourceNotFoundEditorInput extends PlatformObject implements IEditorInput {
 
-    /**
-     * Associated stack frame
-     */
-    private IStackFrame fFrame;
+	/**
+	 * Associated stack frame
+	 */
+	private IStackFrame fFrame;
 
-    /**
-     * Stack frame text (cached on creation)
-     */
-    private String fFrameText;
+	/**
+	 * Stack frame text (cached on creation)
+	 */
+	private String fFrameText;
+	private String fTooltipText;
 
-    /**
-     * Constructs an editor input for the given stack frame,
-     * to indicate source could not be found.
-     * 
-     * @param frame stack frame
-     */
-    public PHPSourceNotFoundEditorInput(PHPSourceNotFoundInput input) {
-        fFrame = input.getStackFrame();
-        IDebugModelPresentation pres = DebugUITools.newDebugModelPresentation(fFrame.getModelIdentifier());
-        fFrameText = pres.getText(fFrame);
-        pres.dispose();
-    }
+	/**
+	 * Constructs an editor input for the given stack frame,
+	 * to indicate source could not be found.
+	 * A default tooltip and text will appear in the editor. 
+	 * 
+	 * @param frame stack frame
+	 * @see #PHPSourceNotFoundEditorInput(PHPSourceNotFoundInput, String)
+	 */
+	public PHPSourceNotFoundEditorInput(PHPSourceNotFoundInput input) {
+		fFrame = input.getStackFrame();
+		IDebugModelPresentation pres = DebugUITools.newDebugModelPresentation(fFrame.getModelIdentifier());
+		fFrameText = pres.getText(fFrame);
+		pres.dispose();
+	}
 
-    /**
-     * @see org.eclipse.ui.IEditorInput#exists()
-     */
-    public boolean exists() {
-        return false;
-    }
+	/**
+	 * Constructs an editor input for the given stack frame,
+	 * to indicate source could not be found.
+	 * 
+	 * @param frame stack frame
+	 * @param tooltipText The text that will appear in the editor.
+	 */
+	public PHPSourceNotFoundEditorInput(PHPSourceNotFoundInput input, String tooltipText) {
+		fFrame = input.getStackFrame();
+		IDebugModelPresentation pres = DebugUITools.newDebugModelPresentation(fFrame.getModelIdentifier());
+		fFrameText = pres.getText(fFrame);
+		pres.dispose();
+		fTooltipText = tooltipText;
+	}
 
-    /**
-     * @see org.eclipse.ui.IEditorInput#getImageDescriptor()
-     */
-    public ImageDescriptor getImageDescriptor() {
-        return DebugUITools.getDefaultImageDescriptor(fFrame);
-    }
+	/**
+	 * @see org.eclipse.ui.IEditorInput#exists()
+	 */
+	public boolean exists() {
+		return false;
+	}
 
-    /**
-     * @see org.eclipse.ui.IEditorInput#getName()
-     */
-    public String getName() {
-        try {
-            return fFrame.getName();
-        } catch (DebugException e) {
-            return PHPDebugUIMessages.SourceNotFoundEditorInput_Source_Not_Found_1; //$NON-NLS-1$
-        }
-    }
+	/**
+	 * @see org.eclipse.ui.IEditorInput#getImageDescriptor()
+	 */
+	public ImageDescriptor getImageDescriptor() {
+		return DebugUITools.getDefaultImageDescriptor(fFrame);
+	}
 
-    /**
-     * @see org.eclipse.ui.IEditorInput#getPersistable()
-     */
-    public IPersistableElement getPersistable() {
-        return null;
-    }
+	/**
+	 * @see org.eclipse.ui.IEditorInput#getName()
+	 */
+	public String getName() {
+		try {
+			return fFrame.getName();
+		} catch (DebugException e) {
+			return PHPDebugUIMessages.SourceNotFoundEditorInput_Source_Not_Found_1; //$NON-NLS-1$
+		}
+	}
 
-    /**
-     * @see org.eclipse.ui.IEditorInput#getToolTipText()
-     */
-    public String getToolTipText() {
-        return MessageFormat.format(PHPDebugUIMessages.SourceNotFoundEditorInput_Source_not_found_for__0__2, new String[] { fFrameText }); //$NON-NLS-1$
-    }
+	/**
+	 * @see org.eclipse.ui.IEditorInput#getPersistable()
+	 */
+	public IPersistableElement getPersistable() {
+		return null;
+	}
+
+	/**
+	 * @see org.eclipse.ui.IEditorInput#getToolTipText()
+	 */
+	public String getToolTipText() {
+		if (fTooltipText == null) {
+			return MessageFormat.format(PHPDebugUIMessages.SourceNotFoundEditorInput_Source_not_found_for__0__2, new String[] { fFrameText }); //$NON-NLS-1$
+		}
+		return fTooltipText;
+	}
 
 }
