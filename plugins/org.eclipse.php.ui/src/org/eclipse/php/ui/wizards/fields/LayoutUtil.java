3ac9091a8d6c7b3b22849e97e3f8b5f3415ed257
slapsha <slapsha>
2006-08-10 16:03:49 +0000

    added getButtonWidthHint() cleaned up the code

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/wizards/fields/LayoutUtil.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/wizards/fields/LayoutUtil.java
@@ -10,30 +10,23 @@
  *******************************************************************************/
 package org.eclipse.php.ui.wizards.fields;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.php.internal.ui.util.PixelConverter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
 public class LayoutUtil {
 
 	/**
-	 * Calculates the number of columns needed by field editors
-	 */
-	public static int getNumberOfColumns(DialogField[] editors) {
-		int nCulumns = 0;
-		for (int i = 0; i < editors.length; i++) {
-			nCulumns = Math.max(editors[i].getNumberOfControls(), nCulumns);
-		}
-		return nCulumns;
-	}
-
-	/**
 	 * Creates a composite and fills in the given editors.
 	 * @param labelOnTop Defines if the label of all fields should be on top of the fields
 	 */
-	public static void doDefaultLayout(Composite parent, DialogField[] editors, boolean labelOnTop) {
+	public static void doDefaultLayout(final Composite parent, final DialogField[] editors, final boolean labelOnTop) {
 		doDefaultLayout(parent, editors, labelOnTop, 0, 0);
 	}
 
@@ -43,47 +36,86 @@ public class LayoutUtil {
 	 * @param marginWidth The margin width to be used by the composite
 	 * @param marginHeight The margin height to be used by the composite
 	 */
-	public static void doDefaultLayout(Composite parent, DialogField[] editors, boolean labelOnTop, int marginWidth, int marginHeight) {
+	public static void doDefaultLayout(final Composite parent, final DialogField[] editors, final boolean labelOnTop, final int marginWidth, final int marginHeight) {
 		int nCulumns = getNumberOfColumns(editors);
-		Control[][] controls = new Control[editors.length][];
-		for (int i = 0; i < editors.length; i++) {
+		final Control[][] controls = new Control[editors.length][];
+		for (int i = 0; i < editors.length; i++)
 			controls[i] = editors[i].doFillIntoGrid(parent, nCulumns);
-		}
 		if (labelOnTop) {
 			nCulumns--;
 			modifyLabelSpans(controls, nCulumns);
 		}
 		GridLayout layout = null;
-		if (parent.getLayout() instanceof GridLayout) {
+		if (parent.getLayout() instanceof GridLayout)
 			layout = (GridLayout) parent.getLayout();
-		} else {
+		else
 			layout = new GridLayout();
-		}
-		if (marginWidth != SWT.DEFAULT) {
+		if (marginWidth != SWT.DEFAULT)
 			layout.marginWidth = marginWidth;
-		}
-		if (marginHeight != SWT.DEFAULT) {
+		if (marginHeight != SWT.DEFAULT)
 			layout.marginHeight = marginHeight;
-		}
 		layout.numColumns = nCulumns;
 		parent.setLayout(layout);
 	}
 
-	private static void modifyLabelSpans(Control[][] controls, int nCulumns) {
-		for (int i = 0; i < controls.length; i++) {
+	public static int getButtonWidthHint(final Button button) {
+		button.setFont(JFaceResources.getDialogFont());
+		final PixelConverter converter = new PixelConverter(button);
+		final int widthHint = converter.convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
+		return Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
+	}
+
+	/**
+	 * Calculates the number of columns needed by field editors
+	 */
+	public static int getNumberOfColumns(final DialogField[] editors) {
+		int nCulumns = 0;
+		for (int i = 0; i < editors.length; i++)
+			nCulumns = Math.max(editors[i].getNumberOfControls(), nCulumns);
+		return nCulumns;
+	}
+
+	private static void modifyLabelSpans(final Control[][] controls, final int nCulumns) {
+		for (int i = 0; i < controls.length; i++)
 			setHorizontalSpan(controls[i][0], nCulumns);
-		}
+	}
+
+	/**
+	 * Sets the heightHint hint of a control. Assumes that GridData is used.
+	 */
+	public static void setHeightHint(final Control control, final int heightHint) {
+		final Object ld = control.getLayoutData();
+		if (ld instanceof GridData)
+			((GridData) ld).heightHint = heightHint;
+	}
+
+	/**
+	 * Sets the horizontal grabbing of a control to true. Assumes that GridData is used.
+	 */
+	public static void setHorizontalGrabbing(final Control control) {
+		final Object ld = control.getLayoutData();
+		if (ld instanceof GridData)
+			((GridData) ld).grabExcessHorizontalSpace = true;
+	}
+
+	/**
+	 * Sets the horizontal indent of a control. Assumes that GridData is used.
+	 */
+	public static void setHorizontalIndent(final Control control, final int horizontalIndent) {
+		final Object ld = control.getLayoutData();
+		if (ld instanceof GridData)
+			((GridData) ld).horizontalIndent = horizontalIndent;
 	}
 
 	/**
 	 * Sets the span of a control. Assumes that GridData is used.
 	 */
-	public static void setHorizontalSpan(Control control, int span) {
-		Object ld = control.getLayoutData();
-		if (ld instanceof GridData) {
+	public static void setHorizontalSpan(final Control control, final int span) {
+		final Object ld = control.getLayoutData();
+		if (ld instanceof GridData)
 			((GridData) ld).horizontalSpan = span;
-		} else if (span != 1) {
-			GridData gd = new GridData();
+		else if (span != 1) {
+			final GridData gd = new GridData();
 			gd.horizontalSpan = span;
 			control.setLayoutData(gd);
 		}
@@ -92,41 +124,10 @@ public class LayoutUtil {
 	/**
 	 * Sets the width hint of a control. Assumes that GridData is used.
 	 */
-	public static void setWidthHint(Control control, int widthHint) {
-		Object ld = control.getLayoutData();
-		if (ld instanceof GridData) {
+	public static void setWidthHint(final Control control, final int widthHint) {
+		final Object ld = control.getLayoutData();
+		if (ld instanceof GridData)
 			((GridData) ld).widthHint = widthHint;
-		}
-	}
-
-	/**
-	 * Sets the heightHint hint of a control. Assumes that GridData is used.
-	 */
-	public static void setHeightHint(Control control, int heightHint) {
-		Object ld = control.getLayoutData();
-		if (ld instanceof GridData) {
-			((GridData) ld).heightHint = heightHint;
-		}
-	}
-
-	/**
-	 * Sets the horizontal indent of a control. Assumes that GridData is used.
-	 */
-	public static void setHorizontalIndent(Control control, int horizontalIndent) {
-		Object ld = control.getLayoutData();
-		if (ld instanceof GridData) {
-			((GridData) ld).horizontalIndent = horizontalIndent;
-		}
-	}
-
-	/**
-	 * Sets the horizontal grabbing of a control to true. Assumes that GridData is used.
-	 */
-	public static void setHorizontalGrabbing(Control control) {
-		Object ld = control.getLayoutData();
-		if (ld instanceof GridData) {
-			((GridData) ld).grabExcessHorizontalSpace = true;
-		}
 	}
 
 }
