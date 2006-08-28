cba40c7e4cd030029c775ae917c1c7f8e32029b3
eklein <eklein>
2006-08-28 10:31:07 +0000

    fix bug #155334 - ProjectParsingManager makes garbled phpdoc comments
    Add file.getCharset() to InputStreamReader initialization

--- plugins/org.eclipse.php.core/src/org/eclipse/php/core/phpModel/parser/management/ProjectParsingManager.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/core/phpModel/parser/management/ProjectParsingManager.java
@@ -12,6 +12,7 @@ package org.eclipse.php.core.phpModel.parser.management;
 
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -77,11 +78,15 @@ class ProjectParsingManager implements IProjectModelListener {
 
 		InputStreamReader inputStreamReader;
 		try {
-			inputStreamReader = new InputStreamReader(file.getContents());
+			inputStreamReader = new InputStreamReader(file.getContents(), file.getCharset());
 		} catch (CoreException e) {
 			PHPCorePlugin.log(e);
 			return;
-		}
+		} catch (UnsupportedEncodingException e) {
+			PHPCorePlugin.log(e);
+			return;
+		} 
+		
 		Pattern[] tasksPatterns = TaskPatternsProvider.getInstance().getPatternsForProject(file.getProject());
 		try {
 			parserManager.parse(inputStreamReader, file.getFullPath().toString(), file.getModificationStamp(), parserClient, tasksPatterns, UseAspTagsHandler.useAspTagsAsPhp(project));
