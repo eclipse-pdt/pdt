e00b9a1b6f8f9806f207a586631e6560fb5b7dd4
slapsha <slapsha>
2006-08-28 03:38:37 +0000

    Bug #150613 fixed.

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/explorer/ExplorerSorter.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/explorer/ExplorerSorter.java
@@ -1,5 +1,7 @@
 package org.eclipse.php.ui.explorer;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.php.core.phpModel.phpElementData.PHPFileData;
 import org.eclipse.php.core.phpModel.phpElementData.PHPIncludeFileData;
@@ -10,12 +12,15 @@ public class ExplorerSorter extends ViewerSorter {
 	 * @see org.eclipse.jface.viewers.ViewerComparator#category(java.lang.Object)
 	 */
 	public int category(Object element) {
-		if (element instanceof PHPFileData) {
+		if (element instanceof IContainer) {
 			return 1;
 		}
-		if (element instanceof PHPIncludeFileData) {
+		if (element instanceof IFile) {
 			return 2;
 		}
-		return 2;
+		if (element instanceof PHPIncludeFileData) {
+			return 3;
+		}
+		return 4;
 	}
 }
