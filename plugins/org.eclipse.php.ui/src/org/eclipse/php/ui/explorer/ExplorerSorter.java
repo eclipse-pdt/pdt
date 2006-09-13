52b3940eb75d9af916b65a0aa8c8108075674452
slapsha <slapsha>
2006-09-13 08:11:40 +0000

    Use PHP Element Sorter for Explorer View and bug #157001 fix.

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/explorer/ExplorerSorter.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/explorer/ExplorerSorter.java
@@ -1,26 +1,7 @@
 package org.eclipse.php.ui.explorer;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.jface.viewers.ViewerSorter;
-import org.eclipse.php.core.phpModel.phpElementData.PHPFileData;
-import org.eclipse.php.core.phpModel.phpElementData.PHPIncludeFileData;
+import org.eclipse.php.ui.util.PHPElementSorter;
 
-public class ExplorerSorter extends ViewerSorter {
+public class ExplorerSorter extends PHPElementSorter {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ViewerComparator#category(java.lang.Object)
-	 */
-	public int category(Object element) {
-		if (element instanceof IContainer) {
-			return 1;
-		}
-		if (element instanceof IFile) {
-			return 2;
-		}
-		if (element instanceof PHPIncludeFileData) {
-			return 3;
-		}
-		return 4;
-	}
 }
