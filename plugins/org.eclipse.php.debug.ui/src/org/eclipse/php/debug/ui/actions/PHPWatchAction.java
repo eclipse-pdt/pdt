c7f6b35d53c0db98567749180ce90c576daa9ee1
sgibly <sgibly>
2006-09-28 11:52:45 +0000

    PHP watch action - Adding watches is now enabled

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/debug/ui/actions/PHPWatchAction.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/debug/ui/actions/PHPWatchAction.java
@@ -10,29 +10,30 @@
  *******************************************************************************/
 package org.eclipse.php.debug.ui.actions;
 
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IExpressionManager;
+import org.eclipse.debug.core.model.IWatchExpression;
+import org.eclipse.debug.internal.ui.actions.expressions.WatchExpressionAction;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.php.debug.ui.Logger;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMText;
 
 /**
  * An action for adding a new PHP watch expressions.
  * 
  * @author shalom
  */
-public class PHPWatchAction implements IWorkbenchWindowActionDelegate, IEditorActionDelegate {
-
-	public void dispose() {
-	}
+public class PHPWatchAction extends WatchExpressionAction implements IWorkbenchWindowActionDelegate, IEditorActionDelegate {
 
 	public void init(IWorkbenchWindow window) {
-	}
-
-	public void run(IAction action) {
-		// TODO
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
@@ -43,6 +44,26 @@ public class PHPWatchAction implements IWorkbenchWindowActionDelegate, IEditorAc
 	}
 
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+	}
+
+	public void run(IAction action) {
+		IStructuredSelection selection = getCurrentSelection();
+		if (selection instanceof TextSelection) {
+			TextSelection textSelection = (TextSelection) selection;
+			IExpressionManager expressionManager = DebugPlugin.getDefault().getExpressionManager();
+			IDOMText domText = (IDOMText) selection.getFirstElement();
+			String expression;
+			try {
+				expression = domText.getFirstStructuredDocumentRegion().getParentDocument().get(textSelection.getOffset(), textSelection.getLength());
+				//	create the new watch expression
+				IWatchExpression watchExpression = expressionManager.newWatchExpression(expression.trim());
+				expressionManager.addExpression(watchExpression);
+				// refresh and re-evaluate
+				watchExpression.setExpressionContext(getContext());
+			} catch (Exception e) {
+				Logger.logException(e);
+			}
+		}
 	}
 
 }
