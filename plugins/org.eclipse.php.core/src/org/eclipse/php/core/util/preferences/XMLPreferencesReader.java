7abf85a3811f83ec757b08e739b463c8c83fbe39
eklein <eklein>
2006-11-23 12:55:19 +0000

    Add project properties read option

--- plugins/org.eclipse.php.core/src/org/eclipse/php/core/util/preferences/XMLPreferencesReader.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/core/util/preferences/XMLPreferencesReader.java
@@ -10,8 +10,10 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.eclipse.core.runtime.Preferences;
+import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.php.core.Logger;
+import org.eclipse.ui.preferences.IWorkingCopyManager;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -28,6 +30,7 @@ public class XMLPreferencesReader {
 	private static final Pattern QUOT_PATTERN = Pattern.compile("&quot;"); //$NON-NLS-1$
 	private static final Pattern APOS_PATTERN = Pattern.compile("&apos;"); //$NON-NLS-1$
 	private static final Pattern AMP_PATTERN = Pattern.compile("&amp;"); //$NON-NLS-1$
+	public static final String STRING_DEFAULT = ""; //$NON-NLS-1$
 
 	public static String getUnEscaped(String s) {
 		s = LT_PATTERN.matcher(s).replaceAll("<"); //$NON-NLS-1$
@@ -92,11 +95,36 @@ public class XMLPreferencesReader {
 	 * @return 
 	 */
 	public static HashMap[] read(Preferences store, String prefsKey) {
+		String storedValue = store.getString(prefsKey);
+		return getHashFromStoredValue(storedValue);
+	}
+	
+	/**
+	 * Reads a map of elements from the project Properties by a given key.
+	 * 
+	 * @param prefsKey The key to store by.
+	 * @param fScopeContext The context for the project Scope
+	 * @param workingCopyManager
+	 * @return 
+	 */
+	public static HashMap[] read(Key prefKey, IScopeContext[] fScopeContext, IWorkingCopyManager workingCopyManager){
+
+		String storedValue = prefKey.getStoredValue(fScopeContext, false, workingCopyManager);
+		if (storedValue == null)
+			storedValue = STRING_DEFAULT;		
+		return getHashFromStoredValue(storedValue);
+	
+	}
+	
+	
+	public static HashMap[] getHashFromStoredValue(String storedValue){
+		
 		ArrayList maps = new ArrayList();
-		StringTokenizer st = new StringTokenizer(store.getString(prefsKey), new String(new char[] { DELIMITER }));
+		StringTokenizer st = new StringTokenizer(storedValue, new String(new char[] { DELIMITER }));
 		while (st.hasMoreTokens()) {
 			maps.add(read(st.nextToken()));
 		}
 		return (HashMap[]) maps.toArray(new HashMap[maps.size()]);
+
 	}
 }
