4856d74af5633f39aa0cef0cf7855cef49b9bb94
mspector <mspector>
2006-12-20 13:43:31 +0000

    Fixed copyrights for files generated by JavaCup

--- plugins/org.eclipse.php.core/src/org/eclipse/php/core/phpModel/javacup/runtime/lr_parser.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/core/phpModel/javacup/runtime/lr_parser.java
@@ -1,12 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Zend and IBM - Initial implementation
+ * Copyright 1996-1999 by Scott Hudson, Frank Flannery, C. Scott Ananian
  *******************************************************************************/
 				    
 package org.eclipse.php.core.phpModel.javacup.runtime;
