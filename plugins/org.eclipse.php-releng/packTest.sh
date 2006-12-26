971c0fe29b0b4fc2213ab97ea4cbaa25bb4e7a9d
rganor <rganor>
2006-12-26 15:35:10 +0000

    yet more work on pack test

--- plugins/org.eclipse.php-releng/packTest.sh
+++ plugins/org.eclipse.php-releng/packTest.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# creates build directory
+md ../src
+
 # operate build test target with nightly build 
-./buildall.sh -vm /shared/common/ibm-java2-ppc-50 -target buildTest -platform "-Dbaseos=linux -Dbasews=gtk -Dbasearch=ppc" N 
+./buildall.sh -vm /shared/common/ibm-java2-ppc-50 -target buildTest -platform "-Dbaseos=linux -Dbasews=gtk -Dbasearch=ppc" N >../src/log
+
+chmod +x ../src/*
+
+../src/runtests -ws gtk -os linux -arch ppc >>../src/log
 
