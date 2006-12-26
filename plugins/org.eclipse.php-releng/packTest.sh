ee58a5d03b8d24697e31166fe9d49ea187cc78f8
rganor <rganor>
2006-12-26 08:09:31 +0000

    removing the arch,ws,os params

--- plugins/org.eclipse.php-releng/packTest.sh
+++ plugins/org.eclipse.php-releng/packTest.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# define os, arch, ws for the packaging tool
+export BASEOS="linx";
+export BASEARCH="ppc";
+export BASEWS="gtk";
+
+# operate build test target with nightly build 
 ./buildall.sh -vm /shared/common/ibm-java2-ppc-50 -target buildTest N 
