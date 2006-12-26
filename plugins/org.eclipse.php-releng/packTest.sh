db78993ce5d4a7d0e59c9e89262632c8aff0e933
rganor <rganor>
2006-12-26 09:58:51 +0000

    yet more work on pack test

--- plugins/org.eclipse.php-releng/packTest.sh
+++ plugins/org.eclipse.php-releng/packTest.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# define os, arch, ws for the packaging tool
-export BASEOS="linx";
-export BASEARCH="ppc";
-export BASEWS="gtk";
-
 # operate build test target with nightly build 
 ./buildall.sh -vm /shared/common/ibm-java2-ppc-50 -target buildTest -platform "-Dbaseos=linux -Dbasews=gtk -Dbasearch=ppc" N 
+
