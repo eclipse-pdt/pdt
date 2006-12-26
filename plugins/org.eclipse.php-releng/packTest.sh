faeb115062a166234e5ce7bf72bc23c2510231da
rganor <rganor>
2006-12-26 16:28:05 +0000

    adding dos2unix to the tests scripts

--- plugins/org.eclipse.php-releng/packTest.sh
+++ plugins/org.eclipse.php-releng/packTest.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# creates build directory
-md ../src
+#dos2unix 
+dos2unix buildall.sh 
 
 # operate build test target with nightly build 
 ./buildall.sh -vm /shared/common/ibm-java2-ppc-50 -target buildTest -platform "-Dbaseos=linux -Dbasews=gtk -Dbasearch=ppc" N >../src/log
 
 chmod +x ../src/*
+
+#dos2unix 
+dos2unix ../src/runtests
 
 ../src/runtests -ws gtk -os linux -arch ppc >>../src/log
 
