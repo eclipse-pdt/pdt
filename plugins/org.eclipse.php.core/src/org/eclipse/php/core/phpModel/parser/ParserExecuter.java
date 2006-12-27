d7a1cdd09015c374799f6324d9b3da97e8a7b6be
rganor <rganor>
2006-12-27 10:00:10 +0000

    revert sysout lines,

--- plugins/org.eclipse.php.core/src/org/eclipse/php/core/phpModel/parser/ParserExecuter.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/core/phpModel/parser/ParserExecuter.java
@@ -65,8 +65,6 @@ public final class ParserExecuter implements Runnable {
 
 		} catch (Exception e) {
 			Logger.logException(e);
-			System.err.println(filename);
-			System.out.println(filename);			
 
 		} finally {
 
