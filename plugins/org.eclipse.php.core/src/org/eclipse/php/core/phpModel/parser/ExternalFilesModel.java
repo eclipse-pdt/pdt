9149bd6c47c93b313c30fd9c9da92965444df3cb
mspector <mspector>
2007-01-23 17:31:07 +0000

    PDT API refactoring: put internal classes under 'org.eclipse.php.internal.*' package.

--- plugins/org.eclipse.php.core/src/org/eclipse/php/core/phpModel/parser/ExternalFilesModel.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.php.core.phpModel.parser;
-
-import org.eclipse.php.core.phpModel.phpElementData.PHPFileData;
-
-public interface ExternalFilesModel extends IPhpModel {
-
-	public Object getExternalResource(PHPFileData fileData);
-}
