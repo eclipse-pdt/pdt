394acfb08cdbdecb65f272f896357d8ad8758668
sgibly <sgibly>
2007-01-28 12:08:05 +0000

    Fixed bug #171910 - The debug console links did not mark the line when opening the file.

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/console/PHPFileLink.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/console/PHPFileLink.java
@@ -1,10 +1,20 @@
 package org.eclipse.php.internal.debug.ui.console;
 
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.ui.console.FileLink;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.php.internal.core.documentModel.DOMModelForPHP;
+import org.eclipse.php.internal.debug.ui.Logger;
+import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
 import org.eclipse.php.internal.ui.util.EditorUtility;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.console.IHyperlink;
+import org.eclipse.wst.sse.core.StructuredModelManager;
 
 /**
  * 
@@ -46,9 +56,25 @@ public class PHPFileLink implements IHyperlink {
 		try {
 			editorPart = EditorUtility.openInEditor(fFile, false);
 			if (editorPart != null && fFileLineNumber > 0) {
+				if (fFileOffset < 0) {
+					PHPStructuredEditor e = (PHPStructuredEditor) editorPart;
+					DOMModelForPHP domModel = ((DOMModelForPHP) StructuredModelManager.getModelManager().getModelForRead((IFile) fFile));
+					IRegion region = domModel.getDocument().getStructuredDocument().getLineInformation(fFileLineNumber - 1);
+					fFileOffset = region.getOffset();
+					fFileLength = region.getLength();
+				}
 				EditorUtility.revealInEditor(editorPart, fFileOffset, fFileLength);
 			}
 		} catch (PartInitException e) {
+			Logger.logException(e);
+		} catch (BadLocationException e) {
+			Logger.logException(e);
+		} catch (IOException e) {
+			Logger.logException(e);
+		} catch (CoreException e) {
+			Logger.logException(e);
+		} catch (NullPointerException npe) {
+			Logger.logException(npe);
 		}
 	}
 
