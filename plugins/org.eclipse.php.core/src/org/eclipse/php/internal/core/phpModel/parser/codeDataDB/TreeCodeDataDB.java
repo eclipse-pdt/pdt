5239ca7790539d2b24af2e2bea25e2ee20bc608d
slapsha <slapsha>
2007-02-06 15:20:23 +0000

    Use BucketMap

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/phpModel/parser/codeDataDB/TreeCodeDataDB.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/phpModel/parser/codeDataDB/TreeCodeDataDB.java
@@ -10,56 +10,48 @@
  *******************************************************************************/
 package org.eclipse.php.internal.core.phpModel.parser.codeDataDB;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.eclipse.php.internal.core.phpModel.phpElementData.CodeData;
+import org.eclipse.php.internal.core.util.collections.BucketMap;
 
 public class TreeCodeDataDB implements CodeDataDB {
 
-	private List chachedList = null;;
+	private List cachedList = null;;
 	private final Set sortedData = new TreeSet();
-	private final Map treeDatabase = new HashMap(1000);
+	private final BucketMap elements = new BucketMap(1000);
 
 	public synchronized void addCodeData(final CodeData codeData) {
-		chachedList = null;
-		final String codeDataIdentifier = getCodeDataIdentifier(codeData.getName());
-		List list = (ArrayList) treeDatabase.get(codeDataIdentifier);
-		if (list == null) {
-			list = new ArrayList();
-			treeDatabase.put(codeDataIdentifier, list);
-		} else
-			list.remove(codeData);
-		list.add(codeData);
-		sortedData.add(codeData);
+		cachedList = null;
+		final String key = getCodeDataIdentifier(codeData.getName());
+		elements.add(key, codeData); // O(N)
+		sortedData.add(codeData); // O(lgN)
 	}
 
 	/*
-	 returns an unmodifiable instance of the (chached) list
+	 returns an unmodifiable instance of the (cached) list
 	 */
 	public synchronized List asList() {
-		if (chachedList != null)
-			return chachedList;
+		if (cachedList != null)
+			return cachedList;
 
-		chachedList = Arrays.asList(sortedData.toArray());
-		return chachedList;
+		cachedList = Arrays.asList(sortedData.toArray());
+		return cachedList;
 	}
 
 	public synchronized void clear() {
-		chachedList = null;
-		treeDatabase.clear();
+		cachedList = null;
+		elements.clear();
 		sortedData.clear();
 	}
 
 	public synchronized List getCodeData(final String name) {
 		if (name == null)
 			return null;
-		return (List) treeDatabase.get(getCodeDataIdentifier(name));
+		return (List) elements.get(getCodeDataIdentifier(name));
 	}
 
 	private String getCodeDataIdentifier(final String name) {
@@ -67,14 +59,9 @@ public class TreeCodeDataDB implements CodeDataDB {
 	}
 
 	public synchronized void removeCodeData(final CodeData codeData) {
-		chachedList = null;
-		final String codeDataIdentifier = getCodeDataIdentifier(codeData.getName());
-		final ArrayList list = (ArrayList) treeDatabase.get(codeDataIdentifier);
-		if (list == null)
-			return;
-		list.remove(codeData);
-		if (list.isEmpty())
-			treeDatabase.remove(list);
+		cachedList = null;
+		final String key = getCodeDataIdentifier(codeData.getName());
+		elements.remove(key, codeData);
 		sortedData.remove(codeData);
 	}
 
---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/collections/BucketMap.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2007 Zend Technologies
+ * 
+ */
+package org.eclipse.php.internal.core.util.collections;
+
+import java.util.*;
+
+public class BucketMap/*<K,V>*/{
+	private Map/*<K, Set<V>>*/map;
+
+	public BucketMap() {
+		map = new HashMap();
+	}
+
+	public BucketMap(int size) {
+		map = new HashMap(size);
+	}
+
+	public void add(/*K*/Object key, /*V*/Object value) {
+		Set/*<V>*/values = (Set) map.get(key);
+		if (values == null) {
+			values = new HashSet(1);
+			map.put(key, values);
+		}
+		values.add(value);
+	}
+
+	public void addAll(/*K*/Object key, Collection/*<V>*/newValues) {
+		Set/*<V>*/values = (Set) map.get(key);
+		if (values == null) {
+			values = new HashSet();
+			map.put(key, values);
+		}
+		values.addAll(newValues);
+	}
+
+	public boolean contains(/*K*/Object key, /*V*/Object value) {
+		Set/*<V>*/values = (Set) map.get(key);
+		if (values == null)
+			return false;
+		return values.contains(value);
+	}
+
+	public Set/*<V>*/get(/*K*/Object key) {
+		Set/*<V>*/values = getSet(key);
+		if (values == null)
+			return new HashSet(0);
+		return values;
+	}
+
+	public Set/*<V>*/getSet(Object key) {
+		return (Set) map.get(key);
+	}
+
+	public Set/*<V>*/getAll() {
+		Set valuesSet = new HashSet();
+		for (Iterator i = map.values().iterator(); i.hasNext();) {
+			Set values = (Set) i.next();
+			for (Iterator j = values.iterator(); j.hasNext();)
+				valuesSet.add(j.next());
+		}
+		return valuesSet;
+	}
+
+	public boolean remove(/*K*/Object key, /*V*/Object value) {
+		Set/*<K>*/values = (Set) map.get(key);
+		if (values == null)
+			return false;
+		boolean result = values.remove(value);
+		if (values.size() == 0) {
+			map.remove(key);
+		}
+		return result;
+	}
+
+	public/*V*/Object[] removeAll(Object/*K*/key) {
+		Set/*<V>*/removedStrings = (Set) map.remove(key);
+		if (removedStrings != null)
+			return removedStrings.toArray();
+		return new Object[0];
+	}
+
+	public void clear() {
+		map.clear();
+	}
+}
