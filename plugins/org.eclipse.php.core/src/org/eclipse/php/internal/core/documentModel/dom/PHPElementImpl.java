fdd14b0f0c75832e7ec216a99b2388c85ae2f8cc
slapsha <slapsha>
2007-02-06 13:49:54 +0000

    Remove usage of PHPElementImpl

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/documentModel/dom/PHPElementImpl.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/documentModel/dom/PHPElementImpl.java
@@ -15,21 +15,12 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.php.internal.core.documentModel.DOMModelForPHP;
 import org.eclipse.php.internal.core.documentModel.validate.HTMLElementPropagatingValidator;
 import org.eclipse.php.internal.core.documentModel.validate.HTMLEmptyValidator;
-import org.eclipse.php.internal.core.phpModel.parser.PHPWorkspaceModelManager;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPClassConstData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPClassData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPClassVarData;
 import org.eclipse.php.internal.core.phpModel.phpElementData.PHPCodeData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPConstantData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPFileData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPFunctionData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.UserData;
 import org.eclipse.wst.sse.core.internal.provisional.INodeAdapter;
 import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocumentRegion;
 import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
 import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegionList;
 import org.eclipse.wst.xml.core.internal.document.ElementImpl;
-import org.eclipse.wst.xml.core.internal.document.NodeImpl;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
@@ -90,58 +81,7 @@ public class PHPElementImpl extends ElementImpl implements IAdaptable {
 	}
 
 	public PHPCodeData getPHPCodeData(int offset) {
-		return getPHPCodeData(this, offset);
-	}
-
-	// return php model item  at offset
-	public static PHPCodeData getPHPCodeData(NodeImpl node, int offset) {
-		String location = node.getModel().getBaseLocation();
-
-		PHPFileData fileData = PHPWorkspaceModelManager.getInstance().getModelForFile(location);
-		if (fileData == null)
-			return null;
-
-		PHPConstantData[] constants = fileData.getConstants();
-		if (constants != null)
-			for (int i = 0; i < constants.length; i++)
-				if (isInside(offset, constants[i]))
-					return constants[i];
-
-		PHPFunctionData[] functions = fileData.getFunctions();
-		if (functions != null)
-			for (int i = 0; i < functions.length; i++)
-				if (isInside(offset, functions[i]))
-					return functions[i];
-
-		PHPClassData[] classes = fileData.getClasses();
-		if (classes != null)
-			for (int i = 0; i < classes.length; i++)
-				if (isInside(offset, classes[i])) {
-					PHPClassVarData[] vars = classes[i].getVars();
-					for (int j = 0; j < vars.length; j++)
-						if (isInside(offset, vars[j]))
-							return vars[j];
-					PHPClassConstData[] consts = classes[i].getConsts();
-					for (int j = 0; j < consts.length; j++)
-						if (isInside(offset, consts[j]))
-							return consts[j];
-					functions = classes[i].getFunctions();
-					for (int j = 0; j < functions.length; j++)
-						if (isInside(offset, functions[j]))
-							return functions[j];
-					return classes[i];
-				}
-		return fileData;
-
-	}
-
-	public static boolean isInside(int offset, PHPCodeData codeData) {
-		UserData userData = codeData.getUserData();
-		if (userData == null)
-			return false;
-		if (offset >= userData.getStartPosition() && offset <= userData.getEndPosition())
-			return true;
-		return false;
+		return Utils.getPHPCodeData(this, offset);
 	}
 
 	public boolean matchTagName(String tagName) {
