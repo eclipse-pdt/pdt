ecf42effdbb381061075ed46396c144c92a8e730
rganor <rganor>
2007-02-25 16:55:58 +0000

    Text search capabilities to : 1) TextSearcher 2) TextSearcherFactory 3) Tests + goldens

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/search/text/TextSearcherFactory.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Zend and IBM - Initial implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.search.text;
+
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.php.internal.ui.search.text.TextSearcher.ResultVisitor;
+import org.eclipse.search.core.text.TextSearchEngine;
+import org.eclipse.search.core.text.TextSearchScope;
+
+/**
+ * Description: Factory class to the {@link TextSearcher}  
+ * @author Roy, 2007
+ */
+public class TextSearcherFactory {
+
+	/**
+	 * default foactory - includes all parameters
+	 * @param engine
+	 * @param newSearchScope
+	 * @param visitor
+	 * @param searchPattern
+	 * @return
+	 */	
+	public final static TextSearcher createSearcher(TextSearchEngine engine, TextSearchScope newSearchScope, ResultVisitor visitor, Pattern searchPattern) {
+		return new TextSearcher(engine, newSearchScope, visitor, searchPattern);
+	}
+
+	public final static TextSearcher createSearcher(TextSearchScope newSearchScope, ResultVisitor visitor, Pattern searchPattern) {
+		final TextSearchEngine engine = TextSearchEngine.createDefault();
+		return createSearcher(engine, newSearchScope, visitor, searchPattern);
+	}
+	
+	public final static TextSearcher createSearcher(IResource resource, ResultVisitor visitor, Pattern searchPattern) {
+		final TextSearchScope searchScope = TextSearchScope.newSearchScope(new IResource[] { resource } , Pattern.compile(".*"), true);
+		return createSearcher(searchScope, visitor, searchPattern);
+	}
+
+	public final static TextSearcher createSearcher(IResource resource, Pattern searchPattern) {
+		final ResultVisitor visitor = new ResultVisitor();
+		return createSearcher(resource, visitor, searchPattern);
+	}
+
+	public final static TextSearcher createSearcher(IResource resource, String searchPattern) {
+		final Pattern pattern = Pattern.compile(searchPattern);
+		return createSearcher(resource, pattern);
+	}
+}
