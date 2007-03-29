880d0c035ccc5fd5153eca36995ab5181b89d51c
ggurfinke <ggurfinke>
2007-03-29 11:24:17 +0000

    Bug fix #168993 - double-click doesn't expend folder

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/OpenEditorActionGroup.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/OpenEditorActionGroup.java
@@ -78,8 +78,10 @@ public class OpenEditorActionGroup extends ActionGroup {
 	 */
 	public void fillContextMenu(IMenuManager menu) {
 		super.fillContextMenu(menu);
+		boolean enabled = fOpen.isEnabled();
 		fOpen = new OpenAction(fSite);        
 		fOpen.setActionDefinitionId(IPHPEditorActionDefinitionIds.OPEN_EDITOR);
+		fOpen.setEnabled(enabled);
 		appendToGroup(menu, fOpen);
 		if (!fIsEditorOwner) {
 			addOpenWithMenu(menu);
