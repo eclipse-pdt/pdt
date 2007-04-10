2f5903467bfb7b11b67f0e438f5109a883092914
slapsha <slapsha>
2007-04-10 00:41:35 +0000

    another utility function

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/text/TextSequenceUtilities.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/text/TextSequenceUtilities.java
@@ -21,8 +21,7 @@ import org.eclipse.wst.sse.core.internal.text.BasicStructuredDocument;
 
 public final class TextSequenceUtilities {
 
-	private TextSequenceUtilities() {
-	}
+	private TextSequenceUtilities() {}
 
 	public static TextSequence createTextSequence(IStructuredDocumentRegion source) {
 		return createTextSequence(source, 0, source.getLength());
@@ -36,12 +35,22 @@ public final class TextSequenceUtilities {
 	}
 
 	public static String getType(TextSequence textSequence, int index) {
-		IStructuredDocumentRegion source = textSequence.getSource();
 		int sourceOffset = textSequence.getOriginalOffset(index);
+		return getTypeByAbsoluteOffset(textSequence, sourceOffset);
+	}
+
+	/**
+	 * @param textSequence
+	 * @param sourceOffset
+	 * @return
+	 */
+	public static String getTypeByAbsoluteOffset(TextSequence textSequence, int sourceOffset) {
+		IStructuredDocumentRegion source = textSequence.getSource();
 		ITextRegion tRegion = source.getRegionAtCharacterOffset(sourceOffset);
 		if (tRegion.getType() == PHPRegionContext.PHP_CONTENT) {
 			try {
-				return ((PhpScriptRegion) tRegion).getPhpTokenType(sourceOffset - source.getStart() - tRegion.getStart());
+				return ((PhpScriptRegion) tRegion).getPhpTokenType(sourceOffset
+					- source.getStart() - tRegion.getStart());
 			} catch (BadLocationException e) {
 				assert false;
 				return null;
@@ -53,11 +62,13 @@ public final class TextSequenceUtilities {
 		return null;
 	}
 
-	////////////////////////////////////////////////////////////////////////////////////////////////
+	// //////////////////////////////////////////////////////////////////////////////////////////////
 	private static abstract class AbstractTextSequence implements TextSequence {
 
 		IStructuredDocumentRegion source;
+
 		Segment segment;
+
 		int segmentOriginalStart;
 
 		protected AbstractTextSequence(IStructuredDocumentRegion source, Segment segment, int segmentOriginalStart) {
@@ -82,11 +93,12 @@ public final class TextSequenceUtilities {
 
 	}
 
-	////////////////////////////////////////////////////////////////////////////////////////////////
+	// //////////////////////////////////////////////////////////////////////////////////////////////
 
 	private static class SimpleTextSequence extends AbstractTextSequence implements TextSequence {
 
 		private int offset;
+
 		private int length;
 
 		SimpleTextSequence(IStructuredDocumentRegion source, Segment segment, int offset, int length, int segmentOriginalStart) {
@@ -108,7 +120,8 @@ public final class TextSequenceUtilities {
 		}
 
 		public TextSequence subTextSequence(int start, int end) {
-			return new SimpleTextSequence(source, segment, offset + start, end - start, segmentOriginalStart);
+			return new SimpleTextSequence(source, segment, offset + start, end
+				- start, segmentOriginalStart);
 		}
 
 		public TextSequence cutTextSequence(int start, int end) {
@@ -119,7 +132,8 @@ public final class TextSequenceUtilities {
 				return subTextSequence(0, start);
 			}
 
-			int[] newIndexes = new int[] { offset, start, offset + end, length - end };
+			int[] newIndexes = new int[] { offset, start, offset + end,
+				length - end };
 			return new CompositeTextSequence(source, segment, newIndexes, segmentOriginalStart);
 		}
 
@@ -129,11 +143,12 @@ public final class TextSequenceUtilities {
 
 	}
 
-	////////////////////////////////////////////////////////////////////////////////////////////////
+	// //////////////////////////////////////////////////////////////////////////////////////////////
 
 	private static class CompositeTextSequence extends AbstractTextSequence implements TextSequence {
 
 		private int[] indexes;
+
 		private int length = -1;
 
 		CompositeTextSequence(IStructuredDocumentRegion source, Segment segment, int[] indexes, int segmentOriginalStart) {
@@ -200,7 +215,8 @@ public final class TextSequenceUtilities {
 			}
 			int newNumberOfParts = endPart - startPart + 1;
 			if (newNumberOfParts == 1) {
-				int newStart = indexes[(startPart << 1)] + start - startPartLength;
+				int newStart = indexes[(startPart << 1)] + start
+					- startPartLength;
 				int newLength = end - start;
 				return new SimpleTextSequence(source, segment, newStart, newLength, segmentOriginalStart);
 			}
@@ -208,7 +224,8 @@ public final class TextSequenceUtilities {
 			int[] newIndexes = new int[newNumberOfParts << 1];
 			// set indexes at start Part
 			newIndexes[0] = indexes[(startPart << 1)] + start - startPartLength;
-			newIndexes[1] = indexes[(startPart << 1) + 1] - (start - startPartLength);
+			newIndexes[1] = indexes[(startPart << 1) + 1]
+				- (start - startPartLength);
 			// set indexes after start Part and before last part
 
 			for (int i = 2; i < newIndexes.length - 3; i++) {
@@ -256,8 +273,10 @@ public final class TextSequenceUtilities {
 			newIndexes[(part << 1) + 1] = start - startPartLength;
 			// set indexes at end part
 			part++;
-			newIndexes[(part << 1)] = indexes[(endPart << 1)] + (end - endPartLength);
-			newIndexes[(part << 1) + 1] = indexes[(endPart << 1) + 1] - (end - endPartLength);
+			newIndexes[(part << 1)] = indexes[(endPart << 1)]
+				+ (end - endPartLength);
+			newIndexes[(part << 1) + 1] = indexes[(endPart << 1) + 1]
+				- (end - endPartLength);
 			// set indexes after end part
 			part++;
 			int diff = numberOfParts - newNumberOfParts;
