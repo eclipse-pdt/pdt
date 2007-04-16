6c4cbf94a7da47376de6fc6dcbec5c7c7dc3bc57
eklein <eklein>
2007-04-16 10:34:56 +0000

    This class behaves like OpenExternalFileAction, with the difference that the run method gets a List  of file paths (Strings) to open in the editor, instead of activating a file browse dialog

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/util/PHPOpenExternalFileAction.java
@@ -0,0 +1,217 @@
+package org.eclipse.php.internal.ui.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.*;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.ui.editors.text.EditorsUI;
+import org.eclipse.ui.internal.editors.text.EditorsPlugin;
+import org.eclipse.ui.internal.editors.text.JavaFileEditorInput;
+import org.eclipse.ui.part.FileEditorInput;
+
+/**
+ * This class behaves like OpenExternalFileAction, with the difference that the run method gets a List
+ * of file paths (Strings) to open in the editor, instead of activating a file browse dialog
+ * @author Eden K., 2007
+ *
+ */
+public class PHPOpenExternalFileAction extends Action implements IWorkbenchWindowActionDelegate {
+	static class FileLabelProvider extends LabelProvider {
+		/*
+		 * @see org.eclipse.jface.viewers.LabelProvider#getText(java.lang.Object)
+		 */
+		public String getText(Object element) {
+			if (element instanceof IFile) {
+				IPath path=  ((IFile) element).getFullPath();
+				return path != null ? path.toString() : ""; //$NON-NLS-1$
+			}
+			return super.getText(element);
+		}
+	}
+
+
+	private IWorkbenchWindow fWindow;
+	private String fFilterPath;
+	private StringBuffer notFound= new StringBuffer();
+
+	public PHPOpenExternalFileAction() {
+		setEnabled(true);
+	}
+
+	/*
+	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#dispose()
+	 */
+	public void dispose() {
+		fWindow= null;
+		fFilterPath= null;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+	 */
+	public void init(IWorkbenchWindow window) {
+		fWindow= window;
+		fFilterPath= System.getProperty("user.home"); //$NON-NLS-1$
+	}
+
+	/*
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	public void run(IAction action) {
+		run();
+	}
+
+	/*
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	/*
+	 * @see org.eclipse.jface.action.Action#run()
+	 */
+	public void run(List filePaths) {
+		if (filePaths != null) {
+			int numberOfFilesNotFound= 0;
+			
+			for (Iterator iter = filePaths.iterator(); iter.hasNext();) {				
+				String currentFilePath = (String) iter.next();
+				IPath path = new Path(currentFilePath);						
+				fFilterPath= path.removeLastSegments(1).toOSString();				
+				IFileStore fileStore= EFS.getLocalFileSystem().getStore(new Path(fFilterPath));
+				fileStore= fileStore.getChild(path.lastSegment());
+				if (!fileStore.fetchInfo().isDirectory() && fileStore.fetchInfo().exists()) {
+					IEditorInput input= createEditorInput(fileStore);
+					String editorId= getEditorId(fileStore);
+					IWorkbenchPage page= fWindow.getActivePage();
+					try {
+						page.openEditor(input, editorId);
+					} catch (PartInitException e) {
+					}
+				} else {
+					if (++numberOfFilesNotFound > 1)
+						notFound.append('\n');
+					notFound.append(currentFilePath);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Returns a String with the names of the files that couldnt be found
+	 */
+	public String getFileNotFoundStr(){
+		return notFound.toString();
+	}
+
+	/*
+	 * XXX: Requested a helper to get the correct editor descriptor
+	 *		see: https://bugs.eclipse.org/bugs/show_bug.cgi?id=110203
+	 */
+	private String getEditorId(IFileStore file) {
+		IWorkbench workbench= fWindow.getWorkbench();
+		IEditorRegistry editorRegistry= workbench.getEditorRegistry();
+		IEditorDescriptor descriptor= editorRegistry.getDefaultEditor(file.getName(), getContentType(file));
+
+		// check the OS for in-place editor (OLE on Win32)
+		if (descriptor == null && editorRegistry.isSystemInPlaceEditorAvailable(file.getName()))
+			descriptor= editorRegistry.findEditor(IEditorRegistry.SYSTEM_INPLACE_EDITOR_ID);
+		
+		// check the OS for external editor
+		if (descriptor == null && editorRegistry.isSystemExternalEditorAvailable(file.getName()))
+			descriptor= editorRegistry.findEditor(IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID);
+		
+		if (descriptor != null)
+			return descriptor.getId();
+		
+		return EditorsUI.DEFAULT_TEXT_EDITOR_ID;
+	}
+
+	private IContentType getContentType (IFileStore fileStore) {
+		if (fileStore == null)
+			return null;
+
+		InputStream stream= null;
+		try {
+			stream= fileStore.openInputStream(EFS.NONE, null);
+			return Platform.getContentTypeManager().findContentTypeFor(stream, fileStore.getName());
+		} catch (IOException x) {
+			EditorsPlugin.log(x);
+			return null;
+		} catch (CoreException x) {
+			// Do not log FileNotFoundException (no access)
+			if (!(x.getStatus().getException() instanceof FileNotFoundException))
+				EditorsPlugin.log(x);
+			
+			return null;
+		} finally {
+			try {
+				if (stream != null)
+					stream.close();
+			} catch (IOException x) {
+				EditorsPlugin.log(x);
+			}
+		}
+	}
+
+	private IEditorInput createEditorInput(IFileStore fileStore) {
+		IFile workspaceFile= getWorkspaceFile(fileStore);
+		if (workspaceFile != null)
+			return new FileEditorInput(workspaceFile);
+		return new JavaFileEditorInput(fileStore);
+	}
+
+	private IFile getWorkspaceFile(IFileStore fileStore) {
+		IWorkspace workspace= ResourcesPlugin.getWorkspace();
+		IFile[] files= workspace.getRoot().findFilesForLocation(new Path(fileStore.toURI().getPath()));
+		files= filterNonExistentFiles(files);
+		if (files == null || files.length == 0)
+			return null;
+		if (files.length == 1)
+			return files[0];
+		return selectWorkspaceFile(files);
+	}
+
+	private IFile[] filterNonExistentFiles(IFile[] files){
+		if (files == null)
+			return null;
+
+		int length= files.length;
+		ArrayList existentFiles= new ArrayList(length);
+		for (int i= 0; i < length; i++) {
+			if (files[i].exists())
+				existentFiles.add(files[i]);
+		}
+		return (IFile[])existentFiles.toArray(new IFile[existentFiles.size()]);
+	}
+
+	private IFile selectWorkspaceFile(IFile[] files) {
+		ElementListSelectionDialog dialog= new ElementListSelectionDialog(fWindow.getShell(), new FileLabelProvider());
+		dialog.setElements(files);
+//		dialog.setTitle(TextEditorMessages.OpenExternalFileAction_title_selectWorkspaceFile);
+//		dialog.setMessage(TextEditorMessages.OpenExternalFileAction_message_fileLinkedToMultiple);
+		if (dialog.open() == Window.OK)
+			return (IFile) dialog.getFirstResult();
+		return null;
+	}
+}
