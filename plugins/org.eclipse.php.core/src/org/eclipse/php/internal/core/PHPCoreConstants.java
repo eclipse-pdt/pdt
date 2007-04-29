0e6424adfc61588aa9236341d21cf4df9255bc41
sgibly <sgibly>
2007-04-29 12:34:16 +0000

    Fixed bug #179484 - Added the @todo as one of the default taks tags

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/PHPCoreConstants.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/PHPCoreConstants.java
@@ -40,8 +40,8 @@ public interface PHPCoreConstants {
 	public static final String TASK_PRIORITY_NORMAL = "NORMAL"; //$NON-NLS-1$
 	public static final String TASK_TAGS = PLUGIN_ID + ".taskTags"; //$NON-NLS-1$
 	public static final String TASK_CASE_SENSITIVE = PLUGIN_ID + ".taskCaseSensitive"; //$NON-NLS-1$
-	public static final String DEFAULT_TASK_TAGS = "TODO,FIXME,XXX"; //$NON-NLS-1$
-	public static final String DEFAULT_TASK_PRIORITIES = "NORMAL,HIGH,NORMAL"; //$NON-NLS-1$
+	public static final String DEFAULT_TASK_TAGS = "TODO,FIXME,XXX,@todo"; //$NON-NLS-1$
+	public static final String DEFAULT_TASK_PRIORITIES = "NORMAL,HIGH,NORMAL,NORMAL"; //$NON-NLS-1$
 	public static final String ENABLED = "enabled"; //$NON-NLS-1$
 	public static final String DISABLED = "disabled"; //$NON-NLS-1$
 	public static final String DEFAULT_INDENTATION_SIZE = "1"; //$NON-NLS-1$
