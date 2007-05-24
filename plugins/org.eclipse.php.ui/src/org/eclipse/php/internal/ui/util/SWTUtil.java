c883492b089470cdc33225afe17d8187c4ae133c
mspector <mspector>
2007-05-24 12:53:10 +0000

    add missing methods

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/util/SWTUtil.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/util/SWTUtil.java
@@ -10,32 +10,21 @@
  *******************************************************************************/
 package org.eclipse.php.internal.ui.util;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.debug.internal.ui.DebugUIPlugin;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.preference.IPreferenceNode;
-import org.eclipse.jface.preference.IPreferencePage;
-import org.eclipse.jface.preference.PreferenceDialog;
-import org.eclipse.jface.preference.PreferenceManager;
-import org.eclipse.jface.preference.PreferenceNode;
+import org.eclipse.jface.preference.*;
 import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.jface.util.Assert;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.dnd.DragSource;
 import org.eclipse.swt.dnd.DropTarget;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Caret;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.ScrollBar;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.Widget;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.PlatformUI;
 
@@ -78,32 +67,6 @@ public class SWTUtil {
 			return ((ScrollBar) widget).getParent().getShell();
 
 		return null;
-	}
-
-	/**
-	 * Returns a width hint for a button control.
-	 */
-	public static int getButtonWidthHint(Button button) {
-		button.setFont(JFaceResources.getDialogFont());
-		PixelConverter converter = new PixelConverter(button);
-		int widthHint = converter.convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
-		return Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
-	}
-
-	/**
-	 * Sets width and height hint for the button control.
-	 * <b>Note:</b> This is a NOP if the button's layout data is not
-	 * an instance of <code>GridData</code>.
-	 * 
-	 * @param button	the button for which to set the dimension hint
-	 */
-	public static void setButtonDimensionHint(Button button) {
-		Assert.isNotNull(button);
-		Object gd = button.getLayoutData();
-		if (gd instanceof GridData) {
-			((GridData) gd).widthHint = getButtonWidthHint(button);
-			((GridData) gd).horizontalAlignment = GridData.FILL;
-		}
 	}
 
 	public static int getTableHeightHint(Table table, int rows) {
@@ -152,4 +115,135 @@ public class SWTUtil {
 			}
 		});		
 	}
+	
+	/**
+	 * Returns a width hint for a button control.
+	 */
+	public static int getButtonWidthHint(Button button) {
+		button.setFont(JFaceResources.getDialogFont());
+		PixelConverter converter= new PixelConverter(button);
+		int widthHint= converter.convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
+		return Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
+	}
+	
+	/**
+	 * Sets width and height hint for the button control.
+	 * <b>Note:</b> This is a NOP if the button's layout data is not
+	 * an instance of <code>GridData</code>.
+	 * 
+	 * @param	the button for which to set the dimension hint
+	 */		
+	public static void setButtonDimensionHint(Button button) {
+		Assert.isNotNull(button);
+		Object gd= button.getLayoutData();
+		if (gd instanceof GridData) {
+			((GridData)gd).widthHint= getButtonWidthHint(button);	
+			((GridData)gd).horizontalAlignment = GridData.FILL;	 
+		}
+	}		
+	
+	
+	/**
+	 * Creates and returns a new push button with the given
+	 * label and/or image.
+	 * 
+	 * @param parent parent control
+	 * @param label button label or <code>null</code>
+	 * @param image image of <code>null</code>
+	 * 
+	 * @return a new push button
+	 */
+	public static Button createPushButton(Composite parent, String label, Image image) {
+		Button button = new Button(parent, SWT.PUSH);
+		button.setFont(parent.getFont());
+		if (image != null) {
+			button.setImage(image);
+		}
+		if (label != null) {
+			button.setText(label);
+		}
+		GridData gd = new GridData();
+		button.setLayoutData(gd);	
+		SWTUtil.setButtonDimensionHint(button);
+		return button;	
+	}	
+
+	/**
+	 * Creates and returns a new radio button with the given
+	 * label.
+	 * 
+	 * @param parent parent control
+	 * @param label button label or <code>null</code>
+	 * 
+	 * @return a new radio button
+	 */
+	public static Button createRadioButton(Composite parent, String label) {
+		Button button = new Button(parent, SWT.RADIO);
+		button.setFont(parent.getFont());
+		if (label != null) {
+			button.setText(label);
+		}
+		GridData gd = new GridData();
+		button.setLayoutData(gd);	
+		SWTUtil.setButtonDimensionHint(button);
+		return button;	
+	}	
+	
+	/**
+	 * Creates a new label widget
+	 * @param parent the parent composite to add this label widget to
+	 * @param text the text for the label
+	 * @param hspan the horizontal span to take up in the parent composite
+	 * @return the new label
+	 * @since 3.2
+	 * 
+	 */
+	public static Label createLabel(Composite parent, String text, int hspan) {
+		Label l = new Label(parent, SWT.NONE);
+		l.setFont(parent.getFont());
+		l.setText(text);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan = hspan;
+		l.setLayoutData(gd);
+		return l;
+	}
+	
+	/**
+	 * Creates a new text widget 
+	 * @param parent the parent composite to add this text widget to
+	 * @param hspan the horizontal span to take up on the parent composite
+	 * @return the new text widget
+	 * @since 3.2
+	 * 
+	 */
+	public static Text createSingleText(Composite parent, int hspan) {
+    	Text t = new Text(parent, SWT.SINGLE | SWT.BORDER);
+    	t.setFont(parent.getFont());
+    	GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+    	gd.horizontalSpan = hspan;
+    	t.setLayoutData(gd);
+    	return t;
+    }
+	
+	/**
+	 * Creates a Group widget
+	 * @param parent the parent composite to add this group to
+	 * @param text the text for the heading of the group
+	 * @param columns the number of columns within the group
+	 * @param hspan the horizontal span the group should take up on the parent
+	 * @param fill the style for how this composite should fill into its parent
+	 * @return the new group
+	 * @since 3.2
+	 * 
+	 */
+	public static Group createGroup(Composite parent, String text, int columns, int hspan, int fill) {
+    	Group g = new Group(parent, SWT.NONE);
+    	g.setLayout(new GridLayout(columns, false));
+    	g.setText(text);
+    	g.setFont(parent.getFont());
+    	GridData gd = new GridData(fill);
+		gd.horizontalSpan = hspan;
+    	g.setLayoutData(gd);
+    	return g;
+    }
 }
