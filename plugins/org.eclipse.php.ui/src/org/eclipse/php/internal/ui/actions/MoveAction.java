4082c1762cf2189d9a8c4cf28eb866903e2a45a3
eklein <eklein>
2007-07-05 11:25:23 +0000

    final decision - enable project move :)

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/MoveAction.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/MoveAction.java
@@ -97,9 +97,8 @@ public class MoveAction extends SelectionDispatchAction {
 		}
 
 		if (!selection.isEmpty()) {
-			if (ActionUtils.containsOnlyProjects(selection.toList())) {				
-				// disable project move since it doesn't work in eclipse
-				setEnabled(false);
+			if (ActionUtils.containsOnlyProjects(selection.toList())) {
+				setEnabled(createWorkbenchAction(selection).isEnabled());
 				return;
 			}
 			List elements = selection.toList();
