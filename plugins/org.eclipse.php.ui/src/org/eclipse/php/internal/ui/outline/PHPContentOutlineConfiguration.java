e87bb39d21fc7da5614ff01e48928f5fceb4ed49
sben-zeev <sben-zeev>
2007-07-10 15:01:44 +0000

    php outline working in html mode show attributes action didn't work

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/outline/PHPContentOutlineConfiguration.java
@@ -0,0 +1,222 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Zend and IBM - Initial implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.outline;
+
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.php.internal.core.documentModel.dom.Utils;
+import org.eclipse.php.internal.core.phpModel.phpElementData.PHPCodeData;
+import org.eclipse.php.internal.ui.PHPUIMessages;
+import org.eclipse.php.internal.ui.actions.SortAction;
+import org.eclipse.php.internal.ui.treecontent.TreeProvider;
+import org.eclipse.php.internal.ui.util.AppearanceAwareLabelProvider;
+import org.eclipse.php.internal.ui.util.EditorUtility;
+import org.eclipse.php.internal.ui.util.PHPElementLabels;
+import org.eclipse.php.internal.ui.util.PHPOutlineElementComparer;
+import org.eclipse.php.ui.treecontent.IPHPTreeContentProvider;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.wst.html.ui.views.contentoutline.HTMLContentOutlineConfiguration;
+import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
+import org.eclipse.wst.sse.core.utils.StringUtils;
+import org.eclipse.wst.xml.core.internal.contentmodel.CMAttributeDeclaration;
+import org.eclipse.wst.xml.core.internal.contentmodel.CMDataType;
+import org.eclipse.wst.xml.core.internal.contentmodel.CMElementDeclaration;
+import org.eclipse.wst.xml.core.internal.contentmodel.modelquery.ModelQuery;
+import org.eclipse.wst.xml.core.internal.document.NodeImpl;
+import org.eclipse.wst.xml.core.internal.modelquery.ModelQueryUtil;
+import org.eclipse.wst.xml.ui.internal.contentoutline.JFaceNodeLabelProvider;
+import org.eclipse.wst.xml.ui.internal.contentoutline.XMLNodeActionManager;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+public class PHPContentOutlineConfiguration extends HTMLContentOutlineConfiguration {
+	protected PHPOutlineContentProvider fContentProvider = null;
+	protected PHPOutlineLabelProvider fLabelProvider = null;
+	IPHPTreeContentProvider[] treeProviders;
+
+	protected IContributionItem[] createMenuContributions(final TreeViewer viewer) {
+		IContributionItem[] items;
+		final IContributionItem showPHPItem = new ActionContributionItem(new ChangeOutlineModeAction(PHPUIMessages.PHPOutlinePage_mode_php, PHPOutlineContentProvider.MODE_PHP, viewer));
+		ChangeOutlineModeAction action = new ChangeOutlineModeAction(PHPUIMessages.PHPOutlinePage_mode_html, PHPOutlineContentProvider.MODE_HTML, viewer);
+		action.addPropertyChangeListener(new IPropertyChangeListener() {
+
+			public void propertyChange(PropertyChangeEvent event) {
+				if (event.getProperty().equals("checked")) {
+					boolean checked = ((Boolean) event.getNewValue()).booleanValue();
+					if (sortAction != null) {
+						sortAction.setEnabled(!checked);
+					}
+				}
+
+			}
+
+		});
+		final IContributionItem showHTMLItem = new ActionContributionItem(action);
+		items = super.createMenuContributions(viewer);
+		if (items == null)
+			items = new IContributionItem[] { showPHPItem, showHTMLItem };
+		else {
+			final IContributionItem[] combinedItems = new IContributionItem[items.length + 2];
+			System.arraycopy(items, 0, combinedItems, 0, items.length);
+			combinedItems[items.length] = showPHPItem;
+			combinedItems[items.length + 1] = showHTMLItem;
+			items = combinedItems;
+		}
+		if (action.isChecked()) {
+			sortAction.setEnabled(false);
+		}
+		return items;
+	}
+
+	public static class DoubleClickListener implements IDoubleClickListener {
+
+		private boolean enabled;
+
+		public void doubleClick(DoubleClickEvent event) {
+			ISelection selection = event.getSelection();
+			if (!(selection instanceof IStructuredSelection)) {
+				return;
+			}
+			Object element = ((IStructuredSelection) selection).getFirstElement();
+			if (!(element instanceof PHPCodeData)) {
+				return;
+			}
+			try {
+				IEditorPart editor = EditorUtility.openInEditor(element, true);
+				if (editor != null) {
+					EditorUtility.revealInEditor(editor, (PHPCodeData) element);
+				}
+			} catch (PartInitException e) {
+			}
+		}
+
+		public void setEnabled(boolean enabled) {
+			this.enabled = enabled;
+		}
+
+		public boolean isEnabled() {
+			return enabled;
+		}
+	}
+
+	DoubleClickListener doubleClickListener = new DoubleClickListener();
+	private SortAction sortAction;
+	private JFaceNodeLabelProvider fSimpleLabelProvider;
+
+	public DoubleClickListener getDoubleClickListener() {
+		return doubleClickListener;
+	}
+
+	protected IContributionItem[] createToolbarContributions(final TreeViewer viewer) {
+		IContributionItem[] items;
+		final IContributionItem showGroupsItem = new ActionContributionItem(new ShowGroupsAction("Show Groups", viewer));
+		final IContributionItem toggleLinking = super.createMenuContributions(viewer)[0];
+		sortAction = new SortAction(viewer);
+		final IContributionItem sortItem = new ActionContributionItem(sortAction);
+
+		items = super.createToolbarContributions(viewer);
+		if (items == null)
+			items = new IContributionItem[] { sortItem, showGroupsItem };
+		else {
+			final IContributionItem[] combinedItems = new IContributionItem[items.length + 3];
+			System.arraycopy(items, 0, combinedItems, 0, items.length);
+			combinedItems[items.length] = sortItem;
+			combinedItems[items.length + 1] = showGroupsItem;
+			combinedItems[items.length + 2] = toggleLinking;
+			items = combinedItems;
+		}
+		return items;
+
+	}
+
+	public IContentProvider getContentProvider(final TreeViewer viewer) {
+		if (fContentProvider == null) {
+			viewer.setComparer(new PHPOutlineElementComparer());
+			PHPOutlineLabelProvider labelProvider = (PHPOutlineLabelProvider) getLabelProvider(viewer);
+			fContentProvider = new PHPOutlineContentProvider(viewer, labelProvider);
+			fContentProvider.phpContentProvider.setTreeProviders(getTreeProviders());
+		}
+		return fContentProvider;
+	}
+
+	public ILabelProvider getLabelProvider(final TreeViewer viewer) {
+		if (fLabelProvider == null) {
+			fLabelProvider = new PHPOutlineLabelProvider();
+			fLabelProvider.setTreeProviders(getTreeProviders());
+		}
+		return fLabelProvider;
+	}
+
+	public ISelection getSelection(final TreeViewer viewer, final ISelection selection) {
+		final IContentProvider contentProvider = viewer.getContentProvider();
+		if (contentProvider instanceof PHPOutlineContentProvider) {
+			final PHPOutlineContentProvider phpOutline = (PHPOutlineContentProvider) contentProvider;
+			if (phpOutline.mode == PHPOutlineContentProvider.MODE_PHP)
+				if (selection instanceof IStructuredSelection && selection instanceof TextSelection) {
+					final IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+					final Object obj = structuredSelection.getFirstElement();
+
+					if (obj instanceof NodeImpl) {
+						final PHPCodeData codeData = Utils.getPHPCodeData((NodeImpl) obj, ((TextSelection) selection).getOffset());
+						if (codeData != null)
+							return new StructuredSelection(codeData);
+					}
+				}
+		}
+		return super.getSelection(viewer, selection);
+	}
+
+	protected IPHPTreeContentProvider[] getTreeProviders() {
+		if (treeProviders == null)
+			treeProviders = TreeProvider.getTreeProviders(IPageLayout.ID_OUTLINE);
+		return treeProviders;
+	}
+
+	public ILabelProvider getStatusLineLabelProvider(TreeViewer treeViewer) {
+		if (fSimpleLabelProvider == null) {
+			fSimpleLabelProvider = new StatusLineLabelProvider(treeViewer);
+		}
+		return fSimpleLabelProvider;
+	}
+
+	private class StatusLineLabelProvider extends JFaceNodeLabelProvider {
+		TreeViewer treeViewer = null;
+
+		public StatusLineLabelProvider(TreeViewer viewer) {
+			treeViewer = viewer;
+		}
+
+		public String getText(Object element) {
+			if (element == null)
+				return null;
+
+			return PHPElementLabels.getTextLabel(element, AppearanceAwareLabelProvider.DEFAULT_TEXTFLAGS);
+		}
+	}
+
+	protected XMLNodeActionManager createNodeActionManager(TreeViewer treeViewer) {
+		return new PHPNodeActionManager((IStructuredModel) treeViewer.getInput(), treeViewer);
+	}
+
+	@Override
+	protected void enableShowAttributes(boolean showAttributes, TreeViewer treeViewer) {
+		super.enableShowAttributes(showAttributes, treeViewer);
+		((PHPOutlineLabelProvider) getLabelProvider(treeViewer)).setShowAttributes(showAttributes);
+	}
+
+}
---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/outline/PHPOutlineLabelProvider.java
@@ -0,0 +1,165 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Zend and IBM - Initial implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.outline;
+
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.php.internal.core.phpModel.phpElementData.PHPCodeData;
+import org.eclipse.php.internal.ui.SuperClassLabelProvider;
+import org.eclipse.php.internal.ui.outline.PHPOutlineContentProvider.GroupNode;
+import org.eclipse.php.internal.ui.treecontent.PHPTreeNode;
+import org.eclipse.php.internal.ui.util.AppearanceAwareLabelProvider;
+import org.eclipse.php.internal.ui.util.PHPElementLabels;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.wst.sse.core.utils.StringUtils;
+import org.eclipse.wst.xml.core.internal.contentmodel.CMAttributeDeclaration;
+import org.eclipse.wst.xml.core.internal.contentmodel.CMDataType;
+import org.eclipse.wst.xml.core.internal.contentmodel.CMElementDeclaration;
+import org.eclipse.wst.xml.core.internal.contentmodel.modelquery.ModelQuery;
+import org.eclipse.wst.xml.core.internal.modelquery.ModelQueryUtil;
+import org.eclipse.wst.xml.ui.internal.contentoutline.JFaceNodeLabelProvider;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+public class PHPOutlineLabelProvider extends AppearanceAwareLabelProvider {
+	AttributeShowingLabelProvider phpLabelProvider = new AttributeShowingLabelProvider();
+
+	protected ILabelProvider superClassLabelProviderFragment = new SuperClassLabelProvider(this);
+
+	private boolean showAttributes;
+
+	public Image getImage(Object element) {
+		if (element instanceof PHPCodeData) {
+			Image image = superClassLabelProviderFragment.getImage(element);
+			if (image != null) {
+				return image;
+			}
+			return super.getImage(element);
+		} else if (element instanceof GroupNode) {
+			return ((GroupNode) element).getImage();
+		} else if (element instanceof PHPTreeNode) {
+			return ((PHPTreeNode) element).getImage();
+		}
+		return phpLabelProvider.getImage(element);
+	}
+
+	public String getText(Object element) {
+		if (element instanceof PHPCodeData) {
+			String text = superClassLabelProviderFragment.getText(element);
+			if (text != null) {
+				return text;
+			}
+			return super.getText(element);
+		} else if (element instanceof GroupNode) {
+			return ((GroupNode) element).getText();
+		} else if (element instanceof PHPTreeNode) {
+			return ((PHPTreeNode) element).getText();
+		}
+
+		return phpLabelProvider.getText(element);
+
+	}
+
+	public String getTooltipText(Object element) {
+		return PHPElementLabels.getTooltipTextLabel(element);
+	}
+
+	private class AttributeShowingLabelProvider extends JFaceNodeLabelProvider {
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.ILabelProvider#getText(java.lang.Object)
+		 */
+		public String getText(Object o) {
+			StringBuffer text = new StringBuffer(super.getText(o));
+			if (o instanceof Node) {
+				Node node = (Node) o;
+				if (node.getNodeType() == Node.ELEMENT_NODE && showAttributes) {
+					// https://bugs.eclipse.org/bugs/show_bug.cgi?id=88444
+					if (node.hasAttributes()) {
+						Element element = (Element) node;
+						NamedNodeMap attributes = element.getAttributes();
+						Node idTypedAttribute = null;
+						Node requiredAttribute = null;
+						boolean hasId = false;
+						boolean hasName = false;
+						Node shownAttribute = null;
+
+						// try to get content model element
+						// declaration
+						CMElementDeclaration elementDecl = null;
+						ModelQuery mq = ModelQueryUtil.getModelQuery(element.getOwnerDocument());
+						if (mq != null) {
+							elementDecl = mq.getCMElementDeclaration(element);
+						}
+						// find an attribute of type (or just named)
+						// ID
+						if (elementDecl != null) {
+							int i = 0;
+							while (i < attributes.getLength() && idTypedAttribute == null) {
+								Node attr = attributes.item(i);
+								String attrName = attr.getNodeName();
+								CMAttributeDeclaration attrDecl = (CMAttributeDeclaration) elementDecl.getAttributes().getNamedItem(attrName);
+								if (attrDecl != null) {
+									if ((attrDecl.getAttrType() != null) && (CMDataType.ID.equals(attrDecl.getAttrType().getDataTypeName()))) {
+										idTypedAttribute = attr;
+									} else if (attrDecl.getUsage() == CMAttributeDeclaration.REQUIRED && requiredAttribute == null) {
+										// as a backup, keep tabs on
+										// any required
+										// attributes
+										requiredAttribute = attr;
+									} else {
+										hasId = hasId || attrName.equals("id"); //$NON-NLS-1$
+										hasName = hasName || attrName.equals("name"); //$NON-NLS-1$
+									}
+								}
+								++i;
+							}
+						}
+
+						/*
+						 * If no suitable attribute was found, try using a
+						 * required attribute, if none, then prefer "id" or
+						 * "name", otherwise just use first attribute
+						 */
+						if (idTypedAttribute != null) {
+							shownAttribute = idTypedAttribute;
+						} else if (requiredAttribute != null) {
+							shownAttribute = requiredAttribute;
+						} else if (hasId) {
+							shownAttribute = attributes.getNamedItem("id"); //$NON-NLS-1$
+						} else if (hasName) {
+							shownAttribute = attributes.getNamedItem("name"); //$NON-NLS-1$
+						}
+						if (shownAttribute == null) {
+							shownAttribute = attributes.item(0);
+						}
+
+						// display the attribute and value (without quotes)
+						String attributeName = shownAttribute.getNodeName();
+						if (attributeName != null && attributeName.length() > 0) {
+							text.append(" " + attributeName); //$NON-NLS-1$
+							String attributeValue = shownAttribute.getNodeValue();
+							if (attributeValue != null && attributeValue.length() > 0) {
+								text.append("=" + StringUtils.strip(attributeValue)); //$NON-NLS-1$
+							}
+						}
+					}
+				}
+			}
+			return text.toString();
+		}
+	}
+
+	public void setShowAttributes(boolean showAttributes) {
+		this.showAttributes = showAttributes;
+	}
+}
