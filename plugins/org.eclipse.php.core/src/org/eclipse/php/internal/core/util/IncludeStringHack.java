d0a44735da3bcd93e725566452b4ebbd67e5d3fc
slapsha <slapsha>
2007-07-17 10:57:10 +0000

    handle partial paths, which end with . or ..

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/IncludeStringHack.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/IncludeStringHack.java
@@ -16,29 +16,17 @@ public class IncludeStringHack {
 
 	public static String hack(String includeString) {
 		includeString = includeString.replaceAll("\\.{2}([\\/])", "||$1");
+		includeString = includeString.replaceAll("\\.{2}$", "||");
 		includeString = includeString.replaceAll("\\.{1}([\\/])", "|$1");
-		
-//		if (includeString.length() != 0 && includeString.charAt(0) == '.') {
-//			if (includeString.length() != 1 && includeString.charAt(1) == '.') {
-//				includeString = "||" + includeString.substring(2);
-//			} else {
-//				includeString = "|" + includeString.substring(1);
-//			}
-//		}
+		includeString = includeString.replaceAll("\\.{1}$", "|");
 		return includeString;
 	}
 
 	public static String unhack(String includeString) {
 		includeString = includeString.replaceAll("\\|{2}([\\/])", "..$1");
+		includeString = includeString.replaceAll("\\|{2}$", "..");
 		includeString = includeString.replaceAll("\\|{1}([\\/])", ".$1");
-
-//		if (includeString.length() != 0 && includeString.charAt(0) == '|') {
-//			if (includeString.length() != 1 && includeString.charAt(1) == '|') {
-//				includeString = ".." + includeString.substring(2);
-//			} else {
-//				includeString = "." + includeString.substring(1);
-//			}
-//		}
+		includeString = includeString.replaceAll("\\|{1}$", ".");
 		return includeString;
 	}
 
