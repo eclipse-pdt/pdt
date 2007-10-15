4486252763b22de6286a9d016f31fb92634f69c5
ggurfinke <ggurfinke>
2007-10-15 11:26:09 +0000

    removed sorting proposals by type. it should be only by name alphabetically. (bug 206265)

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/contentassist/PHPProposalComperator.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/contentassist/PHPProposalComperator.java
@@ -14,22 +14,12 @@ package org.eclipse.php.internal.ui.editor.contentassist;
 import java.util.Comparator;
 
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.php.internal.core.phpModel.phpElementData.CodeData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPClassData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPFunctionData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPKeywordData;
 
 /*
  * Compares between two PHP proposal elements - according to the type
  * @author Roy, 2006
  */
 public class PHPProposalComperator implements Comparator {
-
-	private static final int CLASSES = 1;
-	private static final int FUNCTIONS = 2;
-	private static final int CONSTANTS = 3;
-	private static final int KEYWORDS = 4;
-	private static final int OTHERS = 0;
 
 	public int compare(Object o1, Object o2) {
 		// type checking
@@ -40,11 +30,7 @@ public class PHPProposalComperator implements Comparator {
 		ICompletionProposal element1 = (ICompletionProposal) o1;
 		ICompletionProposal element2 = (ICompletionProposal) o2;
 		
-		int o1Type = getType(o1);
-		int o2Type = getType(o2);
-		
-		int diff = o1Type - o2Type; // the difference between the two types
-		return diff != 0 ? diff : compareDisplayString(element1, element2) ;
+		return compareDisplayString(element1, element2) ;
 	}
 
 	/**
@@ -65,19 +51,4 @@ public class PHPProposalComperator implements Comparator {
 		return result;
 	}
 
-	private int getType(Object object) {
-		if (object instanceof CodeDataCompletionProposal) {
-			CodeData codeData = ((CodeDataCompletionProposal) object).getCodeData();
-			if (codeData instanceof PHPClassData) {
-				return CLASSES;
-			} else if (codeData instanceof PHPFunctionData){
-				return FUNCTIONS;
-			} else if (codeData instanceof PHPKeywordData) {
-				return KEYWORDS;
-			} else {
-				return CONSTANTS;
-			}
-		} 
-		return OTHERS;
-	}
 }
