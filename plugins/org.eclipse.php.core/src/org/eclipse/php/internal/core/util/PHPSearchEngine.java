c8b4dd292fa58413a5bdda3d3f9fd4ce384df57d
mspector <mspector>
2007-10-18 14:28:56 +0000

    fix PHP internal search mechanism, integrate it with debug search engine

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/PHPSearchEngine.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/PHPSearchEngine.java
@@ -5,130 +5,196 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.php.internal.core.project.IIncludePathEntry;
+import org.eclipse.php.internal.core.project.options.PHPProjectOptions;
 import org.eclipse.php.internal.core.project.options.includepath.IncludePathVariableManager;
-
 
 /**
  * This utility implements internal PHP mechanism for searching included files.
  * The algorithm is the following:<br/><br/>
- * 
+ *
  *  Files for including are first looked for in each <b>include_path</b> entry
  *  relative to the current working directory, and then in the directory of current script.
  *  E.g. if your <b>include_path</b> is libraries, current working directory is /www/,
  *  you included include/a.php and there is include "b.php"  in that file,
  *  b.php is first looked in /www/libraries/  and then in /www/include/.
  *  If filename begins with ./ or ../, it is looked only in the current working directory.
- *  
+ *
  * @author michael
  */
 public class PHPSearchEngine {
 
 	/**
 	 * Searches for the given path using internal PHP mechanism
-	 * 
+	 *
 	 * @param path File path to resolve
-	 * @param currentWorkingDir Current working directory (usually: CWD of PHP process)
+	 * @param currentWorkingDir local Current working directory (usually: CWD of PHP process)
 	 * @param currentScriptDir Directory of current script (which is interpreted by the PHP at this time)
 	 * @param currentProject Current project to which current script belongs
 	 * @return resolved path, or <code>null</code> in case of failure
 	 */
-	public static Result find (String path, String currentWorkingDir, String currentScriptDir, IProject currentProject) {
+	public static Result<?, ?> find(String path, String currentWorkingDir, String currentScriptDir, IProject currentProject) {
 		if (path == null || currentWorkingDir == null || currentScriptDir == null || currentProject == null) {
-			throw new NullPointerException();
+			throw new NullPointerException("Parameters can't be null");
 		}
-		
-		// check whether the path is absolute (not depending on current OS)
-		if (path.startsWith("/") || path.startsWith("\\") || path.matches("[A-Za-z]:[/\\\\].*")) {
-			File file = new File(path);
-			if (file.exists()) {
-				return new Result(file.getParentFile(), file);
-			}
-		}
-		else if (path.matches("\\.\\.?[/\\].*")) { // check whether the path starts with ./ or ../
-			File file = new File(currentWorkingDir, path);
-			if (file.exists()) {
-				return new Result(file.getParentFile(), file);
-			}
+
+		// check whether the path is absolute
+		File file = new File(path);
+		if (file.isAbsolute()) {
+			return searchExternalOrWorkspaceFile(file);
+		} else if (path.matches("\\.\\.?[/\\\\].*")) { // check whether the path starts with ./ or ../
+			file = new File(currentWorkingDir, path);
+			return searchExternalOrWorkspaceFile(file);
 		} else {
-			Object[] includePath = buildIncludePath(currentProject);
-			for (int i = 0; i < includePath.length; ++i) {
-				if (includePath[i] instanceof IContainer) {
-					IContainer container = (IContainer) includePath[i];
+			Object[] includePaths = buildIncludePath(currentProject);
+			for (Object includePath : includePaths) {
+				if (includePath instanceof IContainer) {
+					IContainer container = (IContainer) includePath;
 					IResource resource = container.findMember(path);
-					if (resource != null) {
-						return new Result(container, resource);
+					if (resource instanceof IFile) {
+						return new ResourceResult((IFile) resource);
 					}
-				}
-				else if (includePath[i] instanceof IIncludePathEntry) {
-					IIncludePathEntry entry = (IIncludePathEntry) includePath[i];
+				} else if (includePath instanceof IIncludePathEntry) {
+					IIncludePathEntry entry = (IIncludePathEntry) includePath;
 					IPath entryPath = entry.getPath();
 					if (entry.getEntryKind() == IIncludePathEntry.IPE_LIBRARY) {
 						if (entry.getContentKind() != IIncludePathEntry.K_BINARY) { // We don't support lookup in archive
 							File entryDir = entryPath.toFile();
-							File file = new File(entryDir, path);
+							file = new File(entryDir, path);
 							if (file.exists()) {
-								return new Result(includePath[i], file);
+								return new IncludedFileResult(entry, file);
 							}
 						}
 					} else if (entry.getEntryKind() == IIncludePathEntry.IPE_PROJECT) {
 						IProject project = (IProject) entry.getResource();
-						IResource resource = project.findMember(path);
-						if (resource != null) {
-							return new Result(includePath[i], resource);
+						if (project.isAccessible()) {
+							IResource resource = project.findMember(path);
+							if (resource != null) {
+								return new IncludedFileResult(entry, file);
+							}
 						}
 					} else if (entry.getEntryKind() == IIncludePathEntry.IPE_VARIABLE) {
 						entryPath = IncludePathVariableManager.instance().resolveVariablePath(entryPath.toString());
 						File entryDir = entryPath.toFile();
-						File file = new File(entryDir, path);
+						file = new File(entryDir, path);
 						if (file.exists()) {
-							return new Result(includePath[i], file);
+							return new IncludedFileResult(entry, file);
 						}
 					}
 				}
 			}
 		}
-		
 		return null;
 	}
-	
+
+	private static Result<?, ?> searchExternalOrWorkspaceFile(File file) {
+		IFile res = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(Path.fromOSString(file.getAbsolutePath()));
+		if (res != null) {
+			return new ResourceResult(res);
+		}
+		if (file.exists()) {
+			return new ExternalFileResult(file);
+		}
+		return null;
+	}
+
 	/**
 	 * Builds include path for searching by the given project.
 	 * Result contains include path of the given project, referenced projects and their include paths.
+	 * @param project Current project
 	 * @return array of include path objects (it can be one of: IContainer, IncludePathEntry)
 	 */
-	public static Object[] buildIncludePath (IProject project) {
-		Set<? extends Object> results = new HashSet<Object>();
-		
-		internalBuildIncludePath (project, results);
-		
+	public static Object[] buildIncludePath(IProject project) {
+		Set<Object> results = new HashSet<Object>();
+		buildIncludePath(project, results);
 		return results.toArray();
 	}
-	
-	private static void internalBuildIncludePath (IProject project, Set<? extends Object> results) {
+
+	/**
+	 * Builds include path for searching by the given project.
+	 * Result contains include path of the given project, referenced projects and their include paths.
+	 * @param project Current project
+	 * @param results Array of include path objects (it can be one of: IContainer, IncludePathEntry)
+	 */
+	public static void buildIncludePath(IProject project, Set<Object> results) {
+		if (results.contains(project)) {
+			return;
+		}
+		if (!project.isAccessible()) {
+			return;
+		}
+		// Collect include paths:
+		PHPProjectOptions projectOptions = PHPProjectOptions.forProject(project);
+		if (projectOptions != null) {
+			IIncludePathEntry[] includePath = projectOptions.readRawIncludePath();
+			for (IIncludePathEntry entry : includePath) {
+				results.add(entry);
+			}
+		}
+		// Collect referenced projects and their include paths:
+		try {
+			IProject[] referencedProjects = project.getReferencedProjects();
+			for (IProject referencedProject : referencedProjects) {
+				buildIncludePath(referencedProject, results);
+			}
+		} catch (CoreException e) {
+		}
+		// Add current project:
+		results.add(project);
 	}
-	
+
 	/**
 	 * Result returned by PHP search engine
 	 */
-	public static class Result {
-		private Object container;
-		private Object fileObject;
-		
-		public Result(Object container, Object fileObject) {
+	abstract public static class Result<T, S> {
+		private T container;
+		private S file;
+
+		public Result(T container, S file) {
 			this.container = container;
-			this.fileObject = fileObject;
+			this.file = file;
 		}
-		
-		public Object getContainer() {
+
+		public T getContainer() {
 			return container;
 		}
-		
-		public Object getFileObject() {
-			return fileObject;
+
+		public S getFile() {
+			return file;
+		}
+	}
+
+	/**
+	 * Result for Workspace file
+	 */
+	public static class ResourceResult extends Result<Object, IFile> {
+		public ResourceResult(IFile file) {
+			super(null, file);
+		}
+	}
+
+	/**
+	 * Result for included file (from Include Path)
+	 */
+	public static class IncludedFileResult extends Result<IIncludePathEntry, File> {
+		public IncludedFileResult(IIncludePathEntry container, File file) {
+			super(container, file);
+		}
+	}
+
+	/**
+	 * Result for external file (on file system)
+	 */
+	public static class ExternalFileResult extends Result<Object, File> {
+		public ExternalFileResult(File file) {
+			super(null, file);
 		}
 	}
 }
