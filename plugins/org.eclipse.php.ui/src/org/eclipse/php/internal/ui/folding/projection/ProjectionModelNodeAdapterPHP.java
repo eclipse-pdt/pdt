7e7241f0835d9f39f40d5e809d50a72dc9f75349
slapsha <slapsha>
2007-10-23 06:28:39 +0000

    Fix condition

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/folding/projection/ProjectionModelNodeAdapterPHP.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/folding/projection/ProjectionModelNodeAdapterPHP.java
@@ -99,7 +99,7 @@ public class ProjectionModelNodeAdapterPHP extends ProjectionModelNodeAdapterHTM
 			}
 
 			// only update when there is something to update
-			if (oldList != null && oldList.length > 0 || !addedAnnotations.isEmpty() && currentAnnotations.size() > 0) {
+			if (oldList != null && oldList.length > 0 || !addedAnnotations.isEmpty() || currentAnnotations.size() > 0) {
 				fAdapterFactory.queueAnnotationModelChanges(node, oldList, addedAnnotations, currentAnnotations);
 			}
 
