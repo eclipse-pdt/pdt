73ba2f3549a66e8770df694779474f2bb8b84497
rganor <rganor>
2007-10-23 11:36:23 +0000

    Bug #202360 -  Folding marker appears on unfoldable one-line block
     fixed.

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/folding/projection/ElementProjectionAnnotation.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/folding/projection/ElementProjectionAnnotation.java
@@ -13,6 +13,10 @@ package org.eclipse.php.internal.ui.folding.projection;
 import org.eclipse.jface.text.source.projection.ProjectionAnnotation;
 import org.eclipse.php.internal.core.phpModel.phpElementData.PHPCodeData;
 import org.eclipse.php.internal.ui.folding.projection.Element.ElementFactory;
+import org.eclipse.swt.graphics.FontMetrics;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Canvas;
 
 /**
  * Represents an annotation in a php node
@@ -72,4 +76,52 @@ public class ElementProjectionAnnotation extends ProjectionAnnotation {
 		buffer.append("]"); //$NON-NLS-1$
 		return buffer.toString();
 	}
+
+	private boolean fIsVisible = false; /* workaround for BUG85874 */
+
+	/**
+	 * Does not paint hidden annotations. Annotations are hidden when they
+	 * only span one line.
+	 *
+	 * @see ProjectionAnnotation#paint(org.eclipse.swt.graphics.GC,
+	 *      org.eclipse.swt.widgets.Canvas,
+	 *      org.eclipse.swt.graphics.Rectangle)
+	 */
+	@Override
+	public void paint(GC gc, Canvas canvas, Rectangle rectangle) {
+		/* workaround for BUG85874 */
+		/*
+		 * only need to check annotations that are expanded because hidden
+		 * annotations should never have been given the chance to
+		 * collapse.
+		 */
+		if (!isCollapsed()) {
+			// working with rectangle, so line height
+			FontMetrics metrics = gc.getFontMetrics();
+			if (metrics != null) {
+				// do not draw annotations that only span one line and
+				// mark them as not visible
+				if (rectangle.height / metrics.getHeight() <= 1) {
+					fIsVisible = false;
+					return;
+				}
+			}
+		}
+		fIsVisible = true;
+		super.paint(gc, canvas, rectangle);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.jface.text.source.projection.ProjectionAnnotation#markCollapsed()
+	 */
+	@Override
+	public void markCollapsed() {
+		/* workaround for BUG85874 */
+		// do not mark collapsed if annotation is not visible
+		if (fIsVisible)
+			super.markCollapsed();
+	}
+
 }
