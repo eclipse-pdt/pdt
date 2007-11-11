bcb4a8b7939c367f2ec70c828cdffc4c0ea101d4
slapsha <slapsha>
2007-11-11 18:14:06 +0000

    Multi-line String Output Buffer

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/text/StringOutputStream.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/text/StringOutputStream.java
@@ -4,6 +4,8 @@
 package org.eclipse.php.internal.core.util.text;
 
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author seva, 2007
@@ -11,7 +13,8 @@ import java.io.OutputStream;
  */
 public class StringOutputStream extends OutputStream {
 
-	protected StringBuffer buf = new StringBuffer();
+	protected List<StringBuffer> buffers = new ArrayList<StringBuffer>();
+	protected StringBuffer buffer = new StringBuffer();
 
 	public StringOutputStream() {
 	}
@@ -20,24 +23,38 @@ public class StringOutputStream extends OutputStream {
 	}
 
 	public void flush() {
+		buffers.add(buffer);
+		buffer = new StringBuffer();
 	}
 
 	public void write(byte[] b) {
 		String str = new String(b);
-		this.buf.append(str);
+		buffer.append(str);
 	}
 
 	public void write(byte[] b, int off, int len) {
 		String str = new String(b, off, len);
-		this.buf.append(str);
+		buffer.append(str);
 	}
 
 	public void write(int b) {
 		String str = Integer.toString(b);
-		this.buf.append(str);
+		buffer.append(str);
 	}
 
 	public String toString() {
-		return buf.toString();
+		return buffer.toString();
+	}
+
+	public String getString(int i) {
+		return buffers.get(i).toString();
+	}
+
+	public String[] getStrings() {
+		List<String> strings = new ArrayList<String>(buffers.size());
+		for (StringBuffer buffer : buffers) {
+			strings.add(buffer.toString());
+		}
+		return strings.toArray(new String[strings.size()]);
 	}
 }
