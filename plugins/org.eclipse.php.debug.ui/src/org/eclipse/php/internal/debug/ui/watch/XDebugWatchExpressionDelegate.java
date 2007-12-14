ed403bde3b1f500c5d90c2b56e7cfddd879873b1
dkelsey <dkelsey>
2007-12-14 13:54:08 +0000

    Ported fixes. Fix to bug: 204673 - more robust Fix a timing issue where DebugTarget is in running state rather than suspended state.

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/watch/XDebugWatchExpressionDelegate.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/watch/XDebugWatchExpressionDelegate.java
@@ -48,6 +48,9 @@ public class XDebugWatchExpressionDelegate implements IWatchExpressionDelegate {
 				evalJob.schedule();
 			}
 		}
+		else {
+			watchListener.watchEvaluationFinished(null);
+		}
 	}
 
 	/**
