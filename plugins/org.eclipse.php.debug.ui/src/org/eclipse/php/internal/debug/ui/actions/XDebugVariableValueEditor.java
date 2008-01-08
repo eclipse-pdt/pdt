92b5a008eb1f98c8c30dde81efc3a694ea081627
dkelsey <dkelsey>
2008-01-08 12:04:25 +0000

    added support to see the full string in the editor if only a subset is returned due to max_data limit

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/actions/XDebugVariableValueEditor.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/actions/XDebugVariableValueEditor.java
@@ -13,13 +13,18 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.model.IValue;
 import org.eclipse.debug.core.model.IVariable;
+import org.eclipse.debug.internal.ui.actions.variables.ChangeVariableValueInputDialog;
 import org.eclipse.debug.ui.actions.IVariableValueEditor;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.window.Window;
+import org.eclipse.php.internal.debug.core.xdebug.dbgp.model.DBGpStringValue;
+import org.eclipse.php.internal.debug.core.xdebug.dbgp.model.DBGpTarget;
+import org.eclipse.php.internal.debug.core.xdebug.dbgp.model.DBGpVariable;
 import org.eclipse.php.internal.debug.ui.Logger;
 import org.eclipse.swt.widgets.Shell;
+import org.w3c.dom.Node;
 
 /**
  * A variable value editor that prompts the user to set a primitive's value.
@@ -42,11 +47,11 @@ public class XDebugVariableValueEditor implements IVariableValueEditor {
 			String name = variable.getName();
 			String title = "Change Value"; // PHPDebugUIMessages.PHPPrimitiveValueEditor_0
 			// PHPDebugUIMessages.PHPPrimitiveValueEditor_1
-			String message = MessageFormat.format("Enter a new value for {0}:", new String[] { name }); //$NON-NLS-1$
-			IValue value = variable.getValue();
-			String initialValue = value.getValueString();
+			String message = MessageFormat.format("Enter a new value for {0}:", new Object[] { name }); //$NON-NLS-1$
+			String initialValue = getValueString(variable);
+			
 			PrimitiveValidator validator = new PrimitiveValidator(variable);
-			InputDialog dialog = new InputDialog(shell, title, message, initialValue, validator);
+			ChangeVariableValueInputDialog dialog = new ChangeVariableValueInputDialog(shell, title, message, initialValue, validator);
 			if (dialog.open() == Window.OK) {
 				String stringValue = dialog.getValue();
 				variable.setValue(stringValue);
@@ -57,6 +62,33 @@ public class XDebugVariableValueEditor implements IVariableValueEditor {
 			ErrorDialog.openError(shell, "Error Changing Value", "An exception occurred attempting to change the variable value.", status);
 		}
 		return true;
+	}
+
+	private String getValueString(IVariable variable) throws DebugException {
+		IValue value = variable.getValue();
+		String initialValue = value.getValueString();
+		
+		if (value instanceof DBGpStringValue) {
+			DBGpStringValue strValue = (DBGpStringValue)value;
+			if (strValue.isComplete() == false) {
+				DBGpTarget target = (DBGpTarget)value.getDebugTarget();
+				DBGpVariable dbgpVar = (DBGpVariable)variable;
+				String stackLevel = dbgpVar.getStackLevel();
+				Node result = target.getCompleteString(dbgpVar.getFullName(), stackLevel, strValue.getRequiredBytes());
+				if (result != null) {
+					IVariable tempVar = new DBGpVariable(target, result, stackLevel);
+					IValue valRes = null;
+					try {
+						valRes = tempVar.getValue();
+						if (valRes != null) {
+							initialValue = valRes.getValueString();
+						}
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+		return initialValue;
 	}
 
 	/* (non-Javadoc)
