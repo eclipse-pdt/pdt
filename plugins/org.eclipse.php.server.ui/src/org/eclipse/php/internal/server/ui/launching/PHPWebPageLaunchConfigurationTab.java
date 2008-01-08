a4bd90bddaa1382ed0e3c06985948d676ed2df26
ymazor <ymazor>
2008-01-08 09:21:19 +0000

    renamed to  IPHPDebugConstants

--- plugins/org.eclipse.php.server.ui/src/org/eclipse/php/internal/server/ui/launching/PHPWebPageLaunchConfigurationTab.java
+++ plugins/org.eclipse.php.server.ui/src/org/eclipse/php/internal/server/ui/launching/PHPWebPageLaunchConfigurationTab.java
@@ -12,8 +12,9 @@ package org.eclipse.php.internal.server.ui.launching;
 
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
-import org.eclipse.php.internal.debug.core.IPHPConstants;
+import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
 import org.eclipse.php.internal.debug.core.PHPDebugPlugin;
+import org.eclipse.php.internal.debug.core.preferences.PHPDebugCorePreferenceNames;
 import org.eclipse.php.internal.debug.ui.launching.LaunchUtil;
 import org.eclipse.php.internal.server.ui.ServerLaunchConfigurationTab;
 import org.eclipse.swt.widgets.Composite;
@@ -33,7 +34,7 @@ public class PHPWebPageLaunchConfigurationTab extends ServerLaunchConfigurationT
 	}
 
 	protected void applyExtension(ILaunchConfigurationWorkingCopy configuration) {
-		configuration.setAttribute(IPHPConstants.RUN_WITH_DEBUG_INFO, true); // Always run with debug info
+		configuration.setAttribute(IPHPDebugConstants.RUN_WITH_DEBUG_INFO, true); // Always run with debug info
 	}
 
 	protected boolean isValidExtension(ILaunchConfiguration launchConfig) {
