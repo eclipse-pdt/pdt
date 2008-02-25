b13abefd167887a0c9c827b8badfe6a87bcc921a
mspector <mspector>
2008-02-25 15:01:16 +0000

    not needed anymore

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/compiler/ast/parser/PHPSourceElementParser.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/compiler/ast/parser/PHPSourceElementParser.java
@@ -1,11 +1,7 @@
 package org.eclipse.php.internal.core.compiler.ast.parser;
 
-import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.compiler.SourceElementRequestVisitor;
 import org.eclipse.dltk.core.AbstractSourceElementParser;
-import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.core.SourceParserUtil;
-import org.eclipse.dltk.core.ISourceModuleInfoCache.ISourceModuleInfo;
 import org.eclipse.php.internal.core.project.PHPNature;
 
 public class PHPSourceElementParser extends AbstractSourceElementParser {
@@ -17,17 +13,4 @@ public class PHPSourceElementParser extends AbstractSourceElementParser {
 	protected String getNatureId() {
 		return PHPNature.ID;
 	}
-	
-	public void parseSourceModule(char[] contents, ISourceModuleInfo astCache, char[] filename) {
-		ModuleDeclaration moduleDeclaration = SourceParserUtil.getModuleDeclaration(filename, contents, PHPNature.ID, getProblemReporter(), astCache);
-		PHPSourceElementRequestor requestor = new PHPSourceElementRequestor(getRequestor());
-		try {
-			moduleDeclaration.traverse(requestor);
-		} catch (Exception e) {
-			if (DLTKCore.DEBUG) {
-				e.printStackTrace();
-			}
-			throw new RuntimeException(e);
-		}
-	}		
 }
