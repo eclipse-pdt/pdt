d56233f432243d1e0188f31f9b3bd1c0cb7d0969
mspector <mspector>
2008-03-04 08:47:54 +0000

    use hierarchyScope for searching method

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/PHPModelUtils.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/PHPModelUtils.java
@@ -28,48 +28,17 @@ public class PHPModelUtils {
 			throw new NullPointerException();
 		}
 
-		LinkedList<IType> superTypesQ = new LinkedList<IType>();
-		superTypesQ.add(type);
+		IDLTKSearchScope scope = SearchEngine.createHierarchyScope(type);
+		SearchPattern pattern = SearchPattern.createPattern(name, IDLTKSearchConstants.METHOD, IDLTKSearchConstants.DECLARATIONS, SearchPattern.R_EXACT_MATCH);
 
-		while (!superTypesQ.isEmpty()) {
-			IType superType = superTypesQ.removeFirst();
-			IMethod method = superType.getMethod(name);
-			if (method.exists()) {
-				return method;
+		final List<IMethod> methods = new LinkedList<IMethod>();
+		new SearchEngine().search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, scope, new SearchRequestor() {
+			public void acceptSearchMatch(SearchMatch match) throws CoreException {
+				methods.add((IMethod) match.getElement());
 			}
+		}, monitor);
 
-			String[] superClassNames = superType.getSuperClasses();
-			IDLTKSearchScope searchScope = SearchEngine.createSearchScope(new IModelElement[] { type.getScriptProject() });
-
-			for (String superClassName : superClassNames) {
-
-				SearchPattern pattern = SearchPattern.createPattern(superClassName, IDLTKSearchConstants.TYPE, IDLTKSearchConstants.DECLARATIONS, SearchPattern.R_EXACT_MATCH);
-				final List<IType> types = new LinkedList<IType>();
-
-				SearchEngine searchEngine = new SearchEngine();
-				searchEngine.search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, searchScope, new SearchRequestor() {
-					public void acceptSearchMatch(SearchMatch match) throws CoreException {
-						types.add((IType) match.getElement());
-					}
-				}, monitor);
-
-				superTypesQ.addAll(types);
-			}
-		}
-		return null;
-
-//		ITypeHierarchy supertypeHierarchy = type.newSupertypeHierarchy(monitor);
-//		IDLTKSearchScope scope = SearchEngine.createSearchScope(supertypeHierarchy.getAllClasses());
-//		SearchPattern pattern = SearchPattern.createPattern(name, IDLTKSearchConstants.METHOD, IDLTKSearchConstants.DECLARATIONS, SearchPattern.R_EXACT_MATCH);
-//
-//		final List<IMethod> methods = new LinkedList<IMethod>();
-//		new SearchEngine().search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, scope, new SearchRequestor() {
-//			public void acceptSearchMatch(SearchMatch match) throws CoreException {
-//				methods.add((IMethod) match.getElement());
-//			}
-//		}, monitor);
-//
-//		return methods.isEmpty() ? null : methods.get(0);
+		return methods.isEmpty() ? null : methods.get(0);
 	}
 
 	public static MethodDeclaration getNodeByMethod(ModuleDeclaration rootNode, IMethod method) throws ModelException {
