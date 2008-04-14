eeef5abe94de04cfe33696e3cead4211f74864f9
sgibly <sgibly>
2008-04-14 11:42:53 +0000

    Use the IPhpHeloContextIds instead of the IHelpContextId and remove this interface

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/IHelpContextId.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.php.internal.debug.ui;
-
-public interface IHelpContextId {
-
-	public static final String PREFIX = "org.eclipse.php.help."; //$NON-NLS-1$
-
-	public static final String PATH_MAPPING = PREFIX + "path_mapping"; //$NON-NLS-1$
-
-}
