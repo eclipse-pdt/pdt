2c96054edb4ce6cce0e19db6deed9540af0daed9
sgibly <sgibly>
2008-04-23 16:04:26 +0000

    Flush the node

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/util/PreferencesSupport.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/util/PreferencesSupport.java
@@ -7,6 +7,8 @@ import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.Preferences;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.php.internal.core.Logger;
+import org.osgi.service.prefs.BackingStoreException;
 
 public class PreferencesSupport {
 
@@ -108,6 +110,11 @@ public class PreferencesSupport {
         IEclipsePreferences node = scope.getNode(nodeQualifier);
         if (node != null) {
             node.put(key, value);
+            try {
+				node.flush();
+			} catch (BackingStoreException e) {
+				Logger.logException(e);
+			}
             return true;
         }
         return false;
