54e227c5016723e5af7caa84d8f44552061cc350
mspector <mspector>
2008-05-22 16:08:31 +0000

    fix completion caret position

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/contentassist/PHPCompletionProposal.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/contentassist/PHPCompletionProposal.java
@@ -1,6 +1,7 @@
 package org.eclipse.php.internal.ui.editor.contentassist;
 
 import org.eclipse.dltk.ui.text.completion.ScriptCompletionProposal;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.graphics.Image;
 
 public class PHPCompletionProposal extends ScriptCompletionProposal {
@@ -15,5 +16,27 @@ public class PHPCompletionProposal extends ScriptCompletionProposal {
 	
 	protected boolean isSmartTrigger(char trigger) {
 		return trigger == '$';
+	}
+	
+	public void apply(IDocument document, char trigger, int offset) {
+		super.apply(document, trigger, offset);
+		setCursorPosition(calcCursorPosition());
+	}
+	
+	private int calcCursorPosition() {
+		String replacementString = getReplacementString();
+		int i = replacementString.lastIndexOf('(');
+		if (i != -1) {
+			return i + 1;
+		}
+		i = replacementString.lastIndexOf('\'');
+		if (i != -1) {
+			return i;
+		}
+		i = replacementString.lastIndexOf('\"');
+		if (i != -1) {
+			return i;
+		}
+		return replacementString.length();
 	}
 }
