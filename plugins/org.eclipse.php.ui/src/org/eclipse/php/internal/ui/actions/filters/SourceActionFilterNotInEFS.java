5f1ed8d8b1188aa8a6b8b108ec8017e5a57c12a3
rganor <rganor>
2008-06-11 12:33:56 +0000

    remove filter dependencies from old elements
    TODO should check what about external files

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/filters/SourceActionFilterNotInEFS.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/filters/SourceActionFilterNotInEFS.java
@@ -1,60 +1,21 @@
 package org.eclipse.php.internal.ui.actions.filters;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.php.internal.core.documentModel.DOMModelForPHP;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPCodeData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPFileData;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.dltk.core.IModelElement;
 import org.eclipse.php.ui.actions.filters.IActionFilterContributor;
-import org.eclipse.wst.sse.core.StructuredModelManager;
-import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
-import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
-import org.eclipse.wst.xml.core.internal.document.NodeImpl;
 
 public class SourceActionFilterNotInEFS implements IActionFilterContributor {
 
 	public boolean testAttribute(Object target, String name, String value) {
-		String fileName = null;
-		if (target instanceof NodeImpl) {
-			IStructuredDocument structuredDocument = ((NodeImpl) target).getStructuredDocument();
-			IStructuredModel existingModelForRead = StructuredModelManager.getModelManager().getExistingModelForRead(structuredDocument);
-			try {
-				if (existingModelForRead instanceof DOMModelForPHP) {
-					PHPFileData fileData = ((DOMModelForPHP) existingModelForRead).getFileData();
-					if (fileData == null) {
-						return false;
-					}
-					fileName = fileData.getName();
-				}
-			} finally {
-				if (existingModelForRead != null) {
-					existingModelForRead.releaseFromRead();
-				}
-			}
-		}
-		if (target instanceof PHPCodeData) {
-			PHPCodeData codeData = (PHPCodeData) target;
-			if (!codeData.isUserCode()) {
-				return false;
-			}
-			fileName = (codeData).getUserData().getFileName();
-		}
-		// construct a path
-		if (fileName == null) {
-			return false;
-		}
-		Path fileNamePath = new Path(fileName);
-		// if it has only one segment 'getFile' throws an exception so return false anyway
-		if (fileNamePath.segmentCount() <= 1) {
-			return false;
+		if (target instanceof IModelElement) {
+			IModelElement codeData = (IModelElement) target;
+			final IResource resource = codeData.getResource();
+			
+			return resource != null && resource.exists();
 		}
 		// file is not in EFS, e.g include path
-		IFile checkFile = ResourcesPlugin.getWorkspace().getRoot().getFile(fileNamePath);
-		if (checkFile.exists()) {
-			return true;
-		}
-		return false;
+		// TODO what about external files? should be fixed according to the DLTK rules
+		return true;
 	}
 
 }
