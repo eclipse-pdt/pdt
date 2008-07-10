12857815ca077cf70ae5d9c3ae014ddfa62d6720
mspector <mspector>
2008-07-10 08:49:10 +0000

    fix changelog generation scripts

--- plugins/org.eclipse.php-releng/changelog/changelog.sh
+++ plugins/org.eclipse.php-releng/changelog/changelog.sh
@@ -1,34 +1,46 @@
 #!/bin/sh
 
 usage() {
-	echo "usage: changelog [-ant <url to ANT 1.7+ to run build>] [-daysInPast <number of days to include in report>]"
+	echo "USAGE: $0 [-b <branch tag, default: HEAD>]"
 }
 
-# ant used to run the build.  Defaults to ant on system path
-ant=ant
-
-#sets number of days to include in log
-daysInPast=""
-
-if [ "x$1" == "x" ] ; then
-        usage
-        exit 0
+logdir=~/.changelog/
+if [ ! -d $logdir ]; then
+	mkdir $logdir
 fi
+
+branch=HEAD
 
 while [ "$#" -gt 0 ] ; do
         case $1 in
-                '-ant')
-                        ant=$2;
+                '-b')
+                        branch=$2
                         shift 1
                     ;;
-                '-daysInPast')
-                        daysInPast="-DlogTime=$2";
-                        shift 1
-                    ;;
-		        *)
+		*)
+			usage
+			exit 1
                     ;;
         esac
         shift 1
 done
 
-$ant/bin/ant --noconfig $daysInPast
+current=`date +"%d %b %Y"`
+
+# Retrieve last build date:
+last=`cat $logdir/$branch 2>/dev/null`
+
+if [ -z "$last" ]; then
+	last=`date -d "7 days ago" +"%d %b %Y"`
+fi
+
+/shared/common/ibm-java2-ppc-50/bin/java -jar ~/org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar -application org.eclipse.ant.core.antRunner -f build.xml -Dstart="$last" -Dend="$current"
+
+if [ $? -ne 0 ]; then
+	echo ChangeLog build failed
+	exit 1
+fi
+
+# Save current build date:
+echo $current > $logdir/$branch
+
