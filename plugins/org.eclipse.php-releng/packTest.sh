0e98a37899884d5aec9a9a171edef2431b40c1c9
mspector <mspector>
2008-07-10 09:00:00 +0000

    add generation of changelog

--- plugins/org.eclipse.php-releng/packTest.sh
+++ plugins/org.eclipse.php-releng/packTest.sh
@@ -5,25 +5,23 @@
 cd /opt/users/phpBuild/org.eclipse.php-releng/
 rm -rf ../src
 
-#dos2unix 
 dos2unix buildall.sh 
 
 # operate build test target with nightly build 
 ./buildall.sh -vm /shared/common/ibm-java2-ppc-50 -target buildTest -platform "-Dbaseos=linux -Dbasews=gtk -Dbasearch=ppc" N 
 
-cd ../src
+# generate changelog
+cd changelog
+./changelog.sh
 
-chmod +x *
+cd ../../src
 
-#dos2unix 
+# run Unit test
+chmod +x runtests
 dos2unix runtests
-
-# runtests
 ./runtests -ws gtk -os linux -arch ppc -vm /shared/common/ibm-java2-ppc-50/bin/java -xserver
 
 # update by rss + mail
 cd ../rss
 ./rssProducer
-
-
 
