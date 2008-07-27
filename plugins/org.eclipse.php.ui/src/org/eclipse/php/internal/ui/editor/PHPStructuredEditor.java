4d715e33de268e7778fa221b872c2347df845332
rganor <rganor>
2008-07-27 06:56:27 +0000

    Bug #242015 - Add support for JSDT
    fixed.

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/PHPStructuredEditor.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/PHPStructuredEditor.java
@@ -126,10 +126,6 @@ public class PHPStructuredEditor extends StructuredTextEditor implements IPhpScr
 	/** Indicates whether the structure editor is displaying an external file */
 	protected boolean isExternal;
 
-	public final ISourceViewer getSourceViewerPublic() {
-		return getSourceViewer();
-	}
-
 	/** The editor's save policy */
 	protected ISavePolicy fSavePolicy = null;
 
---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/provisional/registry/AdapterFactoryProviderForJSDT.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.php.internal.ui.provisional.registry;
+
+import org.eclipse.php.internal.core.documentModel.handler.PHPModelHandler;
+import org.eclipse.wst.sse.core.internal.ltk.modelhandler.IDocumentTypeHandler;
+
+/**
+ * Support for JavaScript editing in php source
+ * @see issue #242015 
+ */
+public class AdapterFactoryProviderForJSDT extends org.eclipse.wst.jsdt.web.ui.internal.registry.AdapterFactoryProviderForJSDT {
+	
+	@Override
+	public boolean isFor(IDocumentTypeHandler contentTypeDescription) {
+		return contentTypeDescription  instanceof PHPModelHandler;
+	}
+
+}
