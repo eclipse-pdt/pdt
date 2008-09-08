be33a7c9c961aceaa7868b6b15345dda9405d8bc
eklein <eklein>
2008-09-08 11:10:24 +0000

    add copyright

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/compiler/ast/parser/PHPTodoTaskAstParser.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/compiler/ast/parser/PHPTodoTaskAstParser.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2008 xored software, Inc.
- *
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *   Zend and IBM - Initial implementation
  *******************************************************************************/
 package org.eclipse.php.internal.core.compiler.ast.parser;
 
