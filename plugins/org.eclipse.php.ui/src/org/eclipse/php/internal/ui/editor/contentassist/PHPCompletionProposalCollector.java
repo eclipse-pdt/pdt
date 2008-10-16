ee926e0b6a6de01cc7e1e7aef3fc61c953acc7c7
mspector <mspector>
2008-10-16 16:19:44 +0000

    Fixed #251085

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/contentassist/PHPCompletionProposalCollector.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/contentassist/PHPCompletionProposalCollector.java
@@ -10,10 +10,13 @@
  *******************************************************************************/
 package org.eclipse.php.internal.ui.editor.contentassist;
 
-import org.eclipse.dltk.core.IScriptProject;
-import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.*;
+import org.eclipse.dltk.ui.ScriptElementImageDescriptor;
+import org.eclipse.dltk.ui.ScriptElementImageProvider;
+import org.eclipse.dltk.ui.text.completion.IScriptCompletionProposal;
 import org.eclipse.dltk.ui.text.completion.ScriptCompletionProposal;
 import org.eclipse.dltk.ui.text.completion.ScriptCompletionProposalCollector;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.php.core.codeassist.IPHPCompletionRequestor;
 import org.eclipse.swt.graphics.Image;
@@ -22,7 +25,7 @@ public class PHPCompletionProposalCollector extends ScriptCompletionProposalColl
 
 	private IDocument document;
 	private boolean explicit;
-	
+
 	public PHPCompletionProposalCollector(IDocument document, ISourceModule cu, boolean explicit) {
 		super(cu);
 		this.document = document;
@@ -40,13 +43,35 @@ public class PHPCompletionProposalCollector extends ScriptCompletionProposalColl
 	protected ScriptCompletionProposal createScriptCompletionProposal(String completion, int replaceStart, int length, Image image, String displayString, int i, boolean isInDoc) {
 		return new PHPCompletionProposal(completion, replaceStart, length, image, displayString, i, isInDoc);
 	}
-	
+
+	protected IScriptCompletionProposal createScriptCompletionProposal(CompletionProposal proposal) {
+		ScriptCompletionProposal completionProposal = (ScriptCompletionProposal) super.createScriptCompletionProposal(proposal);
+		if (proposal.getKind() == CompletionProposal.METHOD_DECLARATION) {
+			IMethod method = (IMethod) proposal.getModelElement();
+			try {
+				if (method.isConstructor()) {
+					// replace method icon with class icon:
+					int flags = proposal.getFlags();
+					ImageDescriptor typeImageDescriptor = ScriptElementImageProvider.getTypeImageDescriptor(flags, false);
+					int adornmentFlags = ScriptElementImageProvider.computeAdornmentFlags(method.getDeclaringType(), ScriptElementImageProvider.SMALL_ICONS | ScriptElementImageProvider.OVERLAY_ICONS);
+					ScriptElementImageDescriptor descriptor = new ScriptElementImageDescriptor(typeImageDescriptor, adornmentFlags, ScriptElementImageProvider.SMALL_SIZE);
+					completionProposal.setImage(getImage(descriptor));
+				}
+			} catch (ModelException e) {
+				if (DLTKCore.DEBUG_COMPLETION) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return completionProposal;
+	}
+
 	protected char[] getVarTrigger() {
 		// variable proposal will be inserted automatically if one of these characters
 		// is being typed in showing proposal time:
 		return null;
 	}
-	
+
 	public IDocument getDocument() {
 		return document;
 	}
