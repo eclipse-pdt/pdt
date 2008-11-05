33ee06c1e3e8cabe329ac31848029288925566f9
eklein <eklein>
2008-11-05 14:34:29 +0000

    fix bug#253655 - [Includepath] Includepath preference page shoould provide simple management of the entries. Initial implementation

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/includepath/PHPIncludePathsBlock.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.preferences.includepath;
+
+import org.eclipse.dltk.ui.util.IStatusChangeListener;
+import org.eclipse.dltk.ui.wizards.BuildpathsBlock;
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.php.internal.ui.PHPUiPlugin;
+import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
+
+public class PHPIncludePathsBlock extends BuildpathsBlock {
+
+	public PHPIncludePathsBlock(IRunnableContext runnableContext, IStatusChangeListener context, int pageToShow, boolean useNewPage, IWorkbenchPreferenceContainer pageContainer) {
+		super(runnableContext, context, pageToShow, useNewPage, pageContainer);
+	}
+
+	@Override
+	protected IPreferenceStore getPreferenceStore() {
+		return PHPUiPlugin.getDefault().getPreferenceStore();
+	}
+
+	@Override
+	protected boolean supportZips() {
+		return false;
+	}
+
+
+}
