f2066fd555de31440b507e364ab4eb0ff7fdb58c
eklein <eklein>
2008-11-06 08:26:08 +0000

    fix bug#253655 - [Includepath] Includepath preference page shoould provide simple management of the entries. Initial implementation

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/includepath/IncludePathProperties.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.preferences.includepath;
+
+import org.eclipse.dltk.ui.preferences.BuildPathsPropertyPage;
+import org.eclipse.dltk.ui.util.BusyIndicatorRunnableContext;
+import org.eclipse.dltk.ui.wizards.BuildpathsBlock;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
+
+public class IncludePathProperties extends BuildPathsPropertyPage implements IWorkbenchPropertyPage {
+
+	public IncludePathProperties() {
+	}
+
+	@Override
+	protected BuildpathsBlock createBuildPathBlock(IWorkbenchPreferenceContainer pageContainer) {
+		return new PHPIncludePathsBlock(new BusyIndicatorRunnableContext(), this, getSettings().getInt(INDEX), false, pageContainer);
+	}
+}
