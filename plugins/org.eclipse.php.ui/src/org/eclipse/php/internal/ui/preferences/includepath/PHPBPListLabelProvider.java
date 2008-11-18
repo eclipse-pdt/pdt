ca46ddf11907f7391eea44725cfded726ef2c856
eklein <eklein>
2008-11-18 12:03:45 +0000

    fix bug#253655 - [Includepath] Includepath preference page shoould provide simple management of the entries. Temporarily copy the label provider waining for the next dltk integration build

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/includepath/PHPBPListLabelProvider.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/includepath/PHPBPListLabelProvider.java
@@ -2,11 +2,10 @@ package org.eclipse.php.internal.ui.preferences.includepath;
 
 import org.eclipse.dltk.core.IBuildpathEntry;
 import org.eclipse.dltk.internal.ui.wizards.buildpath.BPListElement;
-import org.eclipse.dltk.internal.ui.wizards.buildpath.BPListLabelProvider;
 import org.eclipse.dltk.ui.DLTKPluginImages;
 import org.eclipse.jface.resource.ImageDescriptor;
 
-public class PHPBPListLabelProvider extends BPListLabelProvider {
+public class PHPBPListLabelProvider extends TempBPListLabelProvider {
 
 	protected ImageDescriptor getCPListElementBaseImage(BPListElement cpentry) {
 
