8ac0dec660cf17e7e83ab1371162a49a7c0dce88
mspector <mspector>
2008-11-20 17:16:35 +0000

    Fixed #255958

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/FileDropEditorOpenAction.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui;
+
+import java.io.File;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
+import org.eclipse.php.internal.ui.util.EditorUtility;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.wst.sse.ui.internal.FileDropAction;
+
+public class FileDropEditorOpenAction extends FileDropAction {
+
+	public boolean run(DropTargetEvent event, IEditorPart targetEditor) {
+
+		if (!(targetEditor instanceof PHPStructuredEditor))
+			return super.run(event, targetEditor);
+		
+		final String[] fileNames = (String[]) event.data;
+		if (fileNames == null || fileNames.length == 0) {
+			return false;
+		}
+		
+		// default behavior
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				for (int i = 0; i < fileNames.length; ++i) {
+					IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(new Path(fileNames[i]));
+					if (file != null) {
+						try {
+							EditorUtility.openInEditor(file, true);
+						} catch (PartInitException e) {
+							Logger.logException(e);
+						}
+					} else {
+						if (new File(fileNames[i]).exists()) {
+							EditorUtility.openFileInEditor(fileNames[i], 0);
+						}
+					}
+				}
+			}
+		});
+		return true;
+	}
+}
