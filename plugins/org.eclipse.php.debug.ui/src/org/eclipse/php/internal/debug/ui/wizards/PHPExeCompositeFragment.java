54b1f7edd1f475bdae39769c43d45e505a2817dc
mspector <mspector>
2009-01-03 17:35:01 +0000

    fixed compilation error

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/wizards/PHPExeCompositeFragment.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/wizards/PHPExeCompositeFragment.java
@@ -14,9 +14,9 @@ import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.debug.internal.ui.PixelConverter;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IMessageProvider;
-import org.eclipse.jface.layout.PixelConverter;
 import org.eclipse.php.internal.debug.core.preferences.PHPDebuggersRegistry;
 import org.eclipse.php.internal.debug.core.preferences.PHPexeItem;
 import org.eclipse.php.internal.debug.ui.PHPDebugUIImages;
