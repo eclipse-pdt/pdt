a1856ec0c0bfa33357011bf85e8a2107e7b3960a
mspector <mspector>
2009-01-07 12:19:54 +0000

    Fixed bug 260051: [Usability] Show progress monitor when indexing at first time https://bugs.eclipse.org/bugs/show_bug.cgi?id=260051

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/InitializeAfterLoadJob.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Zend and IBM - Initial implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui;
+
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.php.internal.core.PHPCorePlugin;
+import org.eclipse.ui.progress.UIJob;
+
+public class InitializeAfterLoadJob extends UIJob {
+
+	private final class RealJob extends Job {
+		public RealJob(String name) {
+			super(name);
+		}
+
+		protected IStatus run(IProgressMonitor monitor) {
+			monitor.beginTask("", 10); //$NON-NLS-1$
+			try {
+				PHPCorePlugin.initializeAfterLoad(new SubProgressMonitor(monitor, 6));
+				PHPUiPlugin.initializeAfterLoad(new SubProgressMonitor(monitor, 4));
+			} catch (CoreException e) {
+				PHPCorePlugin.log(e);
+				return e.getStatus();
+			}
+			return new Status(IStatus.OK, PHPCorePlugin.getPluginId(), IStatus.OK, "", null); //$NON-NLS-1$
+		}
+
+		public boolean belongsTo(Object family) {
+			return PHPUiPlugin.ID.equals(family);
+		}
+	}
+
+	public InitializeAfterLoadJob() {
+		super(PHPUIMessages.getString("InitializeAfterLoadJob_0")); //$NON-NLS-1$
+		setSystem(true);
+	}
+
+	public IStatus runInUIThread(IProgressMonitor monitor) {
+		Job job = new RealJob(PHPUIMessages.getString("InitializeAfterLoadJob_1")); //$NON-NLS-1$
+		job.setPriority(Job.SHORT);
+		job.schedule();
+		return new Status(IStatus.OK, PHPCorePlugin.getPluginId(), IStatus.OK, "", null); //$NON-NLS-1$
+	}
+}
