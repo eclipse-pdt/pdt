782d43b6a637f44f51c3ba3552c47cfd9a4c5001
mspector <mspector>
2009-02-01 11:33:18 +0000

    refactor: - PHPModelUtil -> PHPToolkitUtil (resolve ambiguity with PHPModelUtils) - *.phpModel package -> *.keywords (more correct naming for the package contents) - Put Code Assist classes into internal packge - not an API

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/PHPSelectionEngine.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/PHPSelectionEngine.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.php.core.codeassist;
+package org.eclipse.php.internal.core.codeassist;
 
 import java.util.*;
 
