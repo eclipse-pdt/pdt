684b78fa87163e104c73ad33efd37530e5aa7a1e
rganor <rganor>
2009-03-11 15:57:16 +0000

    Bug #253191 - [regression] Untitled PHP Document was immedialely saved using Ctrl+S, without confirmation dialog about parent folder
    contributed by Vadim

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/UntitledPHPEditor.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/UntitledPHPEditor.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.php.internal.ui.editor;
 
+import java.io.File;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -22,6 +24,7 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.php.internal.ui.Logger;
 import org.eclipse.php.internal.ui.PHPUIMessages;
 import org.eclipse.php.internal.ui.dialogs.saveFiles.SaveAsDialog;
+import org.eclipse.php.internal.ui.editor.input.NonExistingPHPFileEditorInput;
 import org.eclipse.php.internal.ui.util.EditorUtility;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorInput;
@@ -102,10 +105,31 @@ public class UntitledPHPEditor extends PHPStructuredEditor {
 				}
 			}
 		}
-
+		
+		deleteUntitledStorageFile();
+		
 		if (progressMonitor != null)
 			progressMonitor.setCanceled(!success);
 
 		return;
+	}
+	
+	@Override
+	public void dispose() {
+		deleteUntitledStorageFile();
+		super.dispose();
+	}
+
+	/**
+	 * This method removes file created as an input for untitled php file, from external storage
+	 */
+	private void deleteUntitledStorageFile() {
+		IPath oldPath = ((NonExistingPHPFileEditorInput) getEditorInput()).getPath(getEditorInput());
+		File oldFile = new File(oldPath.toOSString());
+		if (oldFile.exists() && oldFile.canWrite()) {
+			if (!oldFile.delete()) {
+				Logger.log(Logger.WARNING, PHPUIMessages.getString("UntitledPHPEditor_deleteFailed")); //$NON-NLS-1$
+			}
+		}
 	}
 }
