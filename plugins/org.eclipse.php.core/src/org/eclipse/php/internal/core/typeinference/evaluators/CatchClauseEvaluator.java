275f2cfb1c3c2f1df750bc21d2880b5cca467c93
mspector <mspector>
2009-05-28 19:56:33 +0000

    Fixed copyrights

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/CatchClauseEvaluator.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/CatchClauseEvaluator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
 package org.eclipse.php.internal.core.typeinference.evaluators;
 
 import org.eclipse.dltk.ast.references.SimpleReference;
--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/PHPClassEvaluator.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/PHPClassEvaluator.java
@@ -1,11 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 Zend Technologies.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are the copyright of Zend Technologies and is protected under
- * copyright laws of the United States.
- * You must not copy, adapt or redistribute this document for 
- * any use. 
- *
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.core.typeinference.evaluators;
 
