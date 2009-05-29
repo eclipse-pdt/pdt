364b6c86fd8c733cfa5a44decac8ea42dfb2bb9e
mspector <mspector>
2009-05-29 08:00:00 +0000

    fixed deprecated way of preference initialization

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/CorePreferenceConstants.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/CorePreferenceConstants.java
@@ -13,7 +13,8 @@ package org.eclipse.php.internal.core.preferences;
 
 import java.util.Locale;
 
-import org.eclipse.core.runtime.Preferences;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.php.internal.core.PHPCoreConstants;
 import org.eclipse.php.internal.core.PHPCorePlugin;
 import org.eclipse.php.internal.core.PHPVersion;
@@ -25,52 +26,41 @@ public class CorePreferenceConstants {
 		public static final String EDITOR_USE_ASP_TAGS= "use_asp_tags_as_php"; //$NON-NLS-1$
 	}
 
-	public static Preferences getPreferenceStore() {
-		return PHPCorePlugin.getDefault().getPluginPreferences();
-	}
-
 	/**
 	 * Initializes the given preference store with the default values.
 	 *
 	 * @param store the preference store to be initialized
 	 */
 	public static void initializeDefaultValues() {
-		Preferences store = getPreferenceStore();
-		store.setDefault(Keys.PHP_VERSION, PHPVersion.PHP5.getAlias());
+		IEclipsePreferences node = new DefaultScope().getNode(PHPCorePlugin.ID);
 
-		store.setDefault(PHPCoreConstants.TASK_TAGS, PHPCoreConstants.DEFAULT_TASK_TAGS);
-		store.setDefault(PHPCoreConstants.TASK_PRIORITIES, PHPCoreConstants.DEFAULT_TASK_PRIORITIES);
-		store.setDefault(PHPCoreConstants.TASK_CASE_SENSITIVE, PHPCoreConstants.ENABLED);
-		store.setDefault(Keys.EDITOR_USE_ASP_TAGS, false);
-
-		store.setDefault(PHPCoreConstants.FORMATTER_USE_TABS, true);
-		store.setDefault(PHPCoreConstants.FORMATTER_INDENTATION_SIZE, PHPCoreConstants.DEFAULT_INDENTATION_SIZE);
-
-		store.setDefault(PHPCoreConstants.CODEGEN_ADD_COMMENTS, true);
-		
-		if ((store.getString(PHPCoreConstants.WORKSPACE_DEFAULT_LOCALE)).equals("")) { //$NON-NLS-1$
-			store.setValue(PHPCoreConstants.WORKSPACE_DEFAULT_LOCALE,Locale.getDefault().toString());
-			store.setDefault(PHPCoreConstants.WORKSPACE_LOCALE, Locale.getDefault().toString());
-		}
-		
-		store.setDefault(PHPCoreConstants.CODEASSIST_ADDIMPORT, true);
-		store.setDefault(PHPCoreConstants.CODEASSIST_FILL_ARGUMENT_NAMES, false);
-		store.setDefault(PHPCoreConstants.CODEASSIST_GUESS_METHOD_ARGUMENTS, true);
-		store.setDefault(PHPCoreConstants.CODEASSIST_PREFIX_COMPLETION, false);
-		// implemented:
-		store.setDefault(PHPCoreConstants.CODEASSIST_AUTOINSERT, true);
-		store.setDefault(PHPCoreConstants.CODEASSIST_INSERT_COMPLETION, true);
-		store.setDefault(PHPCoreConstants.CODEASSIST_SHOW_VARIABLES_FROM_OTHER_FILES, false);
-		store.setDefault(PHPCoreConstants.CODEASSIST_SHOW_CONSTANTS_ASSIST, true);
-		store.setDefault(PHPCoreConstants.CODEASSIST_SHOW_NON_STRICT_OPTIONS, false);
-		store.setDefault(PHPCoreConstants.CODEASSIST_GROUP_OPTIONS, false);
-		store.setDefault(PHPCoreConstants.CODEASSIST_CUT_COMMON_PREFIX, false);
-		store.setDefault(PHPCoreConstants.CODEASSIST_CONSTANTS_CASE_SENSITIVE, false);
-		store.setDefault(PHPCoreConstants.CODEASSIST_SHOW_CLASS_NAMES_IN_GLOBAL_COMPLETION, true);
-		store.setDefault(PHPCoreConstants.CODEASSIST_AUTOACTIVATION, true);
-		store.setDefault(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_DELAY, 200);
-		store.setDefault(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_TRIGGERS_PHP, "$:>"); //$NON-NLS-1$
-		store.setDefault(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_TRIGGERS_PHPDOC, "@"); //$NON-NLS-1$
+		node.put(Keys.PHP_VERSION, PHPVersion.PHP5.getAlias());
+		node.put(PHPCoreConstants.TASK_TAGS, PHPCoreConstants.DEFAULT_TASK_TAGS);
+		node.put(PHPCoreConstants.TASK_PRIORITIES, PHPCoreConstants.DEFAULT_TASK_PRIORITIES);
+		node.put(PHPCoreConstants.TASK_CASE_SENSITIVE, PHPCoreConstants.ENABLED);
+		node.putBoolean(Keys.EDITOR_USE_ASP_TAGS, false);
+		node.putBoolean(PHPCoreConstants.FORMATTER_USE_TABS, true);
+		node.put(PHPCoreConstants.FORMATTER_INDENTATION_SIZE, PHPCoreConstants.DEFAULT_INDENTATION_SIZE);
+		node.putBoolean(PHPCoreConstants.CODEGEN_ADD_COMMENTS, true);
+		node.put(PHPCoreConstants.WORKSPACE_DEFAULT_LOCALE,Locale.getDefault().toString());
+		node.put(PHPCoreConstants.WORKSPACE_LOCALE, Locale.getDefault().toString());
+		node.putBoolean(PHPCoreConstants.CODEASSIST_ADDIMPORT, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_FILL_ARGUMENT_NAMES, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_GUESS_METHOD_ARGUMENTS, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_PREFIX_COMPLETION, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_AUTOINSERT, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_INSERT_COMPLETION, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_SHOW_VARIABLES_FROM_OTHER_FILES, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_SHOW_CONSTANTS_ASSIST, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_SHOW_NON_STRICT_OPTIONS, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_GROUP_OPTIONS, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_CUT_COMMON_PREFIX, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_CONSTANTS_CASE_SENSITIVE, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_SHOW_CLASS_NAMES_IN_GLOBAL_COMPLETION, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_AUTOACTIVATION, true);
+		node.putInt(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_DELAY, 200);
+		node.put(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_TRIGGERS_PHP, "$:>"); //$NON-NLS-1$
+		node.put(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_TRIGGERS_PHPDOC, "@"); //$NON-NLS-1$
 	}
 
 	// Don't instantiate
