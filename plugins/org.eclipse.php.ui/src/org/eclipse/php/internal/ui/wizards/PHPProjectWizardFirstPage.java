2856b92e7f6ec5f2fb8ee7e00931880477a4515c
rganor <rganor>
2009-05-29 12:59:18 +0000

    Bug 277441 Text missing from "New PHP Project" widget panel
    fixed

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/wizards/PHPProjectWizardFirstPage.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/wizards/PHPProjectWizardFirstPage.java
@@ -13,6 +13,7 @@ package org.eclipse.php.internal.ui.wizards;
 
 import java.net.URI;
 import java.util.*;
+import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -46,10 +47,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
@@ -188,15 +186,29 @@ public class PHPProjectWizardFirstPage extends WizardPage implements IPHPProject
 	 */
 	protected final class DetectGroup extends Observable implements Observer, SelectionListener {
 		private final Link fHintText;
+		private Label fIcon;
 		private boolean fDetect;
-
-		public DetectGroup(Composite composite) {
-			fHintText = new Link(composite, SWT.WRAP);
+		
+		public DetectGroup(Composite parent) {
+			
+			Composite composite= new Composite(parent, SWT.NONE);
+			composite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+			GridLayout layout= new GridLayout(2, false);
+			layout.horizontalSpacing= 10;
+			composite.setLayout(layout);
+			
+			fIcon= new Label(composite, SWT.LEFT);
+			fIcon.setImage(Dialog.getImage(Dialog.DLG_IMG_MESSAGE_WARNING));
+			GridData gridData= new GridData(SWT.LEFT, SWT.CENTER, false, false);
+			fIcon.setLayoutData(gridData);
+			fIcon.setVisible(false);
+			
+			fHintText= new Link(composite, SWT.WRAP);
 			fHintText.setFont(composite.getFont());
-			fHintText.addSelectionListener(this);
-			GridData gd = new GridData(GridData.FILL, SWT.FILL, true, true);
-			gd.widthHint = convertWidthInCharsToPixels(50);
-			fHintText.setLayoutData(gd);
+			gridData= new GridData(GridData.FILL, SWT.FILL, true, true);
+			gridData.widthHint= convertWidthInCharsToPixels(50);
+			gridData.heightHint= convertHeightInCharsToPixels(3);
+			fHintText.setLayoutData(gridData);
 		}
 
 		private boolean isValidProjectName(String name) {
