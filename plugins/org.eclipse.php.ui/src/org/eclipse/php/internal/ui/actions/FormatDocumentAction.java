effedfb0ad571a29e6ec43ad2fecd853b6a1492a
rganor <rganor>
2009-06-09 10:36:39 +0000

    format action

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/FormatDocumentAction.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2009 Zend Technologies.
+ * All rights reserved. This program and the accompanying materials
+ * are the copyright of Zend Technologies and is protected under
+ * copyright laws of the United States.
+ * You must not copy, adapt or redistribute this document for 
+ * any use. 
+ *
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.php.internal.ui.actions.format.FormatActionDelegate;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class FormatDocumentAction  implements IWorkbenchWindowActionDelegate {
+
+	FormatActionDelegate action = new FormatActionDelegate();
+	
+	public void dispose() {
+	}
+
+	public void init(IWorkbenchWindow window) {
+	}
+
+	public void run(IAction action) {
+		this.action.run(action);
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.action.selectionChanged(action, selection);
+	}
+}
