ac71c75436617e71b53183588ea2afde8007e79e
rganor <rganor>
2009-06-30 13:41:33 +0000

    Bug #281081 - CTRL+Shift+B doesn't work
    contributed by Qiangsheng,

---
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/breakpoint/PHPBreakPointAdapterFactory.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2009 Zend Technologies.
+ * All rights reserved. This program and the accompanying materials
+ * are the copyright of Zend Technologies and is protected under
+ * copyright laws of the United States.
+ * You must not copy, adapt or redistribute this document for 
+ * any use. 
+ *
+ *******************************************************************************/
+package org.eclipse.php.internal.debug.ui.breakpoint;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.debug.ui.actions.IToggleBreakpointsTarget;
+
+public class PHPBreakPointAdapterFactory implements IAdapterFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object, java.lang.Class)
+	 */
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		if (adapterType == IToggleBreakpointsTarget.class) {
+			return new ScriptLineBreakpointAdapter();
+		}
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapterList()
+	 */
+	public Class[] getAdapterList() {
+		return new Class[] { IToggleBreakpointsTarget.class };
+	}
+
+}
