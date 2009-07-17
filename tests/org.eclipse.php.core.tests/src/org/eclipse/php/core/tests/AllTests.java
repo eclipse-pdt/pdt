46b754a7de8ed5718dbb4d44526ed14218dfa7cb
mspector <mspector>
2009-07-17 18:53:06 +0000

    *** empty log message ***

--- tests/org.eclipse.php.core.tests/src/org/eclipse/php/core/tests/AllTests.java
+++ tests/org.eclipse.php.core.tests/src/org/eclipse/php/core/tests/AllTests.java
@@ -30,19 +30,18 @@ import org.eclipse.php.core.tests.errors.ErrorReportingTests;
 import org.eclipse.php.core.tests.filenetwork.FileNetworkTests;
 import org.eclipse.php.core.tests.formatter.FormatterTests;
 import org.eclipse.php.core.tests.includepath.IncludePathManagerTests;
-import org.eclipse.php.core.tests.mixin.MixinTests;
 import org.eclipse.php.core.tests.model_structure.ModelStructureTests;
 import org.eclipse.php.core.tests.search.SearchTests;
 import org.eclipse.php.core.tests.selection.SelectionEngineTests;
 import org.eclipse.php.core.tests.typeinference.TypeInferenceTests;
 
 public class AllTests {
-	
+
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for org.eclipse.php.core");
 
 		// $JUnit-BEGIN$
-		
+
 		// Model tests:
 		suite.addTest(DomParserTests.suite());
 		suite.addTest(CompilerParserTests.suite());
@@ -51,30 +50,29 @@ public class AllTests {
 		suite.addTest(ASTMatcherTests.suite());
 		suite.addTest(CommentMapperTests.suite());
 		suite.addTest(StaticScalarExpressionsTests.suite());
-		
+
 		suite.addTest(CodeAssistTests.suite());
 		suite.addTest(SelectionEngineTests.suite());
 
 		suite.addTest(ModelStructureTests.suite());
-		suite.addTest(MixinTests.suite());
 		suite.addTest(SearchTests.suite());
-		
+
 		suite.addTest(FileNetworkTests.suite());
 		suite.addTest(TypeInferenceTests.suite());
-		
+
 		suite.addTest(PHPDocParserTests.suite());
 		suite.addTest(PHPDocAwareDeclarationTests.suite());
-		
+
 		suite.addTest(IncludePathManagerTests.suite());
 		suite.addTest(BindingTests.suite());
-		
+
 		// Document tests:
 		suite.addTest(DocumentLexerTests.suite());
 		suite.addTest(FormatterTests.suite());
 		suite.addTest(PHPPartitionerTests.suite());
-		
+
 		// $JUnit-END$
-		
+
 		return suite;
 	}
 }
