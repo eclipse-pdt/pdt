be8285e0d3e4f696d95750aa7148c0cff6a2ef9d
vpunski <vpunski>
2009-08-25 14:47:23 +0000

    bug fix 28756

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/explorer/PHPExplorerActionGroup.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/explorer/PHPExplorerActionGroup.java
@@ -21,7 +21,20 @@ import org.eclipse.dltk.internal.ui.scriptview.LayoutActionGroup;
 import org.eclipse.dltk.internal.ui.scriptview.ScriptExplorerActionGroup;
 import org.eclipse.dltk.internal.ui.scriptview.ScriptExplorerPart;
 import org.eclipse.dltk.ui.actions.GenerateActionGroup;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.viewers.OpenEvent;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.actions.ActionGroup;
+import org.eclipse.wst.jsdt.core.IClassFile;
+import org.eclipse.wst.jsdt.core.IJavaScriptElement;
+import org.eclipse.wst.jsdt.core.IType;
+import org.eclipse.wst.jsdt.core.JavaScriptCore;
+import org.eclipse.wst.jsdt.ui.actions.OpenAction;
+import org.eclipse.wst.jsdt.ui.actions.OpenEditorActionGroup;
 
 /**
  * @author nir.c
@@ -52,8 +65,33 @@ public class PHPExplorerActionGroup extends ScriptExplorerActionGroup {
 		filtered.add(new PHPRefactorActionGroup(getPart()));
 		filtered.add(new GenerateIncludePathActionGroup(getPart()));
 		filtered.add(new NamespaceGroupingActionGroup(getPart().getTreeViewer()));
-
+		
 		super.setGroups(filtered.toArray(new ActionGroup[filtered.size()]));
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.dltk.internal.ui.scriptview.ScriptExplorerActionGroup#handleOpen(org.eclipse.jface.viewers.OpenEvent)
+	 */
+	@Override
+	protected void handleOpen(OpenEvent event) {
+		//this code dispatches the selection from javascript library to JS open action
+		ISelection selection = event.getSelection();
+		if(selection instanceof ITreeSelection) {
+			ITreeSelection treeSelection = (ITreeSelection) selection;
+			Object firstElement = treeSelection.getFirstElement();
+			if(firstElement instanceof IJavaScriptElement) {
+				//it's JS element, follow opening JS editor
+				ScriptExplorerPart part = getPart();
+				IViewSite viewSite = part.getViewSite();
+				OpenAction openAction = new OpenAction(viewSite);
+				if (openAction != null && openAction.isEnabled()) {
+					openAction.run();
+					return;
+				}
+			}
+		}
+		super.handleOpen(event);
+		
 	}
 
 }
