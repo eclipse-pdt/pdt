97ab064b48de492c5eb39c5fcbdfb08bbadeb8d6
mspector <mspector>
2009-09-16 11:05:13 +0000

    Fixed bug 287427

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/contexts/ClassInstantiationContext.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/contexts/ClassInstantiationContext.java
@@ -12,28 +12,31 @@
 package org.eclipse.php.internal.core.codeassist.contexts;
 
 import org.eclipse.dltk.core.CompletionRequestor;
-import org.eclipse.dltk.core.DLTKCore;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.php.internal.core.PHPCorePlugin;
-
+import org.eclipse.php.internal.core.compiler.ast.nodes.NamespaceReference;
 
 /**
- * This context represents state when staying in a class instantiation statement.
- * <br/>Examples:
+ * This context represents state when staying in a class instantiation
+ * statement. <br/>
+ * Examples:
+ * 
  * <pre>
  *  1. new |
  *  2. new A|
  * </pre>
+ * 
  * @author michael
  */
 public class ClassInstantiationContext extends StatementContext {
-	
-	public boolean isValid(ISourceModule sourceModule, int offset, CompletionRequestor requestor) {
+
+	public boolean isValid(ISourceModule sourceModule, int offset,
+			CompletionRequestor requestor) {
 		if (!super.isValid(sourceModule, offset, requestor)) {
 			return false;
 		}
-		
+
 		try {
 			String previousWord = getPreviousWord();
 			if ("new".equalsIgnoreCase(previousWord)) {
@@ -42,7 +45,16 @@ public class ClassInstantiationContext extends StatementContext {
 		} catch (BadLocationException e) {
 			PHPCorePlugin.log(e);
 		}
-		
+
 		return false;
+	}
+
+	public String getPrefix() throws BadLocationException {
+		String prefix = super.getPrefix();
+		if (prefix.length() > 0
+				&& prefix.charAt(0) == NamespaceReference.NAMESPACE_SEPARATOR) {
+			return prefix.substring(1);
+		}
+		return prefix;
 	}
 }
