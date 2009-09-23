7f46fc2bb27af16a171e336df18157c91e4de87e
mspector <mspector>
2009-09-23 14:18:12 +0000

    set code assist autoactivation delay to 1/2 of second

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/CorePreferenceConstants.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/CorePreferenceConstants.java
@@ -64,7 +64,7 @@ public class CorePreferenceConstants {
 				false);
 		node.putBoolean(PHPCoreConstants.CODEASSIST_SHOW_STRICT_OPTIONS, false);
 		node.putBoolean(PHPCoreConstants.CODEASSIST_AUTOACTIVATION, true);
-		node.putInt(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_DELAY, 200);
+		node.putInt(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_DELAY, 500);
 	}
 
 	// Don't instantiate
