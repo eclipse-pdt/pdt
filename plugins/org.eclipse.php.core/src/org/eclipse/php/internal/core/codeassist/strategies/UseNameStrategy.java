b9b7e1de6a31325d00ff1a863b8bf73eaa3bfee6
mspector <mspector>
2009-10-02 20:34:52 +0000

    fix suffix after name completion in use statement

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/UseNameStrategy.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
+package org.eclipse.php.internal.core.codeassist.strategies;
+
+import org.eclipse.php.core.codeassist.ICompletionContext;
+import org.eclipse.php.internal.core.codeassist.contexts.AbstractCompletionContext;
+
+public class UseNameStrategy extends GlobalTypesStrategy {
+
+	public UseNameStrategy(ICompletionContext context, int trueFlag,
+			int falseFlag) {
+		super(context, trueFlag, falseFlag);
+	}
+
+	public UseNameStrategy(ICompletionContext context) {
+		super(context);
+	}
+
+	public String getNSSuffix(AbstractCompletionContext abstractContext) {
+		return ""; //$NON-NLS-1$
+	}
+}
