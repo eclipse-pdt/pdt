be62a7d22fe2bf77cc920ac425728ec8efa79d97
mspector <mspector>
2009-10-05 09:23:12 +0000

    fixed opening external file from the debug console

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/sourcelookup/PHPSourceLookupParticipant.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/sourcelookup/PHPSourceLookupParticipant.java
@@ -13,21 +13,25 @@ package org.eclipse.php.internal.debug.core.sourcelookup;
 
 import java.io.File;
 
-import org.eclipse.core.internal.filesystem.local.LocalFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.sourcelookup.AbstractSourceLookupParticipant;
+import org.eclipse.php.internal.core.containers.LocalFileStorage;
 import org.eclipse.php.internal.debug.core.xdebug.dbgp.model.DBGpStackFrame;
 import org.eclipse.php.internal.debug.core.zend.model.PHPStackFrame;
 
 /**
- * The PHP source lookup participant knows how to translate a 
- * PHP stack frame into a source file name 
+ * The PHP source lookup participant knows how to translate a PHP stack frame
+ * into a source file name
  */
 public class PHPSourceLookupParticipant extends AbstractSourceLookupParticipant {
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.core.sourcelookup.ISourceLookupParticipant#getSourceName(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.internal.core.sourcelookup.ISourceLookupParticipant
+	 * #getSourceName(java.lang.Object)
 	 */
 	public String getSourceName(Object object) throws CoreException {
 		if (object instanceof PHPStackFrame) {
@@ -50,11 +54,13 @@ public class PHPSourceLookupParticipant extends AbstractSourceLookupParticipant 
 		try {
 			sourceElements = super.findSourceElements(object);
 		} catch (Throwable e) {
-			// Check if the lookup failed because the source is outside the workspace.
+			// Check if the lookup failed because the source is outside the
+			// workspace.
 		}
 
 		if (sourceElements == EMPTY) {
-			// If the lookup returned an empty elements array, check if the source is outside the workspace.
+			// If the lookup returned an empty elements array, check if the
+			// source is outside the workspace.
 			String fileName = null;
 			if (object instanceof PHPStackFrame) {
 				fileName = ((PHPStackFrame) object).getSourceName();
@@ -67,7 +73,7 @@ public class PHPSourceLookupParticipant extends AbstractSourceLookupParticipant 
 					return EMPTY;
 				}
 
-				LocalFile storage = new LocalFile(file);
+				LocalFileStorage storage = new LocalFileStorage(file);
 				if (storage != null) {
 					return new Object[] { storage };
 				} else {
