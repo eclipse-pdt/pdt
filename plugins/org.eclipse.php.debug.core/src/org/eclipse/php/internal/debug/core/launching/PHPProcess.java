f206da993adbed9964de3411f62ece95af25c544
mspector <mspector>
2009-10-19 14:31:37 +0000

    fire events from PHP process

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/launching/PHPProcess.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/launching/PHPProcess.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.debug.core.launching;
 
@@ -14,7 +15,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.runtime.PlatformObject;
+import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IProcess;
@@ -26,12 +29,13 @@ public class PHPProcess extends PlatformObject implements IProcess {
 
 	private ILaunch fLaunch;
 	private String fName;
-	private Map fAttributes;
+	private Map<String, String> fAttributes;
 	private boolean fTerminated;
 	private IConsole fConsole = null;
 	private PHPStreamsProxy fProxy;
 	private PHPHyperLink fPHPHyperLink;
 	private IDebugTarget fDebugTarget;
+	private int fExitValue;
 
 	public PHPProcess(ILaunch launch, String name) {
 		fLaunch = launch;
@@ -39,6 +43,7 @@ public class PHPProcess extends PlatformObject implements IProcess {
 		fTerminated = false;
 		launch.addProcess(this);
 		fProxy = new PHPStreamsProxy();
+		fireCreationEvent();
 	}
 
 	public String getLabel() {
@@ -60,14 +65,14 @@ public class PHPProcess extends PlatformObject implements IProcess {
 
 	public void setAttribute(String key, String value) {
 		if (fAttributes == null) {
-			fAttributes = new HashMap(5);
+			fAttributes = new HashMap<String, String>(5);
 		}
 		Object origVal = fAttributes.get(key);
 		if (origVal != null && origVal.equals(value)) {
-			return; //nothing changed.
+			return; // nothing changed.
 		}
-
 		fAttributes.put(key, value);
+		fireChangeEvent();
 	}
 
 	public String getAttribute(String key) {
@@ -77,8 +82,12 @@ public class PHPProcess extends PlatformObject implements IProcess {
 		return (String) fAttributes.get(key);
 	}
 
+	public void setExitValue(int exitValue) {
+		this.fExitValue = exitValue;
+	}
+
 	public int getExitValue() throws DebugException {
-		return 0;
+		return fExitValue;
 	}
 
 	public Object getAdapter(Class adapter) {
@@ -112,6 +121,7 @@ public class PHPProcess extends PlatformObject implements IProcess {
 	public void terminate() throws DebugException {
 		// fLaunch.terminate(); // No need starting from Eclipse 3.3
 		fTerminated = true;
+		fireTerminateEvent();
 	}
 
 	public void setPHPHyperLink(PHPHyperLink pLink) {
@@ -136,5 +146,39 @@ public class PHPProcess extends PlatformObject implements IProcess {
 
 	public void setDebugTarget(IDebugTarget target) {
 		fDebugTarget = target;
+	}
+
+	/**
+	 * Fires a creation event.
+	 */
+	protected void fireCreationEvent() {
+		fireEvent(new DebugEvent(this, DebugEvent.CREATE));
+	}
+
+	/**
+	 * Fires the given debug event.
+	 * 
+	 * @param event
+	 *            debug event to fire
+	 */
+	protected void fireEvent(DebugEvent event) {
+		DebugPlugin manager = DebugPlugin.getDefault();
+		if (manager != null) {
+			manager.fireDebugEventSet(new DebugEvent[] { event });
+		}
+	}
+
+	/**
+	 * Fires a terminate event.
+	 */
+	protected void fireTerminateEvent() {
+		fireEvent(new DebugEvent(this, DebugEvent.TERMINATE));
+	}
+
+	/**
+	 * Fires a change event.
+	 */
+	protected void fireChangeEvent() {
+		fireEvent(new DebugEvent(this, DebugEvent.CHANGE));
 	}
 }
