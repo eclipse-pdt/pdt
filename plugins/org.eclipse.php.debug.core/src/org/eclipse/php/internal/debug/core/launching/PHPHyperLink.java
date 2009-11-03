32f3eb3adccbd38b9c52e4787e9cc0ad0f6059fc
vpunski <vpunski>
2009-11-03 11:44:18 +0000

    exception thrown when empty message body received as an empty line

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/launching/PHPHyperLink.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/launching/PHPHyperLink.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.debug.core.launching;
 
@@ -19,28 +20,29 @@ public class PHPHyperLink {
 
 	private List<HyperlinkEntry> fLinks;
 
-    public void addLink(IHyperlink link, String message, int length) {
-        if (fLinks == null) {
-            fLinks = new Vector<HyperlinkEntry>();
-        }
-        HyperlinkEntry hLink = new HyperlinkEntry(link, message, length);
-        fLinks.add(hLink);
-    }
+	public void addLink(IHyperlink link, String message, int length) {
+		if (fLinks == null) {
+			fLinks = new Vector<HyperlinkEntry>();
+		}
+		HyperlinkEntry hLink = new HyperlinkEntry(link, message, length);
+		fLinks.add(hLink);
+	}
 
-    public HyperlinkEntry getHyperlinkEntry(String message) {
-    	if (fLinks != null) {
-	        Object[] alinks = fLinks.toArray();
-	        for (Object element : alinks) {
-	            String linkMessage = ((HyperlinkEntry) element).getMessage();
-	            if ((linkMessage.trim()).equals(message.trim())) {
-	                return ((HyperlinkEntry) element);
-	            }
-	        }
-    	}
-        return null;
-    }
+	public HyperlinkEntry getHyperlinkEntry(String message) {
+		if (fLinks != null) {
+			Object[] alinks = fLinks.toArray();
+			for (Object element : alinks) {
+				String linkMessage = ((HyperlinkEntry) element).getMessage();
+				if (message != null && message.length() > 0
+						&& (linkMessage.trim()).startsWith(message.trim())) {
+					return ((HyperlinkEntry) element);
+				}
+			}
+		}
+		return null;
+	}
 
-    public void dispose() {
-        fLinks = null;
-    }
+	public void dispose() {
+		fLinks = null;
+	}
 }
