6d05c19d738271ec87c4870407d6d3c63c3fe160
vpunski <vpunski>
2010-02-21 12:43:33 +0000

    Bug 301910 -  'Open PHP Manual'(Shift+F2) action doesn't work

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/OpenFunctionsManualAction.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/OpenFunctionsManualAction.java
@@ -1,60 +1,48 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.ui.actions;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.ResourceBundle;
 
-import org.eclipse.php.internal.core.phpModel.phpElementData.CodeData;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPCodeData;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.php.internal.core.language.LanguageModelInitializer;
 import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
 import org.eclipse.php.internal.ui.util.PHPManualFactory;
 import org.eclipse.ui.texteditor.IUpdate;
 
-public class OpenFunctionsManualAction extends PHPEditorResolvingAction implements IUpdate {
-	
+public class OpenFunctionsManualAction extends PHPEditorResolvingAction
+		implements IUpdate {
+
 	private String url;
 
-	public OpenFunctionsManualAction(ResourceBundle resourceBundle, PHPStructuredEditor editor) {
+	public OpenFunctionsManualAction(ResourceBundle resourceBundle,
+			PHPStructuredEditor editor) {
 		super(resourceBundle, "OpenFunctionsManualAction_", editor); //$NON-NLS-1$
 	}
 
-	protected void doRun() {
-		if (url != null) {
-			PHPManualFactory.getManual().showFunctionHelp(url); // XXX handle multiple elements
+	protected void doRun(IModelElement modelElement) {
+		if (isValid(modelElement)) {
+			PHPManualFactory.getManual().showFunctionHelp(url);
 		}
 	}
-	
-	protected boolean isValid() {
-		if (super.isValid()) {
-			url = PHPManualFactory.getManual().getURLForManual((PHPCodeData) getCodeDatas()[0]);
-			return url != null;
+
+	protected boolean isValid(IModelElement modelElement) {
+		if (super.isValid(modelElement)) {
+			if (LanguageModelInitializer.isLanguageModelElement(modelElement)) {
+				url = PHPManualFactory.getManual()
+						.getURLForManual(modelElement);
+				return url != null;
+			}
 		}
 		return false;
 	}
-	
-	public void update() {
-		setEnabled (getTextEditor() != null && isValid());
-	}
-
-	protected CodeData[] filterCodeDatas(CodeData[] codeDatas) {
-		// only show help for built-in elements
-		List<CodeData> nonUserCodeData = new LinkedList();
-		for (CodeData codeData : codeDatas) {
-			if (!codeData.isUserCode() && codeData.getUserData() == null) {
-				nonUserCodeData.add(codeData);
-			}
-		}
-		return nonUserCodeData.toArray(new CodeData[nonUserCodeData.size()]);
-	}
-
 }
