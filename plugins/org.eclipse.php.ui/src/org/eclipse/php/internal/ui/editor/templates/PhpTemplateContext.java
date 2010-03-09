e391783ff65fd4b235aaa4b2aed4f626233289ce
qiwang <qiwang>
2010-03-09 12:20:34 +0000

    Bug 206803 -  Templates don't respect tab policy settings for indentation.
    		Description: Applied the contributed patch.
    
            Tests:  Manual
    
            Notes to build: head
    
            Notes to Developers:
    
            Notes to QA:
    
            Notes to Documentation:

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/templates/PhpTemplateContext.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/templates/PhpTemplateContext.java
@@ -14,12 +14,23 @@ package org.eclipse.php.internal.ui.editor.templates;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.ui.templates.ScriptTemplateContext;
 import org.eclipse.dltk.ui.templates.ScriptTemplateContextType;
+import org.eclipse.dltk.utils.TextUtils;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.templates.Template;
+import org.eclipse.jface.text.templates.TemplateBuffer;
+import org.eclipse.jface.text.templates.TemplateException;
+import org.eclipse.php.internal.core.PHPCoreConstants;
+import org.eclipse.php.internal.core.PHPCorePlugin;
+import org.eclipse.php.internal.core.format.FormatPreferencesSupport;
 
 /**
  * The template's context
  */
 public class PhpTemplateContext extends ScriptTemplateContext {
+
+	public static final char BLANK = ' ';
+	public static final char TAB = '\t';
 
 	public PhpTemplateContext(ScriptTemplateContextType phpTemplateContextType,
 			IDocument document, int offset, int length,
@@ -27,4 +38,35 @@ public class PhpTemplateContext extends ScriptTemplateContext {
 		super(phpTemplateContextType, document, offset, length, sourceModule);
 	}
 
+	@Override
+	public TemplateBuffer evaluate(Template template)
+			throws BadLocationException, TemplateException {
+		boolean useTab = getPreferences().getBoolean(PHPCorePlugin.ID,
+				PHPCoreConstants.FORMATTER_USE_TABS);
+		if (!useTab) {
+			String lengthString = getPreferences().getString(PHPCorePlugin.ID,
+					PHPCoreConstants.FORMATTER_INDENTATION_SIZE);
+			int length = FormatPreferencesSupport.getInstance()
+					.getIndentationSize(null);
+			if (lengthString != null && lengthString.trim().length() != 0) {
+				try {
+					length = Integer.parseInt(lengthString);
+				} catch (Exception e) {
+				}
+			}
+
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < length; i++) {
+				sb.append(" ");
+			}
+			String newPattern = TextUtils.replace(template.getPattern(), TAB,
+					sb.toString());
+			template = new Template(template.getName(), template
+					.getDescription(), template.getContextTypeId(), newPattern,
+					template.isAutoInsertable());
+
+		}
+		TemplateBuffer result = super.evaluate(template);
+		return result;
+	}
 }
