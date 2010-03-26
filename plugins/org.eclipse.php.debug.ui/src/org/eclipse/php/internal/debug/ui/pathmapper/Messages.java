c01e71f029ae6a96d9726763b49a55393f5c8ea1
qiwang <qiwang>
2010-03-26 07:44:09 +0000

    Bugzilla: n/a
    		Description: Validate the dialog when switch the selection of file source. Externalized the sting as well.
    
            Tests:  Manual
    
            Notes to build: wave7_1 branch
    
            Notes to Developers:
    
            Notes to QA:
    
            Notes to Documentation:

---
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/pathmapper/Messages.java
@@ -0,0 +1,32 @@
+package org.eclipse.php.internal.debug.ui.pathmapper;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.php.internal.debug.ui.pathmapper.messages"; //$NON-NLS-1$
+	public static String PathMapperEntryDialog_0;
+	public static String PathMapperEntryDialog_1;
+	public static String PathMapperEntryDialog_10;
+	public static String PathMapperEntryDialog_11;
+	public static String PathMapperEntryDialog_12;
+	public static String PathMapperEntryDialog_13;
+	public static String PathMapperEntryDialog_14;
+	public static String PathMapperEntryDialog_15;
+	public static String PathMapperEntryDialog_16;
+	public static String PathMapperEntryDialog_17;
+	public static String PathMapperEntryDialog_2;
+	public static String PathMapperEntryDialog_3;
+	public static String PathMapperEntryDialog_4;
+	public static String PathMapperEntryDialog_5;
+	public static String PathMapperEntryDialog_6;
+	public static String PathMapperEntryDialog_7;
+	public static String PathMapperEntryDialog_8;
+	public static String PathMapperEntryDialog_9;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
