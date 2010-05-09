cd7acdcf603a4f5ff2231ed4c1f66106093d8967
zzhongwei <zzhongwei>
2010-05-09 09:37:41 +0000

    refactor outline filter to a util class

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/util/OutlineFilter.java
@@ -0,0 +1,99 @@
+package org.eclipse.php.internal.core.util;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Vector;
+
+import org.eclipse.dltk.ast.Modifiers;
+import org.eclipse.dltk.core.IField;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.php.internal.core.compiler.ast.nodes.UseStatement;
+
+public class OutlineFilter {
+
+	public static IModelElement[] filterChildrenForOutline(Object parent,
+			IModelElement[] children) {
+		return filterDuplicatePublicVars(parent, filter(children));
+	}
+
+	public static IModelElement[] filter(IModelElement[] children) {
+		boolean initializers = false;
+		for (int i = 0; i < children.length; i++) {
+			if (matches(children[i])) {
+				initializers = true;
+				break;
+			}
+		}
+
+		if (!initializers) {
+			return children;
+		}
+
+		Vector<IModelElement> v = new Vector<IModelElement>();
+		for (int i = 0; i < children.length; i++) {
+			if (matches(children[i])) {
+				continue;
+			}
+			v.addElement(children[i]);
+		}
+
+		IModelElement[] result = new IModelElement[v.size()];
+		v.copyInto(result);
+		return result;
+	}
+
+	public static IModelElement[] filterDuplicatePublicVars(Object parent,
+			IModelElement[] children) {
+		// public variables can only exist in ISourceModule
+		if (!(parent instanceof ISourceModule)) {
+			return children;
+		}
+		Set<IModelElement> result = new TreeSet<IModelElement>(
+				new Comparator<IModelElement>() {
+					public int compare(IModelElement o1, IModelElement o2) {
+						// filter duplications of variables
+						if (o1 instanceof IField
+								&& o2 instanceof IField
+								&& o1.getElementName().equals(
+										o2.getElementName())
+								&& o1.getParent().equals(o2.getParent())) {
+							return 0;
+						}
+						return 1;
+					}
+				});
+		for (int i = 0; i < children.length; i++) {
+			result.add(children[i]);
+		}
+		return result.toArray(new IModelElement[result.size()]);
+	}
+
+	public static boolean matches(IModelElement element) {
+		if (element.getElementType() == IModelElement.METHOD) {
+			String name = element.getElementName();
+			return (name != null && name.indexOf('<') >= 0);
+		}
+		// Filter out non-class variables:
+		if (element.getElementType() == IModelElement.FIELD) {
+			IField field = (IField) element;
+			try {
+				if ((field.getFlags() & Modifiers.AccConstant) != 0
+						|| (field instanceof UseStatement)) {
+					return false;
+				}
+			} catch (ModelException e) {
+			}
+			IModelElement parent = element.getParent();
+			if (parent != null) {
+				return (parent.getElementType() == IModelElement.METHOD);
+			}
+		}
+		if (element.getElementType() == IModelElement.IMPORT_CONTAINER) {
+			return true;
+		}
+		return false;
+	}
+}
