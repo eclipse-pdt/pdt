7fea048be303bde6c30a92def60fb7519eedda97
zzhongwei <zzhongwei>
2010-05-12 06:44:30 +0000

    Bug 241106	[regression] Background colour list pop-up in New/Edit Templates - Insert Variables is not bright yellow

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/PHPTemplatesPreferencePage.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/PHPTemplatesPreferencePage.java
@@ -11,10 +11,22 @@
  *******************************************************************************/
 package org.eclipse.php.internal.ui.preferences;
 
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.jface.text.source.SourceViewerConfiguration;
+import org.eclipse.jface.text.templates.ContextTypeRegistry;
+import org.eclipse.jface.text.templates.Template;
+import org.eclipse.jface.window.Window;
 import org.eclipse.php.internal.ui.IPHPHelpContextIds;
 import org.eclipse.php.internal.ui.PHPUiPlugin;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.templates.TemplatePreferencePage;
 
@@ -36,5 +48,48 @@ public class PHPTemplatesPreferencePage extends TemplatePreferencePage {
 	@Override
 	protected boolean isShowFormatterSetting() {
 		return false;
+	}
+
+	@Override
+	protected Template editTemplate(Template template, boolean edit,
+			boolean isNameModifiable) {
+		EditTemplateDialog dialog = new PHPEditTemplateDialog(getShell(),
+				template, edit, isNameModifiable, getContextTypeRegistry());
+		if (dialog.open() == Window.OK) {
+			return dialog.getTemplate();
+		}
+		return null;
+	}
+
+	protected static class PHPEditTemplateDialog extends EditTemplateDialog {
+
+		public PHPEditTemplateDialog(Shell parent, Template template,
+				boolean edit, boolean isNameModifiable,
+				ContextTypeRegistry registry) {
+			super(parent, template, edit, isNameModifiable, registry);
+		}
+
+		@Override
+		protected SourceViewer createViewer(Composite parent) {
+			SourceViewer viewer = new SourceViewer(parent, null, null, false,
+					SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+			SourceViewerConfiguration configuration = new SourceViewerConfiguration() {
+				public IContentAssistant getContentAssistant(
+						ISourceViewer sourceViewer) {
+
+					ContentAssistant assistant = new ContentAssistant();
+					assistant.enableAutoActivation(true);
+					assistant.enableAutoInsert(true);
+					assistant.setContentAssistProcessor(getTemplateProcessor(),
+							IDocument.DEFAULT_CONTENT_TYPE);
+					assistant.setProposalSelectorBackground(Display
+							.getCurrent().getSystemColor(
+									SWT.COLOR_INFO_BACKGROUND));
+					return assistant;
+				}
+			};
+			viewer.configure(configuration);
+			return viewer;
+		}
 	}
 }
