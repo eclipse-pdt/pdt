39ad1ef2d0c65bf5afad282ee4ca1aa07e6bcae7
rganor <rganor>
2010-05-19 16:39:03 +0000

    Bug 250941 - [type hierarchy] missing ovverides indicator in the method list
    contributed by Xu, thanks!

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/text/PHPOutlineInformationControl.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/text/PHPOutlineInformationControl.java
@@ -17,10 +17,16 @@ import org.eclipse.dltk.core.IType;
 import org.eclipse.dltk.core.ITypeHierarchy;
 import org.eclipse.dltk.core.ModelException;
 import org.eclipse.dltk.ui.text.ScriptOutlineInformationControl;
+import org.eclipse.dltk.ui.viewsupport.ScriptUILabelProvider;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.php.core.compiler.PHPFlags;
 import org.eclipse.php.internal.ui.PHPUiPlugin;
 import org.eclipse.php.internal.ui.corext.util.SuperTypeHierarchyCache;
+import org.eclipse.php.ui.OverrideIndicatorLabelDecorator;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IDecoratorManager;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * Information control for the PHP language
@@ -68,5 +74,17 @@ public class PHPOutlineInformationControl extends
 			}
 		}
 		return false;
+	}
+
+	protected TreeViewer createTreeViewer(Composite parent, int style) {
+		TreeViewer viewer = super.createTreeViewer(parent, style);
+
+		IDecoratorManager decoratorMgr = PlatformUI.getWorkbench()
+				.getDecoratorManager();
+		if (decoratorMgr.getEnabled("org.eclipse.php.ui.override.decorator")) {//$NON-NLS-1$
+			((ScriptUILabelProvider) viewer.getLabelProvider())
+					.addLabelDecorator(new OverrideIndicatorLabelDecorator());
+		}
+		return viewer;
 	}
 }
