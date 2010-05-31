14843ae1c9b0e3029c0f0711948e1c94901a8008
zzhongwei <zzhongwei>
2010-05-31 02:40:24 +0000

    add null check

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/corext/util/SuperTypeHierarchyCache.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/corext/util/SuperTypeHierarchyCache.java
@@ -118,7 +118,8 @@ public class SuperTypeHierarchyCache {
 			for (Iterator iter = fgMethodOverrideTesterCache.values()
 					.iterator(); iter.hasNext();) {
 				MethodOverrideTester curr = (MethodOverrideTester) iter.next();
-				if (curr != null && curr.getTypeHierarchy().equals(hierarchy)) {
+				if (curr != null && curr.getTypeHierarchy() != null
+						&& curr.getTypeHierarchy().equals(hierarchy)) {
 					iter.remove();
 				}
 			}
