93a1a87470925b4b8e3823dcc4877908f0cddecb
zzhongwei <zzhongwei>
2010-06-10 03:17:11 +0000

    *** empty log message ***

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/ast/nodes/LambdaFunctionDeclaration.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/ast/nodes/LambdaFunctionDeclaration.java
@@ -21,55 +21,65 @@ import org.eclipse.php.internal.core.ast.match.ASTMatcher;
 import org.eclipse.php.internal.core.ast.visitor.Visitor;
 
 /**
- * Represents a lambda function declaration
- * e.g.<pre>
+ * Represents a lambda function declaration e.g.
+ * 
+ * <pre>
  * function & (parameters) use (lexical vars) { body }
  * </pre>
+ * 
  * @see http://wiki.php.net/rfc/closures
  */
 public class LambdaFunctionDeclaration extends Expression {
 
 	private boolean isReference;
-	private final ASTNode.NodeList<FormalParameter> formalParameters = new ASTNode.NodeList<FormalParameter>(FORMAL_PARAMETERS_PROPERTY);
-	private final ASTNode.NodeList<Expression> lexicalVariables = new ASTNode.NodeList<Expression>(LEXICAL_VARIABLES_PROPERTY);
+	private final ASTNode.NodeList<FormalParameter> formalParameters = new ASTNode.NodeList<FormalParameter>(
+			FORMAL_PARAMETERS_PROPERTY);
+	private final ASTNode.NodeList<Expression> lexicalVariables = new ASTNode.NodeList<Expression>(
+			LEXICAL_VARIABLES_PROPERTY);
 	private Block body;
 
 	/**
 	 * The structural property of this node type.
 	 */
-	public static final SimplePropertyDescriptor IS_REFERENCE_PROPERTY = 
-		new SimplePropertyDescriptor(LambdaFunctionDeclaration.class, "isReference", Boolean.class, OPTIONAL); //$NON-NLS-1$
-	public static final ChildListPropertyDescriptor FORMAL_PARAMETERS_PROPERTY = 
-		new ChildListPropertyDescriptor(LambdaFunctionDeclaration.class, "formalParameters", FormalParameter.class, NO_CYCLE_RISK); //$NON-NLS-1$
-	public static final ChildListPropertyDescriptor LEXICAL_VARIABLES_PROPERTY = 
-		new ChildListPropertyDescriptor(LambdaFunctionDeclaration.class, "lexicalVariables", Expression.class, NO_CYCLE_RISK); //$NON-NLS-1$
-	public static final ChildPropertyDescriptor BODY_PROPERTY = 
-		new ChildPropertyDescriptor(LambdaFunctionDeclaration.class, "body", Block.class, OPTIONAL, CYCLE_RISK); //$NON-NLS-1$
-	
+	public static final SimplePropertyDescriptor IS_REFERENCE_PROPERTY = new SimplePropertyDescriptor(
+			LambdaFunctionDeclaration.class,
+			"isReference", Boolean.class, OPTIONAL); //$NON-NLS-1$
+	public static final ChildListPropertyDescriptor FORMAL_PARAMETERS_PROPERTY = new ChildListPropertyDescriptor(
+			LambdaFunctionDeclaration.class,
+			"formalParameters", FormalParameter.class, NO_CYCLE_RISK); //$NON-NLS-1$
+	public static final ChildListPropertyDescriptor LEXICAL_VARIABLES_PROPERTY = new ChildListPropertyDescriptor(
+			LambdaFunctionDeclaration.class,
+			"lexicalVariables", Expression.class, NO_CYCLE_RISK); //$NON-NLS-1$
+	public static final ChildPropertyDescriptor BODY_PROPERTY = new ChildPropertyDescriptor(
+			LambdaFunctionDeclaration.class,
+			"body", Block.class, OPTIONAL, CYCLE_RISK); //$NON-NLS-1$
+
 	/**
-	 * A list of property descriptors (element type: 
-	 * {@link StructuralPropertyDescriptor}),
-	 * or null if uninitialized.
+	 * A list of property descriptors (element type:
+	 * {@link StructuralPropertyDescriptor}), or null if uninitialized.
 	 */
 	private static final List<StructuralPropertyDescriptor> PROPERTY_DESCRIPTORS;
 	static {
-		List<StructuralPropertyDescriptor> propertyList = new ArrayList<StructuralPropertyDescriptor>(4);
+		List<StructuralPropertyDescriptor> propertyList = new ArrayList<StructuralPropertyDescriptor>(
+				4);
 		propertyList.add(IS_REFERENCE_PROPERTY);
 		propertyList.add(FORMAL_PARAMETERS_PROPERTY);
 		propertyList.add(LEXICAL_VARIABLES_PROPERTY);
 		propertyList.add(BODY_PROPERTY);
 		PROPERTY_DESCRIPTORS = Collections.unmodifiableList(propertyList);
-	}	
-	
-	public LambdaFunctionDeclaration(int start, int end, AST ast, List formalParameters, List lexicalVars, Block body, final boolean isReference) {
+	}
+
+	public LambdaFunctionDeclaration(int start, int end, AST ast,
+			List formalParameters, List lexicalVars, Block body,
+			final boolean isReference) {
 		super(start, end, ast);
-		
+
 		if (formalParameters == null) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		setIsReference(isReference);
-		
+
 		Iterator<FormalParameter> paramIt = formalParameters.iterator();
 		while (paramIt.hasNext()) {
 			this.formalParameters.add(paramIt.next());
@@ -81,7 +91,7 @@ public class LambdaFunctionDeclaration extends Expression {
 			}
 		}
 		if (body != null) {
-			setBody(body);	
+			setBody(body);
 		}
 	}
 
@@ -95,7 +105,7 @@ public class LambdaFunctionDeclaration extends Expression {
 			childrenAccept(visitor);
 		}
 		visitor.endVisit(this);
-	}	
+	}
 
 	public void childrenAccept(Visitor visitor) {
 		for (ASTNode node : this.formalParameters) {
@@ -146,14 +156,14 @@ public class LambdaFunctionDeclaration extends Expression {
 			buffer.append("\n"); //$NON-NLS-1$
 		}
 		buffer.append(TAB).append(tab).append("</FormalParameters>\n"); //$NON-NLS-1$
-		
+
 		buffer.append(TAB).append(tab).append("<LexicalVariables>\n"); //$NON-NLS-1$
 		for (ASTNode node : this.lexicalVariables) {
 			node.toString(buffer, TAB + TAB + tab);
 			buffer.append("\n"); //$NON-NLS-1$
 		}
 		buffer.append(TAB).append(tab).append("</LexicalVariables>\n"); //$NON-NLS-1$
-		
+
 		buffer.append(TAB).append(tab).append("<FunctionBody>\n"); //$NON-NLS-1$
 		if (body != null) {
 			body.toString(buffer, TAB + TAB + tab);
@@ -175,19 +185,21 @@ public class LambdaFunctionDeclaration extends Expression {
 	public Block getBody() {
 		return body;
 	}
-	
+
 	/**
 	 * Sets the body part of this function declaration
 	 * <p>
 	 * 
-	 * @param body of this function declaration 
-	 * @exception IllegalArgumentException if:
-	 * <ul>
-	 * <li>the node belongs to a different AST</li>
-	 * <li>the node already has a parent</li>
-	 * <li>a cycle in would be created</li>
-	 * </ul>
-	 */ 
+	 * @param body
+	 *            of this function declaration
+	 * @exception IllegalArgumentException
+	 *                if:
+	 *                <ul>
+	 *                <li>the node belongs to a different AST</li> <li>the node
+	 *                already has a parent</li> <li>a cycle in would be created
+	 *                </li>
+	 *                </ul>
+	 */
 	public void setBody(Block body) {
 		if (body == null) {
 			throw new IllegalArgumentException();
@@ -196,55 +208,59 @@ public class LambdaFunctionDeclaration extends Expression {
 		preReplaceChild(oldChild, body, BODY_PROPERTY);
 		this.body = body;
 		postReplaceChild(oldChild, body, BODY_PROPERTY);
-	}	
+	}
 
 	/**
 	 * List of the formal parameters of this function declaration
 	 * 
-	 * @return the parameters of this declaration   
+	 * @return the parameters of this declaration
 	 */
 	public List<FormalParameter> formalParameters() {
 		return this.formalParameters;
 	}
-	
+
 	/**
 	 * List of the lexical variables of this lambda function declaration
 	 * 
-	 * @return the lexical variables of this declaration 
+	 * @return the lexical variables of this declaration
 	 */
 	public List<Expression> lexicalVariables() {
 		return this.lexicalVariables;
 	}
-	
+
 	/**
-	 * True if this function's return variable will be referenced 
+	 * True if this function's return variable will be referenced
+	 * 
 	 * @return True if this function's return variable will be referenced
 	 */
 	public boolean isReference() {
 		return isReference;
 	}
-	
+
 	/**
 	 * Sets to true if this function's return variable will be referenced
 	 * 
-	 * @param value for referenced function return value 
-	 * @exception IllegalArgumentException if:
-	 * <ul>
-	 * <li>the node belongs to a different AST</li>
-	 * <li>the node already has a parent</li>
-	 * <li>a cycle in would be created</li>
-	 * </ul>
+	 * @param value
+	 *            for referenced function return value
+	 * @exception IllegalArgumentException
+	 *                if:
+	 *                <ul>
+	 *                <li>the node belongs to a different AST</li>
+	 *                <li>the node already has a parent</li>
+	 *                <li>a cycle in would be created</li>
+	 *                </ul>
 	 */
 	public final void setIsReference(boolean value) {
 		preValueChange(IS_REFERENCE_PROPERTY);
 		this.isReference = value;
 		postValueChange(IS_REFERENCE_PROPERTY);
 	}
-	
-	/* (omit javadoc for this method)
-	 * Method declared on ASTNode.
+
+	/*
+	 * (omit javadoc for this method) Method declared on ASTNode.
 	 */
-	final boolean internalGetSetBooleanProperty(SimplePropertyDescriptor property, boolean get, boolean value) {
+	final boolean internalGetSetBooleanProperty(
+			SimplePropertyDescriptor property, boolean get, boolean value) {
 		if (property == IS_REFERENCE_PROPERTY) {
 			if (get) {
 				return isReference();
@@ -256,9 +272,9 @@ public class LambdaFunctionDeclaration extends Expression {
 		// allow default implementation to flag the error
 		return super.internalGetSetBooleanProperty(property, get, value);
 	}
-	
-	
-	final ASTNode internalGetSetChildProperty(ChildPropertyDescriptor property, boolean get, ASTNode child) {
+
+	final ASTNode internalGetSetChildProperty(ChildPropertyDescriptor property,
+			boolean get, ASTNode child) {
 		if (property == BODY_PROPERTY) {
 			if (get) {
 				return getBody();
@@ -270,16 +286,19 @@ public class LambdaFunctionDeclaration extends Expression {
 		// allow default implementation to flag the error
 		return super.internalGetSetChildProperty(property, get, child);
 	}
-	
+
 	final List internalGetChildListProperty(ChildListPropertyDescriptor property) {
 		if (property == FORMAL_PARAMETERS_PROPERTY) {
 			return formalParameters();
 		}
+		if (property == LEXICAL_VARIABLES_PROPERTY) {
+			return lexicalVariables();
+		}
 		// allow default implementation to flag the error
 		return super.internalGetChildListProperty(property);
-	}	
-	
-	/* 
+	}
+
+	/*
 	 * Method declared on ASTNode.
 	 */
 	public boolean subtreeMatch(ASTMatcher matcher, Object other) {
@@ -290,16 +309,21 @@ public class LambdaFunctionDeclaration extends Expression {
 	@Override
 	ASTNode clone0(AST target) {
 		final Block body = ASTNode.copySubtree(target, getBody());
-		final List formalParams = ASTNode.copySubtrees(target, formalParameters());
-		final List lexicalVars = ASTNode.copySubtrees(target, lexicalVariables());
+		final List formalParams = ASTNode.copySubtrees(target,
+				formalParameters());
+		final List lexicalVars = ASTNode.copySubtrees(target,
+				lexicalVariables());
 		final boolean isRef = isReference();
-		
-		final LambdaFunctionDeclaration result = new LambdaFunctionDeclaration(getStart(), getEnd(), target, formalParams, lexicalVars, body, isRef);
+
+		final LambdaFunctionDeclaration result = new LambdaFunctionDeclaration(
+				getStart(), getEnd(), target, formalParams, lexicalVars, body,
+				isRef);
 		return result;
 	}
 
 	@Override
-	List<StructuralPropertyDescriptor> internalStructuralPropertiesForType(PHPVersion apiLevel) {
+	List<StructuralPropertyDescriptor> internalStructuralPropertiesForType(
+			PHPVersion apiLevel) {
 		return PROPERTY_DESCRIPTORS;
 	}
 }
