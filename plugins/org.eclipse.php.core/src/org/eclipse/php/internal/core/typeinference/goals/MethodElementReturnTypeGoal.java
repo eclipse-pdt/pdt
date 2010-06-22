d866250b967167122fea48121592f15c0f1226fc
qiwang <qiwang>
2010-06-22 10:31:40 +0000

    1. Rewrote the equals method of MethodElementReturnTypeGoal. Old equals method causes state issue of GoalEngine.
    Port back the fixing in head to branch.

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/goals/MethodElementReturnTypeGoal.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/goals/MethodElementReturnTypeGoal.java
@@ -1,20 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
 package org.eclipse.php.internal.core.typeinference.goals;
 
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.dltk.core.IMethod;
-import org.eclipse.dltk.ti.goals.AbstractTypeGoal;
+import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.ti.IContext;
+import org.eclipse.dltk.ti.types.IEvaluatedType;
 
-public class MethodElementReturnTypeGoal extends AbstractTypeGoal {
-	
-	private final IMethod methodElement;
+public class MethodElementReturnTypeGoal extends AbstractMethodReturnTypeGoal {
 
-	public IMethod getMethod() {
-		return methodElement;
+	public MethodElementReturnTypeGoal(IContext context,
+			IEvaluatedType evaluatedType, String methodName) {
+		super(context, evaluatedType, methodName);
 	}
 
-	public MethodElementReturnTypeGoal(IMethod methodElement) {
-		super(null);
-		Assert.isNotNull(methodElement);
-		this.methodElement = methodElement;
+	public MethodElementReturnTypeGoal(IContext context, IType[] types,
+			String methodName) {
+		super(context, types, methodName);
+	}
+
+	public boolean equals(Object obj) {
+		if (!(obj instanceof MethodElementReturnTypeGoal)) {
+			return false;
+		}
+		return this == obj;
 	}
 }
