579168309c9bed35d388cb909f583e6a134e010f
qiwang <qiwang>
2010-07-08 07:59:23 +0000

    318133 Supporting the constants from PHP language model. Suport parent:: accessing correctly.

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/ClassFieldsStrategy.java
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
+package org.eclipse.php.internal.core.codeassist.strategies;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.dltk.core.CompletionRequestor;
+import org.eclipse.dltk.core.IField;
+import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.ITypeHierarchy;
+import org.eclipse.dltk.internal.core.SourceRange;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.php.core.codeassist.ICompletionContext;
+import org.eclipse.php.core.codeassist.IElementFilter;
+import org.eclipse.php.internal.core.PHPCorePlugin;
+import org.eclipse.php.internal.core.codeassist.ICompletionReporter;
+import org.eclipse.php.internal.core.codeassist.contexts.ClassMemberContext;
+import org.eclipse.php.internal.core.codeassist.contexts.ClassMemberContext.Trigger;
+import org.eclipse.php.internal.core.codeassist.contexts.ClassStaticMemberContext;
+import org.eclipse.php.internal.core.typeinference.PHPModelUtils;
+
+/**
+ * This strategy completes class constants and variables.
+ * 
+ * @author michael
+ */
+public class ClassFieldsStrategy extends ClassMembersStrategy {
+
+	public ClassFieldsStrategy(ICompletionContext context,
+			IElementFilter elementFilter) {
+		super(context, elementFilter);
+	}
+
+	public ClassFieldsStrategy(ICompletionContext context) {
+		super(context);
+	}
+
+	public void apply(ICompletionReporter reporter) throws BadLocationException {
+		ICompletionContext context = getContext();
+		if (!(context instanceof ClassMemberContext)) {
+			return;
+		}
+
+		ClassMemberContext concreteContext = (ClassMemberContext) context;
+		CompletionRequestor requestor = concreteContext
+				.getCompletionRequestor();
+
+		String prefix = concreteContext.getPrefix();
+		SourceRange replaceRange = getReplacementRange(concreteContext);
+
+		List<IField> result = new LinkedList<IField>();
+		for (IType type : concreteContext.getLhsTypes()) {
+			try {
+				ITypeHierarchy hierarchy = getCompanion()
+						.getSuperTypeHierarchy(type, null);
+				IField[] fields = null;
+
+				if (concreteContext instanceof ClassStaticMemberContext
+						&& concreteContext.getTriggerType() == Trigger.CLASS
+						&& ((ClassStaticMemberContext) concreteContext)
+								.isParent()) {
+					List<IField> superTypes = new ArrayList<IField>();
+					for (IType currType : hierarchy.getAllSupertypes(type)) {
+						superTypes.addAll(Arrays.asList(PHPModelUtils
+								.getTypeField(currType, prefix, requestor
+										.isContextInformationMode())));
+					}
+
+					fields = new IField[superTypes.size()];
+					fields = superTypes.toArray(fields);
+				} else {
+					fields = PHPModelUtils.getTypeHierarchyField(type,
+							hierarchy, prefix, requestor
+									.isContextInformationMode(), null);
+				}
+
+				for (IField field : removeOverriddenElements(Arrays
+						.asList(fields))) {
+					if (!isFiltered(field, concreteContext)) {
+						result.add(field);
+					}
+				}
+			} catch (CoreException e) {
+				PHPCorePlugin.log(e);
+			}
+		}
+		for (IField field : result) {
+			reporter.reportField(field, getSuffix(), replaceRange,
+					concreteContext.getTriggerType() == Trigger.OBJECT);
+		}
+	}
+
+	protected boolean showNonStaticMembers(ClassMemberContext context) {
+		return super.showNonStaticMembers(context) && !isParentCall(context);
+	}
+
+	public String getSuffix() {
+		return "";
+	}
+}
---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/GlobalConstantsStrategy.java
@@ -0,0 +1,152 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
+package org.eclipse.php.internal.core.codeassist.strategies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.dltk.ast.Modifiers;
+import org.eclipse.dltk.core.*;
+import org.eclipse.dltk.core.index2.search.ISearchEngine.MatchRule;
+import org.eclipse.dltk.core.search.IDLTKSearchScope;
+import org.eclipse.dltk.core.search.SearchEngine;
+import org.eclipse.dltk.internal.core.SourceRange;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.php.core.codeassist.ICompletionContext;
+import org.eclipse.php.core.codeassist.IElementFilter;
+import org.eclipse.php.internal.core.PHPCoreConstants;
+import org.eclipse.php.internal.core.PHPCorePlugin;
+import org.eclipse.php.internal.core.codeassist.ICompletionReporter;
+import org.eclipse.php.internal.core.codeassist.contexts.AbstractCompletionContext;
+import org.eclipse.php.internal.core.model.PhpModelAccess;
+
+/**
+ * This strategy completes global constants
+ * 
+ * @author michael
+ */
+public class GlobalConstantsStrategy extends GlobalElementStrategy {
+
+	public GlobalConstantsStrategy(ICompletionContext context,
+			IElementFilter elementFilter) {
+		super(context, elementFilter);
+	}
+
+	public GlobalConstantsStrategy(ICompletionContext context) {
+		super(context);
+	}
+
+	public void apply(ICompletionReporter reporter) throws BadLocationException {
+		ICompletionContext context = getContext();
+
+		AbstractCompletionContext abstractContext = (AbstractCompletionContext) context;
+		CompletionRequestor requestor = abstractContext
+				.getCompletionRequestor();
+
+		String prefix = abstractContext.getPrefix();
+		if (prefix.startsWith("$")) {
+			return;
+		}
+
+		MatchRule matchRule = MatchRule.PREFIX;
+		if (requestor.isContextInformationMode()) {
+			matchRule = MatchRule.EXACT;
+		}
+
+		ISourceModule sourceModule = abstractContext.getSourceModule();
+
+		IType enclosingType = null;
+		try {
+			IModelElement enclosingElement = sourceModule
+					.getElementAt(abstractContext.getOffset());
+
+			if (enclosingElement != null && enclosingElement instanceof IType) {
+				enclosingType = (IType) enclosingElement;
+			}
+
+		} catch (ModelException e) {
+			PHPCorePlugin.log(e);
+		}
+
+		IDLTKSearchScope scope = null;
+		IModelElement[] enclosingTypeConstants = null;
+
+		if (enclosingType != null
+				&& isStartOfStatement(prefix, abstractContext, abstractContext
+						.getOffset())) {
+			// See the case of testClassStatement1.pdtt and
+			// testClassStatement2.pdtt
+			scope = SearchEngine.createSearchScope(enclosingType);
+		} else {
+			scope = getSearchScope(abstractContext);
+		}
+
+		enclosingTypeConstants = PhpModelAccess.getDefault().findFields(prefix,
+				matchRule, Modifiers.AccConstant, 0, scope, null);
+
+		if (isCaseSensitive()) {
+			enclosingTypeConstants = filterByCase(enclosingTypeConstants,
+					prefix);
+		}
+		// workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=310383
+		enclosingTypeConstants = filterClassConstants(enclosingTypeConstants);
+		// workaround end
+		SourceRange replaceRange = getReplacementRange(abstractContext);
+		for (IModelElement constant : enclosingTypeConstants) {
+			reporter.reportField((IField) constant, "", replaceRange, false);
+		}
+
+	}
+
+	private boolean isStartOfStatement(String prefix,
+			AbstractCompletionContext abstractContext, int offset) {
+		IDocument doc = abstractContext.getDocument();
+		try {
+			int pos = offset - prefix.length() - 1;
+			char previousChar = doc.getChar(pos);
+
+			while (Character.isWhitespace(previousChar)) {
+				pos--;
+				previousChar = doc.getChar(pos);
+			}
+
+			return previousChar == '{' || previousChar == ';';
+		} catch (BadLocationException e) {
+		}
+
+		return false;
+	}
+
+	private IModelElement[] filterClassConstants(IModelElement[] elements) {
+		List<IModelElement> result = new ArrayList<IModelElement>(
+				elements.length);
+		for (IModelElement element : elements) {
+			try {
+				if ((((IField) element).getFlags() & PHPCoreConstants.AccClassField) == 0) {
+					result.add(element);
+				}
+			} catch (ModelException e) {
+				PHPCorePlugin.log(e);
+			}
+		}
+		return (IModelElement[]) result
+				.toArray(new IModelElement[result.size()]);
+	}
+
+	private IDLTKSearchScope getSearchScope(
+			AbstractCompletionContext abstractContext) {
+		IDLTKSearchScope scope = createSearchScope();
+		return scope;
+	}
+
+}
