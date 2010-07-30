a19d3e79aeebb55c53aa09e7495b23e1387274c6
zzhongwei <zzhongwei>
2010-07-30 08:35:57 +0000

    Summary:Code assist inside a doc block starts a method call  Description:Expected result:         /**          *          * @return Offers_Model_Object_Offer          */                 Actual Result:         /**          *          * @return Offers_Model_Object_Offer::          */

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/PHPDocReturnTypeStrategy.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
+package org.eclipse.php.internal.core.codeassist.strategies;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.php.core.codeassist.ICompletionContext;
+import org.eclipse.php.internal.core.codeassist.ICompletionReporter;
+import org.eclipse.php.internal.core.codeassist.ProposalExtraInfo;
+import org.eclipse.php.internal.core.codeassist.contexts.PHPDocTagContext;
+
+/**
+ * This strategy completes variable names in 'param' PHPDoc tag
+ * 
+ * @author michael
+ */
+public class PHPDocReturnTypeStrategy extends GlobalClassesStrategy {
+
+	public PHPDocReturnTypeStrategy(ICompletionContext context) {
+		super(context);
+	}
+
+	public void apply(ICompletionReporter reporter) throws BadLocationException {
+		ICompletionContext context = getContext();
+		if (!(context instanceof PHPDocTagContext)) {
+			return;
+		}
+		super.apply(reporter);
+	}
+
+	@Override
+	protected Object getExtraInfo() {
+		return ProposalExtraInfo.TYPE_ONLY;
+	}
+}
