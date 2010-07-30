05c690a6c32a0c8acfad6d42210a7e9f6ccb0b5e
zzhongwei <zzhongwei>
2010-07-30 06:16:39 +0000

    Bug 253921 -  [regression] The text in Code Assist description window is not completed - Text for Value and Description is missing

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/compiler/ast/nodes/PHPModuleDeclaration.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/compiler/ast/nodes/PHPModuleDeclaration.java
@@ -1,25 +1,105 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
 package org.eclipse.php.internal.core.compiler.ast.nodes;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
+import org.eclipse.dltk.ast.ASTNode;
 import org.eclipse.dltk.ast.ASTVisitor;
+import org.eclipse.dltk.ast.declarations.MethodDeclaration;
 import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
+import org.eclipse.dltk.ast.declarations.TypeDeclaration;
 import org.eclipse.dltk.ast.statements.Statement;
+import org.eclipse.dltk.core.DLTKCore;
 import org.eclipse.dltk.utils.CorePrinter;
 import org.eclipse.php.internal.core.compiler.ast.visitor.ASTPrintVisitor;
 
 public class PHPModuleDeclaration extends ModuleDeclaration {
 
 	private List<ASTError> errors;
-	private boolean hasErros;
+	private boolean hasErrors;
+	private List<VarComment> varComments;
+	private List<PHPDocBlock> phpDocBlocks = new LinkedList<PHPDocBlock>();
 
-	public PHPModuleDeclaration(int start, int end, List<Statement> statements, List<ASTError> errors) {
-		super(end - start);
+	public PHPModuleDeclaration(int start, int end, List<Statement> statements,
+			List<ASTError> errors, List<VarComment> varComments) {
+		super(end - start, true);
 		setStatements(statements);
 		setStart(start);
 		setEnd(end);
 		this.errors = errors;
+		this.varComments = varComments;
+	}
+
+	/**
+	 * This method goes over the AST and builds a list of types and methods
+	 * declared in this file
+	 */
+	@SuppressWarnings("unchecked")
+	public void doRebuild() {
+		List statements = getStatements();
+		if (statements != null) {
+			Iterator i = statements.iterator();
+			while (i.hasNext()) {
+				final ASTNode node = (ASTNode) i.next();
+				try {
+					node.traverse(new ASTVisitor() {
+						private Stack<ASTNode> parentStack = new Stack<ASTNode>();
+
+						public boolean visit(MethodDeclaration s)
+								throws Exception {
+							if (s != node
+									&& (parentStack.isEmpty() || !(parentStack
+											.peek() instanceof TypeDeclaration))) {
+								getFunctionList().add(s);
+							}
+							return super.visit(s);
+						}
+
+						public boolean visit(TypeDeclaration s)
+								throws Exception {
+							if (s instanceof NamespaceDeclaration
+									&& ((NamespaceDeclaration) s).isGlobal()) {
+								return super.visit(s);
+							}
+							parentStack.add(s);
+							getTypeList().add(s);
+							return super.visit(s);
+						}
+
+						public boolean endvisit(TypeDeclaration s)
+								throws Exception {
+							if (s instanceof NamespaceDeclaration
+									&& ((NamespaceDeclaration) s).isGlobal()) {
+								return super.endvisit(s);
+							}
+							parentStack.pop();
+							return super.endvisit(s);
+						}
+					});
+				} catch (Exception e) {
+					if (DLTKCore.DEBUG) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+
+	public void addStatement(Statement statement) {
+		super.addStatement(statement);
 	}
 
 	/**
@@ -33,8 +113,9 @@ public class PHPModuleDeclaration extends ModuleDeclaration {
 	}
 
 	/**
-	 * due to the nature of the parser and the error recovery method not all errors can be added to the 
-	 * AST as statements, the error list is made for those errors.
+	 * due to the nature of the parser and the error recovery method not all
+	 * errors can be added to the AST as statements, the error list is made for
+	 * those errors.
 	 */
 	public List<ASTError> getErrors() {
 		return errors;
@@ -51,15 +132,26 @@ public class PHPModuleDeclaration extends ModuleDeclaration {
 		return errorsList;
 	}
 
-	public boolean hasErros() {
-		return hasErros || !errors.isEmpty();
+	public boolean hasErrors() {
+		return hasErrors || !errors.isEmpty();
 	}
 
-	public void setHasErros(boolean hasErros) {
-		this.hasErros = hasErros;
+	/**
+	 * This method fires a flag that says that this file has compilation errors
+	 */
+	public void setHasErrors(boolean hasErrors) {
+		this.hasErrors = hasErrors;
 	}
-	
-	private class ErrorSearcher extends ASTVisitor{
+
+	public List<VarComment> getVarComments() {
+		return varComments;
+	}
+
+	public List<PHPDocBlock> getPhpDocBlocks() {
+		return phpDocBlocks;
+	}
+
+	private class ErrorSearcher extends ASTVisitor {
 		private List<ASTError> errors = new LinkedList<ASTError>();
 
 		public boolean visit(ASTError error) throws Exception {
@@ -68,8 +160,8 @@ public class PHPModuleDeclaration extends ModuleDeclaration {
 		}
 
 		public boolean visit(Statement s) throws Exception {
-			if(s.getKind() == ASTNodeKinds.AST_ERROR){
-				return visit((ASTError)s);
+			if (s.getKind() == ASTNodeKinds.AST_ERROR) {
+				return visit((ASTError) s);
 			}
 			return super.visit(s);
 		}
