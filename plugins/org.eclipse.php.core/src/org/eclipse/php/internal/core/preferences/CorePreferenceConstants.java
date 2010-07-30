81c6335dc07b95e93c32b6557688cb75c6b2716f
zzhongwei <zzhongwei>
2010-07-30 06:22:40 +0000

    add a checkbox option for user to choose if they want to add parameters for method.

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/CorePreferenceConstants.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/CorePreferenceConstants.java
@@ -1,53 +1,69 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.core.preferences;
 
-import java.util.Locale;
-
-import org.eclipse.core.runtime.Preferences;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.php.internal.core.PHPCoreConstants;
 import org.eclipse.php.internal.core.PHPCorePlugin;
+import org.eclipse.php.internal.core.PHPVersion;
+
+import com.ibm.icu.util.ULocale;
 
 public class CorePreferenceConstants {
 
 	public interface Keys {
 		public static final String PHP_VERSION = PHPCoreConstants.PHP_OPTIONS_PHP_VERSION;
-		public static final String EDITOR_USE_ASP_TAGS= "use_asp_tags_as_php"; //$NON-NLS-1$
-	}
-
-	public static Preferences getPreferenceStore() {
-		return PHPCorePlugin.getDefault().getPluginPreferences();
+		public static final String EDITOR_USE_ASP_TAGS = "use_asp_tags_as_php"; //$NON-NLS-1$
 	}
 
 	/**
 	 * Initializes the given preference store with the default values.
-	 *
-	 * @param store the preference store to be initialized
+	 * 
+	 * @param store
+	 *            the preference store to be initialized
 	 */
 	public static void initializeDefaultValues() {
-		Preferences store = getPreferenceStore();
-		store.setDefault(Keys.PHP_VERSION, PHPCoreConstants.PHP5);
+		IEclipsePreferences node = new DefaultScope().getNode(PHPCorePlugin.ID);
 
-		store.setDefault(PHPCoreConstants.TASK_TAGS, PHPCoreConstants.DEFAULT_TASK_TAGS);
-		store.setDefault(PHPCoreConstants.TASK_PRIORITIES, PHPCoreConstants.DEFAULT_TASK_PRIORITIES);
-		store.setDefault(PHPCoreConstants.TASK_CASE_SENSITIVE, PHPCoreConstants.ENABLED);
-		store.setDefault(Keys.EDITOR_USE_ASP_TAGS, false);
-
-		store.setDefault(PHPCoreConstants.FORMATTER_USE_TABS, true);
-		store.setDefault(PHPCoreConstants.FORMATTER_INDENTATION_SIZE, PHPCoreConstants.DEFAULT_INDENTATION_SIZE);
-
-		if ((store.getString(PHPCoreConstants.WORKSPACE_DEFAULT_LOCALE)).equals("")) { //$NON-NLS-1$
-			store.setValue(PHPCoreConstants.WORKSPACE_DEFAULT_LOCALE,Locale.getDefault().toString());
-			store.setDefault(PHPCoreConstants.WORKSPACE_LOCALE, Locale.getDefault().toString());
-		}
+		node.put(Keys.PHP_VERSION, PHPVersion.PHP5.getAlias());
+		node.put(PHPCoreConstants.TASK_TAGS, PHPCoreConstants.DEFAULT_TASK_TAGS);
+		node.put(PHPCoreConstants.TASK_PRIORITIES,
+				PHPCoreConstants.DEFAULT_TASK_PRIORITIES);
+		node.put(PHPCoreConstants.TASK_CASE_SENSITIVE, PHPCoreConstants.ENABLED);
+		node.putBoolean(Keys.EDITOR_USE_ASP_TAGS, false);
+		node.putBoolean(PHPCoreConstants.FORMATTER_USE_TABS, true);
+		node.put(PHPCoreConstants.FORMATTER_INDENTATION_SIZE,
+				PHPCoreConstants.DEFAULT_INDENTATION_SIZE);
+		node.putBoolean(PHPCoreConstants.CODEGEN_ADD_COMMENTS, false);
+		node.put(PHPCoreConstants.WORKSPACE_DEFAULT_LOCALE, ULocale
+				.getDefault().toString());
+		node.put(PHPCoreConstants.WORKSPACE_LOCALE, ULocale.getDefault()
+				.toString());
+		node.putBoolean(PHPCoreConstants.CODEASSIST_ADDIMPORT, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_FILL_ARGUMENT_NAMES, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_GUESS_METHOD_ARGUMENTS,
+				true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_AUTOINSERT, true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_INSERT_COMPLETION, true);
+		node.putBoolean(
+				PHPCoreConstants.CODEASSIST_SHOW_VARIABLES_FROM_OTHER_FILES,
+				false);
+		node.putBoolean(
+				PHPCoreConstants.CODEASSIST_INSERT_FULL_QUALIFIED_NAME_FOR_NAMESPACE,
+				true);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_SHOW_STRICT_OPTIONS, false);
+		node.putBoolean(PHPCoreConstants.CODEASSIST_AUTOACTIVATION, true);
+		node.putInt(PHPCoreConstants.CODEASSIST_AUTOACTIVATION_DELAY, 200);
 	}
 
 	// Don't instantiate
