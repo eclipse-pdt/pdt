9d5c4b039f1dad3818d276461f571bded847bb64
zzhongwei <zzhongwei>
2010-07-30 07:02:22 +0000

    318888: [Content Assist] no content assist for special methods https://bugs.eclipse.org/bugs/show_bug.cgi?id=318888

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/PHPUIPreferenceInitializer.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/PHPUIPreferenceInitializer.java
@@ -1,31 +1,34 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.ui.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.dltk.ui.DLTKUIPlugin;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.editors.text.EditorsUI;
 
-;
-
 public class PHPUIPreferenceInitializer extends AbstractPreferenceInitializer {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
-	 */
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = PreferenceConstants.getPreferenceStore();
 		EditorsUI.useAnnotationsPreferencePage(store);
 		EditorsUI.useQuickDiffPreferencePage(store);
 		PreferenceConstants.initializeDefaultValues();
+
+		IPreferenceStore dltkStore = DLTKUIPlugin.getDefault()
+				.getPreferenceStore();
+		dltkStore.setDefault(
+				org.eclipse.dltk.ui.PreferenceConstants.CODEASSIST_SORTER,
+				"org.eclipse.php.ui.AlphabeticSorter"); //$NON-NLS-1$
 	}
 
 }
