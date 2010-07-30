dd78c567502876891a3b4032d0860100c209782f
zzhongwei <zzhongwei>
2010-07-30 06:50:51 +0000

    move 'use short tags' preference from ui to core

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/autoEdit/TypingPreferences.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/autoEdit/TypingPreferences.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.ui.autoEdit;
 
@@ -28,45 +29,64 @@ public class TypingPreferences {
 	static boolean closePhpdoc;
 	static boolean addDocTags;
 	static boolean addPhpCloseTag;
-
+	// static boolean useShortTags;
 	static {
 		IPreferenceStore store = PHPUiPlugin.getDefault().getPreferenceStore();
 
-		closeQuotes = store.getBoolean(PreferenceConstants.EDITOR_CLOSE_STRINGS);
-		closeBrackets = store.getBoolean(PreferenceConstants.EDITOR_CLOSE_BRACKETS);
-		closeCurlyBracket = store.getBoolean(PreferenceConstants.EDITOR_CLOSE_BRACES);
-		closePhpdoc = store.getBoolean(PreferenceConstants.EDITOR_CLOSE_PHPDOCS_AND_COMMENTS);
-		addDocTags = store.getBoolean(PreferenceConstants.EDITOR_ADD_PHPDOC_TAGS);
-		addPhpCloseTag = store.getBoolean(PreferenceConstants.EDITOR_ADD_PHPCLOSE_TAGS);
-
+		closeQuotes = store
+				.getBoolean(PreferenceConstants.EDITOR_CLOSE_STRINGS);
+		closeBrackets = store
+				.getBoolean(PreferenceConstants.EDITOR_CLOSE_BRACKETS);
+		closeCurlyBracket = store
+				.getBoolean(PreferenceConstants.EDITOR_CLOSE_BRACES);
+		closePhpdoc = store
+				.getBoolean(PreferenceConstants.EDITOR_CLOSE_PHPDOCS_AND_COMMENTS);
+		addDocTags = store
+				.getBoolean(PreferenceConstants.EDITOR_ADD_PHPDOC_TAGS);
+		addPhpCloseTag = store
+				.getBoolean(PreferenceConstants.EDITOR_ADD_PHPCLOSE_TAGS);
+		// useShortTags = store
+		// .getBoolean(PreferenceConstants.EDITOR_USE_SHORT_TAGS);
 		store.addPropertyChangeListener(new IPropertyChangeListener() {
 
 			public void propertyChange(PropertyChangeEvent event) {
 				String property = event.getProperty();
 				if (property == PreferenceConstants.EDITOR_CLOSE_STRINGS) {
-					closeQuotes = Boolean.valueOf((String) event.getNewValue()).booleanValue();
+					closeQuotes = Boolean.valueOf((String) event.getNewValue())
+							.booleanValue();
 					return;
 				}
 				if (property == PreferenceConstants.EDITOR_CLOSE_BRACKETS) {
-					closeBrackets = Boolean.valueOf((String) event.getNewValue()).booleanValue();
+					closeBrackets = Boolean.valueOf(
+							(String) event.getNewValue()).booleanValue();
 					return;
 				}
 				if (property == PreferenceConstants.EDITOR_CLOSE_BRACES) {
-					closeCurlyBracket = Boolean.valueOf((String) event.getNewValue()).booleanValue();
+					closeCurlyBracket = Boolean.valueOf(
+							(String) event.getNewValue()).booleanValue();
 					return;
 				}
 				if (property == PreferenceConstants.EDITOR_CLOSE_PHPDOCS_AND_COMMENTS) {
-					closePhpdoc = Boolean.valueOf((String) event.getNewValue()).booleanValue();
+					closePhpdoc = Boolean.valueOf((String) event.getNewValue())
+							.booleanValue();
 					return;
 				}
 				if (property == PreferenceConstants.EDITOR_ADD_PHPDOC_TAGS) {
-					addDocTags = Boolean.valueOf((String) event.getNewValue()).booleanValue();
+					addDocTags = Boolean.valueOf((String) event.getNewValue())
+							.booleanValue();
 					return;
 				}
 				if (property == PreferenceConstants.EDITOR_ADD_PHPCLOSE_TAGS) {
-					addPhpCloseTag = Boolean.valueOf((String) event.getNewValue()).booleanValue();
+					addPhpCloseTag = Boolean.valueOf(
+							(String) event.getNewValue()).booleanValue();
 					return;
 				}
+				// if (property == PreferenceConstants.EDITOR_USE_SHORT_TAGS) {
+				// useShortTags = Boolean
+				// .valueOf((String) event.getNewValue())
+				// .booleanValue();
+				// return;
+				// }
 			}
 		});
 	}
