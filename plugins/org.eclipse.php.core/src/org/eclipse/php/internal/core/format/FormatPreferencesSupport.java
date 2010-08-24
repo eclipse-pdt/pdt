9138f8c6a63408fda057ae9003a02c2e30edb508
qiwang <qiwang>
2010-08-24 04:42:41 +0000

    The PHPMergeViewer can be used outside Editor. E.g. the preview page. In those cases, the editroModel is null. Do the check and return in null case.

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/format/FormatPreferencesSupport.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/format/FormatPreferencesSupport.java
@@ -1,30 +1,33 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.core.format;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Preferences;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.php.internal.core.PHPCoreConstants;
 import org.eclipse.php.internal.core.PHPCorePlugin;
 import org.eclipse.php.internal.core.documentModel.DOMModelForPHP;
-import org.eclipse.php.internal.core.phpModel.parser.PHPProjectModel;
-import org.eclipse.php.internal.core.phpModel.parser.PHPWorkspaceModelManager;
 import org.eclipse.php.internal.core.preferences.*;
 import org.eclipse.wst.sse.core.StructuredModelManager;
 
 /**
- *
+ * 
  * @author guy.g
- *
+ * 
  */
 public class FormatPreferencesSupport {
 
@@ -42,11 +45,13 @@ public class FormatPreferencesSupport {
 	private PreferencesPropagator preferencesPropagator;
 
 	private static final String NODES_QUALIFIER = PHPCorePlugin.ID;
-	private static final Preferences store = PHPCorePlugin.getDefault().getPluginPreferences();
+	private static final Preferences store = PHPCorePlugin.getDefault()
+			.getPluginPreferences();
 
 	private FormatPreferencesSupport() {
 
-		preferencesPropagator = PreferencePropagatorFactory.getInstance().getPreferencePropagator(NODES_QUALIFIER, store);
+		preferencesPropagator = PreferencePropagatorFactory
+				.getPreferencePropagator(NODES_QUALIFIER, store);
 		preferencesSupport = new PreferencesSupport(PHPCorePlugin.ID, store);
 	}
 
@@ -61,7 +66,8 @@ public class FormatPreferencesSupport {
 
 	public int getIndentationSize(IDocument document) {
 		if (document == null) {
-			String indentSize = preferencesSupport.getWorkspacePreferencesValue(PHPCoreConstants.FORMATTER_INDENTATION_SIZE);
+			String indentSize = preferencesSupport
+					.getWorkspacePreferencesValue(PHPCoreConstants.FORMATTER_INDENTATION_SIZE);
 			if (indentSize == null) {
 				return 1;
 			}
@@ -73,7 +79,8 @@ public class FormatPreferencesSupport {
 
 	public char getIndentationChar(IDocument document) {
 		if (document == null) {
-			String useTab = preferencesSupport.getWorkspacePreferencesValue(PHPCoreConstants.FORMATTER_USE_TABS);
+			String useTab = preferencesSupport
+					.getWorkspacePreferencesValue(PHPCoreConstants.FORMATTER_USE_TABS);
 			if (useTab == null) {
 				return '\t';
 			}
@@ -87,11 +94,35 @@ public class FormatPreferencesSupport {
 		if (fLastDocument != document) {
 			DOMModelForPHP editorModel = null;
 			try {
-				editorModel = (DOMModelForPHP) StructuredModelManager.getModelManager().getExistingModelForRead(document);
-				PHPProjectModel projectModel = editorModel.getProjectModel();
-				if (projectModel == null)
+				editorModel = (DOMModelForPHP) StructuredModelManager
+						.getModelManager().getExistingModelForRead(document);
+
+				// The PHPMergeViewer can be used outside Editor.
+				// E.g. the preview page.
+				// In those cases, the editroModel is null.
+				// Do the check and return in null case.
+				if (editorModel == null) {
 					return;
-				IProject project = PHPWorkspaceModelManager.getInstance().getProjectForModel(projectModel);
+				}
+
+				String baseLocation = editorModel.getBaseLocation();
+				// The baseLocation may be a path on disk or relative to the
+				// workspace root. Don't translate on-disk paths to
+				// in-workspace resources.
+				IPath basePath = new Path(baseLocation);
+				IFile file = null;
+				if (basePath.segmentCount() > 1) {
+					file = ResourcesPlugin.getWorkspace().getRoot().getFile(
+							basePath);
+					if (!file.exists()) {
+						file = null;
+					}
+				}
+				if (file == null) {
+					return;
+				}
+
+				IProject project = file.getProject();
 				if (fLastProject != project) {
 					fLastProject = project;
 					verifyListening();
@@ -103,10 +134,14 @@ public class FormatPreferencesSupport {
 		}
 
 		if (fLastDocument != document || preferencesChanged) {
-			String useTab = preferencesSupport.getPreferencesValue(PHPCoreConstants.FORMATTER_USE_TABS, null, fLastProject);
-			String indentSize = preferencesSupport.getPreferencesValue(PHPCoreConstants.FORMATTER_INDENTATION_SIZE, null, fLastProject);
+			String useTab = preferencesSupport.getPreferencesValue(
+					PHPCoreConstants.FORMATTER_USE_TABS, null, fLastProject);
+			String indentSize = preferencesSupport.getPreferencesValue(
+					PHPCoreConstants.FORMATTER_INDENTATION_SIZE, null,
+					fLastProject);
 
-			indentationChar = (Boolean.valueOf(useTab).booleanValue()) ? '\t' : ' ';
+			indentationChar = (Boolean.valueOf(useTab).booleanValue()) ? '\t'
+					: ' ';
 			indentationSize = Integer.valueOf(indentSize).intValue();
 
 			preferencesChanged = false;
@@ -116,16 +151,21 @@ public class FormatPreferencesSupport {
 
 	private void verifyListening() {
 		if (listener != null) {
-			preferencesPropagator.removePropagatorListener(listener, PHPCoreConstants.FORMATTER_USE_TABS);
-			preferencesPropagator.removePropagatorListener(listener, PHPCoreConstants.FORMATTER_INDENTATION_SIZE);
+			preferencesPropagator.removePropagatorListener(listener,
+					PHPCoreConstants.FORMATTER_USE_TABS);
+			preferencesPropagator.removePropagatorListener(listener,
+					PHPCoreConstants.FORMATTER_INDENTATION_SIZE);
 		}
 
 		listener = new PreferencesPropagatorListener(fLastProject);
-		preferencesPropagator.addPropagatorListener(listener, PHPCoreConstants.FORMATTER_USE_TABS);
-		preferencesPropagator.addPropagatorListener(listener, PHPCoreConstants.FORMATTER_INDENTATION_SIZE);
+		preferencesPropagator.addPropagatorListener(listener,
+				PHPCoreConstants.FORMATTER_USE_TABS);
+		preferencesPropagator.addPropagatorListener(listener,
+				PHPCoreConstants.FORMATTER_INDENTATION_SIZE);
 	}
 
-	private class PreferencesPropagatorListener implements IPreferencesPropagatorListener {
+	private class PreferencesPropagatorListener implements
+			IPreferencesPropagatorListener {
 
 		private IProject project;
 
