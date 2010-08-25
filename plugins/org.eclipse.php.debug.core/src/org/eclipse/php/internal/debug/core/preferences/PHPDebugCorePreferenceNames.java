ca6cdcfc07262f90ab785b9e9efc2ec7350eaa26
rganor <rganor>
2010-08-25 07:01:04 +0000

    Bug 323395 - Add Default Base URL group to the Php Debug Project properties
    contributed by Ronen H.

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/preferences/PHPDebugCorePreferenceNames.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/preferences/PHPDebugCorePreferenceNames.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.debug.core.preferences;
 
@@ -22,28 +23,45 @@ public class PHPDebugCorePreferenceNames {
 
 	private static final String Debug_ID = PHPDebugPlugin.getID();
 
-	public static final String STOP_AT_FIRST_LINE = Debug_ID + "stop_at_first_line_string";//$NON-NLS-1$
+	public static final String STOP_AT_FIRST_LINE = Debug_ID
+			+ "stop_at_first_line_string";//$NON-NLS-1$
 	public static final String ZEND_DEBUG_PORT = Debug_ID + "zend_debug_port";//$NON-NLS-1$
 	public static final String CLIENT_IP = Debug_ID + "client_ip";//$NON-NLS-1$
+	public static final String DEBUG_RESPONSE_TIMEOUT = Debug_ID
+			+ "response_timeout";//$NON-NLS-1$
 
 	// Workspace defaults PHP executables keys
-	public static final String INSTALLED_PHP_NAMES = Debug_ID + "installedPHPNames";//$NON-NLS-1$
-	public static final String INSTALLED_PHP_LOCATIONS = Debug_ID + "installedPHPLocations";//$NON-NLS-1$
-	public static final String INSTALLED_PHP_INIS = Debug_ID + "installedPHPInis";//$NON-NLS-1$
-	public static final String INSTALLED_PHP_DEBUGGERS = Debug_ID + "installedPHPDebuggers";//$NON-NLS-1$
-	public static final String INSTALLED_PHP_DEFAULTS = Debug_ID + "installedPHPDefaults";//$NON-NLS-1$
+	public static final String INSTALLED_PHP_NAMES = Debug_ID
+			+ "installedPHPNames";//$NON-NLS-1$
+	public static final String INSTALLED_PHP_LOCATIONS = Debug_ID
+			+ "installedPHPLocations";//$NON-NLS-1$
+	public static final String INSTALLED_PHP_INIS = Debug_ID
+			+ "installedPHPInis";//$NON-NLS-1$
+	public static final String INSTALLED_PHP_DEBUGGERS = Debug_ID
+			+ "installedPHPDebuggers";//$NON-NLS-1$
+	public static final String INSTALLED_PHP_DEFAULTS = Debug_ID
+			+ "installedPHPDefaults";//$NON-NLS-1$
 
 	// Project default PHP executables keys
 	public static final String DEFAULT_PHP = Debug_ID + "defaultPHP";//$NON-NLS-1$
 
-	public static final String DIALOG_COLUMN_WIDTH = Debug_ID + "phpdebug.dialog.columnwidth";//$NON-NLS-1$
-	public static final String RUN_WITH_DEBUG_INFO = Debug_ID + "run_with_debug_info";//$NON-NLS-1$
+	public static final String DIALOG_COLUMN_WIDTH = Debug_ID
+			+ "phpdebug.dialog.columnwidth";//$NON-NLS-1$
+	public static final String RUN_WITH_DEBUG_INFO = Debug_ID
+			+ "run_with_debug_info";//$NON-NLS-1$
 	public static final String OPEN_IN_BROWSER = Debug_ID + "open_in_browser";//$NON-NLS-1$
 	public static final String AUTO_SAVE_DIRTY = Debug_ID + "auto_save_dirty";//$NON-NLS-1$
 	public static final String OPEN_DEBUG_VIEWS = Debug_ID + "open_debug_views";//$NON-NLS-1$
-	public static final String TRANSFER_ENCODING = Debug_ID + "transfer_encoding";//$NON-NLS-1$
+	public static final String TRANSFER_ENCODING = Debug_ID
+			+ "transfer_encoding";//$NON-NLS-1$
 	public static final String OUTPUT_ENCODING = Debug_ID + "output_encoding";//$NON-NLS-1$
-	public static final String CONFIGURATION_DELEGATE_CLASS = Debug_ID + "configuration_delegate_class";//$NON-NLS-1$
-	
+	public static final String CONFIGURATION_DELEGATE_CLASS = Debug_ID
+			+ "configuration_delegate_class";//$NON-NLS-1$
+
 	public static final String PHP_DEBUGGER_ID = Debug_ID + "php_debugger_id";//$NON-NLS-1$
+
+	public static final String INSTALLED_PHP_DEFAULT_FOR_VERSIONS = Debug_ID
+			+ "php_versions";//$NON-NLS-1$
+
+	public static final String DEFAULT_BASE_PATH = "DefaultProjectBasePath";
 }
--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/preferences/PHPProjectPreferences.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/preferences/PHPProjectPreferences.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 Zend Corporation and IBM Corporation.
+ * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *   Zend and IBM - Initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
  *******************************************************************************/
 package org.eclipse.php.internal.debug.core.preferences;
 
@@ -16,7 +17,6 @@ import org.eclipse.core.runtime.Preferences;
 import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
 import org.eclipse.php.internal.debug.core.PHPDebugPlugin;
-import org.eclipse.php.internal.debug.core.debugger.AbstractDebuggerConfiguration;
 import org.eclipse.php.internal.server.core.manager.ServersManager;
 
 public class PHPProjectPreferences {
@@ -35,7 +35,8 @@ public class PHPProjectPreferences {
 
 	public static boolean getElementSettingsForProject(IProject project) {
 		IScopeContext pScope = getProjectScope(project);
-		return pScope.getNode(getPreferenceNodeQualifier()).getBoolean(getProjectSettingsKey(), false);
+		return pScope.getNode(getPreferenceNodeQualifier()).getBoolean(
+				getProjectSettingsKey(), false);
 	}
 
 	public static String getProjectSettingsKey() {
@@ -44,52 +45,90 @@ public class PHPProjectPreferences {
 
 	public static boolean getStopAtFirstLine(IProject project) {
 		Preferences prefs = getModelPreferences();
-		boolean stop = prefs.getBoolean(PHPDebugCorePreferenceNames.STOP_AT_FIRST_LINE);
+		boolean stop = prefs
+				.getBoolean(PHPDebugCorePreferenceNames.STOP_AT_FIRST_LINE);
 		if (project != null && getElementSettingsForProject(project)) {
 			IScopeContext projectScope = getProjectScope(project);
-			stop = projectScope.getNode(getPreferenceNodeQualifier()).getBoolean(PHPDebugCorePreferenceNames.STOP_AT_FIRST_LINE, stop);
+			stop = projectScope.getNode(getPreferenceNodeQualifier())
+					.getBoolean(PHPDebugCorePreferenceNames.STOP_AT_FIRST_LINE,
+							stop);
 		}
 		return stop;
 
 	}
 
-	public static String getDefaultServerName(IProject project) {
-		Preferences prefs = getModelPreferences();
-		String serverName = prefs.getString(ServersManager.DEFAULT_SERVER_PREFERENCES_KEY);
+	public static String getDefaultBasePath(IProject project) {
+
+		String basePath = null;
 		if (project != null && getElementSettingsForProject(project)) {
 			IScopeContext projectScope = getProjectScope(project);
-			serverName = projectScope.getNode(getPreferenceNodeQualifier()).get(ServersManager.DEFAULT_SERVER_PREFERENCES_KEY, serverName);
+			basePath = projectScope.getNode(getPreferenceNodeQualifier()).get(
+					PHPDebugCorePreferenceNames.DEFAULT_BASE_PATH, basePath);
+		}
+		if (basePath == null) {
+			return "/" + project.getName();
+		}
+		return basePath;
+	}
+
+	public static void setDefaultBasePath(IProject project, String value) {
+		Preferences prefs = getModelPreferences();
+		prefs.setValue(PHPDebugCorePreferenceNames.DEFAULT_BASE_PATH, value);
+		if (project != null && getElementSettingsForProject(project)) {
+			IScopeContext projectScope = getProjectScope(project);
+			projectScope.getNode(getPreferenceNodeQualifier()).put(
+					PHPDebugCorePreferenceNames.DEFAULT_BASE_PATH, value);
+		}
+	}
+
+	public static String getDefaultServerName(IProject project) {
+		Preferences prefs = getModelPreferences();
+		String serverName = prefs
+				.getString(ServersManager.DEFAULT_SERVER_PREFERENCES_KEY);
+		if (project != null && getElementSettingsForProject(project)) {
+			IScopeContext projectScope = getProjectScope(project);
+			serverName = projectScope.getNode(getPreferenceNodeQualifier())
+					.get(ServersManager.DEFAULT_SERVER_PREFERENCES_KEY,
+							serverName);
 		}
 		return serverName;
 	}
-	
+
 	public static String getDefaultDebuggerID(IProject project) {
 		Preferences prefs = getModelPreferences();
-		String debuggerID = prefs.getString(PHPDebugCorePreferenceNames.PHP_DEBUGGER_ID);
+		String debuggerID = prefs
+				.getString(PHPDebugCorePreferenceNames.PHP_DEBUGGER_ID);
 		if (project != null && getElementSettingsForProject(project)) {
 			IScopeContext projectScope = getProjectScope(project);
-			debuggerID = projectScope.getNode(getPreferenceNodeQualifier()).get(PHPDebugCorePreferenceNames.PHP_DEBUGGER_ID, debuggerID);
+			debuggerID = projectScope.getNode(getPreferenceNodeQualifier())
+					.get(PHPDebugCorePreferenceNames.PHP_DEBUGGER_ID,
+							debuggerID);
 		}
 		return debuggerID;
 	}
 
 	public static String getTransferEncoding(IProject project) {
 		Preferences prefs = getModelPreferences();
-		String encoding = prefs.getString(PHPDebugCorePreferenceNames.TRANSFER_ENCODING);
+		String encoding = prefs
+				.getString(PHPDebugCorePreferenceNames.TRANSFER_ENCODING);
 		if (project != null && getElementSettingsForProject(project)) {
 			IScopeContext projectScope = getProjectScope(project);
-			encoding = projectScope.getNode(getPreferenceNodeQualifier()).get(PHPDebugCorePreferenceNames.TRANSFER_ENCODING, encoding);
+			encoding = projectScope.getNode(getPreferenceNodeQualifier()).get(
+					PHPDebugCorePreferenceNames.TRANSFER_ENCODING, encoding);
 		}
 		return encoding;
 	}
 
 	public static String getOutputEncoding(IProject project) {
 		Preferences prefs = getModelPreferences();
-		String encoding = prefs.getString(PHPDebugCorePreferenceNames.OUTPUT_ENCODING);
+		String encoding = prefs
+				.getString(PHPDebugCorePreferenceNames.OUTPUT_ENCODING);
 		if (project != null && getElementSettingsForProject(project)) {
 			IScopeContext projectScope = getProjectScope(project);
-			encoding = projectScope.getNode(getPreferenceNodeQualifier()).get(PHPDebugCorePreferenceNames.OUTPUT_ENCODING, encoding);
+			encoding = projectScope.getNode(getPreferenceNodeQualifier()).get(
+					PHPDebugCorePreferenceNames.OUTPUT_ENCODING, encoding);
 		}
 		return encoding;
 	}
+
 }
