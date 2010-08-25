891f486f1986352b87422b883045a11863b0b72d
zzhongwei <zzhongwei>
2010-08-25 02:55:48 +0000

    323555: another CA bug like bug 321553 https://bugs.eclipse.org/bugs/show_bug.cgi?id=323555

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/AbstractCompletionStrategy.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/AbstractCompletionStrategy.java
@@ -76,7 +76,7 @@ public abstract class AbstractCompletionStrategy implements ICompletionStrategy 
 		AbstractCompletionContext completionContext = (AbstractCompletionContext) context;
 
 		int length = completionContext.getPrefix().length();
-		// PHPTextSequenceUtilities line 134
+		// see PHPTextSequenceUtilities line 134
 		if (completionContext.getOffset() - 1 >= 0
 				&& completionContext.getDocument().getChar(
 						completionContext.getOffset() - 1) == '(') {
@@ -100,6 +100,12 @@ public abstract class AbstractCompletionStrategy implements ICompletionStrategy 
 		AbstractCompletionContext completionContext = (AbstractCompletionContext) context;
 
 		int length = completionContext.getPrefix().length();
+		// see PHPTextSequenceUtilities line 134
+		if (completionContext.getOffset() - 1 >= 0
+				&& completionContext.getDocument().getChar(
+						completionContext.getOffset() - 1) == '(') {
+			length++;
+		}
 		int start = completionContext.getOffset() - length;
 		int prefixEnd = completionContext.getPrefixEnd();
 
