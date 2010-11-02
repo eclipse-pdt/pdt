e64f9e20daa81ea699782b9645976f7fd3456b97
zzhongwei <zzhongwei>
2010-11-02 04:19:01 +0000

    *** empty log message ***

--- tests/org.eclipse.php.core.performance.tests/src/org/eclipse/php/core/tests/performance/PerformanceMonitor.java
+++ tests/org.eclipse.php.core.performance.tests/src/org/eclipse/php/core/tests/performance/PerformanceMonitor.java
@@ -41,7 +41,8 @@ public class PerformanceMonitor {
 
 		pool = JdbcConnectionPool.create("jdbc:h2:"
 				+ new File(resultsDir, "db").getAbsolutePath(), "test", "");
-
+		System.out.println("db file location ================");
+		System.out.println(new File(resultsDir, "db").getAbsolutePath());
 		createSchema();
 		executionId = getExecutionId();
 
