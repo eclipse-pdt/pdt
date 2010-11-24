4388d1a81dfb5af28782f1562a87f60bb4c08c17
qiwang <qiwang>
2010-11-24 03:40:50 +0000

    Split the project creation logic into separate util class, so that the logic can be reused.

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/util/PHPProjectUtils.java
@@ -0,0 +1,43 @@
+package org.eclipse.php.ui.util;
+
+import java.net.URI;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.dltk.internal.ui.wizards.NewWizardMessages;
+
+public class PHPProjectUtils {
+	public static void createProjectAt(IProject project, URI locationURI,
+			IProgressMonitor monitor) throws CoreException {
+		if (monitor == null) {
+			monitor = new NullProgressMonitor();
+		}
+		monitor.beginTask(
+				NewWizardMessages.BuildPathsBlock_operationdesc_project, 100);
+		// create the project
+		try {
+			if (!project.exists()) {
+				IProjectDescription desc = project.getWorkspace()
+						.newProjectDescription(project.getName());
+				if (locationURI != null
+						&& ResourcesPlugin.getWorkspace().getRoot()
+								.getLocationURI().equals(locationURI)) {
+					locationURI = null;
+				}
+				desc.setLocationURI(locationURI);
+				project.create(desc, new SubProgressMonitor(monitor, 50));
+			}
+			if (!project.isOpen()) {
+				project.open(new SubProgressMonitor(monitor, 50));
+			}
+		} finally {
+			// not null
+			monitor.done();
+		}
+	}
+}
