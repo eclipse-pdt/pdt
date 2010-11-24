fd809b02f7112d026e9f1d3584d3b9c98602ce4e
rganor <rganor>
2010-11-24 07:05:15 +0000

    revert fix of Bug 282997 - as it causes to a serious regression

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/zend/debugger/ProcessCrashDetector.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/zend/debugger/ProcessCrashDetector.java
@@ -11,10 +11,23 @@
  *******************************************************************************/
 package org.eclipse.php.internal.debug.core.zend.debugger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IProcess;
+import org.eclipse.debug.internal.ui.DebugUIPlugin;
+import org.eclipse.debug.internal.ui.preferences.IDebugPreferenceConstants;
+import org.eclipse.debug.internal.ui.views.console.ProcessConsole;
 import org.eclipse.php.internal.debug.core.launching.PHPProcess;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleListener;
+import org.eclipse.ui.console.IOConsoleOutputStream;
 
 /**
  * A process crash detector is a {@link Runnable} that hooks a PHP process error
@@ -27,10 +40,11 @@ import org.eclipse.php.internal.debug.core.launching.PHPProcess;
  * @author shalom
  * @since PDT 1.0.1
  */
-public class ProcessCrashDetector implements Runnable {
+public class ProcessCrashDetector implements Runnable, IConsoleListener {
 
 	private ILaunch launch;
 	private Process process;
+	private ProcessConsole console;
 
 	/**
 	 * Constructs a process detector on a given {@link Process}.
@@ -52,6 +66,16 @@ public class ProcessCrashDetector implements Runnable {
 	 */
 	public void run() {
 		try {
+			StreamGobbler errorGobbler = new StreamGobbler(process
+					.getErrorStream(), true);
+			StreamGobbler inputGobbler = new StreamGobbler(process
+					.getInputStream(), false);
+
+			ConsolePlugin.getDefault().getConsoleManager().addConsoleListener(
+					this);
+
+			errorGobbler.start();
+			inputGobbler.start();
 
 			int exitValue = process.waitFor();
 			IDebugTarget debugTarget = launch.getDebugTarget();
@@ -64,8 +88,75 @@ public class ProcessCrashDetector implements Runnable {
 
 		} catch (Throwable t) {
 		} finally {
-
+			ConsolePlugin.getDefault().getConsoleManager()
+					.removeConsoleListener(this);
 		}
 	}
 
+	class StreamGobbler extends Thread {
+		InputStream is;
+		StringBuilder buf;
+		boolean isError;
+		private IOConsoleOutputStream os;
+
+		StreamGobbler(InputStream is, boolean isError) {
+			this.is = is;
+			this.buf = new StringBuilder();
+			this.isError = isError;
+		}
+
+		public void run() {
+			try {
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+
+				String line;
+				while ((line = br.readLine()) != null) {
+					if (!isError) {
+						continue;
+					}
+					if (console != null) {
+						if (os == null) {
+							os = console.newOutputStream();
+							if (isError) {
+								Display.getDefault().syncExec(new Runnable() {
+									public void run() {
+										os
+												.setColor(DebugUIPlugin
+														.getPreferenceColor(IDebugPreferenceConstants.CONSOLE_SYS_ERR_COLOR));
+									}
+								});
+							}
+						}
+						os.write(buf.toString());
+						os.write(line + '\n');
+						buf.delete(0, buf.length());
+					} else {
+						buf.append(line).append('\n');
+					}
+				}
+			} catch (IOException ioe) {
+				// PHPDebugPlugin.log(ioe);
+			} finally {
+				if (os != null) {
+					try {
+						os.close();
+					} catch (IOException e) {
+						// PHPDebugPlugin.log(e);
+					}
+				}
+			}
+		}
+	}
+
+	public void consolesAdded(IConsole[] consoles) {
+		for (IConsole console : consoles) {
+			if (console instanceof ProcessConsole) {
+				this.console = (ProcessConsole) console;
+			}
+		}
+	}
+
+	public void consolesRemoved(IConsole[] consoles) {
+	}
 }
