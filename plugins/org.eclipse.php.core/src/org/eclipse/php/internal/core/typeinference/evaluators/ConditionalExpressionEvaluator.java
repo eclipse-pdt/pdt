269cfbdff0caa6dadcc87a13a84e9b42c1c37938
zzhongwei <zzhongwei>
2011-06-27 06:38:04 +0000

    An internal error occurred during: "Processing Dirty Regions". java.lang.NullPointerException

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/ConditionalExpressionEvaluator.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/ConditionalExpressionEvaluator.java
@@ -1,6 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
 package org.eclipse.php.internal.core.typeinference.evaluators;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.eclipse.dltk.ti.GoalState;
 import org.eclipse.dltk.ti.goals.ExpressionTypeGoal;
@@ -27,8 +40,19 @@ public class ConditionalExpressionEvaluator extends GoalEvaluator {
 
 	public IGoal[] init() {
 		ExpressionTypeGoal typedGoal = (ExpressionTypeGoal) goal;
-		ConditionalExpression conditionalExpression = (ConditionalExpression) typedGoal.getExpression();
-		return new IGoal[] { new ExpressionTypeGoal(goal.getContext(), conditionalExpression.getIfTrue()), new ExpressionTypeGoal(goal.getContext(), conditionalExpression.getIfFalse()) };
+		ConditionalExpression conditionalExpression = (ConditionalExpression) typedGoal
+				.getExpression();
+		// https://bugs.eclipse.org/bugs/show_bug.cgi?id=339405
+		List<IGoal> result = new ArrayList<IGoal>();
+		if (conditionalExpression.getIfTrue() != null) {
+			result.add(new ExpressionTypeGoal(goal.getContext(),
+					conditionalExpression.getIfTrue()));
+		}
+		if (conditionalExpression.getIfFalse() != null) {
+			result.add(new ExpressionTypeGoal(goal.getContext(),
+					conditionalExpression.getIfFalse()));
+		}
+		return result.toArray(new IGoal[result.size()]);
 	}
 
 	public IGoal[] subGoalDone(IGoal subgoal, Object result, GoalState state) {
--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/phpdoc/PHPDocMethodReturnTypeEvaluator.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/evaluators/phpdoc/PHPDocMethodReturnTypeEvaluator.java
@@ -13,12 +13,16 @@ package org.eclipse.php.internal.core.typeinference.evaluators.phpdoc;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.ast.references.SimpleReference;
 import org.eclipse.dltk.core.IMethod;
 import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.core.SourceParserUtil;
 import org.eclipse.dltk.evaluation.types.MultiTypeType;
 import org.eclipse.dltk.ti.GoalState;
 import org.eclipse.dltk.ti.goals.IGoal;
@@ -26,12 +30,14 @@ import org.eclipse.dltk.ti.types.IEvaluatedType;
 import org.eclipse.php.internal.core.compiler.ast.nodes.NamespaceReference;
 import org.eclipse.php.internal.core.compiler.ast.nodes.PHPDocBlock;
 import org.eclipse.php.internal.core.compiler.ast.nodes.PHPDocTag;
+import org.eclipse.php.internal.core.compiler.ast.nodes.UsePart;
 import org.eclipse.php.internal.core.index.IPHPDocAwareElement;
 import org.eclipse.php.internal.core.typeinference.PHPClassType;
 import org.eclipse.php.internal.core.typeinference.PHPModelUtils;
 import org.eclipse.php.internal.core.typeinference.PHPSimpleTypes;
 import org.eclipse.php.internal.core.typeinference.PHPTypeInferenceUtils;
 import org.eclipse.php.internal.core.typeinference.evaluators.AbstractMethodReturnTypeEvaluator;
+import org.eclipse.php.internal.core.typeinference.goals.AbstractMethodReturnTypeGoal;
 
 /**
  * This Evaluator process the phpdoc of a method to determine its returned
@@ -46,6 +52,8 @@ public class PHPDocMethodReturnTypeEvaluator extends
 
 	private final static Pattern ARRAY_TYPE_PATTERN = Pattern
 			.compile("array\\[.*\\]");
+
+	private final static String SELF_RETURN_TYPE = "self";
 
 	/**
 	 * Used for splitting the data types list of the returned tag
@@ -72,8 +80,13 @@ public class PHPDocMethodReturnTypeEvaluator extends
 					for (String typeName : typeNames) {
 						Matcher m = ARRAY_TYPE_PATTERN.matcher(typeName);
 						if (m.find()) {
+							int offset = 0;
+							try {
+								offset = method.getSourceRange().getOffset();
+							} catch (ModelException e) {
+							}
 							evaluated.add(getArrayType(m.group(),
-									currentNamespace));
+									currentNamespace, offset));
 						} else {
 							IEvaluatedType type = getEvaluatedType(typeName,
 									currentNamespace);
@@ -87,6 +100,8 @@ public class PHPDocMethodReturnTypeEvaluator extends
 			} else {
 				PHPDocBlock docBlock = PHPModelUtils.getDocBlock(method);
 				if (docBlock != null) {
+					AbstractMethodReturnTypeGoal typedGoal = (AbstractMethodReturnTypeGoal) goal;
+					IType[] types = typedGoal.getTypes();
 					for (PHPDocTag tag : docBlock.getTags()) {
 						if (tag.getTagKind() == PHPDocTag.RETURN) {
 							// @return datatype1|datatype2|...
@@ -99,12 +114,54 @@ public class PHPDocMethodReturnTypeEvaluator extends
 											.matcher(typeName);
 									if (m.find()) {
 										evaluated.add(getArrayType(m.group(),
-												currentNamespace));
+												currentNamespace, docBlock
+														.sourceStart()));
 									} else {
-										IEvaluatedType type = getEvaluatedType(
-												typeName, currentNamespace);
-										if (type != null) {
-											evaluated.add(type);
+										if (typeName.equals(SELF_RETURN_TYPE)
+												&& types != null) {
+											for (IType t : types) {
+												IEvaluatedType type = getEvaluatedType(
+														t.getElementName(),
+														currentNamespace);
+												if (type != null) {
+													evaluated.add(type);
+												}
+											}
+										} else {
+											if (typeName
+													.indexOf(NamespaceReference.NAMESPACE_SEPARATOR) > 0
+													&& currentNamespace != null) {
+												// check if the first part is an
+												// alias,then get the full name
+												ModuleDeclaration moduleDeclaration = SourceParserUtil
+														.getModuleDeclaration(currentNamespace
+																.getSourceModule());
+												String prefix = typeName
+														.substring(
+																0,
+																typeName.indexOf(NamespaceReference.NAMESPACE_SEPARATOR));
+												final Map<String, UsePart> result = PHPModelUtils
+														.getAliasToNSMap(
+																prefix,
+																moduleDeclaration,
+																docBlock.sourceStart(),
+																currentNamespace,
+																true);
+												if (result.containsKey(prefix)) {
+													String fullName = result
+															.get(prefix)
+															.getNamespace()
+															.getFullyQualifiedName();
+													typeName = typeName
+															.replace(prefix,
+																	fullName);
+												}
+											}
+											IEvaluatedType type = getEvaluatedType(
+													typeName, currentNamespace);
+											if (type != null) {
+												evaluated.add(type);
+											}
 										}
 									}
 								}
@@ -118,19 +175,40 @@ public class PHPDocMethodReturnTypeEvaluator extends
 		return IGoal.NO_GOALS;
 	}
 
-	private MultiTypeType getArrayType(String type, IType currentNamespace) {
+	private MultiTypeType getArrayType(String type, IType currentNamespace,
+			int offset) {
 		int beginIndex = type.indexOf("[") + 1;
 		int endIndex = type.lastIndexOf("]");
 		type = type.substring(beginIndex, endIndex);
 		MultiTypeType arrayType = new MultiTypeType();
 		Matcher m = ARRAY_TYPE_PATTERN.matcher(type);
 		if (m.find()) {
-			arrayType.addType(getArrayType(m.group(), currentNamespace));
+			arrayType
+					.addType(getArrayType(m.group(), currentNamespace, offset));
 			type = m.replaceAll("");
 		}
 		String[] typeNames = type.split(",");
 		for (String name : typeNames) {
 			if (!"".equals(name)) {
+
+				if (name.indexOf(NamespaceReference.NAMESPACE_SEPARATOR) > 0
+						&& currentNamespace != null) {
+					// check if the first part is an
+					// alias,then get the full name
+					ModuleDeclaration moduleDeclaration = SourceParserUtil
+							.getModuleDeclaration(currentNamespace
+									.getSourceModule());
+					String prefix = name.substring(0, name
+							.indexOf(NamespaceReference.NAMESPACE_SEPARATOR));
+					final Map<String, UsePart> result = PHPModelUtils
+							.getAliasToNSMap(prefix, moduleDeclaration, offset,
+									currentNamespace, true);
+					if (result.containsKey(prefix)) {
+						String fullName = result.get(prefix).getNamespace()
+								.getFullyQualifiedName();
+						name = name.replace(prefix, fullName);
+					}
+				}
 				arrayType.addType(getEvaluatedType(name, currentNamespace));
 			}
 		}
