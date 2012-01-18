c6cd1a946f62b987374363362b75dce0278b9ccf
zzhongwei <zzhongwei>
2012-01-18 10:30:29 +0000

    php5.4 support

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/ast/nodes/ArrayCreation.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/ast/nodes/ArrayCreation.java
@@ -21,37 +21,47 @@ import org.eclipse.php.internal.core.ast.visitor.Visitor;
 
 /**
  * Represents array creation
- * <pre>e.g.<pre> array(1,2,3,),
+ * 
+ * <pre>e.g.
+ * 
+ * <pre>
+ * array(1,2,3,),
  * array('Dodo'=>'Golo','Dafna'=>'Dodidu')
  * array($a, $b=>foo(), 1=>$myClass->getFirst())
  */
 public class ArrayCreation extends Expression {
 
-	private final ASTNode.NodeList<ArrayElement> elements = new ASTNode.NodeList<ArrayElement>(ELEMENTS_PROPERTY);
-
+	private final ASTNode.NodeList<ArrayElement> elements = new ASTNode.NodeList<ArrayElement>(
+			ELEMENTS_PROPERTY);
+	private boolean hasArrayKey;
 	/**
 	 * The "statements" structural property of this node type.
 	 */
-	public static final ChildListPropertyDescriptor ELEMENTS_PROPERTY = 
-		new ChildListPropertyDescriptor(ArrayCreation.class, "elements", ArrayElement.class, CYCLE_RISK); //$NON-NLS-1$
+	public static final ChildListPropertyDescriptor ELEMENTS_PROPERTY = new ChildListPropertyDescriptor(
+			ArrayCreation.class, "elements", ArrayElement.class, CYCLE_RISK); //$NON-NLS-1$
 
+	public static final SimplePropertyDescriptor HAS_ARRAY_KEY = new SimplePropertyDescriptor(
+			LambdaFunctionDeclaration.class,
+			"hasArrayKey", Boolean.class, OPTIONAL); //$NON-NLS-1$
 	/**
-	 * A list of property descriptors (element type: 
-	 * {@link StructuralPropertyDescriptor}),
-	 * or null if uninitialized.
+	 * A list of property descriptors (element type:
+	 * {@link StructuralPropertyDescriptor}), or null if uninitialized.
 	 */
 	private static final List<StructuralPropertyDescriptor> PROPERTY_DESCRIPTORS;
 	static {
-		List<StructuralPropertyDescriptor> properyList = new ArrayList<StructuralPropertyDescriptor>(2);
+		List<StructuralPropertyDescriptor> properyList = new ArrayList<StructuralPropertyDescriptor>(
+				2);
 		properyList.add(ELEMENTS_PROPERTY);
+		properyList.add(HAS_ARRAY_KEY);
 		PROPERTY_DESCRIPTORS = Collections.unmodifiableList(properyList);
 	}
-	
+
 	public ArrayCreation(AST ast) {
 		super(ast);
 	}
-	
-	private ArrayCreation(int start, int end, AST ast,ArrayElement[] elements) {
+
+	private ArrayCreation(int start, int end, AST ast, ArrayElement[] elements,
+			boolean hasArrayKey) {
 		super(start, end, ast);
 
 		if (elements == null) {
@@ -61,10 +71,47 @@ public class ArrayCreation extends Expression {
 		for (ArrayElement arrayElement : elements) {
 			this.elements.add(arrayElement);
 		}
+		setHasArrayKey(hasArrayKey);
 	}
 
 	public ArrayCreation(int start, int end, AST ast, List elements) {
-		this(start, end, ast, elements == null ? null : (ArrayElement[]) elements.toArray(new ArrayElement[elements.size()]));
+		this(start, end, ast, elements == null ? null
+				: (ArrayElement[]) elements.toArray(new ArrayElement[elements
+						.size()]), true);
+	}
+
+	public ArrayCreation(int start, int end, AST ast, List elements,
+			boolean hasArrayKey) {
+		this(start, end, ast, elements == null ? null
+				: (ArrayElement[]) elements.toArray(new ArrayElement[elements
+						.size()]), hasArrayKey);
+	}
+
+	public boolean isHasArrayKey() {
+		return hasArrayKey;
+	}
+
+	public void setHasArrayKey(boolean hasArrayKey) {
+		preValueChange(HAS_ARRAY_KEY);
+		this.hasArrayKey = hasArrayKey;
+		postValueChange(HAS_ARRAY_KEY);
+	}
+
+	/*
+	 * (omit javadoc for this method) Method declared on ASTNode.
+	 */
+	final boolean internalGetSetBooleanProperty(
+			SimplePropertyDescriptor property, boolean get, boolean value) {
+		if (property == HAS_ARRAY_KEY) {
+			if (get) {
+				return isHasArrayKey();
+			} else {
+				setHasArrayKey(value);
+				return false;
+			}
+		}
+		// allow default implementation to flag the error
+		return super.internalGetSetBooleanProperty(property, get, value);
 	}
 
 	public void childrenAccept(Visitor visitor) {
@@ -94,7 +141,7 @@ public class ArrayCreation extends Expression {
 		for (ASTNode node : this.elements) {
 			node.toString(buffer, TAB + tab);
 			buffer.append("\n"); //$NON-NLS-1$
-		}				
+		}
 		buffer.append(tab).append("</ArrayCreation>"); //$NON-NLS-1$
 	}
 
@@ -104,8 +151,8 @@ public class ArrayCreation extends Expression {
 			childrenAccept(visitor);
 		}
 		visitor.endVisit(this);
-	}	
-	
+	}
+
 	public int getType() {
 		return ASTNode.ARRAY_CREATION;
 	}
@@ -116,40 +163,42 @@ public class ArrayCreation extends Expression {
 	public ArrayElement[] getElements() {
 		return elements.toArray(new ArrayElement[elements.size()]);
 	}
-	
+
 	/**
 	 * Retrieves elements parts of array creation
+	 * 
 	 * @return elements
 	 */
 	public List<ArrayElement> elements() {
 		return this.elements;
 	}
-	
-	
-	/* (omit javadoc for this method)
-	 * Method declared on ASTNode.
+
+	/*
+	 * (omit javadoc for this method) Method declared on ASTNode.
 	 */
 	public boolean subtreeMatch(ASTMatcher matcher, Object other) {
 		// dispatch to correct overloaded match method
 		return matcher.match(this, other);
 	}
 
-	/* (omit javadoc for this method)
-	 * Method declared on ASTNode.
+	/*
+	 * (omit javadoc for this method) Method declared on ASTNode.
 	 */
 	ASTNode clone0(AST target) {
 		final List elements = ASTNode.copySubtrees(target, elements());
-		final ArrayCreation result = new ArrayCreation(this.getStart(), this.getEnd(), target, elements);
+		final ArrayCreation result = new ArrayCreation(this.getStart(),
+				this.getEnd(), target, elements);
 		return result;
 	}
-	
+
 	@Override
-	List<StructuralPropertyDescriptor> internalStructuralPropertiesForType(PHPVersion apiLevel) {
+	List<StructuralPropertyDescriptor> internalStructuralPropertiesForType(
+			PHPVersion apiLevel) {
 		return PROPERTY_DESCRIPTORS;
 	}
-	
-	/* (omit javadoc for this method)
-	 * Method declared on ASTNode.
+
+	/*
+	 * (omit javadoc for this method) Method declared on ASTNode.
 	 */
 	final List internalGetChildListProperty(ChildListPropertyDescriptor property) {
 		if (property == ELEMENTS_PROPERTY) {
