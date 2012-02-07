93230473c8446b6b9bc7ea45e6b58102881333bb
zzhongwei <zzhongwei>
2012-02-07 09:20:45 +0000

    ZSTD-1346 [Emanuel] Searching for a Method using PHP Search is imposible.

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/search/PHPMatchLocator.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/search/PHPMatchLocator.java
@@ -266,11 +266,22 @@ public class PHPMatchLocator extends MatchLocator {
 					IModelElement[] elements = module.codeSelect(pce
 							.getCallName().sourceStart(), 0);
 					for (int i = 0; i < elements.length; i++) {
-						if (pattern.focus.equals(elements[i])) {
+						if (pattern.focus != null
+								&& pattern.focus.equals(elements[i])) {
 
 							return super.newMethodReferenceMatch(
 									enclosingElement, accuracy, offset, length,
 									isConstructor, isSynthetic, reference);
+						} else {
+							MethodPattern methodPattern = (MethodPattern) pattern;
+							if (new String(methodPattern.selector)
+									.equals(elements[i].getElementName())) {
+								return super.newMethodReferenceMatch(
+										enclosingElement, accuracy, offset,
+										length, isConstructor, isSynthetic,
+										reference);
+							}
+
 						}
 					}
 				} catch (ModelException e) {
