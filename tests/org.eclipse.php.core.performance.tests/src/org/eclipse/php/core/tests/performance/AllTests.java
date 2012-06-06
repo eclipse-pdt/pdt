6a958a5175c092bd0dd938193d67d8e1ced78d7e
nbartol <nbartol>
2012-06-06 17:15:23 +0000

    Test commit

--- tests/org.eclipse.php.core.performance.tests/src/org/eclipse/php/core/tests/performance/AllTests.java
+++ tests/org.eclipse.php.core.performance.tests/src/org/eclipse/php/core/tests/performance/AllTests.java
@@ -8,7 +8,7 @@ import org.eclipse.php.core.tests.performance.zf.ZFPerformanceTests;
 public class AllTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite("Core Performance Tests");
+		TestSuite suite = new TestSuite("Core  Performance Tests");
 
 		// $JUnit-BEGIN$
 		suite.addTest(ZFPerformanceTests.suite());
