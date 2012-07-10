e669c3288c7c61b22c2fa6c996d87295df205c62
zzhongwei <zzhongwei>
2012-07-10 05:52:26 +0000

    ZSTD-1825 Simplify "New" context menu remove "Refactor" menu

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/PHPRefactorActionGroup.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/PHPRefactorActionGroup.java
@@ -11,13 +11,19 @@ package org.eclipse.php.internal.ui.actions;
 
 import org.eclipse.dltk.internal.ui.actions.refactoring.RefactorActionGroup;
 import org.eclipse.dltk.ui.actions.IScriptEditorActionDefinitionIds;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IViewPart;
 
 public class PHPRefactorActionGroup extends RefactorActionGroup {
+	IViewPart part;
 
 	public PHPRefactorActionGroup(IViewPart part) {
 		super(part);
-
+		this.part = part;
 		IPHPActionDelegator renamePHPElement = PHPActionDelegatorRegistry
 				.getActionDelegator("org.eclipse.php.ui.actions.RenameElement");
 
@@ -42,4 +48,46 @@ public class PHPRefactorActionGroup extends RefactorActionGroup {
 
 	}
 
+	/*
+	 * (non-Javadoc) Method declared in ActionGroup
+	 */
+	public void fillContextMenu(IMenuManager menu) {
+		ISelection selection = getSelection();
+		if (selection instanceof IStructuredSelection) {
+			if (!((IStructuredSelection) selection).isEmpty()) {
+				super.fillContextMenu(menu);
+			}
+		} else {
+			super.fillContextMenu(menu);
+		}
+	}
+
+	public ISelectionProvider getSelectionProvider() {
+		return part.getSite().getSelectionProvider();
+	}
+
+	/**
+	 * Returns the selection provided by the site owning this action.
+	 * 
+	 * @return the site's selection
+	 */
+	public ISelection getSelection() {
+		ISelectionProvider selectionProvider = getSelectionProvider();
+		if (selectionProvider != null)
+			return selectionProvider.getSelection();
+		else
+			return null;
+	}
+
+	@Override
+	public void fillActionBars(IActionBars actionBars) {
+		ISelection selection = getSelection();
+		if (selection instanceof IStructuredSelection) {
+			if (!((IStructuredSelection) selection).isEmpty()) {
+				super.fillActionBars(actionBars);
+			}
+		} else {
+			super.fillActionBars(actionBars);
+		}
+	}
 }
