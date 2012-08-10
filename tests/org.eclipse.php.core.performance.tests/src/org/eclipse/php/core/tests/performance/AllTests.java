83f50866a9ad61da56860daf327f35d00ce726d6
Jacek Pospychala <jacek.p@zend.com>
2012-08-10 10:45:41 +0200

    Revert "Bug 386543 - PDT performance tests are too machine sensitive"
    This reverts commit 864a479c8b3f15d82b0e00ed596269adfbf966d4.

--- tests/org.eclipse.php.core.performance.tests/src/org/eclipse/php/core/tests/performance/AllTests.java
+++ tests/org.eclipse.php.core.performance.tests/src/org/eclipse/php/core/tests/performance/AllTests.java
@@ -3,13 +3,15 @@ package org.eclipse.php.core.tests.performance;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.php.core.tests.performance.zf.ZFPerformanceTests;
+
 public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Core Performance Tests");
 
 		// $JUnit-BEGIN$
-		// suite.addTest(ZFPerformanceTests.suite());
+		suite.addTest(ZFPerformanceTests.suite());
 		// $JUnit-END$
 
 		return suite;
