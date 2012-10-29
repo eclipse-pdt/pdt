ec01f0ae0f225aba152e4d5204a41fa27e0b00d3
Robert Gruendler <r.gruendler@gmail.com>
2012-10-29 15:41:05 +0100

    added more license headers

--- plugins/org.eclipse.php.core/Resources/language/php5/OAuth.php
+++ plugins/org.eclipse.php.core/Resources/language/php5/OAuth.php
@@ -1,4 +1,14 @@
 <?php
+/*******************************************************************************
+ * Copyright (c) 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Gruendler - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 
 // Start of OAuth v.1.2.3
 
