865922042488ac5a5199eb77d8f663e46712f412
nbartol <natalia.b@zend.com>
2013-07-09 14:27:47 +0100

    Bug 412345 - It's not possible to disable project-specific formatter preferences

--- plugins/org.eclipse.php.formatter.ui/src/org/eclipse/php/formatter/ui/preferences/ProfileManager.java
+++ plugins/org.eclipse.php.formatter.ui/src/org/eclipse/php/formatter/ui/preferences/ProfileManager.java
@@ -416,15 +416,7 @@ public class ProfileManager extends Observable implements IProfileManager {
 	public static boolean hasProjectSpecificSettings(IScopeContext context) {
 		IEclipsePreferences prefs = context
 				.getNode(FormatterCorePlugin.PLUGIN_ID);
-		for (final Iterator<String> keyIter = fKeys.iterator(); keyIter
-				.hasNext();) {
-			final String key = keyIter.next();
-			Object val = prefs.get(key, null);
-			if (val != null) {
-				return true;
-			}
-		}
-		return false;
+		return prefs.get(PreferenceConstants.FORMATTER_PROFILE, null) != null;
 	}
 
 	/**
@@ -671,7 +663,7 @@ public class ProfileManager extends Observable implements IProfileManager {
 	 */
 	public void setSelected(String profileId) {
 		final Profile newSelected = fProfiles.get(profileId);
-		if (newSelected != null && !newSelected.equals(fSelected)) {
+		if (newSelected != null) {
 			fSelected = newSelected;
 			notifyObservers(SELECTION_CHANGED_EVENT);
 		}
