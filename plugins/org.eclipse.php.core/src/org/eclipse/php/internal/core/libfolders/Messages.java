72b0ccd820c4d7736baa2c7710cca9b456536162
Kaloyan Raev <kaloyan.r@zend.com>
2013-12-09 15:16:46 +0200

    Bug 424340 - Library Folders
    WORK IN PROGRESS - DO NOT MERGE
    
    Bug: 424340
    Change-Id: Ic5ad8452a2908e8f577cdac11601f515cdadc3b1
    Signed-off-by: Kaloyan Raev <kaloyan.r@zend.com>

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/libfolders/Messages.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Zend Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Zend Technologies - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.core.libfolders;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Kaloyan Raev
+ */
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.php.core.libfolders.messages"; //$NON-NLS-1$
+	public static String AutoDetectLibraryFolderListener_JobName;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/refactoring/Messages.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Zend Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Zend Technologies - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.core.refactoring;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Kaloyan Raev
+ */
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.php.internal.core.refactoring.messages"; //$NON-NLS-1$
+	public static String RenameLibraryFolderChange_name;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
