709354346bffcbf15fe3c887aa0cc788d468447a
Kaloyan Raev <kaloyan.r@zend.com>
2014-01-14 17:21:10 +0200

    Bug 412345 - It's not possible to disable project-specific formatter preferences
    This patch improves the previous fix for the bug. The Code Formatter
    preference page now uses correctly a working copy for the changed
    settings and avoids additional flushes of the preferences on selection
    change, which caused a number of problem in other preference types.
    
    Bug: 412345
    Change-Id: Ic0aacd7ff7f5bd87a774a9d5ba614df80ef1c6ba
    Signed-off-by: Kaloyan Raev <kaloyan.r@zend.com>

--- plugins/org.eclipse.php.formatter.ui/src/org/eclipse/php/formatter/ui/preferences/CodeFormatterConfigurationBlock.java
+++ plugins/org.eclipse.php.formatter.ui/src/org/eclipse/php/formatter/ui/preferences/CodeFormatterConfigurationBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Zend Techologies Ltd.
+ * Copyright (c) 2013, 2014 Zend Techologies Ltd.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,6 @@ import java.util.*;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IScopeContext;
@@ -425,10 +424,13 @@ public class CodeFormatterConfigurationBlock extends
 		if (profiles == null)
 			profiles = new ArrayList<Profile>();
 
+		PreferencesAccess access = PreferencesAccess
+				.getWorkingCopyPreferences(fManager);
+
 		if (project != null) {
-			fCurrContext = new ProjectScope(project);
+			fCurrContext = access.getProjectScope(project);
 		} else {
-			fCurrContext = fInstanceScope;
+			fCurrContext = access.getInstanceScope();
 		}
 
 		fProfileManager = new ProfileManager(profiles, fCurrContext);
