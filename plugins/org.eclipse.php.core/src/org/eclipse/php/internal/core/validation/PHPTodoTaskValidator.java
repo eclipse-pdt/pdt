8c419630bbe1a43e60358c017ea186fd560f5aee
Dawid Pakuła <zulus@w3des.net>
2014-02-12 00:31:39 +0100

    Bug 427907 - Move task validator to DLTK Bug 369620 - TASK TAGS - incorrect regex/detection
    Faster validator
    Dedicated test suite
    
    Change-Id: Ia3bdf4206a2698e40c5c831eca07407d998623bc
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=427907
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=369620

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/validation/PHPTodoTaskValidator.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/validation/PHPTodoTaskValidator.java
@@ -1,200 +1,150 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009,2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Zend Technologies
+ *     Dawid Pakuła [427907]
  *******************************************************************************/
 package org.eclipse.php.internal.core.validation;
 
-import org.eclipse.core.resources.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.dltk.core.ISourceRange;
+import org.eclipse.dltk.core.builder.IBuildContext;
+import org.eclipse.dltk.core.builder.IBuildParticipant;
 import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.IRegion;
 import org.eclipse.php.internal.core.CoreMessages;
 import org.eclipse.php.internal.core.Logger;
-import org.eclipse.php.internal.core.PHPCoreConstants;
-import org.eclipse.php.internal.core.PHPToolkitUtil;
+import org.eclipse.php.internal.core.compiler.ast.nodes.PHPModuleDeclaration;
+import org.eclipse.php.internal.core.documentModel.parser.PHPTokenizer;
 import org.eclipse.php.internal.core.documentModel.parser.regions.PHPRegionTypes;
 import org.eclipse.php.internal.core.documentModel.parser.regions.PhpScriptRegion;
 import org.eclipse.php.internal.core.preferences.TaskTagsProvider;
-import org.eclipse.wst.sse.core.StructuredModelManager;
-import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
 import org.eclipse.wst.sse.core.internal.provisional.tasks.TaskTag;
-import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
-import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocumentRegion;
 import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
 import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegionList;
-import org.eclipse.wst.validation.AbstractValidator;
-import org.eclipse.wst.validation.ValidationResult;
-import org.eclipse.wst.validation.ValidationState;
-import org.eclipse.wst.validation.internal.provisional.core.IReporter;
 import org.eclipse.wst.xml.core.internal.parser.ContextRegionContainer;
 
 /**
- * This Validator creates (and removes) the task markers that will eventually
- * show up in the task view
+ * This Validator creates the task markers that will eventually show up in the
+ * task view
  * 
  * @author Eden K.,2008
- * 
  */
-public class PHPTodoTaskValidator extends AbstractValidator {
+public class PHPTodoTaskValidator implements IBuildParticipant {
 
 	protected TaskTag[] taskTags = null;
 
-	public ValidationResult validate(IResource resource, int kind,
-			ValidationState state, IProgressMonitor monitor) {
-		// process only PHP files
-		if (resource.getType() != IResource.FILE
-				|| !(PHPToolkitUtil.isPhpFile((IFile) resource))) {
-			return null;
+	public void build(IBuildContext context) throws CoreException {
+		if (context.get(IBuildContext.ATTR_MODULE_DECLARATION) == null
+				|| !(context.get(IBuildContext.ATTR_MODULE_DECLARATION) instanceof PHPModuleDeclaration)) {
+			return;
 		}
 
-		ValidationResult result = new ValidationResult();
-		IReporter reporter = result.getReporter(monitor);
-		validateFile(reporter, (IFile) resource, kind);
-
-		return result;
+		try {
+			validateContent(context);
+		} catch (IOException e) {
+			Logger.logException(e);
+		} catch (Throwable e) {
+			Logger.logException(e);
+		}
 	}
 
 	/**
 	 * Search for tasks in the validated file and create a marker for each task
 	 * found
+	 * 
+	 * @throws IOException
 	 */
-	public void validateFile(IReporter reporter, IFile file, int kind) {
+	public void validateContent(IBuildContext context) throws IOException {
 
-		// populate the task tags from the preferences
-		// in case it is empty or it is a clean/full build
-		if (taskTags == null || (kind == 0)) {
-			populateTaskTags(file);
+		final String sourceContents = context.getSourceContents();
+		final IProject project = context.getSourceModule().getScriptProject()
+				.getProject();
+
+		if (taskTags == null) {
+			synchronized (TaskTagsProvider.getInstance()) {
+				TaskTagsProvider taskTagsProvider = TaskTagsProvider
+						.getInstance();
+				getTaskTags(project, taskTagsProvider);
+			}
 		}
 
-		// remove the markers currently existing for this resource
-		// in case of project/folder, the markers are deleted recursively
-		try {
-			file.deleteMarkers(PHPCoreConstants.PHP_MARKER_TYPE, false,
-					IResource.DEPTH_INFINITE);
-		} catch (CoreException e) {
-		}
-		IStructuredModel model = null;
-		try {
-			// desperately try to get the model :) In case it doesn't exist yet,
-			// create it
-			try {
-				model = StructuredModelManager.getModelManager()
-						.getExistingModelForRead(file);
-			} catch (Exception e) {
-				try {
-					model = StructuredModelManager.getModelManager()
-							.createUnManagedStructuredModelFor(file);
-				} catch (Exception e2) {
+		final PHPTokenizer tokenizer = new PHPTokenizer(
+				new ByteArrayInputStream(sourceContents.getBytes()));
+		tokenizer.setProject(project);
+
+		ITextRegion textRegion;
+		int regionStart = 0;
+		while ((textRegion = tokenizer.getNextToken()) != null) {
+			// special handling for php tags inside html
+			regionStart = textRegion.getStart();
+
+			if (textRegion instanceof ContextRegionContainer) {
+				ITextRegion tmp = textRegion;
+				textRegion = extractPhpScriptRegion(textRegion);
+				if (tmp != textRegion) {
+					regionStart += textRegion.getStart();
 				}
 			}
-			if (model == null) {
-				return;
+			if (!(textRegion instanceof PhpScriptRegion)) {
+				continue;
 			}
-			// collect the tasks info and report
-			IStructuredDocumentRegion[] sdRegions = model
-					.getStructuredDocument().getStructuredDocumentRegions();
-			for (IStructuredDocumentRegion structuredDocumentRegion : sdRegions) {
+			PhpScriptRegion scriptRegion = (PhpScriptRegion) textRegion;
+			int offset = regionStart;
+			try {
 
-				IStructuredDocument document = structuredDocumentRegion
-						.getParentDocument();
+				// go over the text regions and look for the tasks
+				ITextRegion[] phpTokens = scriptRegion.getPhpTokens(0,
+						textRegion.getLength());
+				for (int j = 0; j < phpTokens.length; j++) {
+					ITextRegion phpToken = phpTokens[j];
+					if (PHPRegionTypes.PHPDOC_TODO.equals(phpToken.getType())) {
+						// get the task information from the
+						// document
+						offset = regionStart + phpToken.getStart();
+						int length = phpToken.getLength();
 
-				ITextRegionList textRegions = structuredDocumentRegion
-						.getRegions();
-				for (int i = 0; i < textRegions.size(); i++) {
-					ITextRegion textRegion = textRegions.get(i);
-					int regionStart = structuredDocumentRegion
-							.getStartOffset(textRegion);
+						String taskKeyword = sourceContents.substring(offset,
+								offset + phpToken.getLength());
+						int priority = getTaskPriority(taskKeyword);
 
-					// special handling for php tags inside html
-					if (textRegion instanceof ContextRegionContainer) {
-						textRegion = extractPhpScriptRegion(textRegion);
-						regionStart += textRegion.getStart();
-					}
-					// parse the actual script
-					if (textRegion instanceof PhpScriptRegion) {
-						PhpScriptRegion scriptRegion = (PhpScriptRegion) textRegion;
-						try {
-
-							// go over the text regions and look for the tasks
-							ITextRegion[] phpTokens = scriptRegion
-									.getPhpTokens(0, textRegion.getLength());
-							for (int j = 0; j < phpTokens.length; j++) {
-								ITextRegion phpToken = phpTokens[j];
-								if (PHPRegionTypes.PHPDOC_TODO.equals(phpToken
-										.getType())) {
-									// get the task information from the
-									// document
-									int offset = regionStart
-											+ phpToken.getStart();
-									int length = phpToken.getLength();
-
-									String taskKeyword = document.get(offset,
-											phpToken.getLength());
-									int priority = getTaskPriority(taskKeyword);
-
-									// get the actual message for this task - if
-									// any
-									if (j + 1 < phpTokens.length) {
-										for (int k = j + 1; k < phpTokens.length; k++) {
-											ITextRegion phpNextToken = phpTokens[k];
-											if (PHPRegionTypes.PHPDOC_TODO
-													.equals(phpNextToken
-															.getType())) {
-												break;
-											}
-											length = length
-													+ phpNextToken.getLength();
-										}
-									}
-
-									try {
-										reportTask(document, file, reporter,
-												offset, length, priority);
-									} catch (CoreException e) {
-										Logger.logException(
-												"Failed creating task", e); //$NON-NLS-1$
-									}
+						// get the actual message for this task - if
+						// any
+						if (j + 1 < phpTokens.length) {
+							for (int k = j + 1; k < phpTokens.length; k++) {
+								ITextRegion phpNextToken = phpTokens[k];
+								if (PHPRegionTypes.PHPDOC_TODO
+										.equals(phpNextToken.getType())) {
+									break;
 								}
+								length = length + phpNextToken.getLength();
 							}
-						} catch (BadLocationException e) {
-							Logger.logException(CoreMessages
-									.getString("PHPTodoTaskAstParser_0"), e); //$NON-NLS-1$
+						}
+
+						try {
+							reportTask(context, sourceContents, offset, length,
+									priority);
+						} catch (CoreException e) {
+							Logger.logException("Failed creating task", e); //$NON-NLS-1$
 						}
 					}
 				}
-			}
-		} catch (Exception e) {
-			Logger.logException("Failed validating file for tasks " + file, e); //$NON-NLS-1$
-		} finally {
-			if (model != null) {
-				model.releaseFromRead();
-				model = null;
+			} catch (BadLocationException e) {
+				Logger.logException(
+						CoreMessages.getString("PHPTodoTaskAstParser_0"), e); //$NON-NLS-1$
 			}
 
 		}
 
-	}
-
-	/**
-	 * Get the project and populate the task tags list from the preferences
-	 * 
-	 * @param helper
-	 */
-	private void populateTaskTags(IFile file) {
-		TaskTagsProvider taskTagsProvider = TaskTagsProvider.getInstance();
-		getTaskTags(file.getProject(), taskTagsProvider);
-	}
-
-	public void cleanup(IReporter reporter) {
 	}
 
 	/**
@@ -208,17 +158,6 @@ public class PHPTodoTaskValidator extends AbstractValidator {
 		if (taskTags == null) {
 			taskTags = taskTagsProvider.getWorkspaceTaskTags();
 		}
-	}
-
-	/**
-	 * @param delta
-	 *            the IFileDelta containing the file name to get
-	 * @return the IFile
-	 */
-	public IFile getFile(String delta) {
-		IResource res = ResourcesPlugin.getWorkspace().getRoot()
-				.getFile(new Path(delta));
-		return res instanceof IFile ? (IFile) res : null;
 	}
 
 	/**
@@ -250,59 +189,35 @@ public class PHPTodoTaskValidator extends AbstractValidator {
 	 * @throws BadLocationException
 	 * @throws CoreException
 	 */
-	private void reportTask(IStructuredDocument document, IFile file,
-			IReporter taskReporter, int offset, int length, int priority)
-			throws BadLocationException, CoreException {
-		int lineNumber = document.getLineOfOffset(offset);
-
-		String taskStr = getTaskStr(document, lineNumber, offset, length);
+	private void reportTask(IBuildContext context, String sourceContents,
+			int offset, int length, int priority) throws BadLocationException,
+			CoreException {
+		int lineNumber = context.getLineTracker().getLineNumberOfOffset(offset);
+		String taskStr = getTaskStr(context, sourceContents, lineNumber,
+				offset, length);
 		// the end of the string to be highlighted
 		int charEnd = offset + taskStr.length();
 
-		// report the task
-		createMarker(file, taskStr, lineNumber, priority, offset, charEnd);
-
-	}
-
-	/**
-	 * Creates a PHP task marker based on the given information
-	 * 
-	 * @param file
-	 * @param taskStr
-	 * @param lineNumber
-	 * @param priority
-	 * @param offset
-	 * @param charEnd
-	 * @throws CoreException
-	 */
-	private void createMarker(IFile file, String taskStr, int lineNumber,
-			int priority, int offset, int charEnd) throws CoreException {
-		IMarker marker = file.createMarker(PHPCoreConstants.PHP_MARKER_TYPE);
-
-		marker.setAttribute(IMarker.TASK, true);
-
-		marker.setAttribute(IMarker.LINE_NUMBER, lineNumber + 1);
-		marker.setAttribute(IMarker.CHAR_START, offset);
-		marker.setAttribute(IMarker.CHAR_END, charEnd);
-		marker.setAttribute(IMarker.MESSAGE, taskStr);
-		marker.setAttribute(IMarker.USER_EDITABLE, false);
-		marker.setAttribute(IMarker.PRIORITY, priority);
+		context.getProblemReporter().reportProblem(
+				new ValidationProblem(taskStr, ProblemIdentifier.TASK,
+						priority, offset, charEnd, lineNumber));
 	}
 
 	/**
 	 * Gets the Task message from the document
 	 * 
-	 * @param document
+	 * @param context
 	 * @param lineNumber
 	 * @param offset
 	 * @param length
 	 * @return
 	 * @throws BadLocationException
 	 */
-	private String getTaskStr(IStructuredDocument document, int lineNumber,
-			int offset, int length) throws BadLocationException {
+	private String getTaskStr(IBuildContext context, String sourceContents,
+			int lineNumber, int offset, int length) throws BadLocationException {
 		// get line info to identify the end of the task
-		IRegion lineInformation = document.getLineInformation(lineNumber);
+		ISourceRange lineInformation = context.getLineTracker()
+				.getLineInformation(lineNumber);
 		int lineStart = lineInformation.getOffset();
 		int lineEnd = lineStart + lineInformation.getLength();
 
@@ -312,9 +227,8 @@ public class PHPTodoTaskValidator extends AbstractValidator {
 		int tokenEnd = offset + length;
 		int taskEnd = Math.min(tokenEnd, lineEnd);
 
-		String taskStr = document.get(offset, taskEnd - offset);
+		String taskStr = sourceContents.substring(offset, taskEnd);
 		taskStr = taskStr.trim();
-
 		return taskStr;
 
 	}
@@ -338,5 +252,4 @@ public class PHPTodoTaskValidator extends AbstractValidator {
 		}
 		return textRegion;
 	}
-
 }
