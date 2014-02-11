a9eb750ac82a54c7ab6368c3989e05410bddaacb
Dawid Pakuła <zulus@w3des.net>
2014-02-12 00:31:39 +0100

    Bug 427907 - Move task validator to DLTK
    Change-Id: Ia3bdf4206a2698e40c5c831eca07407d998623bc
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=427907

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/validation/ProblemFactory.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Dawid Pakuła
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dawid Pakuła - initial api and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.core.validation;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.dltk.compiler.problem.DefaultProblemFactory;
+import org.eclipse.dltk.compiler.problem.IProblem;
+import org.eclipse.dltk.compiler.problem.IProblemFactory;
+
+/**
+ * @author Dawid zulus Pakula <zulus@w3des.net>
+ */
+public class ProblemFactory extends DefaultProblemFactory implements
+		IProblemFactory {
+	@Override
+	public IMarker createMarker(IResource resource, IProblem problem)
+			throws CoreException {
+		IMarker marker = super.createMarker(resource, problem);
+
+		if (problem instanceof ValidationProblem && problem.isTask()) {
+			marker.setAttribute(IMarker.PRIORITY,
+					((ValidationProblem) problem).getPriority());
+		}
+
+		return marker;
+	}
+
+	@Override
+	protected boolean isValidMarkerType(String markerType) {
+		return markerType.startsWith(ProblemIdentifier.MARKER_PREFIX);
+	}
+
+	@Override
+	protected String getTaskMarkerType() {
+		return ProblemIdentifier.MARKER_TASK_ID;
+	}
+}
