ee7c1dd33ff12685eef60e621895bf8b26cbb7fb
Dawid Pakula <zulus@w3des.net>
2014-02-18 20:14:14 +0100

    Bug 418890 - [phpDoc] Problem with @inheritdoc keyword

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/documentation/PHPDocumentationContentAccess.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/documentation/PHPDocumentationContentAccess.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 IBM Corporation and others.
+ * Copyright (c) 2009, 2013, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Zend Technologies
+ *     Dawid Pakuła [418890]
  *******************************************************************************/
 package org.eclipse.php.internal.ui.documentation;
 
@@ -1237,7 +1238,9 @@ public class PHPDocumentationContentAccess {
 	private void handleBlockTags(List tags) {
 		for (Iterator iter = tags.iterator(); iter.hasNext();) {
 			PHPDocTag tag = (PHPDocTag) iter.next();
-			if (tag.getTagKind() == PHPDocTag.VAR) {
+			if (tag.getTagKind() == PHPDocTag.INHERITDOC) {
+				continue;
+			} else if (tag.getTagKind() == PHPDocTag.VAR) {
 				handleBlockTagTitle("Type"); //$NON-NLS-1$
 			} else {
 				handleBlockTagTitle(PHPDocTag.getTagKind(tag.getTagKind()));
