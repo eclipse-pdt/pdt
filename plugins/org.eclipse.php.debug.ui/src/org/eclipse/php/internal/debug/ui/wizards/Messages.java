fbc23b7e50c1d219cd2290899c6302d2748056cc
wgalanciak <wojciech.galanciak@gmail.com>
2014-04-02 15:42:10 +0200

    Bug 431806 - [Refactoring] Remove Path Mapping from PHP executable
    Change-Id: I31a21b5dfdd71f26a820cda4c0c921596f1ad9d1
    Signed-off-by: wgalanciak <wojciech.galanciak@gmail.com>

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/wizards/Messages.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/wizards/Messages.java
@@ -4,12 +4,6 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.php.internal.debug.ui.wizards.messages"; //$NON-NLS-1$
-	public static String PathMapperCompositeFragment_0;
-	public static String PathMapperCompositeFragment_1;
-	public static String PathMapperCompositeFragment_2;
-	public static String PathMapperCompositeFragment_3;
-	public static String PathMapperCompositeFragment_4;
-	public static String PathMapperCompositeFragment_5;
 	public static String PHPExeEditDialog_1;
 	public static String PHPExeWizard_0;
 	static {
