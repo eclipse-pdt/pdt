58520af7f4b3776154bc101dfca851c2f146bf21
Dawid Pakula <zulus@w3des.net>
2014-04-26 22:54:32 +0200

    Bug 430049 - Deprecation highlighter is too slow
    Introduce PHPCachedTypeInferencer and binding sessions
    Use DLTK Element Visitor
    
    Change-Id: Iad50a08a07fd8a6492ea6d794f4d4da4a8f8ce56
    Signed-off-by: Dawid Pakula <zulus@w3des.net>

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/typeinference/PHPCachedTypeInferencer.java
@@ -0,0 +1,209 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *     Dawid Pakuła - cached inferencer
+ *******************************************************************************/
+package org.eclipse.php.internal.core.typeinference;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.dltk.ti.*;
+import org.eclipse.dltk.ti.goals.*;
+import org.eclipse.dltk.ti.statistics.IEvaluationStatisticsRequestor;
+import org.eclipse.dltk.ti.types.IEvaluatedType;
+import org.eclipse.php.internal.core.typeinference.evaluators.PHPGoalEvaluatorFactory;
+import org.eclipse.php.internal.core.typeinference.goals.ClassVariableDeclarationGoal;
+import org.eclipse.php.internal.core.typeinference.goals.MethodElementReturnTypeGoal;
+import org.eclipse.php.internal.core.typeinference.goals.phpdoc.PHPDocClassVariableGoal;
+import org.eclipse.php.internal.core.typeinference.goals.phpdoc.PHPDocMethodReturnTypeGoal;
+
+/**
+ * This class allow persist goal results between inference sessions
+ * 
+ * @author Dawid zulus Pakula <zulus@w3des.net>
+ */
+public class PHPCachedTypeInferencer implements IPHPTypeInferencer {
+	final private GoalEngine engine;
+	final private Map<IGoal, Result> cache = new ConcurrentHashMap<IGoal, Result>();
+	final private IEvaluationStatisticsRequestor stat;
+
+	private class Result {
+		final Object result;
+
+		public Result(Object result) {
+			this.result = result;
+		}
+	}
+
+	private class CachedEvaluatorFactory extends PHPGoalEvaluatorFactory {
+		@Override
+		public GoalEvaluator createEvaluator(IGoal goal) {
+			if (cache.containsKey(goal)) {
+				return new FakeGoalEvaluator(goal, cache.get(goal));
+			}
+
+			GoalEvaluator eval = super.createEvaluator(goal);
+			if (eval == null) { // std evaluator
+				if (goal.getClass() == FieldReferencesGoal.class) {
+					return new FieldReferencesGoalEvaluator(goal);
+				} else if (goal.getClass() == MethodCallsGoal.class) {
+					return new MethodCallsGoalEvaluator(goal);
+				}
+
+				return new NullGoalEvaluator(goal);
+			} else {
+				return eval;
+			}
+		}
+	}
+
+	private class FakeGoalEvaluator extends GoalEvaluator {
+		final Result result;
+
+		public FakeGoalEvaluator(IGoal goal, Result result) {
+			super(goal);
+			this.result = result;
+		}
+
+		@Override
+		public IGoal[] init() {
+			return IGoal.NO_GOALS;
+		}
+
+		@Override
+		public IGoal[] subGoalDone(IGoal subgoal, Object result, GoalState state) {
+			return IGoal.NO_GOALS;
+		}
+
+		@Override
+		public Object produceResult() {
+			return result.result;
+		}
+	}
+
+	private class StatRequestor implements IEvaluationStatisticsRequestor {
+
+		@Override
+		public void evaluationStarted(IGoal rootGoal) {
+
+		}
+
+		@Override
+		public void goalStateChanged(IGoal goal, GoalState state,
+				GoalState oldState) {
+
+		}
+
+		@Override
+		public void goalEvaluatorAssigned(IGoal goal, GoalEvaluator evaluator) {
+		}
+
+		@Override
+		public void evaluatorInitialized(GoalEvaluator evaluator,
+				IGoal[] subgoals, long time) {
+		}
+
+		@Override
+		public void evaluatorReceivedResult(GoalEvaluator evaluator,
+				IGoal finishedGoal, IGoal[] newSubgoals, long time) {
+		}
+
+		@Override
+		public void evaluatorProducedResult(GoalEvaluator evaluator,
+				Object result, long time) {
+			if (!(evaluator instanceof FakeGoalEvaluator)) {
+				cache.put(evaluator.getGoal(), new Result(result));
+			}
+		}
+
+	}
+
+	public PHPCachedTypeInferencer() {
+		engine = new GoalEngine(new CachedEvaluatorFactory());
+		stat = new StatRequestor();
+	}
+
+	@Override
+	public IEvaluatedType evaluateTypePHPDoc(AbstractTypeGoal goal, int timeout) {
+		return evaluateType(goal, new PHPDocGoalsPruner(timeout));
+	}
+
+	@Override
+	public IEvaluatedType evaluateTypePHPDoc(AbstractTypeGoal goal) {
+		return evaluateTypePHPDoc(goal, 3000);
+	}
+
+	@Override
+	public IEvaluatedType evaluateTypeHeavy(AbstractTypeGoal goal, int timeout) {
+		return evaluateType(goal, new PHPDocGoalsPruner(timeout));
+	}
+
+	@Override
+	public IEvaluatedType evaluateType(AbstractTypeGoal goal) {
+		return evaluateType(goal, null);
+	}
+
+	@Override
+	public IEvaluatedType evaluateType(AbstractTypeGoal goal, int timeLimit) {
+		return evaluateType(goal, new TimelimitPruner(timeLimit));
+	}
+
+	@Override
+	public IEvaluatedType evaluateType(AbstractTypeGoal goal, IPruner pruner) {
+		if (!cache.containsKey(goal)) {
+			synchronized (engine) {
+				cache.put(goal,
+						new Result(engine.evaluateGoal(goal, pruner, stat)));
+			}
+		}
+
+		return (IEvaluatedType) cache.get(goal).result;
+	}
+
+	/**
+	 * This class prunes all PHP goals except for PHPDoc based goals
+	 */
+	static class HeavyGoalsPruner extends TimelimitPruner {
+
+		public HeavyGoalsPruner(long timeLimit) {
+			super(timeLimit);
+		}
+
+		public boolean prune(IGoal goal, EvaluatorStatistics stat) {
+			// here are heavy goals pruned
+			if (goal instanceof MethodElementReturnTypeGoal
+					|| goal instanceof ClassVariableDeclarationGoal) {
+				return true;
+			}
+			return super.prune(goal, stat);
+		}
+	}
+
+	/**
+	 * This class prunes all PHPDoc based goals
+	 */
+	static class PHPDocGoalsPruner extends TimelimitPruner {
+
+		public PHPDocGoalsPruner(long timeLimit) {
+			super(timeLimit);
+		}
+
+		public boolean prune(IGoal goal, EvaluatorStatistics stat) {
+			// here are PHPDoc (liteweight) goals pruned
+			if (goal instanceof PHPDocMethodReturnTypeGoal
+					|| goal instanceof PHPDocClassVariableGoal) {
+				return true;
+			}
+			return super.prune(goal, stat);
+		}
+	}
+
+}
--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/highlighter/AbstractSemanticHighlighting.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/highlighter/AbstractSemanticHighlighting.java
@@ -84,10 +84,12 @@ public abstract class AbstractSemanticHighlighting implements
 
 	public Position[] consumes(Program program) {
 		if (program != null) {
+			program.getAST().getBindingResolver().startBindingSession();
 			list = new ArrayList<Position>();
 			sourceModule = program.getSourceModule();
 			AbstractSemanticApply apply = getSemanticApply();
 			program.accept(apply);
+			program.getAST().getBindingResolver().stopBindingSession();
 			return list.toArray(new Position[list.size()]);
 		}
 		return new Position[0];
@@ -122,7 +124,10 @@ public abstract class AbstractSemanticHighlighting implements
 		Program program = null;
 		if (sourceModule != null) {
 			try {
-				program = SharedASTProvider.getAST(sourceModule,SharedASTProvider.WAIT_ACTIVE_ONLY, null);
+				// Wait active_only. Sometimes highliters are called without
+				// reconciling
+				program = SharedASTProvider.getAST(sourceModule,
+						SharedASTProvider.WAIT_ACTIVE_ONLY, null);
 			} catch (ModelException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
