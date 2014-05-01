2880145b3d4701297ed4e18e56d027995c89a23f
Dawid Pakula <zulus@w3des.net>
2014-05-01 13:34:01 +0200

    Bug 255164 - [regression] Both file constants and class constants have wrong icons in PHP Exporer
    Introduce PHPModelLabelProvider and PHPScriptElementLabelProvider
    Deprecation icon
    
    Change-Id: Ia203730f338447daa5f62f8638efe1c2e7e7c840
    Signed-off-by: Dawid Pakula <zulus@w3des.net>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/util/PHPElementImageDescriptor.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/util/PHPElementImageDescriptor.java
@@ -12,6 +12,7 @@
 package org.eclipse.php.internal.ui.util;
 
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.dltk.ui.DLTKPluginImages;
 import org.eclipse.jface.resource.CompositeImageDescriptor;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.php.internal.ui.PHPUiPlugin;
@@ -155,7 +156,7 @@ public class PHPElementImageDescriptor extends CompositeImageDescriptor {
 
 		if ((fFlags & DEPRECATED) != 0) { // over the full image
 			Point size = getSize();
-			ImageData data = getImageData(PHPPluginImages.DESC_OVR_DEPRECATED);
+			ImageData data = getImageData(DLTKPluginImages.DESC_OVR_DEPRECATED);
 			drawImage(data, 0, size.y - data.height);
 		}
 		drawImage(bg, 0, 0);
@@ -185,6 +186,11 @@ public class PHPElementImageDescriptor extends CompositeImageDescriptor {
 		}
 		if ((fFlags & CONSTANT) != 0) {
 			ImageData data = getImageData(PHPPluginImages.DESC_OVR_CONSTANT);
+			x -= data.width;
+			drawImage(data, x, 0);
+		}
+		if ((fFlags & CONSTRUCTOR) != 0) {
+			ImageData data = getImageData(DLTKPluginImages.DESC_OVR_CONSTRUCTOR);
 			x -= data.width;
 			drawImage(data, x, 0);
 		}
