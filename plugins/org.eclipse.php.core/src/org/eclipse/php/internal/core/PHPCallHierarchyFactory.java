47d2429742e15a7091be3162f0d497db0886ddfa
Dawid Pakuła <zulus@w3des.net>
2014-06-22 23:25:00 +0200

    Bug 346785 - Call hierarchy implementation
    Change-Id: I8bb3004314dab39f77b5ed0b1630485dc90a98a1
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/PHPCallHierarchyFactory.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Dawid Pakuła - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.core;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.dltk.core.ICallHierarchyFactory;
+import org.eclipse.dltk.core.ICallProcessor;
+import org.eclipse.dltk.core.ICalleeProcessor;
+import org.eclipse.dltk.core.IMethod;
+import org.eclipse.dltk.core.search.IDLTKSearchScope;
+import org.eclipse.php.internal.core.search.PHPCallProcessor;
+import org.eclipse.php.internal.core.search.PHPCalleeProcessor;
+
+public class PHPCallHierarchyFactory implements ICallHierarchyFactory {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.dltk.core.ICallHierarchyFactory#createCalleeProcessor(org
+	 * .eclipse.dltk.core.IMethod, org.eclipse.core.runtime.IProgressMonitor,
+	 * org.eclipse.dltk.core.search.IDLTKSearchScope)
+	 */
+	@Override
+	public ICalleeProcessor createCalleeProcessor(IMethod method,
+			IProgressMonitor monitor, IDLTKSearchScope scope) {
+		return new PHPCalleeProcessor(method, monitor, scope);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.dltk.core.ICallHierarchyFactory#createCallProcessor()
+	 */
+	@Override
+	public ICallProcessor createCallProcessor() {
+		return new PHPCallProcessor();
+	}
+
+}
---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/search/PHPCallProcessor.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Dawid Pakuła - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.core.search;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.dltk.ast.references.SimpleReference;
+import org.eclipse.dltk.core.ICallProcessor;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.search.*;
+import org.eclipse.php.internal.core.Logger;
+
+public class PHPCallProcessor implements ICallProcessor {
+	private final static String EMPTY_STRING = ""; //$NON-NLS-1$
+	private SearchEngine engine = new SearchEngine();
+
+	private class Requestor extends SearchRequestor {
+
+		public Map<Object, Object> result = new HashMap<Object, Object>();
+
+		@Override
+		public void acceptSearchMatch(SearchMatch match) throws CoreException {
+			if ((match.getAccuracy() != SearchMatch.A_ACCURATE)) {
+				return;
+			}
+
+			if (match.isInsideDocComment()) {
+				return;
+			}
+
+			if (match.getElement() != null
+					&& match.getElement() instanceof IModelElement) {
+				IModelElement member = (IModelElement) match.getElement();
+				SimpleReference ref = new SimpleReference(match.getOffset(),
+						match.getOffset() + match.getLength(), EMPTY_STRING);
+				result.put(ref, member);
+			}
+		}
+
+	}
+
+	@Override
+	public Map process(IModelElement parent, IModelElement member,
+			IDLTKSearchScope scope, IProgressMonitor monitor) {
+
+		SearchPattern pattern = SearchPattern.createPattern(member,
+				IDLTKSearchConstants.METHOD, IDLTKSearchConstants.REFERENCES,
+				scope.getLanguageToolkit());
+		Requestor req = new Requestor();
+		try {
+			engine.search(pattern, new SearchParticipant[] { SearchEngine
+					.getDefaultSearchParticipant() }, scope, req, monitor);
+		} catch (CoreException e) {
+			Logger.logException(e);
+		}
+
+		return req.result;
+	}
+
+}
