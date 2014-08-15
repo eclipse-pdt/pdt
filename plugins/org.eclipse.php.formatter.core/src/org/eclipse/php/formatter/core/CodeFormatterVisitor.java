808afc1e65163afec78c19501cc1afda9582043c
Thierry BLIND <thierryblind@msn.com>
2014-08-15 14:07:07 +0200

    Bug 441825 - Code formatter does wrong line size calculation when PHPDoc comments are preceded by single-line comments
    Change-Id: Ic63a3bb665de388c22047500a8b13a92a91faacb
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.formatter.core/src/org/eclipse/php/formatter/core/CodeFormatterVisitor.java
+++ plugins/org.eclipse.php.formatter.core/src/org/eclipse/php/formatter/core/CodeFormatterVisitor.java
@@ -845,6 +845,9 @@ public class CodeFormatterVisitor extends AbstractVisitor implements
 		return lastPosition;
 	}
 
+	// TODO: Do correct comment placement
+	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=440209
+	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=440820
 	private void handleComments(
 			int offset,
 			int end,
@@ -866,7 +869,7 @@ public class CodeFormatterVisitor extends AbstractVisitor implements
 					.next();
 			int commentStartLine = document.getLineOfOffset(comment
 					.sourceStart() + offset);
-			int position = replaceBuffer.indexOf(lineSeparator);
+			int position = replaceBuffer.lastIndexOf(lineSeparator);
 			boolean startAtFirstColumn = (document
 					.getLineOffset(commentStartLine) == comment.sourceStart()
 					+ offset);
@@ -882,6 +885,8 @@ public class CodeFormatterVisitor extends AbstractVisitor implements
 					indentOnFirstColumn = false;
 					IRegion startLinereg = document
 							.getLineInformation(startLine);
+					// TODO: Do line width calculation based on the
+					// formatted content instead of the original content
 					lineWidth = comment.sourceStart() + offset
 							- startLinereg.getOffset();
 					if (position >= 0) {
@@ -1068,6 +1073,8 @@ public class CodeFormatterVisitor extends AbstractVisitor implements
 				}
 
 				start = comment.sourceEnd() + offset;
+				// https://bugs.eclipse.org/bugs/show_bug.cgi?id=441825
+				lineWidth = 0;
 				break;
 			case org.eclipse.php.internal.core.compiler.ast.nodes.Comment.TYPE_PHPDOC:
 				previousCommentIsSingleLine = false;
@@ -1236,6 +1243,7 @@ public class CodeFormatterVisitor extends AbstractVisitor implements
 				// example while /* kuku */ ( /* kuku */$a > 0 )
 				if (getBufferFirstChar(0) != '\0') {
 					replaceBuffer.setLength(0);
+					// TODO: lineWidth should be updated here
 					resetEnableStatus(document.get(comment.sourceStart()
 							+ offset,
 							comment.sourceEnd() - comment.sourceStart()));
@@ -1258,6 +1266,8 @@ public class CodeFormatterVisitor extends AbstractVisitor implements
 					indentOnFirstColumn = false;
 					IRegion startLinereg = document
 							.getLineInformation(startLine);
+					// TODO: Do line width calculation based on the
+					// formatted content instead of the original content
 					lineWidth = comment.sourceStart() + offset
 							- startLinereg.getOffset();
 					if (position >= 0) {
