f51f67607cbdd03a4ec951d166753497ca572d5b
Dawid Pakuła <zulus@w3des.net>
2014-08-18 17:43:18 +0200

    Bug 441972 - Quick Outline for Core API files hangs PDT
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/ASTProvider.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/ASTProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,7 @@ import java.util.Set;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.*;
+import org.eclipse.dltk.core.IExternalSourceModule;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.php.internal.core.PHPVersion;
 import org.eclipse.php.internal.core.ast.nodes.ASTNode;
@@ -556,7 +557,9 @@ public final class ASTProvider {
 		synchronized (fReconcileLock) {
 			return javaElement != null
 					&& javaElement.equals(fReconcilingJavaElement)
-					&& fIsReconciling && !isValidatorDisabled(javaElement);
+					&& fIsReconciling
+					&& !(javaElement instanceof IExternalSourceModule)
+					&& !isValidatorDisabled(javaElement);
 		}
 	}
 
