bee0bb7689d03feb15447bdc2d607ae842781a9a
Dawid Pakuła <zulus@w3des.net>
2015-02-20 01:23:34 +0100

    Bug 407074 - Use statements in outline are twice present
    Change-Id: I33d64f8cbcdcd19dc51df2f5f0f06ffd50f626f2
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/filters/UseStatementsFilter.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Dawid Pakuła and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Dawid Pakuła - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.filters;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.php.internal.ui.outline.PHPOutlineContentProvider.UseStatementsNode;
+
+public class UseStatementsFilter extends ViewerFilter {
+
+	public UseStatementsFilter() {
+	}
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if (element instanceof UseStatementsNode
+				|| parentElement instanceof UseStatementsNode) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
