25a1f17e76f6ce6ac1cee68e178abd031d01996d
Dawid Pakuła <zulus@w3des.net>
2015-02-20 01:23:34 +0100

    Bug 407074 - Use statements in outline are twice present
    Change-Id: I33d64f8cbcdcd19dc51df2f5f0f06ffd50f626f2
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/filters/ImportDeclarationFilter.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Dawid Pakuła and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Dawid Pakuła - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.filters;
+
+/**
+ * Import Declaration Filter to avoid conflict with user filters
+ */
+@SuppressWarnings("restriction")
+public class ImportDeclarationFilter extends
+		org.eclipse.dltk.internal.ui.filters.ImportDeclarationFilter {
+
+	public ImportDeclarationFilter() {
+	}
+}
