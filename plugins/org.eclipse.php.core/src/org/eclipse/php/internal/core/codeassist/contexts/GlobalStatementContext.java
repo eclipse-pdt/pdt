e187d437e7ae3d516473e92260c85725f7099c9f
Michal Niewrzal <michal.n@zend.com>
2015-03-23 17:40:18 +0100

    Bug 459947 - Autoassist with global function names inside class body
    Change-Id: I9f840d31c962f1cbe1967838ad9472bbfa63bb14
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/contexts/GlobalStatementContext.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/contexts/GlobalStatementContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009,2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,12 @@
 package org.eclipse.php.internal.core.codeassist.contexts;
 
 import org.eclipse.dltk.core.*;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.php.core.compiler.PHPFlags;
 import org.eclipse.php.internal.core.PHPCorePlugin;
+import org.eclipse.php.internal.core.documentModel.parser.regions.PHPRegionTypes;
+import org.eclipse.php.internal.core.format.PHPHeuristicScanner;
+import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
 
 /**
  * This context represents state when staying in a top level statement. <br/>
@@ -39,6 +43,36 @@ public class GlobalStatementContext extends AbstractGlobalStatementContext {
 		// check whether enclosing element is not a class
 		try {
 			IModelElement enclosingElement = sourceModule.getElementAt(offset);
+			if (enclosingElement == null) {
+				int actualOffset = offset - 1;
+				try {
+					PHPHeuristicScanner scanner = PHPHeuristicScanner
+							.createHeuristicScanner(getDocument(),
+									actualOffset, true);
+					int index = scanner.findOpeningPeer(actualOffset,
+							PHPHeuristicScanner.UNBOUND,
+							PHPHeuristicScanner.LBRACE,
+							PHPHeuristicScanner.RBRACE);
+					if (index != PHPHeuristicScanner.NOT_FOUND) {
+						ITextRegion textRegion = getPHPToken(index);
+						while (textRegion.getStart() != 0
+								&& textRegion.getType() != PHPRegionTypes.PHP_CURLY_CLOSE) {
+							if (textRegion.getType() == PHPRegionTypes.PHP_CLASS
+									|| textRegion.getType() == PHPRegionTypes.PHP_INTERFACE
+									|| textRegion.getType() == PHPRegionTypes.PHP_TRAIT
+									|| textRegion.getType() == PHPRegionTypes.PHP_FUNCTION) {
+								return false;
+							}
+
+							actualOffset = textRegion.getStart() - 1;
+							textRegion = getPhpScriptRegion().getPhpToken(
+									actualOffset);
+						}
+					}
+				} catch (BadLocationException e1) {
+					PHPCorePlugin.log(e1);
+				}
+			}
 			while (enclosingElement instanceof IField) {
 				enclosingElement = enclosingElement.getParent();
 			}
