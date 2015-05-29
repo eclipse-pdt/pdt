0ed36e113b51853e80e12cb318deab6f26c6f1df
Bartlomiej Laczkowski <bartlomiej.l@zend.com>
2015-05-29 22:23:32 +0200

    Bug 468285 - [Regression] Installed debugger type is not recognized when adding PHP Executables
    Change-Id: I8efd84225ef11bc40e99918fa2a96514bfc26c37
    Signed-off-by: Bartlomiej Laczkowski <bartlomiej.l@zend.com>

--- plugins/org.eclipse.php.server.ui/src/org/eclipse/php/internal/server/ui/ServerEditDialog.java
+++ plugins/org.eclipse.php.server.ui/src/org/eclipse/php/internal/server/ui/ServerEditDialog.java
@@ -118,7 +118,7 @@ public class ServerEditDialog extends TitleAreaDialog implements
 				setTitle(fragment.getTitle());
 				setDescription(fragment.getDescription());
 				setImageDescriptor(fragment.getImageDescriptor());
-				fragment.validate();
+				fragment.setData(server);
 			}
 		});
 		getShell().setText(
