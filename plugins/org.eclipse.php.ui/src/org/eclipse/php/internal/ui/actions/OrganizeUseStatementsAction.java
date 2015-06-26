a73fd7b82fa2f9354e86311e3624c8dc64f3f3f9
Yannick de Lange <yannick.l.88@gmail.com>
2015-06-26 15:28:56 +0200

    Bug 354782 - Added support for organizing use statements
    Feature to support sorting use-statements alphabatically (following the
    phpcs standard) and also remove statements that are unused.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=354782
    Signed-off-by: Yannick de Lange <yannick.l.88@gmail.com>
    Change-Id: I632c5129da9336e3ace131863a283aeaabeddbc4

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/OrganizeUseStatementsAction.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2015 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *     Yannick de Lange <yannick.l.88@gmail.com>
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.actions;
+
+import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.SourceParserUtil;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.php.internal.core.corext.util.DocumentUtils;
+import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+/**
+ * Our sample action implements workbench action delegate. The action proxy will
+ * be created by the workbench and shown in the UI. When the user tries to use
+ * the action, this delegate will be created and execution will be delegated to
+ * it.
+ *
+ * @see IWorkbenchWindowActionDelegate
+ * @author Yannick de Lange <yannickl88@gmail.com>
+ */
+public class OrganizeUseStatementsAction
+		implements IWorkbenchWindowActionDelegate {
+	private IWorkbenchWindow window;
+
+	/**
+	 * The constructor.
+	 */
+	public OrganizeUseStatementsAction() {
+	}
+
+	/**
+	 * The action has been activated. The argument of the method represents the
+	 * 'real' action sitting in the workbench UI.
+	 *
+	 * @see IWorkbenchWindowActionDelegate#run
+	 */
+	public void run(IAction action) {
+		IEditorPart input = window.getActivePage().getActiveEditor();
+
+		if (input instanceof PHPStructuredEditor) {
+			PHPStructuredEditor ite = (PHPStructuredEditor) input;
+			IDocument doc = ite.getDocumentProvider()
+					.getDocument(ite.getEditorInput());
+
+			ModuleDeclaration moduleDeclaration = SourceParserUtil
+					.getModuleDeclaration(
+							(ISourceModule) ite.getModelElement());
+
+			DocumentUtils.sortUseStatements(moduleDeclaration, doc);
+		}
+	}
+
+	/**
+	 * Selection in the workbench has been changed. We can change the state of
+	 * the 'real' action here if we want, but this can only happen after the
+	 * delegate has been created.
+	 *
+	 * @see IWorkbenchWindowActionDelegate#selectionChanged
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	/**
+	 * We can use this method to dispose of any system resources we previously
+	 * allocated.
+	 *
+	 * @see IWorkbenchWindowActionDelegate#dispose
+	 */
+	public void dispose() {
+	}
+
+	/**
+	 * We will cache window object in order to be able to provide parent shell
+	 * for the message dialog.
+	 *
+	 * @see IWorkbenchWindowActionDelegate#init
+	 */
+	public void init(IWorkbenchWindow window) {
+		this.window = window;
+	}
+}
