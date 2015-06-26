8fd9f3af0511f2ae75401e7accf6a567f3ed5f97
Yannick de Lange <yannick.l.88@gmail.com>
2015-06-26 15:28:56 +0200

    Bug 354782 - Added support for organizing use statements
    Feature to support sorting use-statements alphabatically (following the
    phpcs standard) and also remove statements that are unused.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=354782
    Signed-off-by: Yannick de Lange <yannick.l.88@gmail.com>
    Change-Id: I632c5129da9336e3ace131863a283aeaabeddbc4

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/core/organize/UseStatementBlock.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
+package org.eclipse.php.core.organize;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.eclipse.php.internal.core.compiler.ast.nodes.UseStatement;
+
+/**
+ * @author Yannick de Lange <yannickl88@gmail.com>
+ */
+public class UseStatementBlock {
+	public final int start, end, length;
+	private final Vector<UseStatement> statements;
+
+	public UseStatementBlock(int start, int end,
+			Vector<UseStatement> statements) {
+		this.start = start;
+		this.end = end;
+		this.length = end - start;
+		this.statements = statements;
+	}
+
+	public List<UseStatement> getStatements() {
+		return new Vector<UseStatement>(statements);
+	}
+}
