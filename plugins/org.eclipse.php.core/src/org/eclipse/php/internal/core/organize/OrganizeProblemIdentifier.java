80c60c304ce843444b558e952da1035b2b740973
Yannick de Lange <yannick.l.88@gmail.com>
2015-06-26 15:28:56 +0200

    Bug 354782 - Added support for organizing use statements
    Feature to support sorting use-statements alphabatically (following the
    phpcs standard) and also remove statements that are unused.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=354782
    Signed-off-by: Yannick de Lange <yannick.l.88@gmail.com>
    Change-Id: I632c5129da9336e3ace131863a283aeaabeddbc4

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/organize/OrganizeProblemIdentifier.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/organize/OrganizeProblemIdentifier.java
@@ -1,30 +1,32 @@
 /*******************************************************************************
- * Copyright (c) 2014 Zend Technologies and others.
+ * Copyright (c) 2015 Zend Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Zend Technologies - initial API and implementation
+ *     Yannick de Lange <yannick.l.88@gmail.com>
  *******************************************************************************/
-package org.eclipse.php.internal.core.compiler.ast.parser;
+package org.eclipse.php.internal.core.organize;
 
 import org.eclipse.dltk.compiler.problem.IProblemIdentifier;
 import org.eclipse.dltk.compiler.problem.IProblemIdentifierExtension;
 import org.eclipse.php.internal.core.PHPCorePlugin;
 
 /**
- * Class provides "PHP Syntax Error" marker type for DLTK problem mechanism.
- * 
- * @author Michal Niewrzal
+ * Class provides "Organize Syntax Error" marker type for DLTK problem
+ * mechanism.
+ *
+ * @author Yannick de Lange
  */
-public enum PhpProblemIdentifier
+public enum OrganizeProblemIdentifier
 	implements IProblemIdentifier,IProblemIdentifierExtension {
 
-	SYNTAX;
+	USE_STATEMENTS;
 
-	public static final String MARKER_TYPE_ID = "org.eclipse.php.core.phpproblemmarker"; //$NON-NLS-1$
+	public static final String MARKER_TYPE_ID = "org.eclipse.php.core.organizeproblemmarker"; //$NON-NLS-1$
 
 	@Override
 	public String contributor() {
