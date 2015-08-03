e0084fe811c1468378240274d9ca814defd56d53
Bartlomiej Laczkowski <bartlomiej.l@zend.com>
2015-08-03 19:55:28 +0200

    Bug 474163 - [Zend Debugger] Add possibility to handle externally triggered debug sessions
    Change-Id: Icba4c71d9cac9d50321ef4538e2bfbc0b8c71d6e
    Signed-off-by: Bartlomiej Laczkowski <bartlomiej.l@zend.com>

---
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/launching/PHPRemoteLaunchConfigurationDelegate.java
@@ -0,0 +1,286 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Zend Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Zend Technologies - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.debug.core.launching;
+
+import org.eclipse.core.resources.IMarkerDelta;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.debug.core.*;
+import org.eclipse.debug.core.model.*;
+import org.eclipse.php.debug.core.debugger.parameters.IDebugParametersKeys;
+import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
+import org.eclipse.php.internal.debug.core.PHPDebugPlugin;
+import org.eclipse.php.internal.debug.core.preferences.PHPProjectPreferences;
+import org.eclipse.php.internal.debug.core.zend.communication.DebuggerCommunicationDaemon;
+import org.eclipse.php.internal.server.core.Server;
+
+/**
+ * A launch configuration delegate that is responsible for handling  
+ * debug session that were remotely initialized.
+ * 
+ * @author Shalom Gibly
+ */
+@SuppressWarnings("restriction")
+public class PHPRemoteLaunchConfigurationDelegate
+		implements ILaunchConfigurationDelegate2 {
+
+	private ILaunch launch;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.
+	 * eclipse.debug.core.ILaunchConfiguration, java.lang.String,
+	 * org.eclipse.debug.core.ILaunch,
+	 * org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void launch(ILaunchConfiguration configuration, String mode,
+			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		boolean runWithDebug = configuration
+				.getAttribute(IPHPDebugConstants.RUN_WITH_DEBUG_INFO, true);
+		this.launch = launch;
+		if (mode.equals(ILaunchManager.RUN_MODE) && !runWithDebug) {
+			return;
+		}
+		IProject proj = null;
+		String fileName = configuration.getAttribute(Server.FILE_NAME,
+				(String) null);
+		if (fileName != null) {
+			// Get the project from the file name
+			IPath filePath = new Path(fileName);
+			try {
+				proj = ResourcesPlugin.getWorkspace().getRoot()
+						.getProject(filePath.segment(0));
+			} catch (Throwable t) {
+			}
+		}
+		ILaunchConfigurationWorkingCopy wc = configuration.getWorkingCopy();
+		if (proj != null) {
+			String project = proj.getFullPath().toString();
+			wc.setAttribute(IPHPDebugConstants.PHP_Project, project);
+		}
+		wc.doSave();
+		String URL = configuration.getAttribute(Server.BASE_URL, ""); //$NON-NLS-1$
+		if (mode.equals(ILaunchManager.DEBUG_MODE) || runWithDebug == true) {
+			boolean isStopAtFirstLine = wc.getAttribute(
+					IDebugParametersKeys.FIRST_LINE_BREAKPOINT,
+					PHPProjectPreferences.getStopAtFirstLine(proj));
+			int requestPort = PHPDebugPlugin
+					.getDebugPort(DebuggerCommunicationDaemon.ZEND_DEBUGGER_ID);
+			// Fill all debug attributes:
+			launch.setAttribute(IDebugParametersKeys.PORT,
+					Integer.toString(requestPort));
+			launch.setAttribute(IDebugParametersKeys.WEB_SERVER_DEBUGGER,
+					Boolean.toString(true));
+			launch.setAttribute(IDebugParametersKeys.FIRST_LINE_BREAKPOINT,
+					Boolean.toString(isStopAtFirstLine));
+			launch.setAttribute(IDebugParametersKeys.ORIGINAL_URL, URL);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.core.model.ILaunchConfigurationDelegate2#buildForLaunch
+	 * (org.eclipse.debug.core.ILaunchConfiguration, java.lang.String,
+	 * org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public boolean buildForLaunch(ILaunchConfiguration configuration,
+			String mode, IProgressMonitor monitor) throws CoreException {
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate2#
+	 * finalLaunchCheck (org.eclipse.debug.core.ILaunchConfiguration,
+	 * java.lang.String, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public boolean finalLaunchCheck(ILaunchConfiguration configuration,
+			String mode, IProgressMonitor monitor) throws CoreException {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.core.model.ILaunchConfigurationDelegate2#getLaunch(
+	 * org.eclipse.debug.core.ILaunchConfiguration, java.lang.String)
+	 */
+	public ILaunch getLaunch(ILaunchConfiguration configuration, String mode)
+			throws CoreException {
+		return new PHPLaunch(configuration, mode, null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.core.model.ILaunchConfigurationDelegate2#preLaunchCheck
+	 * (org.eclipse.debug.core.ILaunchConfiguration, java.lang.String,
+	 * org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public boolean preLaunchCheck(ILaunchConfiguration configuration,
+			String mode, IProgressMonitor monitor) throws CoreException {
+		return true;
+	}
+
+	/**
+	 * Called when the debug session was terminated.
+	 */
+	public void terminated() {
+		DebugEvent event = null;
+		if (launch.getDebugTarget() == null) {
+			/*
+			 * We have to force the termination of the ILaunch because at this
+			 * stage there is no PHPDebugTarget, thus we create a dummy debug
+			 * target to overcome this issue and terminate the launch.
+			 */
+			IDebugTarget dummyDebugTarget = new DummyDebugTarget(launch);
+			event = new DebugEvent(dummyDebugTarget, DebugEvent.TERMINATE);
+			if (launch != null) {
+				launch.addDebugTarget(dummyDebugTarget);
+				IDebugEventSetListener launchListener = (IDebugEventSetListener) launch;
+				launchListener.handleDebugEvents(new DebugEvent[] { event });
+			}
+		}
+		event = new DebugEvent(this, DebugEvent.TERMINATE);
+		fireEvent(event);
+	}
+
+	/**
+	 * Fires a debug event
+	 * 
+	 * @param event
+	 *            The event to be fired
+	 */
+	public void fireEvent(DebugEvent event) {
+		DebugPlugin.getDefault().fireDebugEventSet(new DebugEvent[] { event });
+	}
+
+	/*
+	 * A dummy debug target for the termination of the ILaunch.
+	 */
+	private class DummyDebugTarget implements IDebugTarget {
+
+		private ILaunch launch;
+
+		public DummyDebugTarget(ILaunch launch) {
+			this.launch = launch;
+		}
+
+		public String getName() throws DebugException {
+			return "Session Terminated"; //$NON-NLS-1$
+		}
+
+		public IProcess getProcess() {
+			return null;
+		}
+
+		public IThread[] getThreads() throws DebugException {
+			return null;
+		}
+
+		public boolean hasThreads() throws DebugException {
+			return false;
+		}
+
+		public boolean supportsBreakpoint(IBreakpoint breakpoint) {
+			return false;
+		}
+
+		public IDebugTarget getDebugTarget() {
+			return this;
+		}
+
+		public ILaunch getLaunch() {
+			return launch;
+		}
+
+		public String getModelIdentifier() {
+			return ""; //$NON-NLS-1$
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		public Object getAdapter(Class adapter) {
+			return null;
+		}
+
+		public boolean canTerminate() {
+			return true;
+		}
+
+		public boolean isTerminated() {
+			return true;
+		}
+
+		public void terminate() throws DebugException {
+		}
+
+		public boolean canResume() {
+			return false;
+		}
+
+		public boolean canSuspend() {
+			return false;
+		}
+
+		public boolean isSuspended() {
+			return false;
+		}
+
+		public void resume() throws DebugException {
+		}
+
+		public void suspend() throws DebugException {
+		}
+
+		public void breakpointAdded(IBreakpoint breakpoint) {
+		}
+
+		public void breakpointChanged(IBreakpoint breakpoint,
+				IMarkerDelta delta) {
+		}
+
+		public void breakpointRemoved(IBreakpoint breakpoint,
+				IMarkerDelta delta) {
+		}
+
+		public boolean canDisconnect() {
+			return false;
+		}
+
+		public void disconnect() throws DebugException {
+		}
+
+		public boolean isDisconnected() {
+			return false;
+		}
+
+		public IMemoryBlock getMemoryBlock(long startAddress, long length)
+				throws DebugException {
+			return null;
+		}
+
+		public boolean supportsStorageRetrieval() {
+			return false;
+		}
+	}
+
+}
---
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/zend/debugger/handlers/FileContentRequestCurrentHandler.java
@@ -0,0 +1,218 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Zend Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Zend Technologies - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.debug.core.zend.debugger.handlers;
+
+import java.io.*;
+import java.net.URI;
+
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileInfo;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.php.debug.core.debugger.messages.IDebugMessage;
+import org.eclipse.php.debug.core.debugger.messages.IDebugResponseMessage;
+import org.eclipse.php.debug.core.debugger.parameters.IDebugParametersKeys;
+import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
+import org.eclipse.php.internal.debug.core.Logger;
+import org.eclipse.php.internal.debug.core.PHPDebugPlugin;
+import org.eclipse.php.internal.debug.core.pathmapper.VirtualPath;
+import org.eclipse.php.internal.debug.core.zend.debugger.RemoteDebugger;
+import org.eclipse.php.internal.debug.core.zend.debugger.messages.FileContentRequest;
+import org.eclipse.php.internal.debug.core.zend.debugger.messages.FileContentResponse;
+import org.eclipse.php.internal.debug.core.zend.model.PHPDebugTarget;
+import org.eclipse.php.internal.debug.core.zend.model.ResolveBlackList;
+
+/**
+ * Handle a server request for the delivery of the file content. This class is
+ * used if debugger protocol is &gt;= 2006040902 New protocol provides new
+ * message type (StartProcessFileNotification) which allows on-demand path
+ * mapping and lazy breakpoint sending.
+ * 
+ * @author michael
+ */
+public class FileContentRequestCurrentHandler
+		extends AbstractFileContentRequestHandler {
+
+	private static final String EXCLUDED_EXTENSION = "phar"; //$NON-NLS-1$
+
+	private int reqID;
+	private String lastFileName;
+	private String encoding;
+	private PHPDebugTarget debugTarget;
+	private boolean isFirstFileToDebug;
+	private FileContentRequest contentRequest;
+
+	public FileContentRequestCurrentHandler() {
+		isFirstFileToDebug = true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.php.debug.core.debugger.handlers.IDebugMessageHandler#handle(
+	 * org.eclipse.php.debug.core.debugger.messages.IDebugMessage,
+	 * org.eclipse.php.internal.debug.core.zend.model.PHPDebugTarget)
+	 */
+	public void handle(IDebugMessage request, PHPDebugTarget debugTarget) {
+		this.debugTarget = debugTarget;
+		contentRequest = (FileContentRequest) request;
+		reqID = contentRequest.getID();
+		lastFileName = contentRequest.getFileName();
+		encoding = contentRequest.getTransferEncoding();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.php.debug.core.debugger.handlers.IDebugRequestHandler#
+	 * getResponseMessage()
+	 */
+	public IDebugResponseMessage getResponseMessage() {
+		FileContentResponse response = new FileContentResponse();
+		response.setID(reqID);
+		try {
+			byte[] content = null;
+			if (!lastFileName.endsWith(EXCLUDED_EXTENSION)) {
+				if (isFirstFileToDebug
+						&& PHPDebugPlugin.isDummyFile(lastFileName)) {
+					content = getDummyContent();
+					// Blacklist dummy.php file, so it won't be requested for
+					// mapping:
+					debugTarget.getContextManager().addToResolveBlacklist(
+							new VirtualPath(lastFileName),
+							ResolveBlackList.Type.FILE);
+				} else {
+					RemoteDebugger remoteDebugger = (RemoteDebugger) debugTarget
+							.getRemoteDebugger();
+					String localPath = remoteDebugger
+							.convertToLocalFilename(lastFileName);
+					if (localPath != null) {
+						IResource resource = ResourcesPlugin.getWorkspace()
+								.getRoot().findMember(localPath);
+						if (resource != null) {
+							IPath location = resource.getLocation();
+							if (location != null) {
+								content = getBytesFromURI(
+										location.toFile().toURI());
+							} else if (resource.exists()) {
+								// probably RSE
+								content = getBytesFromURI(
+										resource.getLocationURI());
+							}
+						} else {
+							File file = new File(localPath);
+							if (file.exists()) {
+								content = getBytesFromURI(file.toURI());
+							}
+						}
+					}
+				}
+			}
+			if (content == null) {
+				content = new byte[0];
+			}
+			setResponseContent(response, contentRequest, content);
+		} catch (FileNotFoundException e) {
+			// No need to log it. The server will throw an error notification to
+			// the console indicating that the file was not found.
+		} catch (NullPointerException e) {
+			// No need to log it. The server will throw an error notification to
+			// the console indicating that the file was not found.
+		} catch (Throwable t) {
+			Logger.logException("Fail to send the file content to the server", //$NON-NLS-1$
+					t);
+		}
+		isFirstFileToDebug = false;
+		return response;
+	}
+
+	private String getDebugType() {
+		String debugType = ""; //$NON-NLS-1$
+		try {
+			debugType = debugTarget.getLaunch().getLaunchConfiguration()
+					.getAttribute(IDebugParametersKeys.PHP_DEBUG_TYPE, ""); //$NON-NLS-1$
+		} catch (CoreException ce) {
+			PHPDebugPlugin.log(ce);
+		}
+		return debugType;
+	}
+
+	/**
+	 * Returns the dummy content for the current file name.
+	 */
+	private byte[] getDummyContent() {
+		String originalFileName = ""; //$NON-NLS-1$
+		try {
+			ILaunchConfiguration launchConfiguration = debugTarget.getLaunch()
+					.getLaunchConfiguration();
+			// The dummy request should be made on the full path of the debugged
+			// file.
+			originalFileName = launchConfiguration
+					.getAttribute(IPHPDebugConstants.ATTR_FILE_FULL_PATH, ""); //$NON-NLS-1$
+		} catch (CoreException e) {
+		}
+		StringBuilder contentBuf = new StringBuilder("<?php "); //$NON-NLS-1$
+		File originalFile = new File(originalFileName);
+		if (!originalFileName.startsWith("\\\\") && originalFile.exists() //$NON-NLS-1$
+				&& getDebugType()
+						.equals(IDebugParametersKeys.PHP_EXE_SCRIPT_DEBUG)) {
+			String parentDirectory = originalFile.getParentFile()
+					.getAbsolutePath();
+			if (parentDirectory.endsWith(":\\")) { //$NON-NLS-1$
+				parentDirectory += "\\"; //$NON-NLS-1$
+			}
+			contentBuf.append("chdir('").append(parentDirectory).append("'); "); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		originalFileName = originalFileName.replaceAll("\\\\", "\\\\\\\\"); //$NON-NLS-1$ //$NON-NLS-2$
+		contentBuf.append("include('").append(originalFileName) //$NON-NLS-1$
+				.append("'); ?>"); //$NON-NLS-1$
+
+		String content = contentBuf.toString();
+
+		if (encoding != null) {
+			try {
+				return content.getBytes(encoding);
+			} catch (UnsupportedEncodingException e) {
+				Logger.logException("Failed to create dummy content in the '" //$NON-NLS-1$
+						+ encoding
+						+ "' encoding. \nCreating with the default encoding.", //$NON-NLS-1$
+						e);
+			}
+		}
+		return content.getBytes();
+	}
+
+	/**
+	 * Read and returns all the bytes from the given file URI
+	 */
+	private byte[] getBytesFromURI(URI uri) throws Exception {
+		IFileStore fileStore = EFS.getStore(uri);
+		IFileInfo fileInfo = fileStore.fetchInfo();
+		long length = fileInfo.getLength();
+		if (length > Integer.MAX_VALUE) {
+			throw new Exception(
+					"The requested file '" + lastFileName + "' is too big"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		// TODO - There is no handle of file encoding!
+		byte[] bytes = new byte[(int) length];
+		InputStream openInputStream = fileStore.openInputStream(EFS.NONE, null);
+		DataInputStream in = new DataInputStream(openInputStream);
+		in.readFully(bytes);
+		in.close();
+		return bytes;
+	}
+
+}
