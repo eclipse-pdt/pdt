b8decb4533e967fab57f8edf8cac187e8b6bc189
Dariusz Michura <dariusz.m@zend.com>
2015-08-26 14:10:22 +0200

    Bug 475912 - Edit Server dialog should restore its size with every run
    Change-Id: Id2f6cf8bbbe8e8bace9ce9537cf2418172daa567
    Signed-off-by: Dariusz Michura <dariusz.m@zend.com>

---
+++ plugins/org.eclipse.php.server.ui/src/org/eclipse/php/internal/server/ui/ServerEditWizardRunner.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Zend Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Zend Technologies - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.server.ui;
+
+import org.eclipse.jface.dialogs.DialogSettings;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.php.internal.server.core.Server;
+import org.eclipse.php.ui.util.PersistingSizeAndLocationWizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Helper class which opens <b>Edit Server</b> dialog. Main purpose of the class
+ * is to provide settings object the dialog can use to store its settings
+ * between every run.
+ */
+public class ServerEditWizardRunner {
+
+	public static int runWizard(Server server) {
+		return runWizard(getDefaultShell(), server);
+	}
+
+	public static int runWizard(Server server, String tabId) {
+		return runWizard(getDefaultShell(), server, tabId);
+	}
+
+	public static int runWizard(Shell parentShell, Server server) {
+		ServerEditWizard wizard = new ServerEditWizard(server);
+		return runWizard(parentShell, wizard);
+	}
+
+	public static int runWizard(Shell parentShell, Server server,
+			String tabId) {
+		ServerEditWizard wizard = new ServerEditWizard(server, tabId);
+		return runWizard(parentShell, wizard);
+	}
+
+	public static int runWizard(ServerEditWizard wizard) {
+		return runWizard(getDefaultShell(), wizard);
+	}
+
+	public static int runWizard(Shell parentShell, ServerEditWizard wizard) {
+		IDialogSettings dialogSettings = DialogSettings.getOrCreateSection(
+				Activator.getDefault().getDialogSettings(),
+				ServerEditWizard.class.getSimpleName());
+		PersistingSizeAndLocationWizardDialog dialog = new PersistingSizeAndLocationWizardDialog(
+				parentShell, wizard, dialogSettings);
+		return dialog.open();
+	}
+
+	private static Shell getDefaultShell() {
+		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+	}
+}
---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/util/PersistingSizeAndLocationWizardDialog.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Zend Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Zend Technologies - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.ui.util;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * A wizard dialog which can store its current size and location and restore it
+ * with next run.
+ */
+public class PersistingSizeAndLocationWizardDialog extends WizardDialog {
+
+	private IDialogSettings fDialogSettings;
+	private int fPersistingStrategy;
+
+	/**
+	 * Creates a new wizard dialog for the given <code>wizard</code>. The dialog
+	 * does not persist its location and size by default.
+	 * 
+	 * @param parentShell
+	 *            the parent shell
+	 * @param wizard
+	 *            the wizard this dialog is working on
+	 */
+	public PersistingSizeAndLocationWizardDialog(Shell parentShell,
+			IWizard wizard) {
+		this(parentShell, wizard, null, 0);
+	}
+
+	/**
+	 * Creates a new wizard dialog for the given <code>wizard</code>. The dialog
+	 * persists its location and size by default.
+	 * 
+	 * @param parentShell
+	 *            the parent shell
+	 * @param wizard
+	 *            the wizard this dialog is working on
+	 * @param dialogSettings
+	 *            the dialog settings used to store the dialog's location and/or
+	 *            size, or <code>null</code> if the dialog's bounds should never
+	 *            be stored
+	 */
+	public PersistingSizeAndLocationWizardDialog(Shell parentShell,
+			IWizard wizard, IDialogSettings dialogSettings) {
+		this(parentShell, wizard, dialogSettings,
+				DIALOG_PERSISTLOCATION | DIALOG_PERSISTSIZE);
+	}
+
+	/**
+	 * Creates a new wizard dialog for the given <code>wizard</code>.
+	 * 
+	 * @param parentShell
+	 *            the parent shell
+	 * @param wizard
+	 *            the wizard this dialog is working on
+	 * @param dialogSettings
+	 *            the dialog settings used to store the dialog's location and/or
+	 *            size, or <code>null</code> if the dialog's bounds should never
+	 *            be stored
+	 * @param persistingStrategy
+	 *            the integer constant that describes the strategy for
+	 *            persisting the dialog location size
+	 * 
+	 * @see Dialog#DIALOG_PERSISTLOCATION
+	 * @see Dialog#DIALOG_PERSISTSIZE
+	 * @see Dialog#getDialogBoundsSettings()
+	 */
+	public PersistingSizeAndLocationWizardDialog(Shell parentShell,
+			IWizard wizard, IDialogSettings dialogSettings,
+			int persistingStrategy) {
+		super(parentShell, wizard);
+		fDialogSettings = dialogSettings;
+		fPersistingStrategy = persistingStrategy;
+	}
+
+	@Override
+	protected IDialogSettings getDialogBoundsSettings() {
+		return fDialogSettings;
+	}
+
+	@Override
+	protected int getDialogBoundsStrategy() {
+		return fPersistingStrategy;
+	}
+
+}
