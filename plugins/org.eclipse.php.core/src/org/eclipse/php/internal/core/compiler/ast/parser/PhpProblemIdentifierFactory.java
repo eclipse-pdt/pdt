26c8ad5b6fc806e2c47cf9d9f26e3fadd7f45643
Dawid Pakuła <zulus@w3des.net>
2015-08-31 14:52:14 +0200

    Bug 476192 - Register IProblemIdentifierFactory
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

---
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/compiler/ast/parser/PhpProblemIdentifierFactory.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Dawid Pakuła and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Dawid Pakuła - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.core.compiler.ast.parser;
+
+import org.eclipse.dltk.compiler.problem.IProblemIdentifier;
+import org.eclipse.dltk.compiler.problem.IProblemIdentifierFactory;
+
+public class PhpProblemIdentifierFactory implements IProblemIdentifierFactory {
+
+	@Override
+	public IProblemIdentifier valueOf(String localName)
+			throws IllegalArgumentException {
+		return PhpProblemIdentifier.valueOf(localName);
+	}
+
+	@Override
+	public IProblemIdentifier[] values() {
+		return PhpProblemIdentifier.values();
+	}
+
+}
