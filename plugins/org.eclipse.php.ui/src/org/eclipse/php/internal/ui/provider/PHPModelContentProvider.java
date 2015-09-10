8b39104503897c54271295c399adcd9fdb220830
Michal Niewrzal <michal.n@zend.com>
2015-09-10 23:46:13 +0200

    Bug 476253 - Quick outline and PHP Explorer filter anonymous classes and anonymous functions if declared inside another function
    Change-Id: I6661b4e8a940ad35c14bfea95a5012c4c4755b69
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/provider/PHPModelContentProvider.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/provider/PHPModelContentProvider.java
@@ -1,24 +1,54 @@
 package org.eclipse.php.internal.ui.provider;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.dltk.core.IMethod;
+import org.eclipse.dltk.core.*;
 import org.eclipse.dltk.ui.IModelContentProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.php.internal.ui.PHPUiPlugin;
 
 public class PHPModelContentProvider implements IModelContentProvider {
 
 	public PHPModelContentProvider() {
 	}
 
+	@Override
 	public void provideModelChanges(Object parentElement, List children,
 			ITreeContentProvider iTreeContentProvider) {
 		if (parentElement instanceof IMethod) {
-			children.clear();
-		}
+			Iterator<?> childrenIterator = children.iterator();
+			List<Object> innerObjects = new ArrayList<Object>();
+			while (childrenIterator.hasNext()) {
+				Object next = childrenIterator.next();
 
+				collectInnerMembers(next, innerObjects);
+				if (!(next instanceof IType || next instanceof IMethod)) {
+					childrenIterator.remove();
+				}
+			}
+			children.addAll(innerObjects);
+		}
 	}
 
+	private void collectInnerMembers(Object obj, List<Object> innerObjects) {
+		if (obj instanceof IField) {
+			IField field = (IField) obj;
+			try {
+				for (IModelElement child : field.getChildren()) {
+					if (child.getElementType() == IModelElement.METHOD
+							|| child.getElementType() == IModelElement.TYPE) {
+						innerObjects.add(child);
+					}
+				}
+			} catch (ModelException e) {
+				PHPUiPlugin.log(e);
+			}
+		}
+	}
+
+	@Override
 	public Object getParentElement(Object element,
 			ITreeContentProvider iTreeContentProvider) {
 		return null;
