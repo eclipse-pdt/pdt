994dc41274f21c09fd288bde1d5fabfcaef23956
Bartlomiej Laczkowski <bartlomiej.l@zend.com>
2015-11-07 14:55:47 +0100

    Bug 481643 - [Zend Debugger] Changing variable value doesn't work correctly for strings
    Change-Id: I7b0483e046f69d8b078dfb4fb64e117255df496a
    Signed-off-by: Bartlomiej Laczkowski <bartlomiej.l@zend.com>

--- plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/actions/XDebugVariableValueEditor.java
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/actions/XDebugVariableValueEditor.java
@@ -11,71 +11,28 @@
  *******************************************************************************/
 package org.eclipse.php.internal.debug.ui.actions;
 
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.model.IValue;
 import org.eclipse.debug.core.model.IVariable;
-import org.eclipse.debug.internal.ui.actions.variables.ChangeVariableValueInputDialog;
-import org.eclipse.debug.ui.actions.IVariableValueEditor;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.window.Window;
 import org.eclipse.php.internal.debug.core.xdebug.dbgp.model.DBGpStringValue;
 import org.eclipse.php.internal.debug.core.xdebug.dbgp.model.DBGpTarget;
 import org.eclipse.php.internal.debug.core.xdebug.dbgp.model.DBGpVariable;
-import org.eclipse.php.internal.debug.ui.Logger;
-import org.eclipse.php.internal.debug.ui.PHPDebugUIMessages;
-import org.eclipse.swt.widgets.Shell;
 import org.w3c.dom.Node;
-
-import com.ibm.icu.text.MessageFormat;
 
 /**
  * A variable value editor that prompts the user to set a primitive's value.
  */
-public class XDebugVariableValueEditor implements IVariableValueEditor {
-
-	/**
-	 * Creates a new editor for a variable with the given signature
-	 * 
-	 * @param signature
-	 *            the signature of the primitive to be edited
-	 */
-	public XDebugVariableValueEditor() {
-
-	}
+@SuppressWarnings("restriction")
+public class XDebugVariableValueEditor extends AbstractVariableValueEditor {
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.debug.ui.actions.IVariableValueEditor#editVariable(org.
-	 * eclipse .debug.core.model.IVariable, org.eclipse.swt.widgets.Shell)
+	 * @see
+	 * org.eclipse.php.internal.debug.ui.actions.AbstractVariableValueEditor#
+	 * getValueString(org.eclipse.debug.core.model.IVariable)
 	 */
-	public boolean editVariable(IVariable variable, Shell shell) {
-		try {
-			String name = variable.getName();
-			String title = PHPDebugUIMessages.PHPPrimitiveValueEditor_0;
-			String message = MessageFormat.format(PHPDebugUIMessages.PHPPrimitiveValueEditor_1, new Object[] { name });
-			String initialValue = getValueString(variable);
-
-			PrimitiveValidator validator = new PrimitiveValidator(variable);
-			ChangeVariableValueInputDialog dialog = new ChangeVariableValueInputDialog(shell, title, message,
-					initialValue, validator);
-			if (dialog.open() == Window.OK) {
-				String stringValue = dialog.getValue();
-				variable.setValue(stringValue);
-			} else {
-
-			}
-		} catch (DebugException e) {
-			IStatus status = e.getStatus();
-			ErrorDialog.openError(shell, PHPDebugUIMessages.PHPPrimitiveValueEditor_2,
-					PHPDebugUIMessages.PHPPrimitiveValueEditor_3, status);
-		}
-		return true;
-	}
-
-	private String getValueString(IVariable variable) throws DebugException {
+	protected String getValueString(IVariable variable) throws DebugException {
 		IValue value = variable.getValue();
 		String initialValue = value.getValueString();
 
@@ -102,48 +59,6 @@ public class XDebugVariableValueEditor implements IVariableValueEditor {
 			}
 		}
 		return initialValue;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.debug.ui.actions.IVariableValueEditor#saveVariable(org.
-	 * eclipse .debug.core.model.IVariable, java.lang.String,
-	 * org.eclipse.swt.widgets.Shell)
-	 */
-	public boolean saveVariable(IVariable variable, String expression, Shell shell) {
-		return false;
-	}
-
-	/**
-	 * Input validator for primitive types
-	 */
-	protected class PrimitiveValidator implements IInputValidator {
-
-		IVariable var;
-
-		public PrimitiveValidator(IVariable var) {
-			this.var = var;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.jface.dialogs.IInputValidator#isValid(java.lang.String)
-		 */
-		public String isValid(String newText) {
-			String errorMsg = null;
-			try {
-				if (!var.verifyValue(newText)) {
-					errorMsg = PHPDebugUIMessages.XDebugVariableValueEditor_invalidValue;
-				}
-			} catch (DebugException e) {
-				Logger.logException("DebugException", e); //$NON-NLS-1$
-				errorMsg = PHPDebugUIMessages.XDebugVariableValueEditor_0;
-			}
-			return errorMsg;
-		}
 	}
 
 }
