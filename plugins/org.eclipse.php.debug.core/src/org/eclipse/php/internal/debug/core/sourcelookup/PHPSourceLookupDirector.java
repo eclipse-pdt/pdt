e89c9767f7aecff534516f55d92829a2cf355e06
Bartlomiej Laczkowski <bartlomiej.l@zend.com>
2015-11-15 13:45:52 +0100

    Bug 240553 - Breakpoint on Linked resource doesn't work
    Change-Id: Ie587b52d1808c12c19962f8f5da6d6890cb7edc8
    Signed-off-by: Bartlomiej Laczkowski <bartlomiej.l@zend.com>

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/sourcelookup/PHPSourceLookupDirector.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/sourcelookup/PHPSourceLookupDirector.java
@@ -11,32 +11,18 @@
  *******************************************************************************/
 package org.eclipse.php.internal.debug.core.sourcelookup;
 
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.eclipse.core.filesystem.EFS;
-import org.eclipse.core.internal.filesystem.local.LocalFile;
-import org.eclipse.core.internal.resources.LinkDescription;
-import org.eclipse.core.internal.resources.Project;
-import org.eclipse.core.internal.resources.ProjectDescription;
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IStackFrame;
 import org.eclipse.debug.core.sourcelookup.AbstractSourceLookupDirector;
 import org.eclipse.debug.core.sourcelookup.ISourceLookupParticipant;
 import org.eclipse.debug.internal.ui.views.launch.SourceNotFoundEditorInput;
-import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
-import org.eclipse.php.internal.debug.core.zend.model.PHPDebugTarget;
 
 /**
  * PHP source lookup director. For PHP source lookup there is one source lookup
  * participant.
  */
+@SuppressWarnings("restriction")
 public class PHPSourceLookupDirector extends AbstractSourceLookupDirector {
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -47,76 +33,18 @@ public class PHPSourceLookupDirector extends AbstractSourceLookupDirector {
 		addParticipants(new ISourceLookupParticipant[] { new PHPSourceLookupParticipant() });
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.debug.core.sourcelookup.AbstractSourceLookupDirector#
+	 * getSourceElement(java.lang.Object)
+	 */
 	public Object getSourceElement(Object element) {
-		Object obj = super.getSourceElement(element);
-		if (obj == null) {
-			if (element instanceof IStackFrame) {
-				obj = new SourceNotFoundEditorInput((IStackFrame) element);
-			}
+		Object sourceElement = super.getSourceElement(element);
+		if (sourceElement == null && element instanceof IStackFrame) {
+			sourceElement = new SourceNotFoundEditorInput((IStackFrame) element);
 		}
-
-		if (obj instanceof LocalFile && element instanceof IStackFrame) {
-			IStackFrame stackFrame = (IStackFrame) element;
-			LocalFile localFile = (LocalFile) obj;
-			IProject project = null;
-			try {
-				if (stackFrame.getLaunch() != null && stackFrame.getLaunch().getLaunchConfiguration() != null) {
-					String file = stackFrame.getLaunch().getLaunchConfiguration()
-							.getAttribute(IPHPDebugConstants.ATTR_FILE, (String) null);
-					if (file != null) {
-						IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-						IResource resource = workspaceRoot.findMember(file);
-						if (resource != null) {
-							project = resource.getProject();
-						}
-					}
-				}
-			} catch (CoreException e1) {
-			}
-			if (project != null) {
-				IDebugTarget debugTarget = (IDebugTarget) stackFrame.getDebugTarget();
-				try {
-					ProjectDescription desc = ((Project) project).internalGetDescription();
-					if (desc != null) {
-						HashMap links = desc.getLinks();
-						if (links != null) {
-
-							for (Entry entry : (Set<Entry>) links.entrySet()) {
-								IPath relativePath = (IPath) entry.getKey();
-								LinkDescription linkDescription = (LinkDescription) entry.getValue();
-								IPath linkPath = new Path(linkDescription.getLocationURI().getPath());
-								IPath filePath = new Path(localFile.toLocalFile(EFS.NONE, null).getAbsolutePath());
-								if (linkPath.isPrefixOf(filePath)) {
-									filePath = filePath.removeFirstSegments(linkPath.segmentCount()).setDevice(null);
-									relativePath = relativePath.append(filePath);
-
-									IFile file = null;
-									if (debugTarget instanceof PHPDebugTarget) {
-										file = ((PHPDebugTarget) debugTarget).getProject().getFile(relativePath);
-									} else {
-										// not sure what the logic here is
-										// trying to achieve but it
-										// is specific to the zend
-										// implementation, so I have put
-										// something here for xdebug to stop
-										// errors in source lookup occuring
-										file = project.getFile(relativePath);
-									}
-									if (file.isAccessible()) {
-										return file;
-									}
-								}
-							}
-						}
-
-					}
-
-				} catch (CoreException e) {
-				}
-			}
-
-		}
-		return obj;
+		return sourceElement;
 	}
 
 }
