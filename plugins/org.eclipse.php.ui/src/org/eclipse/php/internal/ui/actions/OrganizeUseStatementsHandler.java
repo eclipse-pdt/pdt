ea1d8a9505935bd61a9cdac14b472ccad3324955
Michal Niewrzal <michal.n@zend.com>
2015-11-25 10:15:53 +0100

    Bug 482895 - Organize Use Statements command should be in Refactoring or Source menu?
    Change-Id: I08426955dbc2fa5c9327c74397f60236be5bb9f6
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/OrganizeUseStatementsHandler.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/actions/OrganizeUseStatementsHandler.java
@@ -12,17 +12,18 @@
  *******************************************************************************/
 package org.eclipse.php.internal.ui.actions;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.SourceParserUtil;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.php.internal.core.corext.util.DocumentUtils;
 import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Our sample action implements workbench action delegate. The action proxy will
@@ -33,23 +34,11 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
  * @see IWorkbenchWindowActionDelegate
  * @author Yannick de Lange <yannickl88@gmail.com>
  */
-public class OrganizeUseStatementsAction implements IWorkbenchWindowActionDelegate {
-	private IWorkbenchWindow window;
+public class OrganizeUseStatementsHandler extends AbstractHandler {
 
-	/**
-	 * The constructor.
-	 */
-	public OrganizeUseStatementsAction() {
-	}
-
-	/**
-	 * The action has been activated. The argument of the method represents the
-	 * 'real' action sitting in the workbench UI.
-	 *
-	 * @see IWorkbenchWindowActionDelegate#run
-	 */
-	public void run(IAction action) {
-		IEditorPart input = window.getActivePage().getActiveEditor();
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IEditorPart input = HandlerUtil.getActiveEditor(event);
 
 		if (input instanceof PHPStructuredEditor) {
 			PHPStructuredEditor ite = (PHPStructuredEditor) input;
@@ -60,34 +49,6 @@ public class OrganizeUseStatementsAction implements IWorkbenchWindowActionDelega
 
 			DocumentUtils.sortUseStatements(moduleDeclaration, doc);
 		}
-	}
-
-	/**
-	 * Selection in the workbench has been changed. We can change the state of
-	 * the 'real' action here if we want, but this can only happen after the
-	 * delegate has been created.
-	 *
-	 * @see IWorkbenchWindowActionDelegate#selectionChanged
-	 */
-	public void selectionChanged(IAction action, ISelection selection) {
-	}
-
-	/**
-	 * We can use this method to dispose of any system resources we previously
-	 * allocated.
-	 *
-	 * @see IWorkbenchWindowActionDelegate#dispose
-	 */
-	public void dispose() {
-	}
-
-	/**
-	 * We will cache window object in order to be able to provide parent shell
-	 * for the message dialog.
-	 *
-	 * @see IWorkbenchWindowActionDelegate#init
-	 */
-	public void init(IWorkbenchWindow window) {
-		this.window = window;
+		return null;
 	}
 }
