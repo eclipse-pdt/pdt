4ff351bc77f5e68d914ef874b809861f8123fdd8
Kaloyan Raev <kaloyan.r@zend.com>
2016-01-11 18:04:48 +0200

    Bug 485550 - Improve insert variable comment quick assist
    Change-Id: I438be5fdc7e23561a85257f856be2eb912bd38bb
    Signed-off-by: Kaloyan Raev <kaloyan.r@zend.com>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/quickassist/Messages.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/quickassist/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Kaloyan Raev - Bug 485550 - Improve insert variable comment quick assist
  *******************************************************************************/
 package org.eclipse.php.internal.ui.quickassist;
 
@@ -14,7 +15,11 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.php.internal.ui.quickassist.messages"; //$NON-NLS-1$
+	public static String VarCommentQuickAssistProcessor_AdditionalProposalInfo;
 	public static String VarCommentQuickAssistProcessor_name;
+
+	public static String VarCommentQuickAssistProcessor_OpenTypeAction_dialogMessage;
+	public static String VarCommentQuickAssistProcessor_OpenTypeAction_dialogTitle;
 
 	static {
 		// initialize resource bundle
