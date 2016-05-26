22b210048d79ceb7fe1b36e5ee1cfb19f02ae7d6
Michal Niewrzal <michal.n@zend.com>
2016-05-26 10:37:31 +0200

    No Bug - My experiments with unstable tests
    Change-Id: Iefaa91f672f922fc2530fdd4706fbd4cb6dfbf7f
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- tests/org.eclipse.php.core.tests/src/org/eclipse/php/core/tests/AllTests.java
+++ tests/org.eclipse.php.core.tests/src/org/eclipse/php/core/tests/AllTests.java
@@ -13,51 +13,36 @@
 package org.eclipse.php.core.tests;
 
 import org.eclipse.php.core.tests.codeassist.CodeAssistTests;
-import org.eclipse.php.core.tests.compiler_ast.parser.CompilerParserTests;
-import org.eclipse.php.core.tests.compiler_ast.parser.FindUseStatementByAliasTests;
-import org.eclipse.php.core.tests.compiler_ast.parser.FindUseStatementByNamespaceTests;
-import org.eclipse.php.core.tests.compiler_ast.parser.GetUseStatementsByTests;
-import org.eclipse.php.core.tests.compiler_ast.parser.TraitUseStatementVisitorTests;
-import org.eclipse.php.core.tests.compiler_ast.parser.TypeDeclarationVisitorTests;
-import org.eclipse.php.core.tests.compiler_ast.phpdoc.PHPDocAwareDeclarationTests;
-import org.eclipse.php.core.tests.compiler_ast.phpdoc.PHPDocParserTests;
-import org.eclipse.php.core.tests.document.lexer.DocumentLexerTests;
-import org.eclipse.php.core.tests.document.partitioner.PHPPartitionerTests;
-import org.eclipse.php.core.tests.dom_ast.CommentMapperTests;
-import org.eclipse.php.core.tests.dom_ast.StaticScalarExpressionsTests;
-import org.eclipse.php.core.tests.dom_ast.binding.BindingTests;
-import org.eclipse.php.core.tests.dom_ast.matcher.ASTMatcherTests;
-import org.eclipse.php.core.tests.dom_ast.parser.DomParserTests;
-import org.eclipse.php.core.tests.dom_ast.rewrite.ASTRewriteTests;
-import org.eclipse.php.core.tests.errors.ErrorReportingTests;
-import org.eclipse.php.core.tests.filenetwork.FileNetworkTests;
-import org.eclipse.php.core.tests.includepath.IncludePathManagerTests;
-import org.eclipse.php.core.tests.markoccurrence.MarkOccurrenceTests;
-import org.eclipse.php.core.tests.model_structure.ModelStructureTests;
-import org.eclipse.php.core.tests.phar.PharFileTest;
-import org.eclipse.php.core.tests.selection.SelectionEngineTests;
-import org.eclipse.php.core.tests.text.PHPTextSequenceUtilitiesTests;
-import org.eclipse.php.core.tests.typeinference.TypeInferenceTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 		// Model tests:
-		DomParserTests.class, CompilerParserTests.class, FindUseStatementByAliasTests.class,
-		FindUseStatementByNamespaceTests.class, GetUseStatementsByTests.class, TraitUseStatementVisitorTests.class,
-		TypeDeclarationVisitorTests.class, ErrorReportingTests.class, ASTRewriteTests.Suite.class,
-		ASTMatcherTests.class, CommentMapperTests.class, StaticScalarExpressionsTests.class, CodeAssistTests.class,
-		SelectionEngineTests.class, ModelStructureTests.class, TypeInferenceTests.class, FileNetworkTests.class,
-		PHPDocParserTests.class, PHPDocAwareDeclarationTests.class, IncludePathManagerTests.class, BindingTests.class,
-		// Document tests:
-		DocumentLexerTests.class, PHPPartitionerTests.class,
-		// Phar files
-		PharFileTest.class,
-		// Concilator tests:
-		org.eclipse.php.internal.core.ast.locator.AllTests.class, MarkOccurrenceTests.class,
-		// test language functions
-		org.eclipse.php.core.tests.searchEngine.AllTests.class, PHPTextSequenceUtilitiesTests.class })
+		// DomParserTests.class, CompilerParserTests.class,
+		// FindUseStatementByAliasTests.class,
+		// FindUseStatementByNamespaceTests.class,
+		// GetUseStatementsByTests.class, TraitUseStatementVisitorTests.class,
+		// TypeDeclarationVisitorTests.class, ErrorReportingTests.class,
+		// ASTRewriteTests.Suite.class,
+		// ASTMatcherTests.class, CommentMapperTests.class,
+		// StaticScalarExpressionsTests.class, CodeAssistTests.class,
+		// SelectionEngineTests.class, ModelStructureTests.class,
+		// TypeInferenceTests.class, FileNetworkTests.class,
+		// PHPDocParserTests.class, PHPDocAwareDeclarationTests.class,
+		// IncludePathManagerTests.class, BindingTests.class,
+		// // Document tests:
+		// DocumentLexerTests.class, PHPPartitionerTests.class,
+		// // Phar files
+		// PharFileTest.class,
+		// // Concilator tests:
+		// org.eclipse.php.internal.core.ast.locator.AllTests.class,
+		// MarkOccurrenceTests.class,
+		// // test language functions
+		// org.eclipse.php.core.tests.searchEngine.AllTests.class,
+		// PHPTextSequenceUtilitiesTests.class
+
+		CodeAssistTests.class })
 public final class AllTests {
 
 }
