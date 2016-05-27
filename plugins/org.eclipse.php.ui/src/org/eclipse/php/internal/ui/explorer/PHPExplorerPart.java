f77b0fcca02256bdf5db27124da8984dc0f04598
Dawid Pakuła <zulus@w3des.net>
2016-05-27 21:33:58 +0200

    Bug 356629 - PHP explorer does not sort folders or files alphabetically - sort order is erratic
    Change-Id: I7ba6770ebd18f62bad61cc42d6fa29f43693327f
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/explorer/PHPExplorerPart.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/explorer/PHPExplorerPart.java
@@ -12,6 +12,8 @@
  *******************************************************************************/
 package org.eclipse.php.internal.ui.explorer;
 
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.IProjectFragment;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.internal.ui.navigator.ScriptExplorerContentProvider;
 import org.eclipse.dltk.internal.ui.navigator.ScriptExplorerLabelProvider;
@@ -45,8 +47,8 @@ import org.eclipse.ui.views.navigator.NavigatorDropAdapter;
 
 /**
  * PHP Explorer view part to display the projects, contained files and
- * referenced folders/libraries. The view displays those in a
- * "file-system oriented" manner, and not in a "model oriented" manner.
+ * referenced folders/libraries. The view displays those in a "file-system
+ * oriented" manner, and not in a "model oriented" manner.
  * 
  * @author apeled, ncohen
  * 
@@ -57,10 +59,19 @@ public class PHPExplorerPart extends ScriptExplorerPart {
 		private static final int INCLUDE_PATH_CONTAINER = 59;
 
 		public int category(Object element) {
-			if (element instanceof IncludePathContainer)
+			if (element instanceof IncludePathContainer) {
 				return INCLUDE_PATH_CONTAINER;
-			else
+			} else {
+				if (element instanceof IModelElement) {
+					switch (((IModelElement) element).getElementType()) {
+					case IModelElement.PACKAGE_DECLARATION:
+						return super.category(((IModelElement) element).getResource());
+					default:
+						return super.category(element);
+					}
+				}
 				return super.category(element);
+			}
 		}
 
 		public int compare(Viewer viewer, Object e1, Object e2) {
@@ -77,14 +88,19 @@ public class PHPExplorerPart extends ScriptExplorerPart {
 			Object c1 = e1;
 			if (e1 instanceof ISourceModule) {
 				c1 = ((ISourceModule) e1).getResource();
+			} else if (e1 instanceof IProjectFragment) {
+				c1 = ((IProjectFragment) e1).getResource();
 			}
 			Object c2 = e2;
 			if (e2 instanceof ISourceModule) {
 				c2 = ((ISourceModule) e2).getResource();
+			} else if (e2 instanceof IProjectFragment) {
+				c2 = ((IProjectFragment) e2).getResource();
 			}
 			if (c1 != null && c2 != null) {
 				return super.compare(viewer, c1, c2);
 			}
+
 			return super.compare(viewer, e1, e2);
 		}
 	}
@@ -163,8 +179,8 @@ public class PHPExplorerPart extends ScriptExplorerPart {
 	}
 
 	/**
-	 * Overriding DTLK original setComerator, and setting
-	 * "includePathContainer - aware" comparators
+	 * Overriding DTLK original setComerator, and setting "includePathContainer
+	 * - aware" comparators
 	 */
 	@Override
 	protected void setComparator() {
