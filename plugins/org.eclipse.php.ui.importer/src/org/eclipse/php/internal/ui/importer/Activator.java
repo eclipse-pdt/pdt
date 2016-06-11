9e36fd343672035d2d71708f6a27f18429a45000
Dawid Pakuła <zulus@w3des.net>
2016-06-11 12:25:12 +0200

    No bug - Add missing copyright headers for tests and importer
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- plugins/org.eclipse.php.ui.importer/src/org/eclipse/php/internal/ui/importer/Activator.java
+++ plugins/org.eclipse.php.ui.importer/src/org/eclipse/php/internal/ui/importer/Activator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
 package org.eclipse.php.internal.ui.importer;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -13,26 +24,18 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
