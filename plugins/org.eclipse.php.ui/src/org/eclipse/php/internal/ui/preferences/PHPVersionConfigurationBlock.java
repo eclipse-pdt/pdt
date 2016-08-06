69e27270f70e4ce981c344fd5dd5665614a48a89
Thierry BLIND <thierryblind@msn.com>
2016-08-06 11:45:04 +0200

    [WIP] No bug - preparation work for better custom task tags recognition
    Change-Id: I7a05c45c847bcae6168683245852b10708cf88f9
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/PHPVersionConfigurationBlock.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/PHPVersionConfigurationBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -161,6 +161,10 @@ public class PHPVersionConfigurationBlock extends PHPCoreOptionsConfigurationBlo
 	}
 
 	protected void validateSettings(Key changedKey, String oldValue, String newValue) {
+		if (!areSettingsEnabled()) {
+			return;
+		}
+
 		if (changedKey != null) {
 			if (PREF_PHP_VERSION.equals(changedKey)) {
 				fTaskTagsStatus = validatePHPVersion();
--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/TodoTaskConfigurationBlock.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/preferences/TodoTaskConfigurationBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,16 +17,14 @@ import java.util.List;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IDocumentAdapter;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.jface.window.Window;
 import org.eclipse.php.internal.core.PHPCoreConstants;
 import org.eclipse.php.internal.ui.PHPUIMessages;
-import org.eclipse.php.internal.ui.PHPUiPlugin;
 import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
 import org.eclipse.php.internal.ui.editor.PHPStructuredTextViewer;
 import org.eclipse.php.internal.ui.preferences.util.Key;
+import org.eclipse.php.internal.ui.util.DocumentModelUtils;
 import org.eclipse.php.internal.ui.util.PixelConverter;
 import org.eclipse.php.internal.ui.util.StatusInfo;
 import org.eclipse.php.internal.ui.wizards.fields.*;
@@ -41,7 +39,6 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
-import org.osgi.service.prefs.BackingStoreException;
 
 import com.ibm.icu.text.MessageFormat;
 
@@ -282,6 +279,10 @@ public class TodoTaskConfigurationBlock extends PHPCoreOptionsConfigurationBlock
 	}
 
 	protected void validateSettings(Key changedKey, String oldValue, String newValue) {
+		if (!areSettingsEnabled()) {
+			return;
+		}
+
 		if (changedKey != null) {
 			if (PREF_TASK_TAGS.equals(changedKey)) {
 				fTaskTagsStatus = validateTaskTags();
@@ -387,13 +388,16 @@ public class TodoTaskConfigurationBlock extends PHPCoreOptionsConfigurationBlock
 	}
 
 	@Override
-	protected void prepareForBuild() {
-		try {
-			fManager.applyChanges();
-		} catch (BackingStoreException e) {
-			PHPUiPlugin.log(e);
-			return;
-		}
+	protected void prepareChanges(boolean doBuild) {
+		// if (doBuild) {
+		// try {
+		// fManager.applyChanges();
+		// } catch (BackingStoreException e) {
+		// PHPUiPlugin.log(e);
+		// }
+		// }
+		// XXX: move that code to a new IPreferenceChangeListener inside of
+		// PHPStructuredEditor
 		IEditorReference[] editors = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
 				.getEditorReferences();
 		for (int i = 0; i < editors.length; i++) {
@@ -401,20 +405,8 @@ public class TodoTaskConfigurationBlock extends PHPCoreOptionsConfigurationBlock
 			IEditorPart editor = editorReference.getEditor(false);
 			if (editor instanceof PHPStructuredEditor) {
 				PHPStructuredEditor phpEditor = (PHPStructuredEditor) editor;
-				boolean isDirty = phpEditor.isDirty();
 				if (phpEditor.getTextViewer() instanceof PHPStructuredTextViewer) {
-					PHPStructuredTextViewer textViewer = (PHPStructuredTextViewer) phpEditor.getTextViewer();
-					IDocumentAdapter documentAdapter = textViewer.getDocumentAdapter();
-					IDocument document = phpEditor.getDocument();
-					String content = document.get();
-					if (documentAdapter != null) {
-						documentAdapter.replaceTextRange(0, content.length(), ""); //$NON-NLS-1$
-						documentAdapter.replaceTextRange(0, 0, content);
-					}
-
-				}
-				if (!isDirty) {
-					phpEditor.doSave(null);
+					DocumentModelUtils.reparseAndReconcileDocument((PHPStructuredTextViewer) phpEditor.getTextViewer());
 				}
 			}
 		}
