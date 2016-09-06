088968e5f7ce9b082cb3e5916671d5aea02f07a8
Michal Niewrzal <michal.n@zend.com>
2016-09-06 13:13:46 +0200

    No Bug - Adjust Composer editor icons to dark theme
    Change-Id: I5ed1c0893463f769d578231cf63f9bb003a71a42
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- plugins/org.eclipse.php.composer.ui/src/org/eclipse/php/composer/ui/ComposerUIPluginImages.java
+++ plugins/org.eclipse.php.composer.ui/src/org/eclipse/php/composer/ui/ComposerUIPluginImages.java
@@ -21,35 +21,35 @@ import org.osgi.framework.Bundle;
 public class ComposerUIPluginImages {
 	public static final IPath ICONS_PATH = new Path("/icons/full");
 
-	private static final String T_OBJ16 = "obj16";	
+	private static final String T_OBJ16 = "obj16";
 	private static final String T_WIZBAN = "wizban";
 
 	public static final ImageDescriptor ADD_DEPENDENCY = create(T_WIZBAN, "packagist.png");
 	public static final ImageDescriptor CREATE_PROJECT = create(T_WIZBAN, "newpack_wiz.png");
 	public static final ImageDescriptor CREATE_PROJECT_FROM_PACKAGE = create(T_WIZBAN, "newsrcfldr_wiz.gif");
 	public static final ImageDescriptor IMPORT_PROJECT = create(T_WIZBAN, "packrefact_wiz.gif");
-	
+
 	public static final ImageDescriptor PERSON = create(T_OBJ16, "person.png");
 	public static final ImageDescriptor PAGE = create(T_OBJ16, "page.gif");
 	public static final ImageDescriptor PACKAGE = create(T_OBJ16, "package.gif");
-	public static final ImageDescriptor NAMESPACE = create(T_OBJ16, "namespace_obj.gif");
+	public static final ImageDescriptor NAMESPACE = create(T_OBJ16, "namespace_obj.png");
 	public static final ImageDescriptor PACKAGE_FOLDER = create(T_OBJ16, "packagefolder_obj.gif");
 	public static final ImageDescriptor COMPOSER = create(T_OBJ16, "composer.png");
 	public static final ImageDescriptor PHP = create(T_OBJ16, "php.png");
-	public static final ImageDescriptor BROWSER = create(T_OBJ16, "browser.gif");
-	public static final ImageDescriptor INSTALL = create(T_OBJ16, "install.gif");
-	public static final ImageDescriptor INSTALL_DEV = create(T_OBJ16, "install_dev.gif");
+	public static final ImageDescriptor BROWSER = create(T_OBJ16, "browser.png");
+	public static final ImageDescriptor INSTALL = create(T_OBJ16, "install.png");
+	public static final ImageDescriptor INSTALL_DEV = create(T_OBJ16, "install_dev.png");
 	public static final ImageDescriptor UPDATE = create(T_OBJ16, "update.png");
 	public static final ImageDescriptor UPDATE_DEV = create(T_OBJ16, "update_dev.png");
-	public static final ImageDescriptor SELFUPDATE = create(T_OBJ16, "selfupdate.gif");
+	public static final ImageDescriptor SELFUPDATE = create(T_OBJ16, "selfupdate.png");
 	public static final ImageDescriptor EVENT = create(T_OBJ16, "event3.png");
 	public static final ImageDescriptor SCRIPT = create(T_OBJ16, "script2.png");
 	public static final ImageDescriptor STAR = create(T_OBJ16, "star.png");
 	public static final ImageDescriptor DOWNLOAD = create(T_OBJ16, "download.png");
-	
-	public static final ImageDescriptor CLEAR = create(T_OBJ16, "clear.gif");
-	public static final ImageDescriptor CLEAR_DISABLED = create(T_OBJ16, "clear_disabled.gif");
-	
+
+	public static final ImageDescriptor CLEAR = create(T_OBJ16, "clear.png");
+	public static final ImageDescriptor CLEAR_DISABLED = create(T_OBJ16, "clear_disabled.png");
+
 	public static final ImageDescriptor REPO_GENERIC = create(T_OBJ16, "repo_generic.gif");
 	public static final ImageDescriptor REPO_GIT = create(T_OBJ16, "repo_git.png");
 	public static final ImageDescriptor REPO_SVN = create(T_OBJ16, "repo_svn.png");
@@ -57,7 +57,7 @@ public class ComposerUIPluginImages {
 	public static final ImageDescriptor REPO_PEAR = create(T_OBJ16, "repo_pear.png");
 	public static final ImageDescriptor REPO_PACKAGE = PACKAGE;
 	public static final ImageDescriptor REPO_COMPOSER = COMPOSER;
-	
+
 	public static final ImageDescriptor BUILDPATH_INCLUDE = create(T_OBJ16, "buildpath_include.gif");
 	public static final ImageDescriptor BUILDPATH_EXCLUDE = create(T_OBJ16, "buildpath_exclude.gif");
 
@@ -65,16 +65,13 @@ public class ComposerUIPluginImages {
 		return create(prefix, name, true);
 	}
 
-	private static ImageDescriptor create(String prefix, String name,
-			boolean useMissingImageDescriptor) {
+	private static ImageDescriptor create(String prefix, String name, boolean useMissingImageDescriptor) {
 		IPath path = ICONS_PATH.append(prefix).append(name);
 
-		return createImageDescriptor(ComposerUIPlugin.getDefault().getBundle(),
-				path, useMissingImageDescriptor);
+		return createImageDescriptor(ComposerUIPlugin.getDefault().getBundle(), path, useMissingImageDescriptor);
 	}
 
-	public static ImageDescriptor createImageDescriptor(Bundle bundle,
-			IPath path, boolean useMissingImageDescriptor) {
+	public static ImageDescriptor createImageDescriptor(Bundle bundle, IPath path, boolean useMissingImageDescriptor) {
 		URL url = FileLocator.find(bundle, path, null);
 		if (url != null) {
 			return ImageDescriptor.createFromURL(url);
