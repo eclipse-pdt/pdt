224f32e7f502cdc5d1cfd58e23459a4d2890e49f
Dawid Pakuła <zulus@w3des.net>
2016-09-12 17:35:08 +0200

    Bug 501225 - If composer project haven't composer.json, mark it as invalid
    Change-Id: I69c2bb0a177d323e39264993e4363947d146102d
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

---
+++ plugins/org.eclipse.php.composer.core/src/org/eclipse/php/composer/core/facet/FacetedProjectValidator.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Dawid Pakuła and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dawid Pakuła - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.composer.core.facet;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.php.composer.api.ComposerConstants;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.IFacetedProjectValidator;
+
+public class FacetedProjectValidator implements IFacetedProjectValidator {
+
+	@Override
+	public void validate(IFacetedProject fproj) throws CoreException {
+		if (!FacetManager.hasComposerFacet(fproj.getProject())) {
+			return;
+		}
+		if (!fproj.getProject().getFile(ComposerConstants.COMPOSER_JSON).exists()) {
+			fproj.createErrorMarker(ComposerConstants.PROBLEM_MARKER_TYPE,
+					"Composer Project require composer.json file");
+		}
+	}
+
+}
