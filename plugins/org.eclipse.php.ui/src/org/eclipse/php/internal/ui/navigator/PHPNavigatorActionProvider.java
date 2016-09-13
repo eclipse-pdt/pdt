bc1ab7591bfac0933b9c5296803da843459ddf04
Michal Niewrzal <michal.n@zend.com>
2016-09-13 22:37:06 +0200

    Bug 501296 - "Show In" isn't available for PHP Projects inside Project Explorer
    Change-Id: I843c1b28b178ac2c957f4fb1c9fb8934fbdbec70
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

---
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/navigator/PHPNavigatorActionProvider.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Zend Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Zend Technologies -  initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.ui.navigator;
+
+import org.eclipse.dltk.internal.ui.wizards.buildpath.newsourcepage.GenerateBuildPathActionGroup;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.php.internal.ui.actions.OpenViewActionGroup;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.actions.ActionContext;
+import org.eclipse.ui.navigator.CommonActionProvider;
+import org.eclipse.ui.navigator.ICommonActionExtensionSite;
+import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
+
+public class PHPNavigatorActionProvider extends CommonActionProvider {
+
+	private OpenViewActionGroup fOpenViewGroup;
+	private GenerateBuildPathActionGroup fBuildPathGroup;
+
+	private boolean fInViewPart = false;
+
+	@Override
+	public void fillActionBars(IActionBars actionBars) {
+		if (fInViewPart) {
+			fOpenViewGroup.fillActionBars(actionBars);
+			fBuildPathGroup.fillActionBars(actionBars);
+		}
+	}
+
+	@Override
+	public void fillContextMenu(IMenuManager menu) {
+		if (fInViewPart) {
+			fOpenViewGroup.fillContextMenu(menu);
+			fBuildPathGroup.fillContextMenu(menu);
+		}
+	}
+
+	@Override
+	public void init(ICommonActionExtensionSite site) {
+
+		ICommonViewerWorkbenchSite workbenchSite = null;
+		if (site.getViewSite() instanceof ICommonViewerWorkbenchSite)
+			workbenchSite = (ICommonViewerWorkbenchSite) site.getViewSite();
+
+		if (workbenchSite != null) {
+			if (workbenchSite.getPart() != null && workbenchSite.getPart() instanceof IViewPart) {
+				IViewPart viewPart = (IViewPart) workbenchSite.getPart();
+
+				fOpenViewGroup = new OpenViewActionGroup(viewPart);
+				fOpenViewGroup.containsOpenPropertiesAction(false);
+				fBuildPathGroup = new GenerateBuildPathActionGroup(viewPart);
+
+				fInViewPart = true;
+			}
+		}
+	}
+
+	@Override
+	public void setContext(ActionContext context) {
+		super.setContext(context);
+		if (fInViewPart) {
+			fOpenViewGroup.setContext(context);
+			fBuildPathGroup.setContext(context);
+		}
+	}
+
+	@Override
+	public void dispose() {
+		if (fInViewPart) {
+			fOpenViewGroup.dispose();
+			fBuildPathGroup.dispose();
+		}
+		super.dispose();
+	}
+
+}
