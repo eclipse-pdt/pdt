1e61fe19a97df86c3a8af5d8fbf33815445c6190
tbl0605 <thierryblind@msn.com>
2016-09-29 12:11:39 +0200

    Bug 501974 - Code assist is empty for second item in short array initializer
    Change-Id: I80ebf0f3da4801923a774721b3b6ad12a24b14b9
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/contexts/TypeDeclarationContext.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/contexts/TypeDeclarationContext.java
@@ -39,7 +39,7 @@ import org.eclipse.php.internal.core.util.text.TextSequence;
 public abstract class TypeDeclarationContext extends DeclarationContext {
 
 	protected static final Pattern EXTENDS_PATTERN = Pattern.compile("\\Wextends\\W", Pattern.CASE_INSENSITIVE); //$NON-NLS-1$
-	protected static final Pattern IMPLEMENTS_PATTERN = Pattern.compile("\\Wimplements", Pattern.CASE_INSENSITIVE); //$NON-NLS-1$
+	protected static final Pattern IMPLEMENTS_PATTERN = Pattern.compile("\\Wimplements\\W", Pattern.CASE_INSENSITIVE); //$NON-NLS-1$
 
 	private int typeEnd;
 	private int typeIdentifierEnd;
