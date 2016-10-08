81942f5e4b9a51fd623e41eb6e4b15b29e1c9cda
Thierry BLIND <thierryblind@msn.com>
2016-10-08 13:26:22 +0200

    Bug 504497 - Invalid CA in class context
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/MethodKeywordStrategy.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/MethodKeywordStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -76,6 +76,10 @@ public class MethodKeywordStrategy extends KeywordsStrategy {
 				}
 				return true;
 			}
+		}
+		// https://bugs.eclipse.org/bugs/show_bug.cgi?id=504497
+		if ((keyword.context & PHPKeywords.CLASS_BODY) != 0) {
+			return false;
 		}
 		return (keyword.context & PHPKeywords.METHOD_BODY) == 0;
 	}
