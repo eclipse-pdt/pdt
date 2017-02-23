eb868cd691caaa657e1dfde808ef68ce697e1d51
Thierry BLIND <thierryblind@msn.com>
2017-02-23 22:58:08 +0100

    No bug - simplification of code in class PhpIndentationFormatter
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/format/CommentIndentationStrategy.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/format/CommentIndentationStrategy.java
@@ -73,9 +73,10 @@ public class CommentIndentationStrategy extends DefaultIndentationStrategy {
 			// add the indentation of the previous (non-empty) line belonging to
 			// the comment
 			String blanks = FormatterUtils.getLineBlanks(document, previousLine);
-			// update previous line location
+			// update line location
 			lineNumber--;
-			// check if previous line was not totally blank
+			// check if previous line was not totally blank, otherwise choose
+			// another line
 			while (lineNumber > 0 && blanks.length() == previousLine.getLength()) {
 				previousLine = document.getLineInformation(--lineNumber);
 				blanks = FormatterUtils.getLineBlanks(document, previousLine);
