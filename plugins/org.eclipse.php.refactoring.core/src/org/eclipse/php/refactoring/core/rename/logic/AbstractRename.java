39aac3389a07bd95ef5245025434fdc4b527615f
Thierry BLIND <thierryblind@msn.com>
2017-04-27 12:56:26 +0200

    Bug 515895 - PatternSyntaxException below AbstractRename.<init> (thrown in Pattern.sequence)
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.refactoring.core/src/org/eclipse/php/refactoring/core/rename/logic/AbstractRename.java
+++ plugins/org.eclipse.php.refactoring.core/src/org/eclipse/php/refactoring/core/rename/logic/AbstractRename.java
@@ -78,8 +78,7 @@ public abstract class AbstractRename extends AbstractVisitor {
 	 * @param oldName
 	 * @param newName
 	 */
-	public AbstractRename(IFile file, String oldName, String newName,
-			boolean searchTextual) {
+	public AbstractRename(IFile file, String oldName, String newName, boolean searchTextual) {
 		if (newName == null || file == null || oldName == null) {
 			throw new IllegalArgumentException();
 		}
@@ -140,19 +139,15 @@ public abstract class AbstractRename extends AbstractVisitor {
 	 * @param groups
 	 *            - the groups to add
 	 */
-	private final static void addGroups(TextFileChange change,
-			List<TextEditGroup> groups) {
+	private final static void addGroups(TextFileChange change, List<TextEditGroup> groups) {
 		assert change != null && groups != null;
 
-		TextEditChangeGroup[] textEditChangeGroups = change
-				.getTextEditChangeGroups();
+		TextEditChangeGroup[] textEditChangeGroups = change.getTextEditChangeGroups();
 		OUTER: for (TextEditGroup editGroup : groups) {
-			TextEditChangeGroup textEditChangeGroup = new TextEditChangeGroup(
-					change, editGroup);
+			TextEditChangeGroup textEditChangeGroup = new TextEditChangeGroup(change, editGroup);
 			final TextEdit textEdit = editGroup.getTextEdits()[0];
 			for (TextEditChangeGroup existingTextEditChangeGroup : textEditChangeGroups) {
-				TextEdit existingTextEdit = existingTextEditChangeGroup
-						.getTextEdits()[0];
+				TextEdit existingTextEdit = existingTextEditChangeGroup.getTextEdits()[0];
 				if (existingTextEdit.getOffset() == textEdit.getOffset()) { // avoid
 					// overlapping
 					// edits
@@ -189,84 +184,67 @@ public abstract class AbstractRename extends AbstractVisitor {
 	}
 
 	protected void searchTextualOccurrences(Program program) {
-		TextSearcher searcher = TextSearcherFactory.createSearcher(changedFile,
-				getTextualSearchPattern());
+		TextSearcher searcher = TextSearcherFactory.createSearcher(changedFile, getTextualSearchPattern());
 		searcher.search(null);
 
 		/**
 		 * an iterator of @link FileMatch
 		 */
 		final Iterator<?> searchIterator = searcher.getResults().iterator();
-		FileMatch currentMatch = (FileMatch) (searchIterator.hasNext() ? searchIterator
-				.next() : null);
+		FileMatch currentMatch = (FileMatch) (searchIterator.hasNext() ? searchIterator.next() : null);
 		/**
 		 * an iterator of @link Comment
 		 */
 		final Iterator<Comment> commentIterator = program.comments().iterator();
-		Comment currentComment = (Comment) (commentIterator.hasNext() ? commentIterator
-				.next() : null);
+		Comment currentComment = (Comment) (commentIterator.hasNext() ? commentIterator.next() : null);
 
 		while (currentComment != null && currentMatch != null) {
 
 			while (isInside(currentComment, currentMatch)) {
 				// if the comment abs the text - add change
 				if (textInsideComment(currentComment, currentMatch)) {
-					addChange(currentMatch.getOffset() + 1,
-							PhpRefactoringCoreMessages
-									.getString("AbstractRename_0")); //$NON-NLS-1$
+					addChange(currentMatch.getOffset() + 1, PhpRefactoringCoreMessages.getString("AbstractRename_0")); //$NON-NLS-1$
 				}
-				currentMatch = (FileMatch) (searchIterator.hasNext() ? searchIterator
-						.next() : null);
+				currentMatch = (FileMatch) (searchIterator.hasNext() ? searchIterator.next() : null);
 			}
 
 			while (isCommentBefore(currentMatch, currentComment)) {
-				currentComment = (Comment) (commentIterator.hasNext() ? commentIterator
-						.next() : null);
+				currentComment = (Comment) (commentIterator.hasNext() ? commentIterator.next() : null);
 			}
 
 			while (isMatchBefore(currentMatch, currentComment)) {
-				currentMatch = (FileMatch) (searchIterator.hasNext() ? searchIterator
-						.next() : null);
+				currentMatch = (FileMatch) (searchIterator.hasNext() ? searchIterator.next() : null);
 			}
 		}
 	}
 
 	protected void addChange(int start, String name) {
 		final TextEditGroup textEditGroup = new TextEditGroup(name);
-		final ReplaceEdit replaceEdit = new ReplaceEdit(start,
-				oldName.length(), this.newName);
+		final ReplaceEdit replaceEdit = new ReplaceEdit(start, oldName.length(), this.newName);
 		textEditGroup.addTextEdit(replaceEdit);
 		groups.add(textEditGroup);
 
 	}
 
 	protected String getTextualSearchPattern() {
-		return "\\W" + oldName + "\\W"; //$NON-NLS-1$ //$NON-NLS-2$
+		return "\\W" + Pattern.quote(oldName) + "\\W"; //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
-	private final boolean isMatchBefore(FileMatch currentMatch,
-			Comment currentComment) {
-		return currentComment != null
-				&& currentMatch != null
-				&& currentMatch.getOffset() + currentMatch.getLength() <= currentComment
-						.getStart();
-	}
-
-	private final boolean isCommentBefore(FileMatch currentMatch,
-			Comment currentComment) {
+	private final boolean isMatchBefore(FileMatch currentMatch, Comment currentComment) {
 		return currentComment != null && currentMatch != null
-				&& currentComment.getEnd() <= currentMatch.getOffset();
+				&& currentMatch.getOffset() + currentMatch.getLength() <= currentComment.getStart();
 	}
 
-	protected final boolean isInside(Comment currentComment,
-			FileMatch currentMatch) {
-		return currentComment != null && currentMatch != null
-				&& currentComment.getStart() <= currentMatch.getOffset()
+	private final boolean isCommentBefore(FileMatch currentMatch, Comment currentComment) {
+		return currentComment != null && currentMatch != null && currentComment.getEnd() <= currentMatch.getOffset();
+	}
+
+	protected final boolean isInside(Comment currentComment, FileMatch currentMatch) {
+		return currentComment != null && currentMatch != null && currentComment.getStart() <= currentMatch.getOffset()
 				&& currentComment.getEnd() >= currentMatch.getOffset();
 	}
 
-	protected boolean textInsideComment(final Comment currentComment,
-			final FileMatch currentMatch) {
+	protected boolean textInsideComment(final Comment currentComment, final FileMatch currentMatch) {
 		return currentComment.getStart() <= currentMatch.getOffset() + 1
 				&& currentComment.getEnd() > currentMatch.getOffset() + 1;
 	}
@@ -274,14 +252,11 @@ public abstract class AbstractRename extends AbstractVisitor {
 	public boolean visit(Scalar scalar) {
 		if (searchTextual) {
 			final String stringValue = scalar.getStringValue();
-			if (scalar.getScalarType() == Scalar.TYPE_STRING
-					&& stringValue != null) {
+			if (scalar.getScalarType() == Scalar.TYPE_STRING && stringValue != null) {
 				final Matcher matcher = pattern.matcher(stringValue);
 				while (matcher.find()) {
-					addChange(scalar.getStart() + matcher.start()
-							+ matcher.group().indexOf(oldName),
-							PhpRefactoringCoreMessages
-									.getString("AbstractRename_0")); //$NON-NLS-1$
+					addChange(scalar.getStart() + matcher.start() + matcher.group().indexOf(oldName),
+							PhpRefactoringCoreMessages.getString("AbstractRename_0")); //$NON-NLS-1$
 				}
 			}
 		}
