ea251605e01e7b98fa933550d5c77cf8fa9dfc74
Thierry BLIND <thierryblind@msn.com>
2017-08-29 18:02:02 +0200

    No bug - implement PDT own reconciler
    Signed-off-by: Alex Xu <ibazzi@qq.com>
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/util/DocumentModelUtils.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/util/DocumentModelUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBM Corporation and others.
+ * Copyright (c) 2016, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -53,7 +53,6 @@ public class DocumentModelUtils {
 		IDocument document = textViewer.getDocument();
 		if (document instanceof IStructuredDocument) {
 			reparseDocument(document);
-			textViewer.reconcile();
 		}
 	}
 
@@ -71,7 +70,6 @@ public class DocumentModelUtils {
 		IDocument document = textViewer.getDocument();
 		if (document instanceof IStructuredDocument) {
 			reparseDocument(document, project);
-			textViewer.reconcile();
 		}
 	}
 
