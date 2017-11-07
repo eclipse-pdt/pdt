9c226e1d1a3b25f70597e8b74de2b3b280754988
Dawid Pakuła <zulus@w3des.net>
2017-11-07 15:48:51 +0100

    [WIP] Bug 515714 - Make problem severities configurable
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/PHPCorePreferenceInitializer.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/preferences/PHPCorePreferenceInitializer.java
@@ -12,6 +12,12 @@
 package org.eclipse.php.internal.core.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.dltk.compiler.problem.ProblemSeverity;
+import org.eclipse.php.core.validation.IProblemPreferences;
+import org.eclipse.php.internal.core.PHPCoreConstants;
+import org.eclipse.php.internal.core.PHPCorePlugin;
+import org.eclipse.php.internal.core.compiler.ast.parser.PHPProblemIdentifier;
 
 public class PHPCorePreferenceInitializer extends AbstractPreferenceInitializer {
 
@@ -22,6 +28,37 @@ public class PHPCorePreferenceInitializer extends AbstractPreferenceInitializer 
 	@Override
 	public void initializeDefaultPreferences() {
 		CorePreferenceConstants.initializeDefaultValues();
+
+		IProblemPreferences problemPreferences = PHPCorePlugin.getDefault().getProblemPreferences();
+
+		DefaultScope.INSTANCE.getNode(PHPCoreConstants.PLUGIN_ID).node(PHPCoreConstants.VALIDATOR_PREFERENCES_NODE_ID)
+				.putBoolean(PHPCoreConstants.ENABLED, true);
+
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.SYNTAX, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.MethodRequiresBody, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.BodyForAbstractMethod, ProblemSeverity.ERROR);
+
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.USE_STATEMENTS, ProblemSeverity.WARNING);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.AbstractMethodInAbstractClass,
+				ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.MethodRequiresBody, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.AbstractMethodsInConcreteClass,
+				ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.UndefinedType, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.ClassExtendFinalClass, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.CannotInstantiateType, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.ImportNotFound, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.DuplicateImport, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.UnusedImport, ProblemSeverity.WARNING);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.UnnecessaryImport, ProblemSeverity.WARNING);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.DuplicateDeclaration, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.AbstractMethodMustBeImplemented,
+				ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.SuperclassMustBeAClass, ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.SuperInterfaceMustBeAnInterface,
+				ProblemSeverity.ERROR);
+		problemPreferences.setDefaultSeverity(PHPProblemIdentifier.CannotUseTypeAsTrait, ProblemSeverity.ERROR);
+
 	}
 
 }
