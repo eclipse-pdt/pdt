4bbd31ca708dd59ae355b86ce7c8028f1a5d0ff4
Michele Locati <michele@locati.it>
2017-11-24 15:59:08 +0100

    Bug 463110 - Introduce Factory Method goals
    Change-Id: I1e3cf33109686865002b185bde8f885528758656
    Signed-off-by: Michele Locati <michele@locati.it>

---
+++ tests/org.eclipse.php.core.tests/src/org/eclipse/php/core/tests/typeinference/evaluators/TestFactoryMethodReturnTypeEvaluator.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Michele Locati and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Michele Locati <michele@locati.it> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.core.tests.typeinference.evaluators;
+
+import org.eclipse.dltk.ti.GoalState;
+import org.eclipse.dltk.ti.goals.IGoal;
+import org.eclipse.dltk.ti.types.IEvaluatedType;
+import org.eclipse.php.core.compiler.ast.nodes.NamespaceReference;
+import org.eclipse.php.internal.core.typeinference.PHPClassType;
+import org.eclipse.php.internal.core.typeinference.evaluators.AbstractMethodReturnTypeEvaluator;
+import org.eclipse.php.internal.core.typeinference.goals.FactoryMethodReturnTypeGoal;
+
+public class TestFactoryMethodReturnTypeEvaluator extends AbstractMethodReturnTypeEvaluator {
+
+	private static final String FACTORY_CLASS = "\\FactoryNamespace\\FactoryClass";
+	private static final String FACTORY_METHOD = "factoryMethod";
+	private static final int FACTORY_ARGUMENTINDEX = 0;
+
+	public TestFactoryMethodReturnTypeEvaluator(IGoal goal) {
+		super(goal);
+	}
+
+	public IGoal[] init() {
+		return IGoal.NO_GOALS;
+	}
+
+	public IGoal[] subGoalDone(IGoal subgoal, Object result, GoalState state) {
+		return IGoal.NO_GOALS;
+	}
+
+	public Object produceResult() {
+		IEvaluatedType et = ((FactoryMethodReturnTypeGoal) this.goal).getEvaluatedType();
+		if (et == null) {
+			return null;
+		}
+		String className = et.getTypeName();
+		if (className == null || className.isEmpty()) {
+			return null;
+		}
+		if (!FACTORY_CLASS.equalsIgnoreCase(className)) {
+			return null;
+		}
+		if (!FACTORY_METHOD.equalsIgnoreCase(((FactoryMethodReturnTypeGoal) this.goal).getMethodName())) {
+			return null;
+		}
+		String[] stringArguments = ((FactoryMethodReturnTypeGoal) this.goal).getStringArguments();
+		if (stringArguments.length < FACTORY_ARGUMENTINDEX || stringArguments[FACTORY_ARGUMENTINDEX] == null) {
+			return null;
+		}
+		return new PHPClassType(NamespaceReference.NAMESPACE_DELIMITER.concat(stringArguments[FACTORY_ARGUMENTINDEX]));
+	}
+
+}
