e75fcb4828d814e71ebfc96ef0839eec3699bfab
Dawid Pakuła <zulus@w3des.net>
2018-02-08 11:08:37 +0100

    [WIP] Bug 530721 - Reduce ClassStatementContext#isValid to class body only
    * Temporary move Trait info calculation
    * ITextRegion based scope detector with tests
    
    Change-Id: I155cf0488902d8e157371d7f7520c16692e8b6dd
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/ClassFieldsStrategy.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/codeassist/strategies/ClassFieldsStrategy.java
@@ -22,12 +22,13 @@ import org.eclipse.dltk.core.*;
 import org.eclipse.dltk.internal.core.ModelElement;
 import org.eclipse.dltk.internal.core.hierarchy.FakeType;
 import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.php.core.PHPVersion;
 import org.eclipse.php.core.codeassist.ICompletionContext;
 import org.eclipse.php.core.codeassist.ICompletionReporter;
+import org.eclipse.php.core.codeassist.ICompletionScope.Type;
 import org.eclipse.php.core.codeassist.IElementFilter;
 import org.eclipse.php.internal.core.Logger;
 import org.eclipse.php.internal.core.PHPCorePlugin;
-import org.eclipse.php.core.PHPVersion;
 import org.eclipse.php.internal.core.codeassist.contexts.ClassMemberContext;
 import org.eclipse.php.internal.core.codeassist.contexts.ClassMemberContext.Trigger;
 import org.eclipse.php.internal.core.codeassist.contexts.ClassStaticMemberContext;
@@ -68,12 +69,11 @@ public class ClassFieldsStrategy extends ClassMembersStrategy {
 		ISourceRange replaceRange = getReplacementRange(concreteContext);
 
 		List<IField> result = new LinkedList<>();
-
+		boolean inUseTrait = getCompanion().getScope().findParent(Type.TRAIT_USE) != null;
 		for (IType type : concreteContext.getLhsTypes()) {
 			try {
 				ITypeHierarchy hierarchy = getCompanion().getSuperTypeHierarchy(type, null);
 				IField[] fields = null;
-
 				if (concreteContext instanceof ClassStaticMemberContext
 						&& concreteContext.getTriggerType() == Trigger.CLASS
 						&& ((ClassStaticMemberContext) concreteContext).isParent()) {
@@ -90,7 +90,7 @@ public class ClassFieldsStrategy extends ClassMembersStrategy {
 				}
 
 				for (IField field : removeOverriddenElements(Arrays.asList(fields))) {
-					if (concreteContext.isInUseTraitStatement()) {
+					if (inUseTrait) {
 						result.add(field);
 					} else if (!isFiltered(field, type, concreteContext)) {
 						result.add(field);
@@ -101,8 +101,7 @@ public class ClassFieldsStrategy extends ClassMembersStrategy {
 			}
 		}
 		if (concreteContext instanceof ClassStaticMemberContext && concreteContext.getTriggerType() == Trigger.CLASS
-				&& !concreteContext.isInUseTraitStatement()
-				&& PHPVersion.PHP5_4.isLessThan(concreteContext.getPHPVersion())
+				&& !inUseTrait && PHPVersion.PHP5_4.isLessThan(concreteContext.getPHPVersion())
 				&& (CLASS_KEYWORD.startsWith(prefix.toLowerCase()) || CLASS_KEYWORD.equals(prefix.toLowerCase()))) {
 			try {
 				ITextRegion phpToken = concreteContext.getPHPScriptRegion()
