ed21203b45be54ada0e6cc2945c55ed7ee0fa5e6
Thierry BLIND <thierryblind@msn.com>
2018-11-01 18:14:13 +0100

    [WIP] Bug 537608 - PHP 7.3 Support - initial indentation support in HEREDOC/NOWODOC
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/ast/rewrite/SymbolsProvider.java
+++ plugins/org.eclipse.php.core/src/org/eclipse/php/internal/core/ast/rewrite/SymbolsProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009-2018 IBM Corporation and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -57,6 +57,64 @@ public class SymbolsProvider {
 	public static final int YIELD_ID = 27;
 
 	public static final Symbol ERROR_SYMBOL = new Symbol(Integer.MIN_VALUE);
+
+	// Pre-defined PHP 7.3 Symbols.
+	private static final Symbol OBJECT_OP_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_OBJECT_OPERATOR);
+	private static final Symbol DOT_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_NEKUDA);
+	private static final Symbol LESS_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_RGREATER);
+	private static final Symbol GREATER_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_LGREATER);
+	private static final Symbol RBRACKET_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_CLOSE_RECT);
+	private static final Symbol LBRACKET_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_OPEN_RECT);
+	private static final Symbol RBRACE_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_CURLY_CLOSE);
+	private static final Symbol LBRACE_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_CURLY_OPEN);
+	private static final Symbol LPAREN_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_OPEN_PARENTHESE);
+	private static final Symbol RPAREN_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_CLOSE_PARENTHESE);
+	private static final Symbol INTERFACE_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_INTERFACE);
+	private static final Symbol CLASS_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_CLASS);
+	private static final Symbol RETURN_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_RETURN);
+	private static final Symbol BREAK_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_BREAK);
+	private static final Symbol CONTINUE_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_CONTINUE);
+	private static final Symbol DO_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_DO);
+	private static final Symbol WHILE_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_WHILE);
+	private static final Symbol SEMICOLON_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_SEMICOLON);
+	private static final Symbol THROW_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_THROW);
+	private static final Symbol NEW_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_NEW);
+	private static final Symbol ELSE_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_ELSE);
+	private static final Symbol IMPLEMENTS_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_IMPLEMENTS);
+	private static final Symbol END_IF_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_ENDIF);
+	private static final Symbol END_FOR_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_ENDFOR);
+	private static final Symbol END_WHILE_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_ENDWHILE);
+	private static final Symbol END_FOREACH_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_ENDFOREACH);
+	private static final Symbol END_SWITCH_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_ENDSWITCH);
+	private static final Symbol YIELD_SYMBOL_PHP73 = new Symbol(
+			org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_YIELD);
 
 	// Pre-defined PHP 7.2 Symbols.
 	private static final Symbol OBJECT_OP_SYMBOL_PHP72 = new Symbol(
@@ -517,12 +575,12 @@ public class SymbolsProvider {
 			org.eclipse.php.internal.core.ast.scanner.php5.ParserConstants.T_ENDSWITCH);
 
 	/**
-	 * Returns a {@link Symbol} that holds a sym id that is computed by identifying
-	 * the correct id for the given php version. The given symID should be one of
-	 * this class ID constants, and the PHP version String should be one of the
-	 * {@link AST} defined PHP constants. The returned Symbol will always be a
-	 * non-null Symbol. In case the Symbol was not recognized, an
-	 * {@link SymbolsProvider#ERROR_SYMBOL} will be returned.
+	 * Returns a {@link Symbol} that holds a sym id that is computed by
+	 * identifying the correct id for the given php version. The given symID
+	 * should be one of this class ID constants, and the PHP version String
+	 * should be one of the {@link AST} defined PHP constants. The returned
+	 * Symbol will always be a non-null Symbol. In case the Symbol was not
+	 * recognized, an {@link SymbolsProvider2#ERROR_SYMBOL} will be returned.
 	 * 
 	 * @param symID
 	 *            Symbol ID - One of this class constants.
@@ -1026,6 +1084,69 @@ public class SymbolsProvider {
 				break;
 			}
 		}
+
+		if (PHPVersion.PHP7_3 == phpVersion) {
+			switch (symID) {
+			case DOT_SYMBOL_ID:
+				return DOT_SYMBOL_PHP73;
+			case LESS_ID:
+				return LESS_SYMBOL_PHP73;
+			case GREATER_ID:
+				return GREATER_SYMBOL_PHP73;
+			case RBRACKET_ID:
+				return RBRACKET_SYMBOL_PHP73;
+			case LBRACKET_ID:
+				return LBRACKET_SYMBOL_PHP73;
+			case RBRACE_ID:
+				return RBRACE_SYMBOL_PHP73;
+			case LBRACE_ID:
+				return LBRACE_SYMBOL_PHP73;
+			case RPAREN_ID:
+				return RPAREN_SYMBOL_PHP73;
+			case LPAREN_ID:
+				return LPAREN_SYMBOL_PHP73;
+			case INTERFACE_ID:
+				return INTERFACE_SYMBOL_PHP73;
+			case CLASS_ID:
+				return CLASS_SYMBOL_PHP73;
+			case RETURN_ID:
+				return RETURN_SYMBOL_PHP73;
+			case BREAK_ID:
+				return BREAK_SYMBOL_PHP73;
+			case CONTINUE_ID:
+				return CONTINUE_SYMBOL_PHP73;
+			case DO_ID:
+				return DO_SYMBOL_PHP73;
+			case WHILE_ID:
+				return WHILE_SYMBOL_PHP73;
+			case SEMICOLON_ID:
+				return SEMICOLON_SYMBOL_PHP73;
+			case THROW_ID:
+				return THROW_SYMBOL_PHP73;
+			case NEW_ID:
+				return NEW_SYMBOL_PHP73;
+			case ELSE_ID:
+				return ELSE_SYMBOL_PHP73;
+			case IMPLEMENTS_ID:
+				return IMPLEMENTS_SYMBOL_PHP73;
+			case END_FOR_ID:
+				return END_FOR_SYMBOL_PHP73;
+			case END_FOREACH_ID:
+				return END_FOREACH_SYMBOL_PHP73;
+			case END_IF_ID:
+				return END_IF_SYMBOL_PHP73;
+			case END_WHILE_ID:
+				return END_WHILE_SYMBOL_PHP73;
+			case END_SWITCH_ID:
+				return END_SWITCH_SYMBOL_PHP73;
+			case OBJECT_OP_SYMBOL_ID:
+				return OBJECT_OP_SYMBOL_PHP73;
+			case YIELD_ID:
+				return YIELD_SYMBOL_PHP73;
+			default:
+				break;
+			}
+		}
 		return ERROR_SYMBOL;
 	}
 
@@ -1040,7 +1161,21 @@ public class SymbolsProvider {
 	 * @return The sym id or the ERROR_SYMBOL.sym in case of an error.
 	 */
 	public static int getModifierSym(String modifier, PHPVersion phpVersion) {
-		if (PHPVersion.PHP7_2 == phpVersion) {
+		if (PHPVersion.PHP7_3 == phpVersion) {
+			if (modifier.equals("public")) { //$NON-NLS-1$
+				return org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_PUBLIC;
+			} else if (modifier.equals("private")) { //$NON-NLS-1$
+				return org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_PRIVATE;
+			} else if (modifier.equals("protected")) { //$NON-NLS-1$
+				return org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_PROTECTED;
+			} else if (modifier.equals("static")) { //$NON-NLS-1$
+				return org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_STATIC;
+			} else if (modifier.equals("abstract")) { //$NON-NLS-1$
+				return org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_ABSTRACT;
+			} else if (modifier.equals("final")) { //$NON-NLS-1$
+				return org.eclipse.php.internal.core.ast.scanner.php73.ParserConstants.T_FINAL;
+			}
+		} else if (PHPVersion.PHP7_2 == phpVersion) {
 			if (modifier.equals("public")) { //$NON-NLS-1$
 				return org.eclipse.php.internal.core.ast.scanner.php72.ParserConstants.T_PUBLIC;
 			} else if (modifier.equals("private")) { //$NON-NLS-1$
---
+++ tests/org.eclipse.php.core.tests/src/org/eclipse/php/core/tests/errors/PHP73ErrorReportingTests.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2014, 2018 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *     Dawid Pakuła - convert to JUnit4
+ *******************************************************************************/
+package org.eclipse.php.core.tests.errors;
+
+import org.eclipse.php.core.PHPVersion;
+import org.eclipse.php.core.tests.runner.PDTTList;
+import org.eclipse.php.core.tests.runner.PDTTList.Parameters;
+import org.junit.runner.RunWith;
+
+@RunWith(PDTTList.class)
+public class PHP73ErrorReportingTests extends AbstractErrorReportingTests {
+
+	@Parameters
+	public static final String[] TEST_DIRS = { "/workspace/errors/php54", "/workspace/errors/php55",
+			"/workspace/errors/php56", "/workspace/errors/php7", "/workspace/errors/php71", "/workspace/errors/php72" };
+
+	public PHP73ErrorReportingTests(String[] fileNames) {
+		super(fileNames);
+	}
+
+	@Override
+	protected PHPVersion getPHPVersion() {
+		return PHPVersion.PHP7_3;
+	}
+}
