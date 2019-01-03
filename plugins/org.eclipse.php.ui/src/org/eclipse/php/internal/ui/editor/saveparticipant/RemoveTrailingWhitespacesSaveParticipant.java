3cc7923b95b01e2394038a0b405925028accb580
Thierry BLIND <thierryblind@msn.com>
2019-01-03 14:23:16 +0100

    [WIP] Bug 513852 - add "removing trailing whitespaces" functionality when formatting all project files
    Change-Id: Ibc01ca51352870f862b0189310fd716f53c995eb
    Signed-off-by: Thierry BLIND <thierryblind@msn.com>

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/saveparticipant/RemoveTrailingWhitespacesSaveParticipant.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/editor/saveparticipant/RemoveTrailingWhitespacesSaveParticipant.java
@@ -13,31 +13,24 @@
  *******************************************************************************/
 package org.eclipse.php.internal.ui.editor.saveparticipant;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.dltk.annotations.NonNull;
 import org.eclipse.dltk.annotations.Nullable;
 import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.PreferencesLookupDelegate;
 import org.eclipse.dltk.core.manipulation.SourceModuleChange;
 import org.eclipse.dltk.ui.editor.saveparticipant.IPostSaveListener;
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
-import org.eclipse.php.core.ast.nodes.ASTNode;
 import org.eclipse.php.core.ast.nodes.ASTParser;
 import org.eclipse.php.core.ast.nodes.Program;
-import org.eclipse.php.core.ast.nodes.Quote;
-import org.eclipse.php.core.ast.visitor.ApplyAll;
+import org.eclipse.php.internal.core.format.FormatterUtils;
 import org.eclipse.php.internal.core.preferences.PreferencesSupport;
 import org.eclipse.php.internal.ui.PHPUiPlugin;
 import org.eclipse.php.internal.ui.preferences.PreferenceConstants;
@@ -45,27 +38,6 @@ import org.eclipse.text.edits.DeleteEdit;
 import org.eclipse.text.edits.MultiTextEdit;
 
 public class RemoveTrailingWhitespacesSaveParticipant implements IPostSaveListener {
-
-	private static class TopMostHeredocFinder extends ApplyAll {
-
-		private List<Quote> topMostHeredocs = new ArrayList<>();
-
-		@Override
-		protected boolean apply(ASTNode node) {
-			if (node instanceof Quote && (((Quote) node).getQuoteType() == Quote.QT_NOWDOC
-					|| ((Quote) node).getQuoteType() == Quote.QT_HEREDOC)) {
-				topMostHeredocs.add((Quote) node);
-				// do not look for nested heredocs
-				return false;
-			}
-			return true;
-		}
-
-		public List<Quote> getTopMostHeredocs() {
-			return topMostHeredocs;
-		}
-
-	}
 
 	@Override
 	public String getName() {
@@ -117,84 +89,6 @@ public class RemoveTrailingWhitespacesSaveParticipant implements IPostSaveListen
 		return false;
 	}
 
-	protected @NonNull MultiTextEdit computeTextEdit(@NonNull IDocument document, @Nullable Program astRoot)
-			throws BadLocationException {
-		List<Quote> topMostHeredocs;
-
-		if (astRoot != null) {
-			/*
-			 * It is important that the "topMostHeredocs" list is sorted by
-			 * increasing position, or this method won't work correctly.
-			 */
-			TopMostHeredocFinder visitor = new TopMostHeredocFinder();
-			astRoot.accept(visitor);
-			topMostHeredocs = visitor.getTopMostHeredocs();
-		} else {
-			topMostHeredocs = new ArrayList<>();
-		}
-
-		int lineCount = document.getNumberOfLines();
-		MultiTextEdit multiEdit = new MultiTextEdit();
-
-		for (int i = 0; i < lineCount; i++) {
-			IRegion region = document.getLineInformation(i);
-			if (region.getLength() == 0) {
-				continue;
-			}
-			int lineStart = region.getOffset();
-			int lineExclusiveEnd = lineStart + region.getLength();
-			int lineExclusiveDelimiterEnd = lineStart + document.getLineLength(i);
-			boolean isInHeredoc = false;
-
-			for (int j = 0, len = topMostHeredocs.size(); j < len; j++) {
-				Quote heredoc = topMostHeredocs.get(j);
-				if (lineExclusiveDelimiterEnd <= heredoc.getStart()) {
-					// the next heredocs/nowdocs will all start after
-					// lineExclusiveDelimiterEnd (if the topMostHeredocs list
-					// is correctly sorted), so we're sure current line isn't
-					// part of any heredocs/nowdoc
-					break;
-				}
-				if (heredoc.getEnd() <= lineStart) {
-					// at this point of the document, current heredoc/nowdoc
-					// will never be matched again, so remove it from the
-					// topMostHeredocs list
-					topMostHeredocs.remove(j);
-					len--;
-					// will be re-incremented afterwards
-					j--;
-					continue;
-				}
-				// check if last character of current line (i.e.
-				// lineExclusiveDelimiterEnd - 1) is enclosed in a
-				// heredoc/nowdoc
-				if (heredoc.getStart() < lineExclusiveDelimiterEnd && heredoc.getEnd() >= lineExclusiveDelimiterEnd) {
-					isInHeredoc = true;
-					break;
-				}
-			}
-			if (isInHeredoc) {
-				// don't remove trailing whitespaces inside heredocs or nowdocs
-				continue;
-			}
-
-			int j = lineExclusiveEnd - 1;
-			while (j >= lineStart && Character.isWhitespace(document.getChar(j))) {
-				--j;
-			}
-			++j;
-			// A flag for skipping empty lines, if required
-			if (ignoreEmptyLines && j == lineStart) {
-				continue;
-			}
-			if (j < lineExclusiveEnd) {
-				multiEdit.addChild(new DeleteEdit(j, lineExclusiveEnd - j));
-			}
-		}
-
-		return multiEdit;
-	}
-
 	@Override
 	public void saved(ISourceModule compilationUnit, IRegion[] changedRegions, IProgressMonitor monitor)
 			throws CoreException {
@@ -210,12 +104,14 @@ public class RemoveTrailingWhitespacesSaveParticipant implements IPostSaveListen
 			Program astRoot = parser.createAST(monitor);
 
 			IDocument document = new Document(compilationUnit.getSource());
-			MultiTextEdit edits = computeTextEdit(document, astRoot);
-			if (edits.hasChildren()) {
+			DeleteEdit[] deletes = FormatterUtils.getTrailingWhitespaces(document, astRoot, ignoreEmptyLines);
+			if (deletes.length > 0) {
+				MultiTextEdit multiEdit = new MultiTextEdit();
+				multiEdit.addChildren(deletes);
 				final SourceModuleChange change = new SourceModuleChange(
 						"Remove trailing whitespaces from " + compilationUnit.getElementName(), compilationUnit); //$NON-NLS-1$
 				change.setSaveMode(TextFileChange.LEAVE_DIRTY);
-				change.setEdit(edits);
+				change.setEdit(multiEdit);
 				change.perform(monitor);
 			}
 		} catch (Exception e) {
