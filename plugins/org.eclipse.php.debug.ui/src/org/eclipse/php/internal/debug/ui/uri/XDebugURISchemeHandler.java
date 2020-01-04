7cb15ab3a340452df34632298b22d5e2dc14fd25
Dawid Pakuła <zulus@w3des.net>
2020-01-04 14:00:15 +0100

    Bug 249879 - RFE: provide URL mapping with Xdebug
    Change-Id: I456cb15bfb4f9b191e2ba79639db01dfda872e04
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

---
+++ plugins/org.eclipse.php.debug.ui/src/org/eclipse/php/internal/debug/ui/uri/XDebugURISchemeHandler.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Dawid Pakuła and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Dawid Pakuła - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.php.internal.debug.ui.uri;
+
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.php.internal.debug.ui.Logger;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.Workbench;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.urischeme.IUriSchemeHandler;
+
+public class XDebugURISchemeHandler implements IUriSchemeHandler {
+
+	final static private String SCHEME = "://"; //$NON-NLS-1$
+
+	@Override
+	public void handle(String uri) {
+		String[] split = uri.split(SCHEME, 2);
+		if (split.length != 2) {
+			return;
+		}
+		String fileName = split[1];
+		int pos = fileName.lastIndexOf('@');
+		int lineNumber = pos > 0 ? Integer.valueOf(fileName.substring(pos + 1)) : 0;
+		if (pos > 0) {
+			fileName = fileName.substring(0, pos);
+		}
+		IFileStore store = EFS.getLocalFileSystem().getStore(new Path(fileName));
+		if (store.fetchInfo().exists() && !store.fetchInfo().isDirectory()) {
+			Runnable run = new Runnable() {
+
+				@Override
+				public void run() {
+					IWorkbenchWindow window = Workbench.getInstance().getActiveWorkbenchWindow();
+					try {
+						IEditorPart editor = IDE.openEditorOnFileStore(window.getActivePage(), store);
+						if (editor instanceof ITextEditor) {
+							ITextEditor textEditor = (ITextEditor) editor;
+							IDocument document = textEditor.getDocumentProvider().getDocument(editor.getEditorInput());
+							int lineStart = document.getLineOffset(lineNumber);
+							textEditor.selectAndReveal(lineStart, 0);
+						}
+					} catch (PartInitException e) {
+						Logger.logException(e);
+					} catch (BadLocationException e) {
+						Logger.logException(e);
+					}
+
+				}
+			};
+			if (Display.getCurrent() != null) {
+				run.run();
+			} else {
+				Display.getDefault().asyncExec(run);
+			}
+
+		}
+	}
+
+}
