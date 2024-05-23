0b235699e2623bd7ceee37d2316e1d3826532310
Dawid Pakuła <zulus@w3des.net>
2024-05-23 16:39:18 +0200

    Fix compatibility issues
    * build against 2024-03
    * tycho bump

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/phar/wizard/WizardPharFileResourceImportPage1.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/internal/ui/phar/wizard/WizardPharFileResourceImportPage1.java
@@ -83,7 +83,7 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 
 	public WizardPharFileResourceImportPage1(IWorkbench aWorkbench, IStructuredSelection selection,
 			String[] fileImportMask) {
-		super("zipFileImportPage1", aWorkbench, selection); //$NON-NLS-1$
+		super("zipFileImportPage1", selection); //$NON-NLS-1$
 
 		setTitle(PharImportMessages.ArchiveExport_exportTitle);
 		setDescription(PharImportMessages.ArchiveImport_description);
@@ -96,8 +96,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Called when the user presses the Cancel button. Return a boolean indicating
-	 * permission to close the wizard.
+	 * Called when the user presses the Cancel button. Return a boolean
+	 * indicating permission to close the wizard.
 	 * 
 	 * @return boolean
 	 */
@@ -117,8 +117,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Create the options specification widgets. There is only one in this case so
-	 * create no group.
+	 * Create the options specification widgets. There is only one in this case
+	 * so create no group.
 	 * 
 	 * @param parent
 	 *            org.eclipse.swt.widgets.Composite
@@ -175,8 +175,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Answer a boolean indicating whether the specified source currently exists and
-	 * is valid (ie.- proper format)
+	 * Answer a boolean indicating whether the specified source currently exists
+	 * and is valid (ie.- proper format)
 	 */
 	private boolean ensureZipSourceIsValid() {
 		ZipFile specifiedFile = getSpecifiedZipSourceFile();
@@ -203,8 +203,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Answer a boolean indicating whether the specified source currently exists and
-	 * is valid (ie.- proper format)
+	 * Answer a boolean indicating whether the specified source currently exists
+	 * and is valid (ie.- proper format)
 	 */
 	@Override
 	protected boolean ensureSourceIsValid() {
@@ -218,9 +218,9 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * The Finish button was pressed. Try to do the required work now and answer a
-	 * boolean indicating success. If <code>false</code> is returned then the wizard
-	 * will not close.
+	 * The Finish button was pressed. Try to do the required work now and answer
+	 * a boolean indicating success. If <code>false</code> is returned then the
+	 * wizard will not close.
 	 * 
 	 * @return boolean
 	 */
@@ -245,8 +245,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Returns a content provider for <code>FileSystemElement</code>s that returns
-	 * only files as children.
+	 * Returns a content provider for <code>FileSystemElement</code>s that
+	 * returns only files as children.
 	 */
 	@Override
 	protected ITreeContentProvider getFileProvider() {
@@ -300,8 +300,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Returns a content provider for <code>FileSystemElement</code>s that returns
-	 * only folders as children.
+	 * Returns a content provider for <code>FileSystemElement</code>s that
+	 * returns only folders as children.
 	 */
 	@Override
 	protected ITreeContentProvider getFolderProvider() {
@@ -334,7 +334,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Answer the string to display as the label for the source specification field
+	 * Answer the string to display as the label for the source specification
+	 * field
 	 */
 	@Override
 	protected String getSourceLabel() {
@@ -404,7 +405,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Open a FileDialog so that the user can specify the source file to import from
+	 * Open a FileDialog so that the user can specify the source file to import
+	 * from
 	 */
 	@Override
 	protected void handleSourceBrowseButtonPressed() {
@@ -467,8 +469,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Opens a file selection dialog and returns a string representing the selected
-	 * file, or <code>null</code> if the dialog was canceled.
+	 * Opens a file selection dialog and returns a string representing the
+	 * selected file, or <code>null</code> if the dialog was canceled.
 	 */
 	protected String queryZipFileToImport() {
 		FileDialog dialog = new FileDialog(sourceNameField.getShell(), SWT.OPEN | SWT.SHEET);
@@ -495,8 +497,8 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	}
 
 	/**
-	 * Use the dialog store to restore widget values to the values that they held
-	 * last time this wizard was used to completion
+	 * Use the dialog store to restore widget values to the values that they
+	 * held last time this wizard was used to completion
 	 */
 	@Override
 	protected void restoreWidgetValues() {
@@ -521,9 +523,9 @@ public class WizardPharFileResourceImportPage1 extends WizardFileSystemResourceI
 	 * Since Finish was pressed, write widget values to the dialog store so that
 	 * they will persist into the next invocation of this wizard page.
 	 * 
-	 * Note that this method is identical to the one that appears in the superclass.
-	 * This is necessary because proper overriding of instance variables is not
-	 * occurring.
+	 * Note that this method is identical to the one that appears in the
+	 * superclass. This is necessary because proper overriding of instance
+	 * variables is not occurring.
 	 */
 	@Override
 	protected void saveWidgetValues() {
